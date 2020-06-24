1d5148ec40cfe7922a62fc8633fa70fe22944fd5
Noyes <dnnoyes@3c22fb0f428c.ant.amazon.com>
2020-06-24 14:27:48 -0700

    removing boilerplate comments

--- packages/amplify_core/android/src/main/kotlin/com/amplifyframework/flutter/amplify_core/AmplifyCorePlugin.kt
+++ packages/amplify_core/android/src/main/kotlin/com/amplifyframework/flutter/amplify_core/AmplifyCorePlugin.kt
@@ -21,10 +21,7 @@ import java.io.Serializable
 
 /** AmplifyCorePlugin */
 public class AmplifyCorePlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
+
     private lateinit var channel: MethodChannel
     private lateinit var context: Context
     private var mainActivity: Activity? = null
@@ -36,15 +33,6 @@ public class AmplifyCorePlugin : FlutterPlugin, ActivityAware, MethodCallHandler
         Log.i("Amplify Flutter", "Added Core plugin")
     }
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
