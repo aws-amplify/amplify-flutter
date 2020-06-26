526fc45fef2e75c771f740d1b976c7970635a658
Noyes <dnnoyes@3c22fb0f428c.ant.amazon.com>
2020-06-26 11:34:36 -0700

    simplifying class name

--- packages/amplify_core/lib/amplify_core.dart
+++ packages/amplify_core/lib/amplify_core.dart
@@ -3,10 +3,10 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:amplify_core_plugin_interface/amplify_core_plugin_interface.dart';
 
-class AmplifyCore {
+class Amplify {
   Future<bool> configure(String configuration) async {
     assert(configuration != null, 'configuration is null');
-    var res = await AmplifyCorePlatform.instance.configure(configuration);
+    var res = await AmplifyCorePlugin.instance.configure(configuration);
     return res;
   }
 }
--- packages/amplify_core_plugin_interface/lib/method_channel_amplify.dart
+++ packages/amplify_core_plugin_interface/lib/method_channel_amplify.dart
@@ -2,8 +2,8 @@ part of amplify_flutter_platform_interface;
 
 const MethodChannel _channel = MethodChannel('amplify/amplify_core');
 
-/// An implementation of [AmplifyPlatform] that uses method channels.
-class MethodChannelAmplifyCore extends AmplifyCorePlatform {
+/// An implementation of [AmplifyCorePlugin] that uses method channels.
+class MethodChannelAmplifyCore extends AmplifyCorePlugin {
   @override
   Future<bool> configure(String configuration) {
     return _channel.invokeMethod<bool>(
