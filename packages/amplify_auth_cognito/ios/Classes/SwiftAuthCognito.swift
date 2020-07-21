0bb143638cab417ff915e8873928070319b71115
Noyes <dnnoyes@3c22fb0f428c.ant.amazon.com>
2020-07-21 15:09:25 -0700

    added request class for signup

--- packages/amplify_auth_cognito/ios/Classes/SwiftAuthCognito.swift
+++ packages/amplify_auth_cognito/ios/Classes/SwiftAuthCognito.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 import Amplify
 import AmplifyPlugins
+import SwiftyJSON
 
 public class SwiftAuthCognito: NSObject, FlutterPlugin {
     
@@ -32,98 +33,19 @@ public class SwiftAuthCognito: NSObject, FlutterPlugin {
   }
 
   private func onSignUp(flutterResult: @escaping FlutterResult, request: FlutterSignUpRequest) {
-        
-    _ = Amplify.Auth.signUp(username: request.getUsername(), password:request.password, options:  AuthSignUpRequest.Options(userAttributes:request.userAttributes)) { response in
-     switch response {
-       case .success(let signUpResult):
-        if case .done = signUpResult.nextStep {
-            var responseDict: [String: Any] = [:]
-            let providerDataDict: [String: Any] = [:]
-            responseDict["signUpState"] = "DONE"
-            responseDict["providerData"] = providerDataDict;
-            flutterResult(responseDict)
-         }
-         if case let .confirmUser(deliveryDetails, _) = signUpResult.nextStep {
-            /* 
-               The following code is accounting for the fact that swift auth types
-               currently don't conform to codeable, as well as mismatches between ios
-               and android response data/enums.  If swift auth types were to become codeable,
-               we could potentially deal with mismatches on the dart side and simply serialize
-               the whole response type here.
+    let options = AuthSignUpRequest.Options(userAttributes: request.userAttributes)
 
-               TODO: IMPROVE THIS
-            */
-            var responseDict: [String: Any] = [:]
-            var providerDataDict: [String: Any] = [:]
-            var nextStepDict: [String: Any] = [:]
-            var codeDeliveryDetailsDict: [String: Any] = [:]
-            responseDict["signUpState"] = "CONFIRM_SIGN_UP_STEP"
+    _ = Amplify.Auth.signUp(username: request.getUsername(), password:request.password, options: options) { response in
+        switch response {
+       case .success:
+        let signUpData = FlutterSignUpResult(res: response)
+        flutterResult(signUpData.toJSON())
 
-            
-            if case let .email(e) = deliveryDetails?.destination {
-              codeDeliveryDetailsDict["destination"] = e! as String
-              codeDeliveryDetailsDict["attributeName"] = "email"
-              codeDeliveryDetailsDict["deliveryMedium"] = "EMAIL"
-            }
-        
-            if case let .phone(e) = deliveryDetails?.destination {
-              codeDeliveryDetailsDict["destination"] = e! as String
-              codeDeliveryDetailsDict["attributeName"] = "phone"
-            }
-        
-            if case let .sms(e) = deliveryDetails?.destination {
-              codeDeliveryDetailsDict["destination"] = e! as String
-              codeDeliveryDetailsDict["attributeName"] = "sms"
-              codeDeliveryDetailsDict["deliveryMedium"] = "SMS"
-            }
-        
-            if case let .unknown(e) = deliveryDetails?.destination {
-              codeDeliveryDetailsDict["destination"] = e! as String
-              codeDeliveryDetailsDict["attributeName"] = "unknown"
-            }
-        
-            nextStepDict["codeDeliveryDetails"] = codeDeliveryDetailsDict
-            providerDataDict["nextStep"] = nextStepDict
-            responseDict["providerData"] = providerDataDict
-        
-            flutterResult(responseDict);
-        }
        case .failure(let signUpError):
         print("An error occurred while registering a user")
-        if case .service(let description, let suggestion, let error?) = signUpError {
-          var errorDict: [String: Any] = [:]
-          // should consider doing this with string manipulation, but that could be fragile
-          switch "\(error)" {
-            case "invalidParameter":
-              errorDict["INVALID_PARAMETER"] = description;
-            case "usernameExists":
-              errorDict["USERNAME_EXISTS"] = description;
-            case "aliasExists":
-              errorDict["ALIAS_EXISTS"] = description;
-            case "codeDeliveryFailure":
-              errorDict["CODE_DELIVERY_FAILURE"] = description
-            case "internalError":
-              errorDict["INTERNAL_ERROR"] = description
-            case "invalidLambdaResponse":
-              errorDict["INVALID_LAMBDA_RESPONSE"] = description
-            case "invalidPassword":
-              errorDict["INVALID_PASSWORD"] = description
-            case "notAuthorized":
-              errorDict["NOT_AUTHORIZED"] = description
-            case "resourceNotFound":
-              errorDict["RESOURCE_NOT_FOUND"] = description
-            case "tooManyRequests":
-              errorDict["TOO_MANY_REQUESTS"] = description
-            case "unexpectedLambda":
-              errorDict["UNEXPECTED_LAMBDA"] = description
-            case "userLambdaValidation":
-              errorDict["USER_LAMBDA_VALIDATION"] = description
-            case "tooManyFailedAttempts":
-              errorDict["TOO_MANY_FAILED_ATTEMPTS"] = description
-            default:
-              errorDict["UNKNOWN"] = "An unrecognized Sign Up has occurred."
-          }
-          self.prepareError(flutterResult: flutterResult, error: signUpError, msg: self.signUpFailure, errorMap: errorDict)
+        if case .service( _,  _, let error) = signUpError {
+            let errorCode = error != nil ? "\(error!)" : "UNKNOWN"
+            self.prepareError(flutterResult: flutterResult, error: signUpError, msg: self.signUpFailure, errorMap: self.formatErrorMap(errorCode: errorCode))
         }
       }
     }
@@ -131,9 +53,47 @@ public class SwiftAuthCognito: NSObject, FlutterPlugin {
 
   private func prepareError(flutterResult: FlutterResult, error: Error, msg: String, errorMap: [String: Any]) {
         print("\(msg): \(error)?")
+        
         flutterResult(FlutterError(
           code: "AmplifyException",
           message: msg,
           details: errorMap))
+  }
+    
+    private func formatErrorMap(errorCode: String) -> [String: Any] {
+        var errorDict: [String: Any] = [:]
+        // should consider doing this with string manipulation, but that could be fragile
+        switch errorCode {
+          case "invalidParameter":
+            errorDict["INVALID_PARAMETER"] = description;
+        case "usernameExists":
+            errorDict["USERNAME_EXISTS"] = description;
+          case "aliasExists":
+            errorDict["ALIAS_EXISTS"] = description;
+          case "codeDeliveryFailure":
+            errorDict["CODE_DELIVERY_FAILURE"] = description
+          case "internalError":
+            errorDict["INTERNAL_ERROR"] = description
+          case "invalidLambdaResponse":
+            errorDict["INVALID_LAMBDA_RESPONSE"] = description
+          case "invalidPassword":
+            errorDict["INVALID_PASSWORD"] = description
+          case "notAuthorized":
+            errorDict["NOT_AUTHORIZED"] = description
+          case "resourceNotFound":
+            errorDict["RESOURCE_NOT_FOUND"] = description
+          case "tooManyRequests":
+            errorDict["TOO_MANY_REQUESTS"] = description
+          case "unexpectedLambda":
+            errorDict["UNEXPECTED_LAMBDA"] = description
+          case "userLambdaValidation":
+            errorDict["USER_LAMBDA_VALIDATION"] = description
+          case "tooManyFailedAttempts":
+            errorDict["TOO_MANY_FAILED_ATTEMPTS"] = description
+          default:
+            errorDict["UNKNOWN"] = "An unrecognized Sign Up has occurred."
+        }
+        return errorDict
     }
 }
+
