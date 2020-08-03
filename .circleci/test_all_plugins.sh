97c67e787e45bb0dae11b275a2ec8c46c07417a8
Kyle Yan <kyleyan@cs.washington.edu>
2020-08-03 10:40:40 -0700

    ci: improve test script

--- .circleci/test_all_plugins.sh
+++ .circleci/test_all_plugins.sh
@@ -2,6 +2,7 @@
 
 cd packages || exit
 
+passed_plugins=()
 failed_plugins=()
 skipped_plugins=()
 
@@ -14,47 +15,73 @@ for plugin_dir in */; do
         flutter-test)
             echo "=== Running Flutter unit tests for $plugin ==="
             if [ -d "test" ]; then
-                flutter test
-                if [ $? -ne 0 ]; then
+                if flutter test; then
+                    echo "PASSED: Flutter unit tests for $plugin passed."
+                    passed_plugins+=("$plugin")
+                else
                     echo "FAILED: Flutter unit tests for $plugin failed."
-                    failed_plugins+=($plugin)
+                    failed_plugins+=("$plugin")
                 fi
             else
-                echo "Android unit tests for $plugin don't exist. Skipping."
-                skipped_plugins+=($plugin)
+                echo "SKIPPED: Flutter unit tests for $plugin don't exist. Skipping."
+                skipped_plugins+=("$plugin")
             fi
             ;;
         android-test)
             echo "=== Running Android unit tests for $plugin ==="
             if [ -d "android/src/test" ]; then
-                cd example/android || continue
-                flutter build apk
-                ./gradlew testDebugUnitTest --info
-                if [ $? -ne 0 ]; then
+                if [ ! -d "example/android" ]; then
+                    echo "FAILED: example/android missing, can't run tests."
+                    failed_plugins+=("$plugin")
+                    continue
+                fi
+                cd example/android
+                if ! flutter build apk; then
+                    echo "FAILED: Android example failed to build."
+                    failed_plugins+=("$plugin")
+                    continue
+                fi
+                
+                if ./gradlew testDebugUnitTest --info; then
+                    echo "PASSED: Android unit tests for $plugin passed."
+                    passed_plugins+=("$plugin")
+                else
                     echo "FAILED: Android unit tests for $plugin failed."
-                    failed_plugins+=($plugin)
+                    failed_plugins+=("$plugin")
                 fi
                 cd ../..
             else
-                echo "Android unit tests for $plugin don't exist. Skipping."
-                skipped_plugins+=($plugin)
+                echo "SKIPPED: Android unit tests for $plugin don't exist. Skipping."
+                skipped_plugins+=("$plugin")
             fi
             ;;
         ios-test)
             echo "=== Running iOS unit tests for $plugin ==="
             if [ -d "ios/Tests" ]; then
                 XCODEBUILD_DESTINATION="platform=iOS Simulator,name=iPhone 11,OS=13.6"
-                cd example/ios || continue
-                flutter build ios --no-codesign
-                xcodebuild test -workspace Runner.xcworkspace -scheme Runner -destination "$XCODEBUILD_DESTINATION"
-                if [ $? -ne 0 ]; then
+                if [ ! -d "example/ios" ]; then
+                    echo "FAILED: example/ios missing, can't run tests."
+                    failed_plugins+=("$plugin")
+                    continue
+                fi
+                cd example/ios
+                if ! flutter build ios --no-codesign; then
+                    echo "FAILED: iOS example failed to build."
+                    failed_plugins+=("$plugin")
+                    continue
+                fi
+                
+                if xcodebuild test -workspace Runner.xcworkspace -scheme Runner -destination "$XCODEBUILD_DESTINATION"; then
+                    echo "PASSED: iOS unit tests for $plugin passed."
+                    passed_plugins+=("$plugin")
+                else
                     echo "FAILED: iOS unit tests for $plugin failed."
-                    failed_plugins+=($plugin)
+                    failed_plugins+=("$plugin")
                 fi
                 cd ../..
             else
-                echo "iOS unit tests for $plugin don't exist. Skipping."
-                skipped_plugins+=($plugin)
+                echo "SKIPPED: iOS unit tests for $plugin don't exist. Skipping."
+                skipped_plugins+=("$plugin")
             fi
             ;;
     esac
@@ -63,11 +90,19 @@ for plugin_dir in */; do
 done
 
 echo "=== Unit test complete ==="
-echo "${#failed_plugins[@]} failed plugins:"
-echo $failed_plugins
 echo
+
+echo "${#passed_plugins[@]} passed plugins:"
+printf "* %s\n" "${passed_plugins[@]}"
+echo
+
+echo "${#failed_plugins[@]} failed plugins:"
+printf "* %s\n" "${failed_plugins[@]}"
+echo
+
 echo "${#skipped_plugins[@]} skipped plugins:"
-echo $skipped_plugins
+printf "* %s\n" "${skipped_plugins[@]}"
+echo
 
 cd ..
 
