6d440af5680650598f433abdce739348cfe776f7
Ashish Nanda <ashish.nanda.5591@gmail.com>
2020-08-17 17:56:10 -0700

    Addressing PR comments

--- packages/amplify_storage_plugin_interface/lib/src/Remove/RemoveOptions.dart
+++ packages/amplify_storage_plugin_interface/lib/src/Remove/RemoveOptions.dart
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 import '../Storage/StorageAccessLevel.dart';
 import '../Storage/StorageOptions.dart';
 import 'package:flutter/foundation.dart';
@@ -10,9 +25,7 @@ class RemoveOptions extends StorageOptions {
   @override
   Map<String, dynamic> serializeAsMap() {
     final Map<String, dynamic> optionsMap = <String, dynamic>{};
-    if (accessLevel != null) {
-      optionsMap["accessLevel"] = describeEnum(accessLevel);
-    }
+    optionsMap["accessLevel"] = describeEnum(accessLevel);
     return optionsMap;
   }
 }
