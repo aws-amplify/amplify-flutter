e0b1cd0471178d67f70146f57b899008c3de6fc2
Dustin Noyes <dustin.noyes.dev@gmail.com>
2020-09-02 12:58:22 -0700

    removed extra key

--- packages/amplify_auth_cognito/android/src/main/kotlin/com/amazonaws/amplify/amplify_auth_cognito/types/FlutterResendSignUpCodeResult.kt
+++ packages/amplify_auth_cognito/android/src/main/kotlin/com/amazonaws/amplify/amplify_auth_cognito/types/FlutterResendSignUpCodeResult.kt
@@ -23,11 +23,9 @@ data class FlutterResendSignUpCodeResult(private var raw: AuthSignUpResult) {
   val codeDeliveryDetails: Map<String, Any> = setCodeDeliveryDetails();
   private fun setCodeDeliveryDetails(): Map<String, Any> {
     return mapOf(
-      "codeDeliveryDetails" to mapOf(
         "destination" to (raw.nextStep.codeDeliveryDetails?.destination ?: ""),
         "deliveryMedium" to (raw.nextStep.codeDeliveryDetails?.deliveryMedium?.name ?: ""),
         "attributeName" to (raw.nextStep.codeDeliveryDetails?.attributeName ?: "")
-      )
     )
   }
 
