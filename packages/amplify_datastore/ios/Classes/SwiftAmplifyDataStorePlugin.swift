709018e1bb4685921d19cef7e08210aa212b7de0
Amplifiyer <51211245+Amplifiyer@users.noreply.github.com>
2020-11-04 14:03:06 -0800

    feat(amplify_datastore): query API for iOS (#167)
    * scaffolding for datastore package
    
    * s/Datastore/DataStore/g
    
    * file name change s/Datastore/DataStore/g
    
    * Delete AmplifyDatastorePlugin.kt
    
    * Delete AmplifyDatastorePlugin.h
    
    * Delete AmplifyDatastorePlugin.m
    
    * Delete SwiftAmplifyDatastorePlugin.swift
    
    * Code review comment updates
    
    * feat(amplify_datastore) First cut for datastore category.
    Query API with android platform plugin
    
    * Delete Post.java
    
    * Delete PostEditor.java
    
    * Delete PostStatus.java
    
    * Delete User.java
    
    * Add license information to new files
    
    * Format kotlin code
    
    * Fixed dates handling
    
    * Replace usage of date formating with the util
    
    * Support for not type query predicates
    
    * Add unit tests for query pagination and query sort inputs
    
    * Update datastore android code to be null and cast safe
    
    * Empty out the configuration file as it's not needed
    
    * Updated git ignore to add coverage files
    
    * Add datastore android plugin unit tests
    
    * iOS integration
    
    * Remove local testing artifacts
    
    * removing unneccessary test file
    
    * Add Query parameters such as predicates, pagination and sorting
    
    * PR feedback update
    
    * Change android imports to not use wildcards
    
    * Fix flutter test to use iso8601 date string instead of date instance
    
    * Updated todo comment
    
    * PR feedback update
    
    * Update packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/AmplifyDataStorePlugin.kt
    
    Co-authored-by: Jameson Williams <jhwilliams@gmail.com>
    
    * Update packages/amplify_datastore/android/src/test/kotlin/com/amazonaws/amplify/amplify_datastore/AmplifyDataStorePluginTest.kt
    
    Co-authored-by: Jameson Williams <jhwilliams@gmail.com>
    
    * PR feedback update
    
    * Remove unnecessary argument
    
    * Add unit tests for iOS
    
    * Add left over test resource file
    
    * Code cleanup
    
    * Remove unnecessary files
    
    * Remove local testing artifacts
    
    * Code cleanup
    
    * Code cleanup
    
    * Update packages/amplify_datastore/ios/Classes/types/query/QuerySortBuilder.swift
    
    Co-authored-by: Jithin Roy <51138777+royjit@users.noreply.github.com>
    
    * PR comments
    
    Co-authored-by: Jameson Williams <jhwilliams@gmail.com>
    Co-authored-by: Jithin Roy <51138777+royjit@users.noreply.github.com>

--- packages/amplify_datastore/ios/Classes/SwiftAmplifyDataStorePlugin.swift
+++ packages/amplify_datastore/ios/Classes/SwiftAmplifyDataStorePlugin.swift
@@ -15,15 +15,175 @@
 
 import Flutter
 import UIKit
+import Amplify
+import AmplifyPlugins
+import AWSCore
 
 public class SwiftAmplifyDataStorePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amplify_datastore", binaryMessenger: registrar.messenger())
-    let instance = SwiftAmplifyDataStorePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    
+    private let bridge: DataStoreBridge
+    private let flutterModelRegistration: FlutterModels
+    
+    init(bridge: DataStoreBridge = DataStoreBridge(), flutterModelRegistration: FlutterModels = FlutterModels()) {
+        self.bridge = bridge
+        self.flutterModelRegistration = flutterModelRegistration
+    }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.amazonaws.amplify/datastore", binaryMessenger: registrar.messenger())
+        let instance = SwiftAmplifyDataStorePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        var arguments: [String: Any] = [:]
+        do {
+            try arguments = checkArguments(args: call.arguments as Any)
+        } catch {
+            FlutterDataStoreErrorHandler.prepareError(
+                flutterResult: result,
+                msg: FlutterDataStoreErrorMessage.MALFORMED.rawValue,
+                errorMap: ["UNKNOWN": "\(error.localizedDescription).\nAn unrecognized error has occurred. See logs for details." ])
+            return
+        }
+        
+        switch call.method {
+        case "configure":
+            onConfigure(args: arguments, result: result)
+        case "query":
+            // try! createTempPosts()
+            onQuery(args: arguments, flutterResult: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func onConfigure(args: [String: Any], result: @escaping FlutterResult) {
+        guard let modelSchemaList = args["modelSchemas"] as? [[String: Any]] else {
+            result(false)
+            return //TODO
+        }
+        
+        let modelSchemas: [ModelSchema] = modelSchemaList.map {
+            FlutterModelSchema.init(serializedData: $0).convertToNativeModelSchema()
+        }
+        
+        modelSchemas.forEach { (modelSchema) in
+            flutterModelRegistration.addModelSchema(modelName: modelSchema.name, modelSchema: modelSchema)
+        }
+        do {
+            let dataStorePlugin = AWSDataStorePlugin(modelRegistration: flutterModelRegistration)
+            try Amplify.add(plugin: dataStorePlugin)
+            Amplify.Logging.logLevel = .verbose
+            print("Amplify configured with DataStore plugin")
+            result(true)
+        } catch {
+            print("Failed to initialize DataStore with \(error)")
+            result(false)
+            return
+        }
+    }
+    
+    func onQuery(args: [String: Any], flutterResult: @escaping FlutterResult) {
+        do {
+            guard let modelName = args["modelName"] as? String else {
+                FlutterDataStoreErrorHandler.prepareError(
+                    flutterResult: flutterResult,
+                    msg: FlutterDataStoreErrorMessage.MALFORMED.rawValue,
+                    errorMap: ["MALFORMED_REQUEST": "modelName was not passed in the arguments." ])
+                return
+            }
+            guard let modelSchema = flutterModelRegistration.modelSchemas[modelName] else {
+                FlutterDataStoreErrorHandler.prepareError(
+                    flutterResult: flutterResult,
+                    msg: FlutterDataStoreErrorMessage.MALFORMED.rawValue,
+                    errorMap: ["MALFORMED_REQUEST": "schema for model \(modelName) is not registered." ])
+                return
+            }
+            let queryPredicates = try QueryPredicateBuilder.fromSerializedMap(args["queryPredicate"] as? [String : Any])
+            let querySortInput = try QuerySortBuilder.fromSerializedList(args["querySort"] as? [[String: Any]])
+            let queryPagination = QueryPaginationBuilder.fromSerializedMap(args["queryPagination"] as? [String: Any])
+            try bridge.onQuery(SerializedModel.self,
+                              modelSchema: modelSchema,
+                              where: queryPredicates,
+                              sort: querySortInput,
+                              paginate: queryPagination) { (result) in
+                switch result {
+                case .failure(let error):
+                    print("Query API failed. Error = \(error)")
+                    FlutterDataStoreErrorHandler.handleDataStoreError(error: error,
+                                                                      flutterResult: flutterResult,
+                                                                      msg: FlutterDataStoreErrorMessage.QUERY_FAILED.rawValue)
+                case .success(let res):
+                    let serializedResults = res.map { (queryResult) -> [String: Any] in
+                        return queryResult.toJSON(modelSchema: modelSchema)
+                    }
+                    flutterResult(serializedResults)
+                    return
+                }
+            }
+        } catch {
+            print("Failed to parse query arguments with \(error)")
+            FlutterDataStoreErrorHandler.prepareError(
+                flutterResult: flutterResult,
+                msg: FlutterDataStoreErrorMessage.MALFORMED.rawValue,
+                errorMap: ["UNKNOWN": "\(error.localizedDescription).\nAn unrecognized error has occurred. See logs for details." ])
+            return
+        }
+    }
+    
+    private func createTempPosts() throws {
+        _ = try getPlugin().clear()
+        func getJSONValue(_ jsonDict: [String: Any]) -> [String: JSONValue]{
+            guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonDict) else {
+                print("JSON error")
+                return [:]
+            }
+            guard let jsonValue = try? JSONDecoder().decode(Dictionary<String, JSONValue>.self,
+                                                            from: jsonData) else {
+                print("JSON error")
+                return [:]
+            }
+            return jsonValue
+        }
+        
+        let models = [SerializedModel(map: getJSONValue(["id": UUID().uuidString,
+                                                         "title": "Title 1",
+                                                         "rating": 5,
+                                                         "created": "2020-02-20T20:20:20-08:00"] as [String : Any])),
+                      SerializedModel(map: getJSONValue(["id": UUID().uuidString,
+                                                         "title": "Title 2",
+                                                         "rating": 3] as [String : Any])),
+                      SerializedModel(map: getJSONValue(["id": UUID().uuidString,
+                                                         "title": "Title 3",
+                                                         "rating": 2,
+                                                         "created": "2020-02-02T20:20:20-08:00"] as [String : Any])),
+                      SerializedModel(map: getJSONValue(["id": UUID().uuidString,
+                                                         "title": "Title 4",
+                                                         "created": "2020-02-22T20:20:20-08:00"] as [String : Any]))]
+        try models.forEach { model in
+            try getPlugin().save(model, modelSchema: flutterModelRegistration.modelSchemas["Post"]!) { (result) in
+                switch result {
+                case .failure(let error):
+                    print("Save error = \(error)")
+                case .success(let post):
+                    print("Saved post - \(post)")
+                }
+            }
+        }
+    }
+    
+    private func checkArguments(args: Any) throws -> [String: Any] {
+        guard let res = args as? [String: Any] else {
+            throw DataStoreError.decodingError("Flutter method call arguments are not a map.",
+                                               "Check the values that are being passed from Dart.")
+        }
+        return res;
+    }
+    
+    // TODO: Remove once all configure is moved to the bridge
+    func getPlugin() throws -> AWSDataStorePlugin {
+        return try Amplify.DataStore.getPlugin(for: "awsDataStorePlugin") as! AWSDataStorePlugin
+    }
+    
 }
