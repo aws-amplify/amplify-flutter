67b16c46690b5f69d30b299c7b0f01f358096ff8
Noyes <dnnoyes@f8ffc25e9e15.ant.amazon.com>
2020-11-04 12:59:58 -0800

    removes query predicate from interface; makes delete void

--- packages/amplify_datastore/test/amplify_datastore_delete_test.dart
+++ packages/amplify_datastore/test/amplify_datastore_delete_test.dart
@@ -19,6 +19,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import './test_models/Post.dart';
 import './utils/get_json_from_file.dart';
+import './resources/delete_api/InvalidModel.dart';
 
 void main() {
   const MethodChannel dataStoreChannel =
@@ -28,12 +29,17 @@ void main() {
 
   TestWidgetsFlutterBinding.ensureInitialized();
   
+<<<<<<< HEAD
+=======
+
+>>>>>>> removes query predicate from interface; makes delete void
   setUp(() {});
 
   tearDown(() {
     dataStoreChannel.setMockMethodCallHandler(null);
   });
 
+<<<<<<< HEAD
   test('delete with a valid model executes without an error ', () async {
     var json = await getJsonFromFile('delete_api/request/instance_no_predicate.json');
     var model = json["model"];
@@ -41,5 +47,40 @@ void main() {
     Post instance = Post(title: model["title"], rating: model["rating"], created: DateTime.parse(model["created"]), id: model["id"]);
     Future<void> deleteFuture = dataStore.delete(instance);
     expect(deleteFuture, completes);
+=======
+
+  test('delete with a valid model executes without an error ', () async {
+    var json = await getJsonFromFile('delete_api/request/instance_no_predicate.json');
+
+    dataStoreChannel.setMockMethodCallHandler((MethodCall methodCall) async {});
+
+    Post instance = Post(title: json["model"]["title"], rating: json["model"]["rating"], created: DateTime.parse(json["model"]["created"]), id: json["model"]["id"]);
+    bool executed;
+
+    try {
+      await dataStore.delete(instance);
+      executed = true;
+    } catch (e) {
+      executed = false;
+    }
+
+    expect(executed, true);
+  });
+
+  test('delete with an invalid model executes with an error ', () async {
+    dataStoreChannel.setMockMethodCallHandler((MethodCall methodCall) async {});
+
+    bool executed;
+    InvalidModel invalidModel = InvalidModel();
+
+    try {
+      await dataStore.delete(invalidModel);
+      executed = true;
+    } catch (e) {
+      executed = false;
+    }
+
+    expect(executed, false);
+>>>>>>> removes query predicate from interface; makes delete void
   });
 }
