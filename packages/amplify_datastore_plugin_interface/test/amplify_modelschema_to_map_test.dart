a0dd82cda13d74fc3ac9b6c487ac3bd25013c3fe
Kyle <kylcheng@gmail.com>
2020-11-04 18:37:49 -0800

    Dart ModelSchema Unit Tests

---
+++ packages/amplify_datastore_plugin_interface/test/amplify_modelschema_to_map_test.dart
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import './testData/index.dart';
+
+void main() {
+  test('Blog codegen model generates modelschema with proper fields', () async {
+    ModelSchema blogSchema = Blog.schema;
+    Map<String, dynamic> map = blogSchema.toMap();
+
+    expect(map, {
+      'name': "Blog",
+      'pluralName': "Blogs",
+      'authRules': const [],
+      'fields': {
+        'id': {
+          'name': "id",
+          'type': {'fieldType': 0, 'ofModelName': ""},
+          'isRequired': true,
+          'isArray': false,
+          'association': const {},
+          'authRules': const [],
+        },
+        'name': {
+          'name': "name",
+          'type': {'fieldType': 0, 'ofModelName': ""},
+          'isRequired': true,
+          'isArray': false,
+          'association': const {},
+          'authRules': const []
+        },
+        'posts': {
+          'name': "posts",
+          'type': {'fieldType': 10, 'ofModelName': "Post"},
+          'isRequired': false,
+          'isArray': true,
+          'association': const {
+            'associationType': 0,
+            'targetName': "",
+            'associatedName': "blog",
+            'associatedType': "Blog"
+          },
+          'authRules': const []
+        },
+      }
+    });
+  });
+
+  test('Comment codegen model generates modelschema with proper fields',
+      () async {
+    ModelSchema commentSchema = Comment.schema;
+    Map<String, dynamic> map = commentSchema.toMap();
+
+    expect(map, {
+      'name': 'Comment',
+      'pluralName': 'Comments',
+      'authRules': [],
+      'fields': {
+        'id': {
+          'name': 'id',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': true,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        },
+        'post': {
+          'name': 'post',
+          'type': {'fieldType': 9, 'ofModelName': 'Post'},
+          'isRequired': false,
+          'isArray': false,
+          'association': {
+            'associationType': 2,
+            'targetName': 'postID',
+            'associatedName': null,
+            'associatedType': null
+          },
+          'authRules': []
+        },
+        'content': {
+          'name': 'content',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': true,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        }
+      }
+    });
+  });
+
+  test('Post codegen model generates modelschema with proper fields', () async {
+    ModelSchema postSchema = Post.schema;
+    Map<String, dynamic> map = postSchema.toMap();
+
+    expect(map, {
+      'name': 'Post',
+      'pluralName': 'Posts',
+      'authRules': [],
+      'fields': {
+        'id': {
+          'name': 'id',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': true,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        },
+        'title': {
+          'name': 'title',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': true,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        },
+        'blog': {
+          'name': 'blog',
+          'type': {'fieldType': 9, 'ofModelName': 'Blog'},
+          'isRequired': false,
+          'isArray': false,
+          'association': {
+            'associationType': 2,
+            'targetName': 'blogID',
+            'associatedName': null,
+            'associatedType': null
+          },
+          'authRules': []
+        },
+        'comments': {
+          'name': 'comments',
+          'type': {'fieldType': 10, 'ofModelName': 'Comment'},
+          'isRequired': false,
+          'isArray': true,
+          'association': {
+            'associationType': 0,
+            'targetName': '',
+            'associatedName': 'post',
+            'associatedType': 'Post'
+          },
+          'authRules': []
+        }
+      }
+    });
+  });
+
+  test('PostAuthComplex codegen model generates modelschema with proper fields',
+      () async {
+    ModelSchema postAuthComplexSchema = PostAuthComplex.schema;
+    Map<String, dynamic> map = postAuthComplexSchema.toMap();
+
+    expect(map, {
+      'name': 'PostAuthComplex',
+      'pluralName': 'PostAuthComplexes',
+      'authRules': [
+        {
+          'authStrategy': 0,
+          'ownerField': 'owner',
+          'identityClaim': 'cognito:username',
+          'groupClaim': '',
+          'groups': null,
+          'groupsField': '',
+          'operations': [0, 1, 2, 3]
+        }
+      ],
+      'fields': {
+        'id': {
+          'name': 'id',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': true,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        },
+        'title': {
+          'name': 'title',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': true,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        },
+        'owner': {
+          'name': 'owner',
+          'type': {'fieldType': 0, 'ofModelName': ''},
+          'isRequired': false,
+          'isArray': false,
+          'association': {},
+          'authRules': []
+        }
+      }
+    });
+  });
+}
---
+++ packages/amplify_datastore_plugin_interface/test/testData/Post.dart
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import 'package:flutter/foundation.dart';
+import 'package:collection/collection.dart';
+import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
+
+import 'Blog.dart';
+import 'Comment.dart';
+
+/*
+type Post @model @key(name: "byBlog", fields: ["blogID"]) {
+  id: ID!
+  title: String!
+  blogID: ID!
+  blog: Blog @connection(fields: ["blogID"])
+  comments: [Comment] @connection(keyName: "byPost", fields: ["id"])
+}
+ */
+
+@immutable
+class Post extends Model {
+  static const classType = PostType();
+
+  final String id;
+  final String title;
+  final Blog blog;
+  final List<Comment> comments;
+
+  @override
+  String getId() {
+    return id;
+  }
+
+  const Post._internal(
+      {@required this.id, @required this.title, this.blog, this.comments});
+
+  factory Post(
+      {String id, @required String title, Blog blog, List<Comment> comments}) {
+    return Post._internal(
+        id: id == null ? UUID.getUUID() : id,
+        title: title,
+        blog: blog,
+        comments: comments != null ? List.unmodifiable(comments) : null);
+  }
+
+  bool equals(Object other) {
+    return this == other;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is Post &&
+        id == other.id &&
+        title == other.title &&
+        blog == other.blog &&
+        DeepCollectionEquality().equals(comments, other.comments);
+  }
+
+  @override
+  int get hashCode => toString().hashCode;
+
+  @override
+  String toString() {
+    var buffer = new StringBuffer();
+
+    buffer.write("Post {");
+    buffer.write("id=" + id + ", ");
+    buffer.write("title=" + title + ", ");
+    buffer.write("blog=" + blog.toString());
+    buffer.write("}");
+
+    return buffer.toString();
+  }
+
+  Post copyWith(
+      {@required String id,
+      @required String title,
+      Blog blog,
+      List<Comment> comments}) {
+    return Post(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        comments: comments ?? this.comments);
+  }
+
+  Post.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        blog = json['blog'] is Map<String, dynamic>
+            ? Blog.fromJson(json['blog'] as Map<String, dynamic>)
+            : null,
+        comments = json['comments'] is List
+            ? (json['comments'] as List)
+                .map((e) => Comment.fromJson(e as Map<String, dynamic>))
+                .toList()
+            : null;
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'blog': blog.toJson(),
+        'comments': comments.map((comment) => comment.toJson())
+      };
+
+  static final QueryField ID = QueryField(fieldName: "id");
+  static final QueryField TITLE = QueryField(fieldName: "title");
+  static final QueryField BLOG = new QueryField(
+      fieldName: "blog",
+      fieldType: ModelFieldType(ModelFieldTypeEnum.model,
+          ofModelName: (Blog).toString()));
+  static final QueryField COMMENTS = new QueryField(
+      fieldName: "comments",
+      fieldType: ModelFieldType(ModelFieldTypeEnum.model,
+          ofModelName: (Comment).toString()));
+
+  static var schema =
+      Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "Post";
+    modelSchemaDefinition.pluralName = "Posts";
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.id());
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+        key: Post.TITLE,
+        isRequired: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.belongsTo(
+        key: Post.BLOG,
+        isRequired: false,
+        targetName: "blogID",
+        ofModelName: (Blog).toString()));
+
+    modelSchemaDefinition.addField(ModelFieldDefinition.hasMany(
+      key: Post.COMMENTS,
+      isRequired: false,
+      ofModelName: (Comment).toString(),
+      associatedKey: Comment.POST,
+    ));
+  });
+}
+
+class PostType extends ModelType<Post> {
+  const PostType();
+
+  @override
+  Post fromJson(Map<String, dynamic> jsonData) {
+    return Post.fromJson(jsonData);
+  }
+}
