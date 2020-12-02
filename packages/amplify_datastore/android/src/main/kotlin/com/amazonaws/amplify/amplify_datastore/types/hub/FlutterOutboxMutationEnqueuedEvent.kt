f1cd9164ad02d1478eed07ec4e77a7aca153ea52
Dustin Noyes <dustin.noyes.dev@gmail.com>
2020-12-01 18:49:24 -0800

    Hub outboxmutation fix (#222)

--- packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/types/hub/FlutterOutboxMutationEnqueuedEvent.kt
+++ packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/types/hub/FlutterOutboxMutationEnqueuedEvent.kt
@@ -17,11 +17,12 @@ package com.amazonaws.amplify.amplify_datastore.types.hub
 
 import com.amplifyframework.core.model.Model
 import com.amplifyframework.datastore.appsync.ModelWithMetadata
+import com.amplifyframework.datastore.syncengine.OutboxMutationEvent
 
 class FlutterOutboxMutationEnqueuedEvent(
         override val eventName: String,
         private val modelName: String,
-        private val element: ModelWithMetadata<*>
+        private val element: OutboxMutationEvent.OutboxMutationEventElement<out Model>
 ) : FlutterHubEvent {
     override fun toValueMap(): Map<String, Any> {
 
