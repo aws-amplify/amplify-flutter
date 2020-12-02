f966f655f95917e9c35d01ae72762b093d773ee1
Noyes <dnnoyes@f8ffc25e9e15.ant.amazon.com>
2020-12-01 18:05:42 -0800

    fix(amplify_datastore) uses new type of outboxmutationevent

--- packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/types/hub/FlutterSyncMetaData.kt
+++ packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/types/hub/FlutterSyncMetaData.kt
@@ -19,11 +19,12 @@ import com.amplifyframework.core.model.Model
 import com.amplifyframework.core.model.temporal.Temporal
 import com.amplifyframework.datastore.appsync.ModelMetadata
 import com.amplifyframework.datastore.appsync.ModelWithMetadata
+import com.amplifyframework.datastore.syncengine.OutboxMutationEvent
 
-class FlutterSyncMetaData(var metadata: ModelMetadata) {
+class FlutterSyncMetaData(var metadata: OutboxMutationEvent.OutboxMutationEventElement<out Model>) {
     fun toValueMap(): Map<String, Any?> {
         return mapOf(
-                "id" to this.metadata.id,
+                "id" to this.metadata.model.id,
                 "_deleted" to this.metadata.isDeleted,
                 "_version" to this.metadata.version,
                 "_lastChangedAt" to this.metadata.lastChangedAt
