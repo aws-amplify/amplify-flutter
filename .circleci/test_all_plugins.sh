c5b7f6c6459a4996cecbc5deaed2d409b2ad9eff
Praveen Gupta <pravgupt@amazon.com>
2020-12-10 14:37:00 -0800

    Generate coverage for flutter

--- .circleci/test_all_plugins.sh
+++ .circleci/test_all_plugins.sh
@@ -17,7 +17,7 @@ for plugin_dir in */; do
             echo "=== Running Flutter unit tests for $plugin ==="
             if [ -d "test" ]; then
                 mkdir -p test-results
-                if flutter test --machine | tojunit --output "test-results/$plugin-flutter-test.xml"; then
+                if flutter test --machine --coverage | tojunit --output "test-results/$plugin-flutter-test.xml"; then
                     echo "PASSED: Flutter unit tests for $plugin passed."
                     passed_plugins+=("$plugin")
                 else
