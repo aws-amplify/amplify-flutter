fb22953f3e926926b7504be2dfa53411a40df076
Noyes <dnnoyes@f8ffc25e9e15.ant.amazon.com>
2021-01-12 09:21:03 -0800

    PR Suggestions

--- packages/amplify_auth_cognito/example/lib/main.dart
+++ packages/amplify_auth_cognito/example/lib/main.dart
@@ -15,6 +15,7 @@
 
 import 'dart:async';
 
+import 'package:amplify_flutter/amplify_hub.dart';
 import 'package:flutter/material.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify.dart';
--- packages/amplify_auth_cognito/ios/Classes/AuthCognitoHubEventStreamHandler.swift
+++ packages/amplify_auth_cognito/ios/Classes/AuthCognitoHubEventStreamHandler.swift
@@ -60,3 +60,4 @@ public class AuthCognitoHubEventStreamHandler: NSObject, FlutterStreamHandler {
         return nil
     }
 }
+
