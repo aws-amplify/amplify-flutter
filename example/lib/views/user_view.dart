f0a2bf045691c9e768f9b593ac16e5d8c30b85e1
viveky259259 <viveky259259@gmail.com>
2021-04-04 11:39:51 +0530

    renamed example/lib files as per flutter guidelines

--- example/lib/views/user_view.dart
+++ example/lib/views/user_view.dart
@@ -16,7 +16,7 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
-import 'package:sample_app/Pages/LandingPage.dart';
+import 'package:sample_app/pages/landing_page.dart';
 
 class UserView extends StatefulWidget {
   @override
