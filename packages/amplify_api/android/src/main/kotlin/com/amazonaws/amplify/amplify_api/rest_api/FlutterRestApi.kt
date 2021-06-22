6d405ea8c46baf7d8175d62f9037c8757e27bc24
Travis Sheppard <tshepp@amazon.com>
2021-06-22 07:17:40 -0700

    add blank body to android requests that need it

--- packages/amplify_api/android/src/main/kotlin/com/amazonaws/amplify/amplify_api/rest_api/FlutterRestApi.kt
+++ packages/amplify_api/android/src/main/kotlin/com/amazonaws/amplify/amplify_api/rest_api/FlutterRestApi.kt
@@ -54,7 +54,7 @@ class FlutterRestApi {
             try {
                 cancelToken = FlutterApiRequest.getCancelToken(request)
                 apiName = FlutterApiRequest.getApiPath(request)
-                options = FlutterApiRequest.getRestOptions(request)
+                options = FlutterApiRequest.getRestOptions(request, methodName)
             } catch (e: Exception) {
                 handler.post {
                     ExceptionUtil.postExceptionToFlutterChannel(flutterResult, "ApiException",
