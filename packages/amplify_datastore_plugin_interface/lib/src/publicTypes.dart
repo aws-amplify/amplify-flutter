b432213fdfbacf7e0ec29981e95b81c1cbfd22c3
Mo Malaka <mmalaka@gmail.com>
2021-06-24 16:53:23 -0700

    Pagination with Datastore #500 (#673)

--- packages/amplify_datastore_plugin_interface/lib/src/publicTypes.dart
+++ packages/amplify_datastore_plugin_interface/lib/src/publicTypes.dart
@@ -4,3 +4,5 @@ export 'types/temporal/temporal_datetime.dart';
 export 'types/temporal/temporal_timestamp.dart';
 
 export 'types/exception/DataStoreException.dart';
+
+export 'types/query/query_field.dart';
