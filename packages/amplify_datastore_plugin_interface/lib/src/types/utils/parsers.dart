fda91d34a0f616c4dbdd1fb2c00243234ea19d92
Dillon Nys <nydillon@amazon.com>
2021-06-29 15:54:46 -0700

    Fix enum string function

--- packages/amplify_datastore_plugin_interface/lib/src/types/utils/parsers.dart
+++ packages/amplify_datastore_plugin_interface/lib/src/types/utils/parsers.dart
@@ -22,5 +22,5 @@ String? enumToString(Object? o) =>
     o != null ? o.toString().split('.').last : null;
 
 // only to be used internally by amplify-flutter library
-T? enumFromString<T>(String key, List<T> values) =>
+T? enumFromString<T>(String? key, List<T> values) =>
     values.firstWhereOrNull((v) => key == enumToString(v));
