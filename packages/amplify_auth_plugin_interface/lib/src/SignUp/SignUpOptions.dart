25f89dbec35a85f019892a8f830e0515ba20f830
Dillon Nys <nydillon@amazon.com>
2021-07-02 16:35:05 -0700

    Update docs

--- packages/amplify_auth_plugin_interface/lib/src/SignUp/SignUpOptions.dart
+++ packages/amplify_auth_plugin_interface/lib/src/SignUp/SignUpOptions.dart
@@ -15,11 +15,10 @@
 
 import 'package:flutter/foundation.dart';
 
-class SignUpOptions {
+abstract class SignUpOptions {
   final Map<String, String> userAttributes;
+
   const SignUpOptions({@required this.userAttributes});
 
-  Map<String, dynamic> serializeAsMap() {
-    throw UnimplementedError('serializeAsMap() has not been implemented on SignUpOptions.');
-  }
+  Map<String, dynamic> serializeAsMap();
 }
