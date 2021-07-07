2d87e19b036b0f15155922d2ff8d5c5bdb7f9eb4
Dillon Nys <nydillon@amazon.com>
2021-07-07 08:40:17 -0700

    Add doc comment

--- packages/amplify_api_plugin_interface/lib/src/util.dart
+++ packages/amplify_api_plugin_interface/lib/src/util.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+/// Encodes [json] with indentation for easier debugging.
 String prettyPrintJson(dynamic json) {
   const defaultIndent = '  ';
   const jsonIndent = JsonEncoder.withIndent(defaultIndent);
