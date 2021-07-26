3d2373e4d1b9e7d0d07602cd29af751656f255d5
israx <70438514+israx@users.noreply.github.com>
2021-07-26 14:04:33 -0700

    Update amplify_authenticator.dart

--- packages/amplify_authenticator/lib/amplify_authenticator.dart
+++ packages/amplify_authenticator/lib/amplify_authenticator.dart
@@ -161,7 +161,7 @@ class Authenticator extends StatefulWidget {
   final ConfirmSignUpForm confirmSignUpForm = DefaultForms.confirmSignUpForm();
 
   /// This widget will be displayed after a user has signed in with some verified credentials.
-  Widget child;
+final Widget child;
 
   @override
   _AuthenticatorState createState() => _AuthenticatorState();
@@ -226,7 +226,7 @@ class _AuthenticatorState extends State<Authenticator> {
 
   @override
   void dispose() {
-    super.dispose();
     _stateMachineBloc.dispose();
+      super.dispose();
   }
 }
