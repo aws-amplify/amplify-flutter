359fe714c06ba658c457077946677d4fb3d6e1ac
israx <70438514+israx@users.noreply.github.com>
2021-07-27 08:20:42 -0700

    refactor: null return type

--- packages/amplify_authenticator/lib/src/enums/confirm_signup_types.dart
+++ packages/amplify_authenticator/lib/src/enums/confirm_signup_types.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
+
 enum ConfirmSignUpType { username, password, email, phone_number, code }
 
-ConfirmSignUpType fromStringToConfirmSignUpType(String str) {
+ConfirmSignUpType? fromStringToConfirmSignUpType(String str) {
   return ConfirmSignUpType.values
-      .firstWhere((value) => value.toString().split('.')[1] == str);
+      .firstWhereOrNull((value) => value.toString().split('.')[1] == str);
 }
