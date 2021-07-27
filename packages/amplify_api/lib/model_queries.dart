4bfa9115b9e231d5dfc7d84d2a2960eea9b1b2fd
Elijah Quartey <quaelija@amazon.com>
2021-07-27 16:35:44 -0700

    fix: file structure adjustments based on feedback

--- packages/amplify_api/lib/model_queries.dart
+++ packages/amplify_api/lib/model_queries.dart
@@ -14,7 +14,7 @@
  */
 
 import 'package:amplify_api/amplify_api.dart';
-import 'package:amplify_api/src/graphql/factories/model_queries_factory.dart';
+import 'package:amplify_api/src/graphql/model_queries_factory.dart';
 import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
 
 // This class provides static method calls to enable a simpler DX,
