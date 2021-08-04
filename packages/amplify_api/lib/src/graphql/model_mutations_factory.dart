a458a2653577189368ef691e862334c6318738f6
Elijah Quartey <Equartey@users.noreply.github.com>
2021-08-04 09:50:42 -0600

    feat(amplify_api): GraphQL Mutate Helper - Create (#778)

---
+++ packages/amplify_api/lib/src/graphql/model_mutations_factory.dart
@@ -0,0 +1,45 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_api/src/graphql/graphql_request_factory.dart';
+import 'package:amplify_datastore_plugin_interface/src/types/query/query_field.dart';
+import 'package:amplify_datastore_plugin_interface/src/types/models/model.dart';
+
+class ModelMutationsFactory extends ModelMutationsInterface {
+  // Singleton methods/properties
+  // usage: ModelQueriesFactory.instance;
+  ModelMutationsFactory._();
+
+  static final ModelMutationsFactory _instance = ModelMutationsFactory._();
+
+  static ModelMutationsFactory get instance => _instance;
+
+  @override
+  GraphQLRequest<T> create<T extends Model>(T model) {
+    Map<String, dynamic> variables = model.toJson();
+
+    return GraphQLRequestFactory.instance.buildQuery(
+        model: model,
+        variables: variables,
+        modelType: model.getInstanceType(),
+        requestType: GraphQLRequestType.mutation,
+        requestOperation: GraphQLRequestOperation.create);
+  }
+
+  @override
+  GraphQLRequest<T> delete<T extends Model>(T model, {QueryPredicate? where}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  GraphQLRequest<T> deleteById<T extends Model>(
+      ModelType<T> modelType, String id) {
+    // TODO: implement deleteById
+    throw UnimplementedError();
+  }
+
+  @override
+  GraphQLRequest<T> update<T extends Model>(T model, {QueryPredicate? where}) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
