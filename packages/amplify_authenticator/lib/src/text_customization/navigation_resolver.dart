b499d65661613ea57b625e764cb48c2d68b3a094
Noyes <dnnoyes@f8ffc25e9e15.ant.amazon.com>
2021-08-04 14:56:45 -0700

    backed out gradle and locale changes

--- packages/amplify_authenticator/lib/src/text_customization/navigation_resolver.dart
+++ packages/amplify_authenticator/lib/src/text_customization/navigation_resolver.dart
@@ -19,7 +19,6 @@ import 'package:flutter/widgets.dart';
 /// The resolver class for shared button Widgets
 class NavigationResolver {
   /// Text for navigating to the signup Widget
-<<<<<<< HEAD
   StringResolver navigateSignup = (_) => 'I am the default';
 
   /// Text for navigating to the signin Widget
@@ -59,35 +58,5 @@ class NavigationResolver {
     this.forgotPasswordQuestion =
         forgotPasswordQuestion ?? this.forgotPasswordQuestion;
     this.backToSignin = backToSignin ?? this.backToSignin;
-=======
-  late StringResolver navigate_signup;
-
-  /// Text for navigating to the signin Widget
-  late StringResolver navigate_signin;
-
-  /// Text asking if user does not have an account
-  late StringResolver no_account_question;
-
-  /// Text asking if user has have an account
-  late StringResolver have_account_question;
-
-  /// Text asking if user has have an account
-  late StringResolver back_to_signin;
-
-  // ignore: public_member_api_docs
-  NavigationResolver(
-      {StringResolver? navigate_signin,
-      StringResolver? navigate_signup,
-      StringResolver? no_account_question,
-      StringResolver? have_account_question,
-      StringResolver? back_to_signin}) {
-    // We are assigning in this way due to limitations in assigning closures as defaults
-    this.navigate_signin = navigate_signin ?? (_) => "Sign In";
-    this.navigate_signup = navigate_signup ?? (_) => "Sign Up";
-    this.no_account_question = no_account_question ?? (_) => "No account?";
-    this.have_account_question =
-        have_account_question ?? (_) => "Have an account?";
-    this.back_to_signin = back_to_signin ?? (_) => "Back to Sign In";
->>>>>>> Added inherited widget
   }
 }
