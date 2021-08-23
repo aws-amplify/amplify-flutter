ef52033abd7c056e47ea04535ad284484a9cb68f
Dillon Nys <nydillon@amazon.com>
2021-08-23 12:43:13 -0700

    Fix scripts

--- build-support/lint_android.sh
+++ build-support/lint_android.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -e
+
 cd android
 
 # Run in background to prevent Melos from hanging
-./gradlew lint --quiet --console plain &
+./gradlew lint --no-rebuild --quiet --console plain &
+gradle_pid=$!
+
+if ! wait $gradle_pid; then
+    exit 1
+fi
