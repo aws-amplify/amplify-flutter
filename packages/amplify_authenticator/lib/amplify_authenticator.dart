01141f9660afb766fea0a6c69ea687c21b179c36
Noyes <dnnoyes@f8ffc25e9e15.ant.amazon.com>
2021-09-03 13:55:33 -0700

    fix error message typo

--- packages/amplify_authenticator/lib/amplify_authenticator.dart
+++ packages/amplify_authenticator/lib/amplify_authenticator.dart
@@ -254,7 +254,7 @@ class _AuthenticatorState extends State<Authenticator> {
 
     if (_configInitialized && _missingConfigValues.isNotEmpty) {
       throw StateError(
-          'Encountered problem(s) building the Authenticator because your amplifyconfigurate.dart file is missing required values: ${_missingConfigValues.join('/n')}). You should correct your amplifyconfiguration.dart file and restart your app.');
+          'Encountered problem(s) building the Authenticator because your amplifyconfiguration.dart file is missing required values: ${_missingConfigValues.join('/n')}). You should correct your amplifyconfiguration.dart file and restart your app.');
     }
 
     /// Check for customizable forms passed into the Authenticator
