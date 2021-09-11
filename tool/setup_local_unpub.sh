ef75c9e98bc1d0ae972ae4869c0347f16c486e56
Dillon Nys <nydillon@amazon.com>
2021-09-10 17:55:40 -0700

    Add unpub step to CI builds

---
+++ tool/setup_local_unpub.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! command -v yq &>/dev/null; then
+    echo "Must install yq before proceeding: `brew install yq`"
+    exit 1
+fi
+
+if ! command -v docker-compose &>/dev/null || ; then
+    echo "Must install Docker Compose before proceeding."
+    exit 1
+fi
+
+UNPUB_DIR=~/unpub
+
+# Check if UNPUB_DIR is already in use.
+if [[ -d $UNPUB_DIR ]]; then
+    echo -n "$UNPUB_DIR already exists." >&2
+    if [[ $CI == "true" ]]; then
+        exit 1
+    fi
+    echo "Please confirm which directory you want to use." >&2
+    echo -n ": "
+    OVERRIDE_DIR=""
+    read $OVERRIDE_DIR
+    if [[ -n $OVERRIDE_DIR ]]; then
+        UNPUB_DIR=$OVERRIDE_DIR
+    fi
+fi
+
+echo "Launching unpub server at $UNPUB_DIR..."
+
+# Launch unpub server
+mkdir -p $UNPUB_DIR
+pushd $UNPUB_DIR
+curl -L https://raw.githubusercontent.com/dnys1/unpub-launcher/main/docker/latest/docker-compose.yml \
+    -o docker-compose.yml
+docker-compose up -d
+popd
+
+DEPS='. as $doc | (.dependencies | keys | .[] | select(. == "amplify*") as $k ireduce ({}; $doc.dependencies[$k] = {"hosted": { "url": "http://localhost:8000", "name": $k }, "version": "any" })) | $doc'
+DEV_DEPS='. as $doc | (.dev_dependencies | keys | .[] | select(. == "amplify*") as $k ireduce ({}; $doc.dev_dependencies[$k] = {"hosted": { "url": "http://localhost:8000", "name": $k }, "version": "any" })) | $doc'
+
+# Fix example
+pushd example
+
+# Replace Amplify dependencies with hosted version
+yq e "$DEPS" -i pubspec.yaml
+
+popd
+
+# Fix individual packages
+for PACKAGE in packages/*; do
+    pushd $PACKAGE
+
+    set +e
+
+    # Replace Amplify dependencies with hosted version
+    yq e "$DEPS" -i pubspec.yaml
+    
+    # Replace Amplify dev dependencies with hosted version
+    yq e "$DEV_DEPS" -i pubspec.yaml
+
+    set -e
+
+    if [[ -d example ]]; then 
+        pushd example
+        yq e "$DEPS" -i pubspec.yaml
+        popd
+    fi
+
+    popd
+done
