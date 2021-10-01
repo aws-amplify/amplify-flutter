ccff951f281fd96598d939f4174267475aa10412
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2021-10-01 08:52:47 -0700

    Fix API auth for REST (#925)

--- packages/amplify_api/lib/method_channel_api.dart
+++ packages/amplify_api/lib/method_channel_api.dart
@@ -249,6 +249,9 @@ class AmplifyAPIMethodChannel extends AmplifyAPI {
     inputsMap['restOptions'] = restOptions.serializeAsMap();
     inputsMap['cancelToken'] = cancelToken;
 
+    // Refresh auth tokens
+    await _authProviderRefresher();
+
     // Attempt switch to proper async
     try {
       final Map<String, dynamic>? data = (await (_channel
