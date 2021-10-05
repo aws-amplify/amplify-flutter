8e580cdde703dbba02f35efee7015d6b214fc305
Jordan Nelson <nejrd@amazon.com>
2021-10-05 09:52:23 -0400

    chore: add missing license

--- packages/amplify_datastore_plugin_interface/lib/src/types/models/sorted_list.dart
+++ packages/amplify_datastore_plugin_interface/lib/src/types/models/sorted_list.dart
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 /// An Object that holds a list that maintains a specifc sort order
 ///
 /// methods that modify the list of items will respect the sort order
