38ad6c668d4d5a24bd8c266d931e7638f9c4290b
Jordan Nelson <nejrd@amazon.com>
2021-10-07 15:15:05 -0400

    test: query predicate comparison test

--- packages/amplify_datastore/test/query_predicate_test.dart
+++ packages/amplify_datastore/test/query_predicate_test.dart
@@ -24,84 +24,162 @@ import 'test_models/ModelProvider.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  dynamic getJsonFromFile(String path) async {
-    path = 'resources/query_predicate/' + path;
-    String jsonString = '';
-    try {
-      jsonString = await File(path).readAsString();
-    } catch (e) {
-      jsonString = await File('test/' + path).readAsString();
+  group('query predicate serialization', () {
+    dynamic getJsonFromFile(String path) async {
+      path = 'resources/query_predicate/' + path;
+      String jsonString = '';
+      try {
+        jsonString = await File(path).readAsString();
+      } catch (e) {
+        jsonString = await File('test/' + path).readAsString();
+      }
+      return jsonDecode(jsonString);
     }
-    return jsonDecode(jsonString);
-  }
 
-  test('when id not equals', () async {
-    expect(Post.ID.ne("123").serializeAsMap(),
-        await getJsonFromFile('id_not_equals.json'));
+    test('when id not equals', () async {
+      expect(Post.ID.ne("123").serializeAsMap(),
+          await getJsonFromFile('id_not_equals.json'));
+    });
+
+    test('when rating greater or equal', () async {
+      expect(Post.RATING.ge(4).serializeAsMap(),
+          await getJsonFromFile('rating_greater_or_equal.json'));
+      expect((Post.RATING >= 4).serializeAsMap(),
+          await getJsonFromFile('rating_greater_or_equal.json'));
+    });
+
+    test('when rating less or equal AND \(id contains or title begins with\)',
+        () async {
+      QueryPredicate testPredicate = Post.RATING
+          .le(4)
+          .and(Post.ID.contains("abc").or(Post.TITLE.beginsWith("def")));
+
+      expect(testPredicate.serializeAsMap(),
+          await getJsonFromFile('complex_nested.json'));
+    });
+
+    test(
+        'when rating between AND id contains AND title begins_with AND created equals',
+        () async {
+      QueryPredicate testPredicate = Post.RATING
+          .between(1, 4)
+          .and(Post.ID.contains("abc"))
+          .and(Post.TITLE.beginsWith("def"))
+          .and(Post.CREATED.eq("2020-02-20T20:20:20-08:00"));
+
+      expect(testPredicate.serializeAsMap(),
+          await getJsonFromFile('group_with_only_and.json'));
+    });
+
+    test('when rating lt AND id eq OR title contains', () async {
+      QueryPredicate testPredicate = Post.RATING
+          .lt(4)
+          .and(Post.ID.contains("abc"))
+          .or(Post.TITLE.contains("def"));
+
+      expect(testPredicate.serializeAsMap(),
+          await getJsonFromFile('group_mixed_and_or.json'));
+    });
+
+    test('when rating gt but not eq', () async {
+      QueryPredicate testPredicate =
+          Post.RATING.gt(4).and(not(Post.RATING.eq(1)));
+
+      expect(testPredicate.serializeAsMap(),
+          await getJsonFromFile('mixed_with_not.json'));
+    });
+
+    test('when negate complex predicate', () async {
+      QueryPredicate testPredicate = not(Post.RATING
+          .eq(1)
+          .and(Post.RATING.eq(4).or(Post.TITLE.contains("crap"))));
+
+      expect(testPredicate.serializeAsMap(),
+          await getJsonFromFile('negate_complex_predicate.json'));
+    });
+
+    test('when operands are bool and double', () async {
+      QueryPredicate testPredicate =
+          Post.RATING.eq(1.3).and(Post.CREATED.eq(true));
+
+      expect(testPredicate.serializeAsMap(),
+          await getJsonFromFile('bool_and_double_operands.json'));
+    });
   });
 
-  test('when rating greater or equal', () async {
-    expect(Post.RATING.ge(4).serializeAsMap(),
-        await getJsonFromFile('rating_greater_or_equal.json'));
-    expect((Post.RATING >= 4).serializeAsMap(),
-        await getJsonFromFile('rating_greater_or_equal.json'));
-  });
+  group('query predicate comparison', () {
+    Post post1 = Post(
+      title: 'post one',
+      rating: 1,
+      created: TemporalDateTime(DateTime(2020, 01, 01, 10, 30)),
+    );
 
-  test('when rating less or equal AND \(id contains or title begins with\)',
-      () async {
-    QueryPredicate testPredicate = Post.RATING
-        .le(4)
-        .and(Post.ID.contains("abc").or(Post.TITLE.beginsWith("def")));
+    Post post2 = Post(
+      title: 'post two',
+      rating: 10,
+      created: TemporalDateTime(DateTime(2020, 01, 01, 12, 00)),
+    );
 
-    expect(testPredicate.serializeAsMap(),
-        await getJsonFromFile('complex_nested.json'));
-  });
+    Post post3 = Post(
+      title: 'post three',
+      rating: 100,
+      created: TemporalDateTime(DateTime(2021, 01, 01, 12, 00)),
+    );
 
-  test(
-      'when rating between AND id contains AND title begins_with AND created equals',
-      () async {
-    QueryPredicate testPredicate = Post.RATING
-        .between(1, 4)
-        .and(Post.ID.contains("abc"))
-        .and(Post.TITLE.beginsWith("def"))
-        .and(Post.CREATED.eq("2020-02-20T20:20:20-08:00"));
+    test('equals', () async {
+      QueryPredicate testPredicate = Post.TITLE.eq("post one");
+      expect(testPredicate.evaluate(post1), isTrue);
+      expect(testPredicate.evaluate(post2), isFalse);
+    });
+    test('not equals', () async {
+      QueryPredicate testPredicate = Post.TITLE.ne("post one");
+      expect(testPredicate.evaluate(post1), isFalse);
+      expect(testPredicate.evaluate(post2), isTrue);
+    });
 
-    expect(testPredicate.serializeAsMap(),
-        await getJsonFromFile('group_with_only_and.json'));
-  });
+    test('less than', () async {
+      QueryPredicate testPredicate = Post.RATING.lt(5);
+      expect(testPredicate.evaluate(post1), isTrue);
+      expect(testPredicate.evaluate(post2), isFalse);
+    });
 
-  test('when rating lt AND id eq OR title contains', () async {
-    QueryPredicate testPredicate = Post.RATING
-        .lt(4)
-        .and(Post.ID.contains("abc"))
-        .or(Post.TITLE.contains("def"));
+    test('less than or equal', () async {
+      QueryPredicate testPredicate = Post.RATING.le(10);
+      expect(testPredicate.evaluate(post1), isTrue);
+      expect(testPredicate.evaluate(post2), isTrue);
+      expect(testPredicate.evaluate(post3), isFalse);
+    });
 
-    expect(testPredicate.serializeAsMap(),
-        await getJsonFromFile('group_mixed_and_or.json'));
-  });
+    test('greater than', () async {
+      QueryPredicate testPredicate = Post.RATING.gt(5);
+      expect(testPredicate.evaluate(post1), isFalse);
+      expect(testPredicate.evaluate(post2), isTrue);
+    });
 
-  test('when rating gt but not eq', () async {
-    QueryPredicate testPredicate =
-        Post.RATING.gt(4).and(not(Post.RATING.eq(1)));
+    test('greater than or equal', () async {
+      QueryPredicate testPredicate = Post.RATING.ge(10);
+      expect(testPredicate.evaluate(post1), isFalse);
+      expect(testPredicate.evaluate(post2), isTrue);
+      expect(testPredicate.evaluate(post3), isTrue);
+    });
 
-    expect(testPredicate.serializeAsMap(),
-        await getJsonFromFile('mixed_with_not.json'));
-  });
+    test('between', () async {
+      QueryPredicate testPredicate = Post.RATING.between(5, 100);
+      expect(testPredicate.evaluate(post1), isFalse);
+      expect(testPredicate.evaluate(post2), isTrue);
+      expect(testPredicate.evaluate(post3), isTrue);
+    });
 
-  test('when negate complex predicate', () async {
-    QueryPredicate testPredicate = not(Post.RATING
-        .eq(1)
-        .and(Post.RATING.eq(4).or(Post.TITLE.contains("crap"))));
+    test('contains', () async {
+      QueryPredicate testPredicate = Post.TITLE.contains("one");
+      expect(testPredicate.evaluate(post1), isTrue);
+      expect(testPredicate.evaluate(post2), isFalse);
+    });
 
-    expect(testPredicate.serializeAsMap(),
-        await getJsonFromFile('negate_complex_predicate.json'));
-  });
-
-  test('when operands are bool and double', () async {
-    QueryPredicate testPredicate =
-        Post.RATING.eq(1.3).and(Post.CREATED.eq(true));
-
-    expect(testPredicate.serializeAsMap(),
-        await getJsonFromFile('bool_and_double_operands.json'));
+    test('beginsWith', () async {
+      QueryPredicate testPredicate = Post.TITLE.beginsWith("post o");
+      expect(testPredicate.evaluate(post1), isTrue);
+      expect(testPredicate.evaluate(post2), isFalse);
+    });
   });
 }
