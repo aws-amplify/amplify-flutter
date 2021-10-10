0cc017d46e33e84a5d1c9e46376dc1878bd1c634
Kyle <kylcheng@gmail.com>
2021-10-10 08:46:01 -0700

    Update packages/amplify_storage_plugin_interface/lib/src/Storage/TransferProgress.dart
    Co-authored-by: Dillon Nys <24740863+dnys1@users.noreply.github.com>

--- packages/amplify_storage_plugin_interface/lib/src/Storage/TransferProgress.dart
+++ packages/amplify_storage_plugin_interface/lib/src/Storage/TransferProgress.dart
@@ -1,10 +1,18 @@
+/// {@template amplify_storage_s3.transfer_progress}
+/// The progress of a storage transfer operation.
+/// {@endtemplate}
 class TransferProgress {
-  int currentProgress;
-  int totalProgress;
+  /// The current progress, in bytes, for the storage transfer operation.
+  final int currentProgress;
+  
+  /// The total number of bytes for the storage transfer operation.
+  final int totalProgress;
 
-  TransferProgress(this.currentProgress, this.totalProgress);
+  /// {@macro amplify_storage_s3.transfer_progress}
+  const TransferProgress(this.currentProgress, this.totalProgress);
 
-  double getFractionCompleted() {
-    return currentProgress / totalProgress;
-  }
+  /// The fractional progress of the storage transfer operation.
+  /// 
+  /// 0 <= `fractionCompleted` <= 1
+  double get fractionCompleted => currentProgress / totalProgress;
 }
