92db8c5075f83ce1c04e49552f3c24e0f9e0a279
Hui Zhao <zhohz@amazon.com>
2021-11-01 13:06:57 -0700

    Reformat kt file and address linter suggested issue

--- packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/types/model/FlutterAuthRule.kt
+++ packages/amplify_datastore/android/src/main/kotlin/com/amazonaws/amplify/amplify_datastore/types/model/FlutterAuthRule.kt
@@ -17,23 +17,22 @@ package com.amazonaws.amplify.amplify_datastore.types.model
 
 import com.amplifyframework.core.model.AuthRule
 import com.amplifyframework.core.model.AuthStrategy
-import com.amplifyframework.core.model.ModelField
 import com.amplifyframework.core.model.ModelOperation
 
 data class FlutterAuthRule(val map: Map<String, Any>) {
-    
+
     private val authStrategy: AuthStrategy =
-            stringToAuthStrategy(map["authStrategy"] as String)
+        stringToAuthStrategy(map["authStrategy"] as String)
     private val ownerField: String? = map["ownerField"] as String?
     private val identityClaim: String? = map["identityClaim"] as String?
     private val groupClaim: String? = map["groupClaim"] as String?
     private val groups: List<String>? = map["groups"] as List<String>?
     private val groupsField: String? = map["groupsField"] as String?
     private val operations: List<ModelOperation>? =
-            (map["operations"] as List<String>?)?.map { stringToModelOperation(it) }
+        (map["operations"] as List<String>?)?.map { stringToModelOperation(it) }
 
-    fun stringToAuthStrategy(string: String): AuthStrategy {
-        return when(string){
+    private fun stringToAuthStrategy(string: String): AuthStrategy {
+        return when (string) {
             "OWNER" -> AuthStrategy.OWNER
             "GROUPS" -> AuthStrategy.GROUPS
             "PRIVATE" -> AuthStrategy.PRIVATE
@@ -42,8 +41,8 @@ data class FlutterAuthRule(val map: Map<String, Any>) {
         }
     }
 
-    fun stringToModelOperation(string: String): ModelOperation {
-        return when(string){
+    private fun stringToModelOperation(string: String): ModelOperation {
+        return when (string) {
             "CREATE" -> ModelOperation.CREATE
             "UPDATE" -> ModelOperation.UPDATE
             "DELETE" -> ModelOperation.DELETE
@@ -54,30 +53,30 @@ data class FlutterAuthRule(val map: Map<String, Any>) {
 
     fun convertToNativeAuthRule(): AuthRule {
 
-        var builder: AuthRule.Builder = AuthRule.builder()
-                .authStrategy( authStrategy )
+        val builder: AuthRule.Builder = AuthRule.builder()
+            .authStrategy(authStrategy)
 
-        if(groups != null && groups.isNotEmpty()){
+        if (groups != null && groups.isNotEmpty()) {
             builder.groups(groups)
         }
 
-        if(!ownerField.isNullOrEmpty()){
+        if (!ownerField.isNullOrEmpty()) {
             builder.ownerField(ownerField)
         }
 
-        if(!identityClaim.isNullOrEmpty()){
+        if (!identityClaim.isNullOrEmpty()) {
             builder.identityClaim(identityClaim)
         }
 
-        if(!groupClaim.isNullOrEmpty()){
+        if (!groupClaim.isNullOrEmpty()) {
             builder.groupClaim(groupClaim)
         }
 
-        if(!groupsField.isNullOrEmpty()){
+        if (!groupsField.isNullOrEmpty()) {
             builder.groupsField(groupsField)
         }
 
-        if(!operations.isNullOrEmpty()){
+        if (!operations.isNullOrEmpty()) {
             builder.operations(operations)
         }
 
