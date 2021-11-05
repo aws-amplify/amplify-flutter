ccada62cb6208b28e082002aa0c994ec6b69caac
Dillon Nys <nydillon@amazon.com>
2021-11-05 09:39:13 -0700

    Make non-breaking

--- packages/amplify_auth_cognito/lib/src/CognitoSignIn/CognitoConfirmSignInOptions.dart
+++ packages/amplify_auth_cognito/lib/src/CognitoSignIn/CognitoConfirmSignInOptions.dart
@@ -18,7 +18,7 @@ import 'package:amplify_auth_plugin_interface/amplify_auth_plugin_interface.dart
 
 class CognitoConfirmSignInOptions extends ConfirmSignInOptions {
   Map<String, String>? clientMetadata;
-  Map<CognitoUserAttributes, String>? userAttributes;
+  Map<Object, String>? userAttributes;
   CognitoConfirmSignInOptions({this.clientMetadata, this.userAttributes})
       : super();
   Map<String, dynamic> serializeAsMap() {
@@ -28,7 +28,9 @@ class CognitoConfirmSignInOptions extends ConfirmSignInOptions {
     }
     if (this.userAttributes != null) {
       pendingRequest["userAttributes"] = userAttributes
-        ?..removeWhere((key, value) => key.readOnly);
+        ?..removeWhere(
+          (key, value) => key is CognitoUserAttributes && key.readOnly,
+        );
     }
     return pendingRequest;
   }
