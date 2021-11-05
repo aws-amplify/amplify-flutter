9d196867e6678e9457f448dced0a00d19772ddc2
Hui Zhao <10602282+HuiSF@users.noreply.github.com>
2021-11-05 14:50:54 -0700

    chore(datastore): Update codegen generated files (#1038)

--- packages/amplify_datastore/example/lib/models/BoolTypeModel.dart
+++ packages/amplify_datastore/example/lib/models/BoolTypeModel.dart
@@ -13,7 +13,11 @@
 * permissions and limitations under the License.
 */
 
-// ignore_for_file: public_member_api_docs
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
 
 import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
 import 'package:flutter/foundation.dart';
