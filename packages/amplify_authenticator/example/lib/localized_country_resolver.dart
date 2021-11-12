eec17eb6db65bdfe8086eff0df5fd91aabb85deb
Dustin Noyes <dustin.noyes.dev@gmail.com>
2021-11-12 15:01:18 -0800

    Update localized_country_resolver.dart

--- packages/amplify_authenticator/example/lib/localized_country_resolver.dart
+++ packages/amplify_authenticator/example/lib/localized_country_resolver.dart
@@ -2,7 +2,7 @@ import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-/// Our custom button string resolver which calls into our localizations class.
+/// Our custom country string resolver which calls into our localizations class.
 class LocalizedCountryResolver extends CountryResolver {
   const LocalizedCountryResolver();
 
