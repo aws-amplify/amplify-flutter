19a99f91bc6fd4ed645a18c24b0ad42d1a53e865
Dillon Nys <nydillon@amazon.com>
2021-11-18 11:03:27 -0700

    Fix duplicate test member

--- packages/amplify_flutter/android/src/test/kotlin/com/amazonaws/amplify/AmplifyTest.kt
+++ packages/amplify_flutter/android/src/test/kotlin/com/amazonaws/amplify/AmplifyTest.kt
@@ -43,8 +43,6 @@ class AmplifyTest {
 
     private val testDispatcher = TestCoroutineDispatcher()
 
-    private val testDispatcher = TestCoroutineDispatcher()
-
     @Before
     fun setup() {
         plugin = Amplify(testDispatcher)
