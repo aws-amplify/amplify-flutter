1c2576d196192625b7f3405aba92aeee57be093a
Jordan Nelson <nejrd@amazon.com>
2021-12-14 12:44:08 -0500

    test: add/fix integ tests

--- packages/amplify_authenticator/test/ui/tab_view_test.dart
+++ packages/amplify_authenticator/test/ui/tab_view_test.dart
@@ -302,4 +302,10 @@ class MockBloc implements StateMachineBloc {
 
   @override
   Stream<AuthState> get stream => Stream.value(AuthFlow.signin);
+
+  @override
+  Future<void> cancelHubSubscription() async {}
+
+  @override
+  Future<void> setUpHubSubscription() async {}
 }
