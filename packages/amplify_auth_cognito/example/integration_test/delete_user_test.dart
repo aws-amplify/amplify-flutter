4470476b1f66dc706795241a6869017f7edfb44b
Dustin Noyes <dnnoyes@amazon.com>
2022-01-03 08:46:14 -0800

    tests

--- packages/amplify_auth_cognito/example/integration_test/delete_user_test.dart
+++ packages/amplify_auth_cognito/example/integration_test/delete_user_test.dart
@@ -137,12 +137,18 @@ void main() {
         final password = generatePassword();
 
         // Create a confirmed user
-        await adminCreateUser(username, password,
-            autoConfirm: true, verifyAttributes: true);
+        await adminCreateUser(
+          username,
+          password,
+          autoConfirm: true,
+          verifyAttributes: true,
+        );
 
         // Sign the user in
-        SignInResult preDeleteSignIn =
-            await Amplify.Auth.signIn(username: username, password: password);
+        SignInResult preDeleteSignIn = await Amplify.Auth.signIn(
+          username: username,
+          password: password,
+        );
         expect(preDeleteSignIn.isSignedIn, true);
 
         // Delete the user
@@ -150,7 +156,10 @@ void main() {
 
         // Expect subsequent sign in to fail
         try {
-          await Amplify.Auth.signIn(username: username, password: password);
+          await Amplify.Auth.signIn(
+            username: username,
+            password: password,
+          );
         } catch (e) {
           expect(e, TypeMatcher<UserNotFoundException>());
           return;
