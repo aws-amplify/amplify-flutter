a119bc640c43b87a806a42d417a7fddef1bfdc75
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2022-01-13 11:16:19 -0700

    chore(config): Add tests for CLI versions 4-7 (#1234) (#1267)
    * Add tests for CLI v4-v6
    
    * Add e2e tests
    
    * Format config files
    
    * Update README
    
    * Add notes about configs

---
+++ packages/amplify_flutter/test/config/testdata/e2e/v6/amplifyconfiguration.dart
@@ -0,0 +1,74 @@
+/// **Note**: The values in these configs do not represent live services.
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "cliv6": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://a7xz4kyolrg7tjcq6dvcprvpfy.appsync-api.us-west-2.amazonaws.com/graphql",
+                    "region": "us-west-2",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-wtkeqfdaqne3xmqymmuwe34rou"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "AppSync": {
+                    "Default": {
+                        "ApiUrl": "https://a7xz4kyolrg7tjcq6dvcprvpfy.appsync-api.us-west-2.amazonaws.com/graphql",
+                        "Region": "us-west-2",
+                        "AuthMode": "API_KEY",
+                        "ApiKey": "da2-wtkeqfdaqne3xmqymmuwe34rou",
+                        "ClientDatabasePrefix": "cliv6_API_KEY"
+                    }
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-west-2:92924d58-6110-4c5e-8789-9098d819e51d",
+                            "Region": "us-west-2"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-west-2_meqxwVOqm",
+                        "AppClientId": "7faa3hukjo6ekqg088t6v6427m",
+                        "Region": "us-west-2"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
