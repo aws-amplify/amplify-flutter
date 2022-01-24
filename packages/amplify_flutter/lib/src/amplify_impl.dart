e6986b8991f9820ab732516faf5a5d62ace1bbbf
Hui Zhao <10602282+HuiSF@users.noreply.github.com>
2022-01-24 10:09:24 -0800

    chore: bump version 0.3.2 (#1293)
    * chore: Bump version to 0.3.1
    
    * Bump amplify-android to 1.31.2
    
    * revert unintended change
    
    * bump 0.3.2
    
    * Fix coroutine test
    
    * feat(amplify_auth_cognito): deleteUser API (#1236)
    
    * Update auth plugin changelog about deleteUser API
    
    Co-authored-by: Dillon Nys <nydillon@amazon.com>
    Co-authored-by: Dustin Noyes <dustin.noyes.dev@gmail.com>

--- packages/amplify_flutter/lib/src/amplify_impl.dart
+++ packages/amplify_flutter/lib/src/amplify_impl.dart
@@ -131,7 +131,7 @@ class AmplifyClass extends PlatformInterface {
   }
 
   String _getVersion() {
-    return '0.3.1';
+    return '0.3.2';
   }
 
   /// Configures Amplify with the provided configuration string.
