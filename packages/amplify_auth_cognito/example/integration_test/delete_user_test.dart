cdcea0e47678118c42b00c12c20811a4efb74b93
Dustin Noyes <dnnoyes@amazon.com>
2022-01-24 13:04:44 -0800

    fixed bad merge

--- packages/amplify_auth_cognito/example/integration_test/delete_user_test.dart
+++ packages/amplify_auth_cognito/example/integration_test/delete_user_test.dart
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
  * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-=======
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
->>>>>>> handles android; hub; integ tests
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -29,33 +25,18 @@ import 'utils/setup_utils.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   group('deleteUser (iOS)', () {
     setUpAll(() async {
       await configureAuth(additionalPlugins: [
-=======
-  group('deleteUser', () {
-=======
-  group('deleteUser (iOS)', () {
->>>>>>> PR suggestions + integ test to verify signout
-    setUpAll(() async {
-      await configureAuth(additionalConfigs: [
->>>>>>> handles android; hub; integ tests
         AmplifyAPI(),
       ]);
       await signOutUser();
     });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> PR suggestions + integ test to verify signout
     testWidgets('should delete a confirmed user on iOS',
         (WidgetTester tester) async {
       final username = generateUsername();
       final password = generatePassword();
-<<<<<<< HEAD
 
       // Create a confirmed user
       await adminCreateUser(
@@ -136,109 +117,4 @@ void main() {
       fail('Expected UnimplementedError');
     });
   }, skip: !Platform.isAndroid);
-=======
-    if (Platform.isIOS) {
-      testWidgets('should delete a confirmed user on iOS',
-          (WidgetTester tester) async {
-        final username = generateUsername();
-        final password = generatePassword();
-=======
->>>>>>> PR suggestions + integ test to verify signout
-
-      // Create a confirmed user
-      await adminCreateUser(
-        username,
-        password,
-        autoConfirm: true,
-        verifyAttributes: true,
-      );
-
-      // Sign the user in
-      SignInResult preDeleteSignIn = await Amplify.Auth.signIn(
-        username: username,
-        password: password,
-      );
-      expect(preDeleteSignIn.isSignedIn, true);
-
-      // Delete the user
-      await Amplify.Auth.deleteUser();
-
-      // Expect subsequent sign in to fail
-      try {
-        await Amplify.Auth.signIn(
-          username: username,
-          password: password,
-        );
-      } catch (e) {
-        expect(e, TypeMatcher<UserNotFoundException>());
-        return;
-      }
-      fail('Expected UserNotFoundException');
-    });
-
-    testWidgets(
-        'fetchAuthSession should throw SignedOutException after user deletion',
-        (WidgetTester tester) async {
-      final username = generateUsername();
-      final password = generatePassword();
-
-      // Create a confirmed user
-      await adminCreateUser(
-        username,
-        password,
-        autoConfirm: true,
-        verifyAttributes: true,
-      );
-
-      // Sign the user in
-      SignInResult preDeleteSignIn = await Amplify.Auth.signIn(
-        username: username,
-        password: password,
-      );
-      expect(preDeleteSignIn.isSignedIn, true);
-
-<<<<<<< HEAD
-    if (Platform.isAndroid) {
-      testWidgets('should throw an UnimplementedError on Android',
-          (WidgetTester tester) async {
-        try {
-          await Amplify.Auth.deleteUser();
-        } catch (e) {
-          expect(e, TypeMatcher<UnimplementedError>());
-          return;
-        }
-        fail('Expected UnimplementedError');
-      });
-    }
-  });
->>>>>>> handles android; hub; integ tests
-=======
-      // Delete the user
-      await Amplify.Auth.deleteUser();
-
-      // Expect fetchAuthSession to throw a SignedOutException (the tokens have been cleared)
-      try {
-        await Amplify.Auth.fetchAuthSession(
-            options: CognitoSessionOptions(getAWSCredentials: true));
-      } catch (e) {
-        expect(e, TypeMatcher<SignedOutException>());
-        return;
-      }
-      fail('Expected SignedOutException');
-    });
-  }, skip: !Platform.isIOS);
-
-  group('deleteUser (Android)', () {
-    testWidgets('should throw an UnimplementedError on Android',
-        (WidgetTester tester) async {
-      try {
-        await Amplify.Auth.deleteUser();
-      } catch (e) {
-        expect(e, TypeMatcher<UnimplementedError>());
-        return;
-      }
-      fail('Expected UnimplementedError');
-    });
-  }, skip: !Platform.isAndroid);
->>>>>>> PR suggestions + integ test to verify signout
 }
