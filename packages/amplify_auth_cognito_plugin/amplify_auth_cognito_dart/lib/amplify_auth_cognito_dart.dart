2b5b0b230470aaa3115fe32aaa0133c5a85fa104
Dustin Noyes <dnnoyes@amazon.com>
2022-01-31 17:07:12 -0800

    moved auth integ tests

--- packages/amplify_auth_cognito_plugin/amplify_auth_cognito_dart/lib/amplify_auth_cognito_dart.dart
+++ packages/amplify_auth_cognito_plugin/amplify_auth_cognito_dart/lib/amplify_auth_cognito_dart.dart
@@ -1,10 +1,6 @@
 library amplify_auth_cognito_dart;
 
-import 'dart:async';
-
 import 'package:amplify_auth_cognito_dart/amplify_auth_cognito_mixin.dart';
-import 'package:amplify_auth_cognito_dart/src/types.dart';
-import 'package:amplify_core/amplify_core.dart';
 
 export 'src/types.dart';
 
