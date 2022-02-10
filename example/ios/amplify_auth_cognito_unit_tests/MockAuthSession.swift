367197869914de5ec83a58b38d2853b201908d46
Dustin Noyes <dnnoyes@amazon.com>
2022-02-10 13:26:40 -0800

    try parallelism

---
+++ example/ios/amplify_auth_cognito_unit_tests/MockAuthSession.swift
@@ -0,0 +1,33 @@
+//
+//  MockAuthSession.swift
+//  amplify_auth_cognito_unit_tests
+//
+//  Created by Noyes, Dustin on 2/10/22.
+//
+
+import XCTest
+
+class MockAuthSession: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
---
+++ example/ios/amplify_auth_cognito_unit_tests/MockErrorConstants.swift
@@ -0,0 +1,8 @@
+//
+//  MockErrorConstants.swift
+//  amplify_auth_cognito_unit_tests
+//
+//  Created by Noyes, Dustin on 2/10/22.
+//
+
+import Foundation
