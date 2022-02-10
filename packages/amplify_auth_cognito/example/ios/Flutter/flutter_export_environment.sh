68ae9957ef1314058f5c1db28ecd6cc6305090ff
Dustin Noyes <dnnoyes@amazon.com>
2022-02-10 13:34:13 -0800

    removed parallelism

---
+++ packages/amplify_auth_cognito/example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/dnnoyes/fvm/versions/2.10.0"
+export "FLUTTER_APPLICATION_PATH=/Users/dnnoyes/Projects/amplify-flutter/packages/amplify_auth_cognito/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
---
+++ packages/amplify_auth_cognito/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,49 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<amplify_api/AmplifyApiPlugin.h>)
+#import <amplify_api/AmplifyApiPlugin.h>
+#else
+@import amplify_api;
+#endif
+
+#if __has_include(<amplify_auth_cognito/AuthCognito.h>)
+#import <amplify_auth_cognito/AuthCognito.h>
+#else
+@import amplify_auth_cognito;
+#endif
+
+#if __has_include(<amplify_core/AmplifyCorePlugin.h>)
+#import <amplify_core/AmplifyCorePlugin.h>
+#else
+@import amplify_core;
+#endif
+
+#if __has_include(<amplify_flutter/Amplify.h>)
+#import <amplify_flutter/Amplify.h>
+#else
+@import amplify_flutter;
+#endif
+
+#if __has_include(<integration_test/IntegrationTestPlugin.h>)
+#import <integration_test/IntegrationTestPlugin.h>
+#else
+@import integration_test;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AmplifyApiPlugin registerWithRegistrar:[registry registrarForPlugin:@"AmplifyApiPlugin"]];
+  [AuthCognito registerWithRegistrar:[registry registrarForPlugin:@"AuthCognito"]];
+  [AmplifyCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"AmplifyCorePlugin"]];
+  [Amplify registerWithRegistrar:[registry registrarForPlugin:@"Amplify"]];
+  [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
+}
+
+@end
