5ced05ff5f5d7e51a3f569ca92923e9b2026933a
Dustin Noyes <dnnoyes@amazon.com>
2022-02-15 08:03:16 -0800

    removed skipped plugins variable

--- .circleci/test_all_plugins.sh
+++ .circleci/test_all_plugins.sh
@@ -2,7 +2,16 @@
 
 cd packages || exit
 
+<<<<<<< HEAD
 test_failure=0
+=======
+passed_plugins=()
+failed_plugins=()
+
+set +e
+set -o pipefail
+
+>>>>>>> 7583a1c4 (removed skipped plugins variable)
 # TODO: FIND OUT WHY ARGS ARE SENT OUT OF ORDER
 test_suite=$1
 plugin=$3
@@ -16,7 +25,11 @@ set -o pipefail
 if [ -d "${plugin}_plugin" ]; then
     cd "./${plugin}_plugin/" || exit
 else
+<<<<<<< HEAD
     cd "./$plugin" || exit
+=======
+    cd "./$plugin_dir" || exit
+>>>>>>> 7583a1c4 (removed skipped plugins variable)
 fi
 
 case $test_suite in
@@ -37,7 +50,11 @@ case $test_suite in
             fi
         else
             echo "FAILED: Expected Flutter unit tests for $plugin don't exist or where not found."
+<<<<<<< HEAD
             test_failure=1
+=======
+            failed_plugins+=("$plugin")
+>>>>>>> 7583a1c4 (removed skipped plugins variable)
         fi
         cd $project_root_dir
         ;;
@@ -75,7 +92,11 @@ case $test_suite in
 
         else
             echo "FAILED: Expected Android unit tests for $plugin don't exist or where not found."
+<<<<<<< HEAD
             test_failure=1
+=======
+            failed_plugins+=("$plugin")
+>>>>>>> 7583a1c4 (removed skipped plugins variable)
         fi
         ;;
     ios-test)
