b3302361c0faf0b4c663faf9c3f3f8f5142743fa
Dustin Noyes <dnnoyes@amazon.com>
2022-02-15 12:45:07 -0800

    updated timeout in test_multiple_concurrent_replies_are_not_sent (again)

--- packages/amplify_flutter/example/ios/unit_tests/AtomicResultTests.swift
+++ packages/amplify_flutter/example/ios/unit_tests/AtomicResultTests.swift
@@ -90,6 +90,6 @@ class AtomicResultTests: XCTestCase {
             exp.fulfill()
         }
         
-        waitForExpectations(timeout: 0.3)
+        waitForExpectations(timeout: 0.5)
     }
 }
