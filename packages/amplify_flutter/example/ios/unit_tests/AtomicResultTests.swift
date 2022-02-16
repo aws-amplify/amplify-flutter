5653903c1d7533ad3aa5f8f813e914a189f5f686
Dustin Noyes <dnnoyes@amazon.com>
2022-02-16 15:07:37 -0800

    rolled back timeout bump

--- packages/amplify_flutter/example/ios/unit_tests/AtomicResultTests.swift
+++ packages/amplify_flutter/example/ios/unit_tests/AtomicResultTests.swift
@@ -90,6 +90,6 @@ class AtomicResultTests: XCTestCase {
             exp.fulfill()
         }
         
-        waitForExpectations(timeout: 0.5)
+        waitForExpectations(timeout: 0.2)
     }
 }
