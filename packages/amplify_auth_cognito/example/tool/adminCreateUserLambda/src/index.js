8ae2f1509a7d47f3a5dc399f0c051a174e502a00
Dustin Noyes <dnnoyes@amazon.com>
2022-02-21 14:33:29 -0800

    updated provision script

---
+++ packages/amplify_auth_cognito/example/tool/adminCreateUserLambda/src/index.js
@@ -0,0 +1,77 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+var aws = require('aws-sdk');
+var cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
+    "region": process.env.REGION,
+});
+
+exports.handler = async (event) => {
+    var response = {success: false }
+    var baseParams = {
+        UserPoolId: process.env.AUTH_USERPOOLID,
+        Username: event.arguments.Username
+      }
+
+    var createUserParams = Object.assign({
+        TemporaryPassword: event.arguments.Password
+    }, baseParams);
+    
+    await cognitoidentityserviceprovider.adminCreateUser(createUserParams).promise().catch(function(err) {
+      response['error'] = err.toString();
+      return response;
+    });
+    
+    if (event.arguments.AutoConfirm) {
+      var passwordParams = Object.assign({
+        Password:  event.arguments.Password,
+        Permanent: true,
+      }, baseParams);
+      
+      await cognitoidentityserviceprovider.adminSetUserPassword(passwordParams).promise().catch(function(err) {
+        response['error'] = err.toString();
+        return response;
+      })
+    } 
+    
+    if (event.arguments.EnableMFA) {
+      var mfaParams = Object.assign({
+          MFAOptions: [
+            {
+                AttributeName: 'phone_number',
+                DeliveryMedium: 'SMS'
+            }
+          ]
+        }, baseParams);
+      
+      await cognitoidentityserviceprovider.adminSetUserSettings(mfaParams).promise().then().catch(function(err) {
+        response['error'] = err.toString();
+        return response;
+      })
+    }
+
+    if (event.arguments.VerifyAttributes) {
+      var verifyParams = Object.assign({
+        UserAttributes: [
+          {
+            Name: 'phone_number_verified',
+            Value: 'true'
+          },
+          {
+            Name: 'email_verified',
+            Value: 'true'
+          }
+        ]
+      }, baseParams);
+
+      await cognitoidentityserviceprovider.adminUpdateUserAttributes(verifyParams).promise().then().catch(function(err) {
+        response['error'] = err.toString();
+        return response;
+      })
+    }
+    
+    response['success'] = true;
+    return response;
+};
