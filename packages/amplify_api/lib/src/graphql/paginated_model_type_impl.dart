06b6124e91d13dfa90f91d16ca87b09e7f0ec1e0
Travis Sheppard <tshepp@amazon.com>
2022-03-01 13:48:14 -0800

    dart docs

--- packages/amplify_api/lib/src/graphql/paginated_model_type_impl.dart
+++ packages/amplify_api/lib/src/graphql/paginated_model_type_impl.dart
@@ -2,7 +2,24 @@ import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_api/src/graphql/paginated_result_impl.dart';
 import 'package:amplify_core/amplify_core.dart';
 
+/// An implementation of [PaginatedModelType] used as the [modelType] for decoding list query requests.
 class PaginatedModelTypeImpl<T extends Model> extends PaginatedModelType<T> {
+  /// Pass a [modelType] to create the [PaginatedModelType] used to decode list requests
+  /// ```dart
+  /// String graphQLDocument = '''query GetBlogsCustomDecoder {
+  ///   listBlogs {
+  ///     items {
+  ///       id
+  ///       name
+  ///       createdAt
+  ///     }
+  ///   }
+  /// }''';
+  /// final request = GraphQLRequest<PaginatedResult<Blog>>(
+  ///     document: graphQLDocument,
+  ///     modelType: const PaginatedModelTypeImpl(Blog.classType),
+  ///     decodePath: 'listBlogs');
+  /// ```
   const PaginatedModelTypeImpl(ModelType<T> modelType) : super(modelType);
 
   @override
