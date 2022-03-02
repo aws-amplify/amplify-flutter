76f1f75a0171133e890616a1f0e30bfdc620b671
Dillon Nys <nydillon@amazon.com>
2022-03-02 11:28:24 -0700

    chore(smithy): Update HTTP checksum interceptors

--- packages/smithy/lib/src/http/interceptors/checksum.dart
+++ packages/smithy/lib/src/http/interceptors/checksum.dart
@@ -2,21 +2,71 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
-import 'package:http/http.dart';
 import 'package:smithy/smithy.dart';
 
-/// Intercepts HTTP operations to include an MD5 content hash in the header.
+const _header = 'Content-MD5';
+
+/// {@template smithy.with_checksum}
+/// Intercepts HTTP requests to include an MD5 content hash in the header.
 ///
 /// See: https://awslabs.github.io/smithy/1.0/spec/core/behavior-traits.html#httpchecksumrequired-trait
-class WithChecksum extends HttpInterceptor {
+/// {@endtemplate}
+class WithChecksum extends HttpRequestInterceptor {
+  /// {@macro smithy.with_checksum}
   const WithChecksum();
 
   @override
   Future<AWSStreamedHttpRequest> intercept(
     AWSStreamedHttpRequest request,
   ) async {
-    final bytes = await ByteStream(request.split()).toBytes();
-    request.headers['Content-MD5'] = base64Encode(md5.convert(bytes).bytes);
+    if (request.headers.containsKey(_header)) {
+      return request;
+    }
+    final digest = await md5.bind(request.split()).last;
+    request.headers[_header] = base64Encode(digest.bytes);
     return request;
   }
+}
+
+/// {@template smithy.validate_checksum}
+/// Intercepts HTTP responses to validate the body against an MD5 content hash
+/// in the header.
+///
+/// See: https://awslabs.github.io/smithy/1.0/spec/core/behavior-traits.html#httpchecksumrequired-trait
+/// {@endtemplate}
+class ValidateChecksum extends HttpResponseInterceptor {
+  /// {@macro smithy.validate_checksum}
+  const ValidateChecksum();
+
+  @override
+  Future<void> intercept(AWSStreamedHttpResponse response) async {
+    final checksum = response.headers[_header];
+    if (checksum == null) {
+      return;
+    }
+    final digest = await md5.bind(response.split()).last;
+    final computed = base64Encode(digest.bytes);
+    if (checksum != computed) {
+      throw ChecksumValidationException(
+        'Content validation failed.\n'
+        'Expected: $checksum\n'
+        'Got: $computed',
+      );
+    }
+  }
+}
+
+/// {@template smithy.checksum_validation_exception}
+/// Thrown when the computed hash of a response payload does not match the
+/// `Content-MD5` header returned from the server.
+/// {@endtemplate}
+class ChecksumValidationException implements SmithyException {
+  /// {@macro smithy.checksum_validation_exception}
+  const ChecksumValidationException(this.message);
+
+  @override
+  final String message;
+
+  @override
+  RetryConfig? get retryConfig => null;
 }
