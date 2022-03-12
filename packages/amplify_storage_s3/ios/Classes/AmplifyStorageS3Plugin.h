df878c86c41d98f17f9384029f16dede6b50aa02
Kyle <kylcheng@gmail.com>
2022-03-11 17:32:57 -0800

    chore(amplify_storage_s3): federated plugin (#1407)

--- packages/amplify_storage_s3/ios/Classes/AmplifyStorageS3Plugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AmplifyStorageS3Plugin : NSObject<FlutterPlugin>
-@end
---
+++ packages/storage/amplify_storage_s3_ios/ios/Classes/StorageS3.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface StorageS3 : NSObject<FlutterPlugin>
+@end
