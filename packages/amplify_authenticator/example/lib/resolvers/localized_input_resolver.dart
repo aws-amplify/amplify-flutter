3be7a488bde79cf066d2d347a752df98748532f0
Dustin Noyes <dnnoyes@amazon.com>
2022-03-21 10:52:05 -0700

    adds input resolver to example app

---
+++ packages/amplify_authenticator/example/lib/resolvers/localized_input_resolver.dart
@@ -0,0 +1,29 @@
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class LocalizedInputResolver extends InputResolver {
+  const LocalizedInputResolver();
+
+  /// Override the title function
+  /// Since this function handles the title for numerous fields,
+  /// we recommend using a switch statement so that `super` can be called
+  /// in the default case.
+  @override
+  String title(BuildContext context, InputField field) {
+    switch (field) {
+      case InputField.username:
+        return 'Custom Username Title';
+      case InputField.email:
+        return 'Custom Email Title';
+      case InputField.password:
+        return 'Custom Password Title';
+      case InputField.passwordConfirmation:
+        return 'Custom Password Confirmation Title';
+      case InputField.customAuthChallenge:
+        return 'Custom Auth Challenge Title';
+      default:
+        return super.title(context, field);
+    }
+  }
+}
