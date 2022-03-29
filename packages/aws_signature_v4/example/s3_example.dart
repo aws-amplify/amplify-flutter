2695ab3027ee411b2572b79e047d62f68df7018d
Dillon Nys <nydillon@amazon.com>
2022-03-29 08:07:03 -0700

    chore(sigv4): Clean up S3 example

--- packages/aws_signature_v4/example/s3_example.dart
+++ packages/aws_signature_v4/example/s3_example.dart
@@ -12,69 +12,69 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// ignore_for_file: avoid_print
-
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:args/args.dart';
 import 'package:aws_common/aws_common.dart';
 import 'package:aws_signature_v4/aws_signature_v4.dart';
 import 'package:collection/collection.dart';
+import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 
 Future<void> main(List<String> args) async {
   final argParser = ArgParser();
 
   const accessKeyIdArg = 'access-key-id';
-  const accessKeyIdEnv = 'AWS_ACCESS_KEY_ID';
-
   const secretAccessKeyArg = 'secret-access-key';
-  const secretAccessKeyEnv = 'AWS_SECRET_ACCESS_KEY';
-
   const sessionTokenArg = 'session-token';
-  const sessionTokenEnv = 'AWS_SESSION_TOKEN';
 
   const bucketArg = 'bucket';
   const regionArg = 'region';
 
   argParser.addOption(accessKeyIdArg,
-      abbr: 'a', valueHelp: accessKeyIdEnv, mandatory: false);
+      abbr: 'a', valueHelp: $awsAccessKeyId, mandatory: false);
   argParser.addOption(secretAccessKeyArg,
-      abbr: 's', valueHelp: secretAccessKeyEnv, mandatory: false);
+      abbr: 's', valueHelp: $awsSecretAccessKey, mandatory: false);
   argParser.addOption(sessionTokenArg,
-      abbr: 't', valueHelp: sessionTokenEnv, mandatory: false);
-  argParser.addOption(bucketArg,
-      abbr: 'b', valueHelp: 'BUCKET', mandatory: true);
+      abbr: 't', valueHelp: $awsSessionToken, mandatory: false);
+  argParser.addOption(
+    bucketArg,
+    abbr: 'b',
+    help: 'The name of the bucket to create',
+    valueHelp: 'BUCKET',
+    mandatory: false,
+  );
   argParser.addOption(
     regionArg,
     abbr: 'r',
+    help: 'The region of the bucket',
     valueHelp: 'REGION',
     mandatory: true,
   );
 
   final pArgs = argParser.parse(args);
   final String? accessKeyId =
-      Platform.environment[accessKeyIdEnv] ?? pArgs[accessKeyIdArg];
+      Platform.environment[$awsAccessKeyId] ?? pArgs[accessKeyIdArg];
   final String? secretAccessKey =
-      Platform.environment[secretAccessKeyEnv] ?? pArgs[secretAccessKeyArg];
+      Platform.environment[$awsSecretAccessKey] ?? pArgs[secretAccessKeyArg];
   final String? sessionToken =
-      Platform.environment[sessionTokenEnv] ?? pArgs[sessionTokenArg];
+      Platform.environment[$awsSessionToken] ?? pArgs[sessionTokenArg];
 
   if (accessKeyId == null || secretAccessKey == null) {
-    stderr.writeln('No AWS credentials found');
-    exit(1);
+    exitError('No AWS credentials found');
   }
 
-  final bucket = pArgs[bucketArg] as String;
+  final bucket =
+      pArgs[bucketArg] as String? ?? 'mybucket-${Random().nextInt(1 << 30)}';
   final region = pArgs[regionArg] as String;
   final filename = pArgs.rest.singleOrNull;
 
   if (filename == null) {
-    stderr.writeln(
+    exitError(
       'Usage: dart s3_example.dart --bucket=... --region=... <FILE_TO_UPLOAD>',
     );
-    exit(1);
   }
 
   final AWSCredentials credentials =
@@ -107,24 +107,26 @@ Future<void> main(List<String> args) async {
     },
   );
 
+  stdout.writeln('Creating bucket $bucket...');
   final AWSSignedRequest signedCreateRequest = await signer.sign(
     createRequest,
     credentialScope: scope,
     serviceConfiguration: serviceConfiguration,
   );
-  final createResponse = await signedCreateRequest.send();
-  final createStatus = createResponse.statusCode;
-  print('Create Bucket Response: $createStatus');
+  final http.StreamedResponse createResponse = await signedCreateRequest.send();
+  final int createStatus = createResponse.statusCode;
+  stdout.writeln('Create Bucket Response: $createStatus');
   if (createStatus == 409) {
-    print('Bucket name already exists!');
+    exitError('Bucket name already exists!');
   }
   if (createStatus != 200) {
-    exit(1);
+    exitError('Bucket creation failed');
   }
+  stdout.writeln('Bucket creation succeeded!');
 
   // Upload the file
   final Stream<List<int>> file = File(filename).openRead();
-  final path = '/${p.basename(filename)}';
+  final String path = '/${p.basename(filename)}';
   final AWSStreamedHttpRequest uploadRequest = AWSStreamedHttpRequest(
     method: HttpMethod.put,
     host: host,
@@ -132,21 +134,23 @@ Future<void> main(List<String> args) async {
     body: file,
     headers: {
       AWSHeaders.host: host,
+      AWSHeaders.contentType: 'text/plain',
     },
   );
 
+  stdout.writeln('Uploading file $filename to $path...');
   final AWSSignedRequest signedUploadRequest = await signer.sign(
     uploadRequest,
     credentialScope: scope,
     serviceConfiguration: serviceConfiguration,
   );
-  final uploadResponse = await signedUploadRequest.send();
-  final uploadStatus = uploadResponse.statusCode;
-  print('Upload File Response: $uploadStatus');
+  final http.StreamedResponse uploadResponse = await signedUploadRequest.send();
+  final int uploadStatus = uploadResponse.statusCode;
+  stdout.writeln('Upload File Response: $uploadStatus');
   if (uploadStatus != 200) {
-    stderr.writeln('Could not upload file');
-    exit(1);
+    exitError('Could not upload file');
   }
+  stdout.writeln('File uploaded successfully!');
 
   // Create a pre-signed URL for downloading the file
   final AWSHttpRequest urlRequest = AWSHttpRequest(
@@ -161,6 +165,13 @@ Future<void> main(List<String> args) async {
     urlRequest,
     credentialScope: scope,
     serviceConfiguration: serviceConfiguration,
+    expiresIn: const Duration(minutes: 10),
   );
-  print('Download URL: $signedUrl');
+  stdout.writeln('Download URL: $signedUrl');
+}
+
+/// Exits the script with an [error].
+Never exitError(String error) {
+  stderr.writeln(error);
+  exit(1);
 }
