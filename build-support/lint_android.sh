557666e7ac0ae959846c0003c0448e9108dd4746
Hui Zhao <zhohz@amazon.com>
2022-03-30 13:55:40 -0700

    chore: Update android lint runner script

--- build-support/lint_android.sh
+++ build-support/lint_android.sh
@@ -10,8 +10,10 @@ popd
 
 cd android
 
-# Run in background to prevent Melos from hanging
-./gradlew :$project:lintDebug --no-rebuild --no-daemon --stacktrace &
+# * Run in background to prevent Melos from hanging
+# * can't use -no-rebuild here as the lint depends on local_aar_for_lint the
+#   intermediates to work, but flutter build doesn't generate it
+./gradlew :$project:lintDebug --no-daemon --stacktrace &
 gradle_pid=$!
 
 if ! wait $gradle_pid; then
