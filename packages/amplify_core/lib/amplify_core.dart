1c75b89ec13a4e341d6b75265646036696552de6
Dustin Noyes <dnnoyes@amazon.com>
2022-04-27 10:25:03 -0700

    interface hierarchy fix

--- packages/amplify_core/lib/amplify_core.dart
+++ packages/amplify_core/lib/amplify_core.dart
@@ -57,7 +57,7 @@ export 'src/types/plugin/amplify_analytics_plugin_interface.dart';
 export 'src/types/plugin/amplify_api_plugin_interface.dart';
 export 'src/types/plugin/amplify_auth_plugin_interface.dart';
 export 'src/types/plugin/amplify_plugin_interface.dart';
-export 'src/types/plugin/amplify_storage_plugin_interface.dart';
+export 'src/types/plugin/amplify_storage_category_interface.dart';
 
 /// Query
 export 'src/types/query/query_field.dart';
