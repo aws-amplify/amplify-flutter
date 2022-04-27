6d53dff500a343669f53efbaed7654cd88bbf823
Dustin Noyes <dnnoyes@amazon.com>
2022-04-27 08:41:00 -0700

    updated analytics test

--- packages/analytics/amplify_analytics_pinpoint/test/amplify_analytics_pinpoint_test.dart
+++ packages/analytics/amplify_analytics_pinpoint/test/amplify_analytics_pinpoint_test.dart
@@ -14,6 +14,7 @@
  */
 
 import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
