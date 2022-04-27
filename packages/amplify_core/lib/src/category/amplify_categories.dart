41cee525e1db618d73e56be4ea08105207f39aeb
Dustin Noyes <dnnoyes@amazon.com>
2022-04-27 12:51:11 -0700

    storage interfaces

--- packages/amplify_core/lib/src/category/amplify_categories.dart
+++ packages/amplify_core/lib/src/category/amplify_categories.dart
@@ -22,6 +22,7 @@ import 'package:amplify_core/amplify_core.dart';
 import 'package:amplify_core/src/types/plugin/amplify_analytics_plugin_interface.dart';
 import 'package:amplify_core/src/types/plugin/amplify_auth_plugin_interface.dart';
 import 'package:amplify_core/src/types/plugin/amplify_plugin_interface.dart';
+import 'package:amplify_core/src/types/plugin/amplify_storage_plugin_interface.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
