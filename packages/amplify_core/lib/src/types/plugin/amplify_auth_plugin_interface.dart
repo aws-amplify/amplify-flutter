00cf6bc2ef3bc380317890f87a9f7edadfa15545
Dustin Noyes <dnnoyes@amazon.com>
2022-05-03 09:37:36 -0700

    more linting fixes plus moving tests

--- packages/amplify_core/lib/src/types/plugin/amplify_auth_plugin_interface.dart
+++ packages/amplify_core/lib/src/types/plugin/amplify_auth_plugin_interface.dart
@@ -16,7 +16,6 @@
 import 'dart:async';
 
 import 'package:amplify_core/amplify_core.dart';
-import 'package:amplify_core/src/types/plugin/amplify_plugin_interface.dart';
 import 'package:meta/meta.dart';
 
 abstract class AuthPluginInterface extends AmplifyPluginInterface {
