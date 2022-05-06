22a1d178ea8d8ccbbc6035bf1d9162b3ddbc187b
Dustin Noyes <dnnoyes@amazon.com>
2022-05-06 16:19:47 -0700

    auth changes

--- packages/amplify_core/lib/src/types/auth/common/auth_next_step.dart
+++ packages/amplify_core/lib/src/types/auth/common/auth_next_step.dart
@@ -16,7 +16,7 @@
 import 'package:amplify_core/amplify_core.dart';
 
 class AuthNextStep {
-  Map<String, String>? additionalInfo;
+  Map<String, dynamic>? additionalInfo;
   AuthCodeDeliveryDetails? codeDeliveryDetails;
   AuthNextStep({this.codeDeliveryDetails, this.additionalInfo});
 }
