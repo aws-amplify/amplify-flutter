f210c1bb25b74fa0c292cec7717d63d0bb45cd25
Travis Sheppard <tshepp@amazon.com>
2022-05-25 15:42:13 -0700

    delete a block

--- packages/amplify_core/lib/src/types/api/exceptions/api_exception.dart
+++ packages/amplify_core/lib/src/types/api/exceptions/api_exception.dart
@@ -42,11 +42,6 @@ class ApiException extends AmplifyException {
   /// Instantiates and return a new `ApiException` from the
   /// serialized exception data
   static ApiException fromMap(Map<String, String> serializedException) {
-    var statusCode = int.tryParse(serializedException['httpStatusCode'] ?? '');
-    // Ensure a valid HTTP status code for an error.
-    if (statusCode != null && (statusCode < 300 || statusCode > 511)) {
-      statusCode = null;
-    }
     return ApiException._private(
       AmplifyException.fromMap(serializedException),
     );
