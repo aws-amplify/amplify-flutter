5babd981b7d176b8b490adfc3f45825fc343f0a0
Jordan Nelson <nejrd@amazon.com>
2022-06-01 16:11:35 -0400

    fix: pass correct params on Android

--- packages/secure_storage/amplify_secure_storage/lib/src/amplify_secure_storage.android.dart
+++ packages/secure_storage/amplify_secure_storage/lib/src/amplify_secure_storage.android.dart
@@ -26,16 +26,16 @@ class AmplifySecureStorageAndroid extends AmplifySecureStorageInterface {
 
   @override
   Future<String?> read({required String key}) {
-    return _api.read(config.scope, key);
+    return _api.read(config.defaultNamespace, key);
   }
 
   @override
   Future<void> write({required String key, required String value}) {
-    return _api.write(config.scope, key, value);
+    return _api.write(config.defaultNamespace, key, value);
   }
 
   @override
   Future<void> delete({required String key}) {
-    return _api.delete(config.scope, key);
+    return _api.delete(config.defaultNamespace, key);
   }
 }
