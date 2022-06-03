b7e63c48867f4333b98c88efed7bc5f99a90727a
Dillon Nys <nydillon@amazon.com>
2022-06-03 09:12:35 -0700

    chore(auth): Dart-only package
    Creates a Dart-only Auth package (`amplify_auth_cognito_dart`) so that worker generation is possible, and Web testing is smoother.
    
    commit-id:689c05a9

--- packages/auth/amplify_auth_cognito/lib/amplify_auth_cognito.dart
+++ packages/auth/amplify_auth_cognito/lib/amplify_auth_cognito.dart
@@ -15,51 +15,7 @@
 /// Amplify Auth Cognito
 library amplify_auth_cognito;
 
-export 'package:amplify_core/src/types/auth/auth_types.dart';
+export 'package:amplify_auth_cognito_dart/amplify_auth_cognito_dart.dart'
+    hide AmplifyAuthCognitoDart;
 
 export 'src/auth_plugin_impl.dart';
-
-export 'src/flows/hosted_ui/hosted_ui_platform.dart';
-
-// Models
-export 'src/model/attribute/cognito_resend_user_attribute_confirmation_code_options.dart';
-export 'src/model/attribute/cognito_update_user_attribute_options.dart';
-export 'src/model/attribute/cognito_update_user_attribute_step.dart';
-export 'src/model/attribute/cognito_update_user_attributes_options.dart';
-export 'src/model/device/cognito_device.dart';
-export 'src/model/hosted_ui/oauth_parameters.dart';
-export 'src/model/password/cognito_confirm_reset_password_options.dart';
-export 'src/model/password/cognito_reset_password_options.dart';
-export 'src/model/password/cognito_reset_password_result.dart';
-export 'src/model/password/cognito_update_password_options.dart';
-export 'src/model/session/cognito_auth_session.dart';
-export 'src/model/session/cognito_session_options.dart';
-export 'src/model/session/cognito_user_pool_tokens.dart';
-export 'src/model/signin/cognito_confirm_sign_in_options.dart';
-export 'src/model/signin/cognito_sign_in_options.dart';
-export 'src/model/signin/cognito_sign_in_result.dart';
-export 'src/model/signin/cognito_sign_in_step.dart';
-export 'src/model/signin/cognito_sign_in_with_web_ui_options.dart';
-export 'src/model/signup/cognito_confirm_sign_up_options.dart';
-export 'src/model/signup/cognito_resend_sign_up_code_options.dart';
-export 'src/model/signup/cognito_resend_sign_up_code_result.dart';
-export 'src/model/signup/cognito_sign_up_options.dart';
-export 'src/model/signup/cognito_sign_up_result.dart';
-export 'src/model/signup/cognito_sign_up_step.dart';
-
-export 'src/state/cognito_state_machine.dart';
-
-// State Machine
-export 'src/state/event/auth_event.dart';
-export 'src/state/event/credential_store_event.dart';
-export 'src/state/event/fetch_auth_session_event.dart';
-export 'src/state/event/hosted_ui_event.dart';
-export 'src/state/event/sign_in_event.dart';
-export 'src/state/event/sign_up_event.dart';
-
-export 'src/state/state/auth_state.dart';
-export 'src/state/state/credential_store_state.dart';
-export 'src/state/state/fetch_auth_session_state.dart';
-export 'src/state/state/hosted_ui_state.dart';
-export 'src/state/state/sign_in_state.dart';
-export 'src/state/state/sign_up_state.dart';
---
+++ packages/auth/amplify_auth_cognito_dart/test/common/mock_secure_storage.dart
@@ -0,0 +1,97 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:amplify_auth_cognito_dart/src/credentials/cognito_keys.dart';
+import 'package:amplify_secure_storage_dart/amplify_secure_storage_dart.dart';
+
+import 'mock_config.dart';
+
+class MockSecureStorage implements SecureStorageInterface {
+  final Map<String, String> _storage = {};
+
+  @override
+  void delete({required String key}) => _storage.remove(key);
+
+  @override
+  String? read({required String key}) => _storage[key];
+
+  @override
+  void write({required String key, required String value}) =>
+      _storage[key] = value;
+}
+
+void seedStorage(
+  SecureStorageInterface secureStorage, {
+  CognitoUserPoolKeys? userPoolKeys,
+  CognitoIdentityPoolKeys? identityPoolKeys,
+  HostedUiKeys? hostedUiKeys,
+}) {
+  if (userPoolKeys != null) {
+    secureStorage
+      ..write(
+        key: userPoolKeys[CognitoUserPoolKey.accessToken],
+        value: accessToken.raw,
+      )
+      ..write(
+        key: userPoolKeys[CognitoUserPoolKey.refreshToken],
+        value: refreshToken,
+      )
+      ..write(
+        key: userPoolKeys[CognitoUserPoolKey.idToken],
+        value: idToken.raw,
+      )
+      ..write(
+        key: userPoolKeys[CognitoUserPoolKey.userSub],
+        value: userSub,
+      );
+  }
+  if (identityPoolKeys != null) {
+    secureStorage
+      ..write(
+        key: identityPoolKeys[CognitoIdentityPoolKey.accessKeyId],
+        value: accessKeyId,
+      )
+      ..write(
+        key: identityPoolKeys[CognitoIdentityPoolKey.secretAccessKey],
+        value: secretAccessKey,
+      )
+      ..write(
+        key: identityPoolKeys[CognitoIdentityPoolKey.sessionToken],
+        value: sessionToken,
+      )
+      ..write(
+        key: identityPoolKeys[CognitoIdentityPoolKey.expiration],
+        value: expiration.toIso8601String(),
+      )
+      ..write(
+        key: identityPoolKeys[CognitoIdentityPoolKey.identityId],
+        value: identityId,
+      );
+  }
+  if (hostedUiKeys != null) {
+    secureStorage
+      ..write(
+        key: hostedUiKeys[HostedUiKey.accessToken],
+        value: accessToken.raw,
+      )
+      ..write(
+        key: hostedUiKeys[HostedUiKey.refreshToken],
+        value: refreshToken,
+      )
+      ..write(
+        key: hostedUiKeys[HostedUiKey.idToken],
+        value: idToken.raw,
+      );
+  }
+}
