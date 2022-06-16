4b9fffa9b2a43e9488647f42b110ee89fd0a1995
Jordan Nelson <nejrd@amazon.com>
2022-06-16 15:38:38 -0400

    chore: move apt-get

--- packages/secure_storage/amplify_secure_storage_dart/tool/test-desktop.sh
+++ packages/secure_storage/amplify_secure_storage_dart/tool/test-desktop.sh
@@ -4,6 +4,8 @@ set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
+    sudo apt-get update && apt install -y libsecret-1-dev libglib2.0-dev dbus gnome-keyring
+
     # If running in headless mode, re-run script in dbus session.
     if [ -z $DBUS_SESSION_BUS_ADDRESS ]; then
         exec dbus-run-session -- $0
@@ -11,7 +13,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
     # Set up keyring in CI env
     if [ -n $CI ]; then
-        sudo apt-get update && apt install -y libsecret-1-dev libglib2.0-dev dbus gnome-keyring
         echo 'password' | gnome-keyring-daemon --start --replace --daemonize --unlock
     fi
 fi
