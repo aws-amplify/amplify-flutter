357e7da018a7f9a053a88d59652cd96ec7f3079a
Jordan Nelson <nejrd@amazon.com>
2022-06-20 14:51:54 -0400

    chore(secure_storage): enable ci  for linux (#1755)

--- tool/ci.sh
+++ tool/ci.sh
@@ -88,16 +88,28 @@ for PKG in ${PKGS}; do
         git submodule update --init || EXIT_CODE=$?
         ;;
       command_2)
+        echo 'tool/test-desktop.sh'
+        tool/test-desktop.sh || EXIT_CODE=$?
+        ;;
+      command_3)
+        echo 'tool/test-web.sh -p chrome'
+        tool/test-web.sh -p chrome || EXIT_CODE=$?
+        ;;
+      command_4)
+        echo 'tool/test-web.sh -p firefox'
+        tool/test-web.sh -p firefox || EXIT_CODE=$?
+        ;;
+      command_5)
+        echo 'dart run build_runner build --delete-conflicting-outputs'
+        dart run build_runner build --delete-conflicting-outputs || EXIT_CODE=$?
+        ;;
+      command_6)
         echo 'tool/test.sh -p chrome'
         tool/test.sh -p chrome || EXIT_CODE=$?
         ;;
-      command_3)
+      command_7)
         echo 'tool/test.sh -p firefox'
         tool/test.sh -p firefox || EXIT_CODE=$?
-        ;;
-      command_4)
-        echo 'dart run build_runner build --delete-conflicting-outputs'
-        dart run build_runner build --delete-conflicting-outputs || EXIT_CODE=$?
         ;;
       format)
         echo 'dart format --output=none --set-exit-if-changed .'
