1d1ce6858db55b4154bdaae3af62e1a984d92c03
Travis Sheppard <tshepp@amazon.com>
2022-06-27 14:28:39 -0800

    feat(api): REST methods in dart with auth mode none (#1783)

---
+++ packages/api/amplify_api/lib/src/util.dart
@@ -0,0 +1,32 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:amplify_core/amplify_core.dart';
+import 'package:meta/meta.dart';
+
+/// Sets the 'Content-Type' of headers to match the [HttpPayload] body.
+@internal
+Map<String, String>? addContentTypeToHeaders(
+  Map<String, String>? headers,
+  HttpPayload? body,
+) {
+  final contentType = body?.contentType;
+  if (contentType == null) {
+    return headers;
+  }
+  // Create new map to avoid modifying input headers which may be unmodifiable.
+  final modifiedHeaders = Map<String, String>.of(headers ?? {});
+  modifiedHeaders.putIfAbsent(AWSHeaders.contentType, () => contentType);
+  return modifiedHeaders;
+}
