9a8767eb7a61879b9150f66fc9408b45fc7dd994
Jordan Nelson <nejrd@amazon.com>
2022-07-07 19:39:22 -0400

    test(authenticator): add golden tests for new configs (#1831)
    * chore: add stubs for auth cognito and amplify
    
    * test: refactor tests, add tests for new configs
    
    * chore: regenerate golden files
    
    * chore: update comment, fix accidential cmd f change
    
    * chore: add license headers
    
    * chore: make internal mocks private

---
+++ packages/amplify_authenticator/test/ui/configs/phone_config.dart
@@ -0,0 +1,53 @@
+const phoneNumberConfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "",
+                            "Region": ""
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "",
+                        "AppClientId": "",
+                        "Region": ""
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "PHONE_NUMBER"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
