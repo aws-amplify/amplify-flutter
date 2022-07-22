6ae99b5e751fd1a480c8a42001093d47eeddf9ed
Dillon Nys <nydillon@amazon.com>
2022-07-22 09:34:01 -0700

    fix(hub): Ensure stream is available before plugin registration
    Allow subscribing to streams via `availableStreams` before plugins are registered in the same way that `listen` allows.
    
    commit-id:0562e3e1

--- packages/amplify_core/test/hub/hub_test.dart
+++ packages/amplify_core/test/hub/hub_test.dart
@@ -33,169 +33,186 @@ void main() {
       await additionalController.close();
     });
 
-    test('single channel, single subscription', () {
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+    group('listen', () {
+      test('single channel, single subscription', () {
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
 
-      final Completer<AuthHubEvent> subscriber = Completer();
-      Amplify.Hub.listen(HubChannel.Auth, subscriber.complete);
+        final Completer<AuthHubEvent> subscriber = Completer();
+        Amplify.Hub.listen(HubChannel.Auth, subscriber.complete);
 
-      expect(
-        subscriber.future,
-        completes,
-        reason: 'The subscriber should receive the event',
-      );
+        expect(
+          subscriber.future,
+          completes,
+          reason: 'The subscriber should receive the event',
+        );
 
-      controller.add(AuthHubEvent.sessionExpired());
-    });
-
-    test('can listen before channel registered', () {
-      final Completer<AuthHubEvent> subscriber = Completer();
-      expect(
-        () => Amplify.Hub.listen(HubChannel.Auth, subscriber.complete),
-        returnsNormally,
-        reason: 'Listen before add should not throw',
-      );
-
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
-
-      expect(
-        subscriber.future,
-        completes,
-        reason: 'The subscriber should receive the event even though it '
-            'subscribed before the stream was added',
-      );
-
-      controller.add(AuthHubEvent.sessionExpired());
-    });
-
-    test('single channel, multiple subscriptions', () {
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
-
-      final Completer<AuthHubEvent> subscriber1 = Completer();
-      Amplify.Hub.listen(HubChannel.Auth, subscriber1.complete);
-
-      final Completer<AuthHubEvent> subscriber2 = Completer();
-      Amplify.Hub.listen(HubChannel.Auth, subscriber2.complete);
-
-      expect(
-        subscriber1.future,
-        completes,
-        reason: 'Both subscribers should receive the event',
-      );
-      expect(
-        subscriber2.future,
-        completes,
-        reason: 'Both subscribers should receive the event',
-      );
-
-      controller.add(AuthHubEvent.sessionExpired());
-    });
-
-    test('multiple channel, single subscription', () {
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
-      Amplify.Hub.addChannel(HubChannel.Auth, additionalController.stream);
-
-      var events = 0;
-      final Completer<void> subscriber = Completer();
-      Amplify.Hub.listen(HubChannel.Auth, (AuthHubEvent event) {
-        if (++events == 2) {
-          subscriber.complete();
-        }
+        controller.add(AuthHubEvent.sessionExpired());
       });
 
-      expect(
-        subscriber.future,
-        completes,
-        reason: 'The subscriber should receive the events of both streams',
-      );
+      test('can listen before channel registered', () {
+        final Completer<AuthHubEvent> subscriber = Completer();
+        expect(
+          () => Amplify.Hub.listen(HubChannel.Auth, subscriber.complete),
+          returnsNormally,
+          reason: 'Listen before add should not throw',
+        );
 
-      controller.add(AuthHubEvent.sessionExpired());
-      additionalController.add(AuthHubEvent.sessionExpired());
-    });
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
 
-    test('multiple channel, multiple subscriptions', () {
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
-      Amplify.Hub.addChannel(HubChannel.Auth, additionalController.stream);
+        expect(
+          subscriber.future,
+          completes,
+          reason: 'The subscriber should receive the event even though it '
+              'subscribed before the stream was added',
+        );
 
-      var subscriber1Events = 0;
-      final Completer<void> subscriber1 = Completer();
-      Amplify.Hub.listen(HubChannel.Auth, (AuthHubEvent event) {
-        if (++subscriber1Events == 2) {
-          subscriber1.complete();
-        }
+        controller.add(AuthHubEvent.sessionExpired());
       });
 
-      var subscriber2Events = 0;
-      final Completer<void> subscriber2 = Completer();
-      Amplify.Hub.listen(HubChannel.Auth, (AuthHubEvent event) {
-        if (++subscriber2Events == 2) {
-          subscriber2.complete();
-        }
+      test('single channel, multiple subscriptions', () {
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+
+        final Completer<AuthHubEvent> subscriber1 = Completer();
+        Amplify.Hub.listen(HubChannel.Auth, subscriber1.complete);
+
+        final Completer<AuthHubEvent> subscriber2 = Completer();
+        Amplify.Hub.listen(HubChannel.Auth, subscriber2.complete);
+
+        expect(
+          subscriber1.future,
+          completes,
+          reason: 'Both subscribers should receive the event',
+        );
+        expect(
+          subscriber2.future,
+          completes,
+          reason: 'Both subscribers should receive the event',
+        );
+
+        controller.add(AuthHubEvent.sessionExpired());
       });
 
-      controller.add(AuthHubEvent.sessionExpired());
-      additionalController.add(AuthHubEvent.sessionExpired());
+      test('multiple channel, single subscription', () {
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+        Amplify.Hub.addChannel(HubChannel.Auth, additionalController.stream);
 
-      expect(
-        subscriber1.future,
-        completes,
-        reason: 'Both subscribers should receive the events of both streams',
-      );
-      expect(
-        subscriber1.future,
-        completes,
-        reason: 'Both subscribers should receive the events of both streams',
-      );
+        var events = 0;
+        final Completer<void> subscriber = Completer();
+        Amplify.Hub.listen(HubChannel.Auth, (AuthHubEvent event) {
+          if (++events == 2) {
+            subscriber.complete();
+          }
+        });
+
+        expect(
+          subscriber.future,
+          completes,
+          reason: 'The subscriber should receive the events of both streams',
+        );
+
+        controller.add(AuthHubEvent.sessionExpired());
+        additionalController.add(AuthHubEvent.sessionExpired());
+      });
+
+      test('multiple channel, multiple subscriptions', () {
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+        Amplify.Hub.addChannel(HubChannel.Auth, additionalController.stream);
+
+        var subscriber1Events = 0;
+        final Completer<void> subscriber1 = Completer();
+        Amplify.Hub.listen(HubChannel.Auth, (AuthHubEvent event) {
+          if (++subscriber1Events == 2) {
+            subscriber1.complete();
+          }
+        });
+
+        var subscriber2Events = 0;
+        final Completer<void> subscriber2 = Completer();
+        Amplify.Hub.listen(HubChannel.Auth, (AuthHubEvent event) {
+          if (++subscriber2Events == 2) {
+            subscriber2.complete();
+          }
+        });
+
+        controller.add(AuthHubEvent.sessionExpired());
+        additionalController.add(AuthHubEvent.sessionExpired());
+
+        expect(
+          subscriber1.future,
+          completes,
+          reason: 'Both subscribers should receive the events of both streams',
+        );
+        expect(
+          subscriber1.future,
+          completes,
+          reason: 'Both subscribers should receive the events of both streams',
+        );
+      });
+
+      test('single subscriber error', () {
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+
+        final Completer<void> finished = Completer();
+        Amplify.Hub.listen(
+          HubChannel.Auth,
+          (_) {},
+          onError: finished.complete,
+        );
+
+        controller.addError(Exception());
+
+        expect(
+          finished.future,
+          completes,
+          reason: 'Subscriptions should be able to capture errors',
+        );
+      });
+
+      test('multiple subscriber error', () {
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+
+        final Completer<void> finished1 = Completer();
+        Amplify.Hub.listen(
+          HubChannel.Auth,
+          (_) {},
+          onError: finished1.complete,
+        );
+
+        final Completer<void> finished2 = Completer();
+        Amplify.Hub.listen(
+          HubChannel.Auth,
+          (_) {},
+          onError: finished2.complete,
+        );
+
+        controller.addError(Exception());
+
+        expect(
+          finished1.future,
+          completes,
+          reason: 'Errors should be forwarded to all subscribers',
+        );
+        expect(
+          finished2.future,
+          completes,
+          reason: 'Errors should be forwarded to all subscribers',
+        );
+      });
     });
 
-    test('single subscriber error', () {
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+    group('availableStreams', () {
+      test('can listen to channel before plugin registration', () {
+        final stream = Amplify.Hub.availableStreams[HubChannel.Auth];
+        expect(stream, isNotNull);
 
-      final Completer<void> finished = Completer();
-      Amplify.Hub.listen(
-        HubChannel.Auth,
-        (_) {},
-        onError: finished.complete,
-      );
+        final Completer<void> gotEvent = Completer();
+        stream!.listen((_) => gotEvent.complete());
 
-      controller.addError(Exception());
+        Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
+        controller.add(AuthHubEvent.sessionExpired());
 
-      expect(
-        finished.future,
-        completes,
-        reason: 'Subscriptions should be able to capture errors',
-      );
-    });
-
-    test('multiple subscriber error', () {
-      Amplify.Hub.addChannel(HubChannel.Auth, controller.stream);
-
-      final Completer<void> finished1 = Completer();
-      Amplify.Hub.listen(
-        HubChannel.Auth,
-        (_) {},
-        onError: finished1.complete,
-      );
-
-      final Completer<void> finished2 = Completer();
-      Amplify.Hub.listen(
-        HubChannel.Auth,
-        (_) {},
-        onError: finished2.complete,
-      );
-
-      controller.addError(Exception());
-
-      expect(
-        finished1.future,
-        completes,
-        reason: 'Errors should be forwarded to all subscribers',
-      );
-      expect(
-        finished2.future,
-        completes,
-        reason: 'Errors should be forwarded to all subscribers',
-      );
+        expect(gotEvent.future, completes);
+      });
     });
   });
 }
