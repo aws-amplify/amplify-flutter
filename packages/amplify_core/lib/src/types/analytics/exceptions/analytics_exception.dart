0cdc80bc57f16d9f223ef477aa2300c2cdac0ed5
Dillon Nys <nydillon@amazon.com>
2022-07-25 09:15:40 -0700

    chore(core): Migrate exceptions to use raw exception in `underlyingException`
    commit-id:d3a10a0a

--- packages/amplify_core/lib/src/types/analytics/exceptions/analytics_exception.dart
+++ packages/amplify_core/lib/src/types/analytics/exceptions/analytics_exception.dart
@@ -18,14 +18,10 @@ import 'package:amplify_core/amplify_core.dart';
 /// Base Class for Analytics Exceptions
 class AnalyticsException extends AmplifyException {
   const AnalyticsException(
-    String message, {
-    String? recoverySuggestion,
-    String? underlyingException,
-  }) : super(
-          message,
-          recoverySuggestion: recoverySuggestion,
-          underlyingException: underlyingException,
-        );
+    super.message, {
+    super.recoverySuggestion,
+    super.underlyingException,
+  });
 
   /// Constructor for down casting an AmplifyException to this exception
   AnalyticsException._private(AmplifyException exception)
