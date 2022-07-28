bc0126bce43a6d975bf2fe27b5fae44afd9d0f8c
Dillon Nys <nydillon@amazon.com>
2022-07-28 14:40:02 -0700

    chore(smithy): Clean up `union.dart`
    Removes the enum example from union
    
    commit-id:afe5426b

--- packages/smithy/smithy/lib/src/types/union.dart
+++ packages/smithy/smithy/lib/src/types/union.dart
@@ -16,48 +16,13 @@ import 'package:aws_common/aws_common.dart';
 import 'package:built_value/serializer.dart';
 import 'package:smithy/smithy.dart';
 
+/// {@template smithy.smithy_union}
 /// An union representation in Smithy, i.e. a value which is one of a fixed set
 /// of types.
-///
-/// For example, the following Smithy model:
-///
-/// ```
-/// @enum("YES": {}, "NO": {})
-/// string SimpleYesNo
-/// ```
-///
-/// will be translated to an equivalent Dart model.
-///
-/// ```dart
-/// class SimpleYesNo extends SmithyEnum<SimpleYesNo> {
-///   const SimpleYesNo._(int index, String name, String value)
-///       : super(index, name, value);
-///   const SimpleYesNo.unknown(String value) : super.unknown(value);
-///
-///   static const yes = SimpleYesNo._(0, 'yes', 'YES');
-///   static const no = SimpleYesNo._(1, 'no', 'NO');
-///
-///   static const List<SimpleYesNo> values = [
-///     yes,
-///     no,
-///   ];
-/// }
-///
-/// extension $SimpleYesNoSmithy on List<SimpleYesNo> {
-///   SimpleYesNo? byName(String? name) =>
-///       firstWhereOrNull((el) => el.name == name);
-///
-///   SimpleYesNo? byValue(String? value) {
-///     if (value == null) return null;
-///     return firstWhere(
-///       (el) => el.value == value,
-///       orElse: () => SimpleYesNo.unknown(value),
-///     );
-///   }
-/// }
-/// ```
+/// {@endtemplate}
 abstract class SmithyUnion<U extends SmithyUnion<U>>
     with AWSEquatable<SmithyUnion<U>> {
+  /// {@macro smithy.smithy_union}
   const SmithyUnion();
 
   /// The key or property name of [value].
