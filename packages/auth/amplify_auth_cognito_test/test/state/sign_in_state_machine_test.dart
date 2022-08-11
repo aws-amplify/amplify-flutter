aws-amplify/amplify-flutter

commit 7e754cab2623932d493d72662e85ddc05bc0c1b0

fix(auth): Remember device metadata (#2010)
Fixes issue 1 of #2009 by not clearing the device credentials on log out, only when uninstalling the app or calling `forgetDevice`. Refactors the internal device metadata as well. Related to #1929.

packages/auth/amplify_auth_cognito_dart/lib/src/model/cognito_user.dart
@@ -38,8 +38,9 @@ abstract class CognitoUser implements Built<CognitoUser, CognitoUserBuilder> {
     b.username ??= b.userId!;
 
     // Assert that all device info is included if any is included.
-    if (b.device != null) {
-      ArgumentError.checkNotNull(b.device, 'device');
+    if (b.deviceSecrets.deviceKey != null ||
+        b.deviceSecrets.deviceGroupKey != null ||
+        b.deviceSecrets.devicePassword != null) {
       ArgumentError.checkNotNull(
         b.deviceSecrets.deviceGroupKey,
         'deviceGroupKey',
@@ -47,6 +48,10 @@ abstract class CognitoUser implements Built<CognitoUser, CognitoUserBuilder> {
       ArgumentError.checkNotNull(
         b.deviceSecrets.deviceKey,
         'deviceKey',
+      );
+      ArgumentError.checkNotNull(
+        b.deviceSecrets.devicePassword,
+        'devicePassword',
       );
     }
   }
@@ -59,9 +64,6 @@ abstract class CognitoUser implements Built<CognitoUser, CognitoUserBuilder> {
 
   /// Cognito User Pool tokens
   CognitoUserPoolTokens? get userPoolTokens;
-
-  /// Confirmed device
-  CognitoDevice? get device;
 
   /// Confirmed device secrets
   CognitoDeviceSecrets? get deviceSecrets;

packages/auth/amplify_auth_cognito_dart/lib/src/model/cognito_user.g.dart
@@ -14,8 +14,6 @@ class _$CognitoUser extends CognitoUser {
   @override
   final CognitoUserPoolTokens? userPoolTokens;
   @override
-  final CognitoDevice? device;
-  @override
   final CognitoDeviceSecrets? deviceSecrets;
   @override
   final String userId;
@@ -31,7 +29,6 @@ class _$CognitoUser extends CognitoUser {
       {this.identityId,
       this.awsCredentials,
       this.userPoolTokens,
-      this.device,
       this.deviceSecrets,
       required this.userId,
       required this.username,
@@ -57,7 +54,6 @@ class _$CognitoUser extends CognitoUser {
         identityId == other.identityId &&
         awsCredentials == other.awsCredentials &&
         userPoolTokens == other.userPoolTokens &&
-        device == other.device &&
         deviceSecrets == other.deviceSecrets &&
         userId == other.userId &&
         username == other.username &&
@@ -71,11 +67,9 @@ class _$CognitoUser extends CognitoUser {
             $jc(
                 $jc(
                     $jc(
-                        $jc(
-                            $jc($jc(0, identityId.hashCode),
-                                awsCredentials.hashCode),
-                            userPoolTokens.hashCode),
-                        device.hashCode),
+                        $jc($jc(0, identityId.hashCode),
+                            awsCredentials.hashCode),
+                        userPoolTokens.hashCode),
                     deviceSecrets.hashCode),
                 userId.hashCode),
             username.hashCode),
@@ -88,7 +82,6 @@ class _$CognitoUser extends CognitoUser {
           ..add('identityId', identityId)
           ..add('awsCredentials', awsCredentials)
           ..add('userPoolTokens', userPoolTokens)
-          ..add('device', device)
           ..add('deviceSecrets', deviceSecrets)
           ..add('userId', userId)
           ..add('username', username)
@@ -114,10 +107,6 @@ class CognitoUserBuilder implements Builder<CognitoUser, CognitoUserBuilder> {
       _$this._userPoolTokens ??= new CognitoUserPoolTokensBuilder();
   set userPoolTokens(CognitoUserPoolTokensBuilder? userPoolTokens) =>
       _$this._userPoolTokens = userPoolTokens;
-
-  CognitoDevice? _device;
-  CognitoDevice? get device => _$this._device;
-  set device(CognitoDevice? device) => _$this._device = device;
 
   CognitoDeviceSecretsBuilder? _deviceSecrets;
   CognitoDeviceSecretsBuilder get deviceSecrets =>
@@ -147,7 +136,6 @@ class CognitoUserBuilder implements Builder<CognitoUser, CognitoUserBuilder> {
       _identityId = $v.identityId;
       _awsCredentials = $v.awsCredentials;
       _userPoolTokens = $v.userPoolTokens?.toBuilder();
-      _device = $v.device;
       _deviceSecrets = $v.deviceSecrets?.toBuilder();
       _userId = $v.userId;
       _username = $v.username;
@@ -180,7 +168,6 @@ class CognitoUserBuilder implements Builder<CognitoUser, CognitoUserBuilder> {
               identityId: identityId,
               awsCredentials: awsCredentials,
               userPoolTokens: _userPoolTokens?.build(),
-              device: device,
               deviceSecrets: _deviceSecrets?.build(),
               userId: BuiltValueNullFieldError.checkNotNull(
                   userId, r'CognitoUser', 'userId'),
@@ -192,7 +179,6 @@ class CognitoUserBuilder implements Builder<CognitoUser, CognitoUserBuilder> {
       try {
         _$failedField = 'userPoolTokens';
         _userPoolTokens?.build();
-
         _$failedField = 'deviceSecrets';
         _deviceSecrets?.build();
 

packages/auth/amplify_auth_cognito_dart/lib/src/state/machines/sign_in_state_machine.dart
@@ -18,6 +18,7 @@ import 'dart:convert';
 import 'package:amplify_auth_cognito_dart/amplify_auth_cognito_dart.dart'
     hide UpdateUserAttributesRequest;
 import 'package:amplify_auth_cognito_dart/src/credentials/cognito_keys.dart';
+import 'package:amplify_auth_cognito_dart/src/credentials/device_metadata_repository.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/constants.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/device/confirm_device_worker.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/helpers.dart';
@@ -248,7 +249,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         ..clientId = config.appClientId
         ..clientSecret = config.appClientSecret
         ..poolId = config.poolId
-        ..deviceKey = user.device?.id
+        ..deviceKey = user.deviceSecrets.deviceKey
         ..challengeParameters = challengeParameters
         ..parameters = SignInParameters(
           (b) => b
@@ -273,7 +274,8 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         ..challengeName = ChallengeNameType.deviceSrpAuth
         ..challengeResponses.addAll({
           CognitoConstants.challengeParamUsername: user.username!,
-          CognitoConstants.challengeParamDeviceKey: user.device!.id,
+          CognitoConstants.challengeParamDeviceKey:
+              user.deviceSecrets.deviceKey!,
           CognitoConstants.challengeParamSrpA:
               initResult.publicA.toRadixString(16),
         });
@@ -434,7 +436,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         );
       }
 
-      final deviceKey = user.device?.id;
+      final deviceKey = user.deviceSecrets.deviceKey;
       if (deviceKey != null) {
         b.authParameters[CognitoConstants.challengeParamDeviceKey] = deviceKey;
       }
@@ -466,7 +468,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         );
       }
 
-      final deviceKey = user.device?.id;
+      final deviceKey = user.deviceSecrets.deviceKey;
       if (deviceKey != null) {
         b.authParameters[CognitoConstants.challengeParamDeviceKey] = deviceKey;
       }
@@ -507,7 +509,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         );
       }
 
-      final deviceKey = user.device?.id;
+      final deviceKey = user.deviceSecrets.deviceKey;
       if (deviceKey != null) {
         b.authParameters[CognitoConstants.challengeParamDeviceKey] = deviceKey;
       }
@@ -609,9 +611,8 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
 
     // Collect current user info which may influence SRP flow.
     try {
-      final result =
-          await expect(CredentialStoreStateMachine.type).getCredentialsResult();
-      final deviceSecrets = result.data.deviceSecrets;
+      final deviceSecrets = await getOrCreate(DeviceMetadataRepository.token)
+          .get(event.parameters.username);
       if (deviceSecrets != null) {
         user.deviceSecrets.replace(deviceSecrets);
       }
@@ -638,7 +639,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
   /// If device tracking is set to opt-in, a second call to `rememberDevice` is
   /// needed to remember the device. If device tracking is set to always, then
   /// the device is remembered as part of this call.
-  Future<void> _createDevice(
+  Future<String> _createDevice(
     String accessToken,
     NewDeviceMetadataType newDeviceMetadata,
   ) async {
@@ -650,9 +651,10 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
           ..newDeviceMetadata.replace(newDeviceMetadata),
       ),
     );
-    final request = await worker.stream.first;
+    final response = await worker.stream.first;
+    await cognitoIdentityProvider.confirmDevice(response.request);
 
-    await cognitoIdentityProvider.confirmDevice(request);
+    return response.devicePassword;
   }
 
   /// Update any user attributes which could not be sent in the
@@ -703,19 +705,15 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
           hasIdentityPool) {
         user.deviceSecrets
           ..deviceGroupKey = newDeviceMetadata.deviceGroupKey
-          ..deviceKey = newDeviceMetadata.deviceKey;
+          ..deviceKey = newDeviceMetadata.deviceKey
+          ..devicePassword = await _createDevice(
+            accessToken,
+            newDeviceMetadata,
+          );
 
-        await _createDevice(
-          accessToken,
-          newDeviceMetadata,
-        );
-
-        dispatch(
-          CredentialStoreEvent.storeCredentials(
-            CredentialStoreData(
-              deviceSecrets: user.deviceSecrets.build(),
-            ),
-          ),
+        await getOrCreate(DeviceMetadataRepository.token).put(
+          user.username!,
+          user.deviceSecrets.build(),
         );
       }
 

packages/auth/amplify_auth_cognito_dart/lib/src/auth_plugin_impl.dart
@@ -15,6 +15,7 @@
 import 'dart:async';
 
 import 'package:amplify_auth_cognito_dart/amplify_auth_cognito_dart.dart';
+import 'package:amplify_auth_cognito_dart/src/credentials/device_metadata_repository.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/constants.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/helpers.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/hosted_ui/initial_parameters_stub.dart'
@@ -115,6 +116,10 @@ class AmplifyAuthCognitoDart extends AuthPluginInterface
   /// The Cognito identity pool configuration.
   CognitoIdentityCredentialsProvider? get _identityPoolConfig =>
       _stateMachine.get();
+
+  /// The device metadata repository, used for handling device operations.
+  DeviceMetadataRepository get _deviceRepo =>
+      _stateMachine.getOrCreate(DeviceMetadataRepository.token);
 
   final StreamController<AuthHubEvent> _hubEventController =
       StreamController.broadcast();
@@ -750,7 +755,7 @@ class AmplifyAuthCognitoDart extends AuthPluginInterface
   }) async {
     final userPoolTokens = await getUserPoolTokens();
     final userId = userPoolTokens.idToken.userId;
-    final username = CognitoIdToken(userPoolTokens.idToken).username;
+    final username = userPoolTokens.username;
     return AuthUser(
       userId: userId,
       username: username,
@@ -759,21 +764,16 @@ class AmplifyAuthCognitoDart extends AuthPluginInterface
 
   @override
   Future<void> rememberDevice() async {
-    // Use credentials state machine since we need device info as well.
-    final result = await _stateMachine
-        .expect(CredentialStoreStateMachine.type)
-        .getCredentialsResult();
-    final deviceKey = result.data.deviceSecrets?.deviceKey;
+    final tokens = await getUserPoolTokens();
+    final username = tokens.username;
+    final deviceSecrets = await _deviceRepo.get(username);
+    final deviceKey = deviceSecrets?.deviceKey;
     if (deviceKey == null) {
       throw const DeviceNotTrackedException();
     }
-    final accessToken = result.data.userPoolTokens?.accessToken;
-    if (accessToken == null) {
-      throw const SignedOutException('No user is currently signed in');
-    }
     await _cognitoIdp.updateDeviceStatus(
       cognito.UpdateDeviceStatusRequest(
-        accessToken: accessToken.raw,
+        accessToken: tokens.accessToken.raw,
         deviceKey: deviceKey,
         deviceRememberedStatus: cognito.DeviceRememberedStatusType.remembered,
       ),
@@ -782,17 +782,17 @@ class AmplifyAuthCognitoDart extends AuthPluginInterface
 
   @override
   Future<void> forgetDevice([AuthDevice? device]) async {
-    // Use credentials state machine since we need device info as well.
-    final result = await _stateMachine
-        .expect(CredentialStoreStateMachine.type)
-        .getCredentialsResult();
-    final deviceKey = device?.id ?? result.data.deviceSecrets?.deviceKey;
+    final tokens = await getUserPoolTokens();
+    final username = tokens.username;
+    final deviceSecrets = await _deviceRepo.get(username);
+    final deviceKey = device?.id ?? deviceSecrets?.deviceKey;
     if (deviceKey == null) {
       throw const DeviceNotTrackedException();
     }
+    await _deviceRepo.remove(username);
     await _cognitoIdp.forgetDevice(
       cognito.ForgetDeviceRequest(
-        accessToken: result.data.userPoolTokens?.accessToken.raw,
+        accessToken: tokens.accessToken.raw,
         deviceKey: deviceKey,
       ),
     );
@@ -918,6 +918,7 @@ class AmplifyAuthCognitoDart extends AuthPluginInterface
     await _stateMachine.dispatch(
       const CredentialStoreEvent.clearCredentials(),
     );
+    await _deviceRepo.remove(tokens.username);
     _hubEventController
       ..add(AuthHubEvent.signedOut())
       ..add(AuthHubEvent.userDeleted());

packages/auth/amplify_auth_cognito_dart/lib/src/credentials/cognito_keys.dart
@@ -21,7 +21,9 @@ import 'package:amplify_auth_cognito_dart/amplify_auth_cognito_dart.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:meta/meta.dart';
 
+/// {@template amplify_auth_cognito_dart.cognito_user_pool_key}
 /// Discrete keys stored for Cognito User Pool operations in secure storage.
+/// {@endtemplate}
 enum CognitoUserPoolKey {
   /// The access token, serialized as a JWT.
   accessToken,
@@ -34,12 +36,19 @@ enum CognitoUserPoolKey {
 
   /// The user ID.
   userSub,
+}
 
+/// Discrete keys stored for Cognito User Pool device tracking operations in
+/// secure storage.
+enum CognitoDeviceKey {
   /// The device key.
   deviceKey,
 
   /// The device group key.
   deviceGroupKey,
+
+  /// The device password.
+  devicePassword
 }
 
 /// Discrete keys stored for Cognito Identity Pool operations in secure storage.
@@ -119,6 +128,27 @@ class CognitoUserPoolKeys extends CognitoKeys<CognitoUserPoolKey> {
 
   @override
   String get prefix => config.appClientId;
+}
+
+/// {@template amplify_auth_cognito.cognito_user_pool_keys}
+/// Enumerates and iterates over the keys stored in secure storage by
+/// Cognito User Pool device tracking operations.
+/// {@endtemplate}
+class CognitoDeviceKeys extends CognitoKeys<CognitoDeviceKey> {
+  /// {@macro amplify_auth_cognito.cognito_user_pool_keys}
+  const CognitoDeviceKeys(this.config, this.username);
+
+  /// The Cognito user pool configuration, used to determine the key prefixes.
+  final CognitoUserPoolConfig config;
+
+  /// Device keys are tracked by username.
+  final String username;
+
+  @override
+  List<CognitoDeviceKey> get _values => CognitoDeviceKey.values;
+
+  @override
+  String get prefix => '${config.appClientId}.$username';
 }
 
 /// {@template amplify_auth_cognito.hosted_ui_keys}

packages/auth/amplify_auth_cognito_dart/lib/src/credentials/device_metadata_repository.dart
@@ -1,0 +1,88 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:amplify_auth_cognito_dart/src/credentials/cognito_keys.dart';
+import 'package:amplify_auth_cognito_dart/src/model/cognito_device_secrets.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:amplify_secure_storage_dart/amplify_secure_storage_dart.dart';
+
+/// {@template amplify_auth_cognito_dart.credentials.device_metadata_repository}
+/// Repository for device secrets and metadata, used to isolate the loading
+/// {@endtemplate}
+class DeviceMetadataRepository {
+  /// {@macro amplify_auth_cognito_dart.credentials.device_metadata_repository}
+  const DeviceMetadataRepository(
+    this._userPoolConfig,
+    this._secureStorage,
+  );
+
+  /// The token of [DeviceMetadataRepository] for use with a
+  /// [DependencyManager].
+  static const token = Token<DeviceMetadataRepository>([
+    Token<CognitoUserPoolConfig>(),
+    Token<SecureStorageInterface>(),
+  ]);
+
+  final CognitoUserPoolConfig _userPoolConfig;
+  final SecureStorageInterface _secureStorage;
+
+  /// Retrieves the device secrets for [username].
+  Future<CognitoDeviceSecrets?> get(String username) async {
+    CognitoDeviceSecrets? deviceSecrets;
+    final deviceKeys = CognitoDeviceKeys(_userPoolConfig, username);
+    final deviceKey = await _secureStorage.read(
+      key: deviceKeys[CognitoDeviceKey.deviceKey],
+    );
+    final deviceGroupKey = await _secureStorage.read(
+      key: deviceKeys[CognitoDeviceKey.deviceGroupKey],
+    );
+    final devicePassword = await _secureStorage.read(
+      key: deviceKeys[CognitoDeviceKey.devicePassword],
+    );
+    if (deviceKey != null && deviceGroupKey != null && devicePassword != null) {
+      deviceSecrets = CognitoDeviceSecrets(
+        (b) => b
+          ..deviceKey = deviceKey
+          ..deviceGroupKey = deviceGroupKey
+          ..devicePassword = devicePassword,
+      );
+    }
+    return deviceSecrets;
+  }
+
+  /// Save the [deviceSecrets] for [username].
+  Future<void> put(String username, CognitoDeviceSecrets deviceSecrets) async {
+    final deviceKeys = CognitoDeviceKeys(_userPoolConfig, username);
+    await _secureStorage.write(
+      key: deviceKeys[CognitoDeviceKey.deviceKey],
+      value: deviceSecrets.deviceKey,
+    );
+    await _secureStorage.write(
+      key: deviceKeys[CognitoDeviceKey.deviceGroupKey],
+      value: deviceSecrets.deviceGroupKey,
+    );
+    await _secureStorage.write(
+      key: deviceKeys[CognitoDeviceKey.devicePassword],
+      value: deviceSecrets.devicePassword,
+    );
+  }
+
+  /// Clears the device secrets for [username].
+  Future<void> remove(String username) async {
+    final deviceKeys = CognitoDeviceKeys(_userPoolConfig, username);
+    for (final key in deviceKeys) {
+      await _secureStorage.delete(key: key);
+    }
+  }
+}

commit 33dcc36c36720886563c185b5d32ecca27cf4f71

fix(auth): CognitoDeviceSecrets attempts to build without secrets
Fixes a regression caused by #2010 for non-device flows where the state machine expects device secrets even though none are present.

commit-id:f4ab4a52

packages/auth/amplify_auth_cognito_dart/lib/src/model/cognito_user.dart
@@ -36,24 +36,6 @@ abstract class CognitoUser implements Built<CognitoUser, CognitoUserBuilder> {
   @BuiltValueHook(finalizeBuilder: true)
   static void _finalize(CognitoUserBuilder b) {
     b.username ??= b.userId!;
-
-    // Assert that all device info is included if any is included.
-    if (b.deviceSecrets.deviceKey != null ||
-        b.deviceSecrets.deviceGroupKey != null ||
-        b.deviceSecrets.devicePassword != null) {
-      ArgumentError.checkNotNull(
-        b.deviceSecrets.deviceGroupKey,
-        'deviceGroupKey',
-      );
-      ArgumentError.checkNotNull(
-        b.deviceSecrets.deviceKey,
-        'deviceKey',
-      );
-      ArgumentError.checkNotNull(
-        b.deviceSecrets.devicePassword,
-        'devicePassword',
-      );
-    }
   }
 
   /// AWS Identity ID
@@ -66,6 +48,7 @@ abstract class CognitoUser implements Built<CognitoUser, CognitoUserBuilder> {
   CognitoUserPoolTokens? get userPoolTokens;
 
   /// Confirmed device secrets
+  @BuiltValueField(autoCreateNestedBuilder: false)
   CognitoDeviceSecrets? get deviceSecrets;
 
   /// Cognito user ID

packages/auth/amplify_auth_cognito_dart/lib/src/model/cognito_user.g.dart
@@ -109,8 +109,7 @@ class CognitoUserBuilder implements Builder<CognitoUser, CognitoUserBuilder> {
       _$this._userPoolTokens = userPoolTokens;
 
   CognitoDeviceSecretsBuilder? _deviceSecrets;
-  CognitoDeviceSecretsBuilder get deviceSecrets =>
-      _$this._deviceSecrets ??= new CognitoDeviceSecretsBuilder();
+  CognitoDeviceSecretsBuilder? get deviceSecrets => _$this._deviceSecrets;
   set deviceSecrets(CognitoDeviceSecretsBuilder? deviceSecrets) =>
       _$this._deviceSecrets = deviceSecrets;
 

packages/auth/amplify_auth_cognito_dart/lib/src/state/machines/sign_in_state_machine.dart
@@ -27,6 +27,7 @@ import 'package:amplify_auth_cognito_dart/src/flows/srp/srp_init_result.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/srp/srp_init_worker.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/srp/srp_password_verifier_worker.dart';
 import 'package:amplify_auth_cognito_dart/src/jwt/jwt.dart';
+import 'package:amplify_auth_cognito_dart/src/model/cognito_device_secrets.dart';
 import 'package:amplify_auth_cognito_dart/src/model/cognito_user.dart';
 import 'package:amplify_auth_cognito_dart/src/model/sign_in_parameters.dart';
 import 'package:amplify_auth_cognito_dart/src/sdk/cognito_identity_provider.dart'
@@ -249,7 +250,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         ..clientId = config.appClientId
         ..clientSecret = config.appClientSecret
         ..poolId = config.poolId
-        ..deviceKey = user.deviceSecrets.deviceKey
+        ..deviceKey = user.deviceSecrets?.deviceKey
         ..challengeParameters = challengeParameters
         ..parameters = SignInParameters(
           (b) => b
@@ -275,7 +276,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         ..challengeResponses.addAll({
           CognitoConstants.challengeParamUsername: user.username!,
           CognitoConstants.challengeParamDeviceKey:
-              user.deviceSecrets.deviceKey!,
+              user.deviceSecrets!.deviceKey!,
           CognitoConstants.challengeParamSrpA:
               initResult.publicA.toRadixString(16),
         });
@@ -305,7 +306,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
     final worker = await devicePasswordVerifierWorker;
     final workerMessage = SrpDevicePasswordVerifierMessage((b) {
       b
-        ..deviceSecrets = user.deviceSecrets.build()
+        ..deviceSecrets = user.deviceSecrets!.build()
         ..initResult = _initResult
         ..clientId = config.appClientId
         ..clientSecret = config.appClientSecret
@@ -436,7 +437,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         );
       }
 
-      final deviceKey = user.deviceSecrets.deviceKey;
+      final deviceKey = user.deviceSecrets?.deviceKey;
       if (deviceKey != null) {
         b.authParameters[CognitoConstants.challengeParamDeviceKey] = deviceKey;
       }
@@ -468,7 +469,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         );
       }
 
-      final deviceKey = user.deviceSecrets.deviceKey;
+      final deviceKey = user.deviceSecrets?.deviceKey;
       if (deviceKey != null) {
         b.authParameters[CognitoConstants.challengeParamDeviceKey] = deviceKey;
       }
@@ -509,7 +510,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
         );
       }
 
-      final deviceKey = user.deviceSecrets.deviceKey;
+      final deviceKey = user.deviceSecrets?.deviceKey;
       if (deviceKey != null) {
         b.authParameters[CognitoConstants.challengeParamDeviceKey] = deviceKey;
       }
@@ -614,7 +615,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
       final deviceSecrets = await getOrCreate(DeviceMetadataRepository.token)
           .get(event.parameters.username);
       if (deviceSecrets != null) {
-        user.deviceSecrets.replace(deviceSecrets);
+        user.deviceSecrets = deviceSecrets.toBuilder();
       }
     } on Exception catch (e, st) {
       logger.debug('Could not retrieve credentials', e, st);
@@ -703,7 +704,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
       if (newDeviceMetadata != null &&
           // ConfirmDevice API requires an identity pool
           hasIdentityPool) {
-        user.deviceSecrets
+        user.deviceSecrets = CognitoDeviceSecretsBuilder()
           ..deviceGroupKey = newDeviceMetadata.deviceGroupKey
           ..deviceKey = newDeviceMetadata.deviceKey
           ..devicePassword = await _createDevice(
@@ -713,7 +714,7 @@ class SignInStateMachine extends StateMachine<SignInEvent, SignInState> {
 
         await getOrCreate(DeviceMetadataRepository.token).put(
           user.username!,
-          user.deviceSecrets.build(),
+          user.deviceSecrets!.build(),
         );
       }
 

packages/auth/amplify_auth_cognito_test/test/state/sign_in_state_machine_test.dart
@@ -113,5 +113,46 @@ void main() {
         AuthFlowType.customAuth,
       );
     });
+
+    test('smoke test', () async {
+      stateMachine.dispatch(AuthEvent.configure(userPoolOnlyConfig));
+      await expectLater(
+        stateMachine.stream.whereType<AuthState>().firstWhere(
+              (event) => event is AuthConfigured || event is AuthFailure,
+            ),
+        completion(isA<AuthConfigured>()),
+      );
+
+      final mockClient = MockCognitoIdentityProviderClient(
+        initiateAuth: () async => InitiateAuthResponse(
+          authenticationResult: AuthenticationResultType(
+            accessToken: accessToken.raw,
+            refreshToken: refreshToken,
+            idToken: idToken.raw,
+          ),
+        ),
+      );
+      stateMachine
+        ..addInstance<CognitoIdentityProviderClient>(mockClient)
+        ..dispatch(
+          SignInEvent.initiate(
+            authFlowType: AuthFlowType.customAuth,
+            parameters: SignInParameters(
+              (p) => p
+                ..username = 'username'
+                ..password = 'password',
+            ),
+          ),
+        );
+
+      final signInStateMachine = stateMachine.expect(SignInStateMachine.type);
+      expect(
+        signInStateMachine.stream,
+        emitsInOrder([
+          isA<SignInInitiating>(),
+          isA<SignInSuccess>(),
+        ]),
+      );
+    });
   });
 }