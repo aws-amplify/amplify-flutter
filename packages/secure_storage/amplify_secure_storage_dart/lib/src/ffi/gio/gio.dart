2ba38413310e132c3e0c9ffa9164ba76806632fc
Jordan Nelson <nejrd@amazon.com>
2022-08-17 15:43:27 -0400

    chore: clean up unused code in gio

--- packages/secure_storage/amplify_secure_storage_dart/lib/src/ffi/gio/gio.dart
+++ packages/secure_storage/amplify_secure_storage_dart/lib/src/ffi/gio/gio.dart
@@ -12,43 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// ignore_for_file: constant_identifier_names
-
 import 'dart:ffi';
-import 'dart:io';
 
 import 'package:amplify_secure_storage_dart/src/ffi/gio/gio.bindings.g.dart';
 import 'package:amplify_secure_storage_dart/src/ffi/utils/dynamic_library_utils.dart';
-import 'package:ffi/ffi.dart';
-import 'package:path/path.dart' as path;
 
 export 'gio.bindings.g.dart' hide Gio;
 
 final DynamicLibrary glibDyLib = openDynamicLibrary('libgio-2.0');
 
 final Gio gio = Gio(glibDyLib);
-
-extension GioX on Gio {
-  /// Returns the Application ID if available, with the executable
-  /// name as a fallback.
-  ///
-  /// A wrapper around Gio functions.
-  Future<String> getApplicationID() async {
-    final application = g_application_get_default();
-    if (application == nullptr) {
-      return _getExecutableName();
-    }
-    final id = g_application_get_application_id(application);
-    return id.toDartString();
-  }
-}
-
-/// Gets the name of this executable.
-///
-/// Reference: https://github.com/flutter/plugins/blob/main/packages/path_provider/path_provider_linux/lib/src/path_provider_linux.dart
-Future<String> _getExecutableName() async {
-  final executableName = path.basenameWithoutExtension(
-    await File('/proc/self/exe').resolveSymbolicLinks(),
-  );
-  return executableName;
-}
