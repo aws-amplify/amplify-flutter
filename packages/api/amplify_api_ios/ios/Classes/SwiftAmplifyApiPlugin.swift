07557b5dcd32eaeca03635e046389d3fed274e2f
Travis Sheppard <tshepp@amazon.com>
2022-08-22 16:49:54 -0700

    delete ios stuff

--- packages/api/amplify_api_ios/ios/Classes/SwiftAmplifyApiPlugin.swift
+++ packages/api/amplify_api_ios/ios/Classes/SwiftAmplifyApiPlugin.swift
@@ -21,55 +21,18 @@ import amplify_flutter_ios
 import AWSPluginsCore
 
 public class SwiftAmplifyApiPlugin: NSObject, FlutterPlugin, NativeApiBridge {
-    private let bridge: ApiBridge
-    private let graphQLSubscriptionsStreamHandler: GraphQLSubscriptionsStreamHandler
     static var methodChannel: FlutterMethodChannel!
-
-    init(
-        bridge: ApiBridge = ApiBridge(),
-        graphQLSubscriptionsStreamHandler: GraphQLSubscriptionsStreamHandler = GraphQLSubscriptionsStreamHandler()
-    ) {
-        self.bridge = bridge
-        self.graphQLSubscriptionsStreamHandler = graphQLSubscriptionsStreamHandler
-    }
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         methodChannel = FlutterMethodChannel(
             name: "com.amazonaws.amplify/api",
             binaryMessenger: registrar.messenger())
-        let eventchannel = FlutterEventChannel(
-            name: "com.amazonaws.amplify/api_observe_events",
-            binaryMessenger: registrar.messenger())
         let instance = SwiftAmplifyApiPlugin()
-        eventchannel.setStreamHandler(instance.graphQLSubscriptionsStreamHandler)
+
         registrar.addMethodCallDelegate(instance, channel: methodChannel)
         NativeApiBridgeSetup(registrar.messenger(), instance)
     }
-
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        let result = AtomicResult(result, call.method)
-
-        innerHandle(method: call.method, callArgs: call.arguments as Any, result: result)
-    }
-
-    // Create separate method to allow unit testing as we cannot mock "FlutterMethodCall"
-    public func innerHandle(method: String, callArgs: Any, result: @escaping FlutterResult) {
-        do {
-            if method == "cancel"{
-                let cancelToken = try FlutterApiRequest.getCancelToken(args: callArgs)
-                onCancel(flutterResult: result, cancelToken: cancelToken)
-                return
-            }
-
-            let arguments = try FlutterApiRequest.getMap(args: callArgs)
-
-            try innerHandle(method: method, arguments: arguments, result: result)
-        } catch {
-            print("Failed to parse query arguments with \(error)")
-            FlutterApiErrorHandler.handleApiError(error: APIError(error: error), flutterResult: result)
-        }
-    }
-    
+   
     public func addPluginAuthProvidersList(_ authProvidersList: [String]) async -> FlutterError? {
         do {
             let authProviders = authProvidersList.compactMap {
@@ -77,7 +40,6 @@ public class SwiftAmplifyApiPlugin: NSObject, FlutterPlugin, NativeApiBridge {
             }
             try Amplify.add(
                 plugin: AWSAPIPlugin(
-                    sessionFactory: FlutterURLSessionBehaviorFactory(),
                     apiAuthProviderFactory: FlutterAuthProviders(authProviders)))
             return nil
         } catch let apiError as APIError {
@@ -110,48 +72,6 @@ public class SwiftAmplifyApiPlugin: NSObject, FlutterPlugin, NativeApiBridge {
                 message: e.localizedDescription,
                 details: nil
             )
-        }
-    }
-
-    private func innerHandle(
-        method: String,
-        arguments: [String: Any],
-        result: @escaping FlutterResult
-    ) throws {
-        switch method {
-        case "get": FlutterRestApi.get(flutterResult: result, arguments: arguments, bridge: bridge)
-        case "post": FlutterRestApi.post(flutterResult: result, arguments: arguments, bridge: bridge)
-        case "put": FlutterRestApi.put(flutterResult: result, arguments: arguments, bridge: bridge)
-        case "delete": FlutterRestApi.delete(flutterResult: result, arguments: arguments, bridge: bridge)
-        case "head": FlutterRestApi.head(flutterResult: result, arguments: arguments, bridge: bridge)
-        case "patch": FlutterRestApi.patch(flutterResult: result, arguments: arguments, bridge: bridge)
-        case "query":
-            FlutterGraphQLApi.query(flutterResult: result, request: arguments, bridge: bridge)
-        case "mutate":
-            FlutterGraphQLApi.mutate(flutterResult: result, request: arguments, bridge: bridge)
-        case "subscribe":
-            FlutterGraphQLApi.subscribe(
-                flutterResult: result,
-                request: arguments, bridge: bridge,
-                graphQLSubscriptionsStreamHandler: graphQLSubscriptionsStreamHandler
-            )
-        default:
-            result(FlutterMethodNotImplemented)
-        }
-    }
-
-    public func onCancel(flutterResult: @escaping FlutterResult, cancelToken: String) {
-        if OperationsManager.containsOperation(cancelToken: cancelToken) {
-            OperationsManager.cancelOperation(cancelToken: cancelToken)
-            flutterResult("Operation Canceled")
-        } else {
-            flutterResult(FlutterError(
-                            code: "AmplifyAPI-CancelError",
-                            message: """
-                            The Operation may have already been completed or expired and cannot \
-                            be canceled anymore
-                            """,
-                            details: "Operation does not exist"))
         }
     }
 }
