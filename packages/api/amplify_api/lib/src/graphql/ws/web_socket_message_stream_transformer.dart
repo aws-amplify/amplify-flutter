c7057951b1297b885f645ce032103f6be7206f7d
Travis Sheppard <tshepp@amazon.com>
2022-08-26 08:13:39 -0800

    feat(api): .subscribe() for GraphQL (#1915)
    Co-authored-by: Elijah Quartey <Equartey@users.noreply.github.com>

---
+++ packages/api/amplify_api/lib/src/graphql/ws/web_socket_message_stream_transformer.dart
@@ -0,0 +1,94 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@internal
+library amplify_api.graphql.ws.web_socket_message_stream_transformer;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:amplify_api/src/util.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:meta/meta.dart';
+
+import '../graphql_response_decoder.dart';
+import 'web_socket_types.dart';
+
+/// Top-level transformer.
+class WebSocketMessageStreamTransformer
+    extends StreamTransformerBase<dynamic, WebSocketMessage> {
+  /// Transforms raw web socket response (String) to `WebSocketMessage` for all input
+  /// from channel.
+  const WebSocketMessageStreamTransformer();
+
+  @override
+  Stream<WebSocketMessage> bind(Stream<dynamic> stream) {
+    return stream.cast<String>().map<Map<String, Object?>>((str) {
+      return json.decode(str) as Map<String, Object?>;
+    }).map(WebSocketMessage.fromJson);
+  }
+}
+
+/// Final level of transformation for converting `WebSocketMessage`s to stream
+/// of `GraphQLResponse` that is eventually passed to public API `Amplify.API.subscribe`.
+class WebSocketSubscriptionStreamTransformer<T>
+    extends StreamTransformerBase<WebSocketMessage, GraphQLResponse<T>> {
+  /// request for this stream, needed to properly decode response events
+  final GraphQLRequest<T> request;
+
+  /// logs complete messages to better provide visibility to cancels
+  final AmplifyLogger logger;
+
+  /// executes when start_ack message received
+  final void Function()? onEstablished;
+
+  /// [request] is used to properly decode response events
+  /// [onEstablished] is executed when start_ack message received
+  /// [logger] logs cancel messages when complete message received
+  const WebSocketSubscriptionStreamTransformer(
+    this.request,
+    this.onEstablished, {
+    required this.logger,
+  });
+
+  @override
+  Stream<GraphQLResponse<T>> bind(Stream<WebSocketMessage> stream) async* {
+    await for (var event in stream) {
+      switch (event.messageType) {
+        case MessageType.startAck:
+          onEstablished?.call();
+          break;
+        case MessageType.data:
+          final payload = event.payload as SubscriptionDataPayload;
+          // TODO(ragingsquirrel3): refactor decoder
+          final errors = deserializeGraphQLResponseErrors(payload.toJson());
+          yield GraphQLResponseDecoder.instance.decode<T>(
+            request: request,
+            data: json.encode(payload.data),
+            errors: errors,
+          );
+
+          break;
+        case MessageType.error:
+          final error = event.payload as WebSocketError;
+          throw error;
+        case MessageType.complete:
+          logger.info('Cancel succeeded for Operation: ${event.id}');
+          return;
+        default:
+          break;
+      }
+    }
+  }
+}
--- packages/api/amplify_api/test/util.dart
+++ packages/api/amplify_api/test/util.dart
@@ -12,8 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:amplify_api/src/graphql/app_sync_api_key_auth_provider.dart';
+import 'package:amplify_api/src/graphql/ws/web_socket_connection.dart';
+import 'package:amplify_api/src/graphql/ws/web_socket_types.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:aws_signature_v4/aws_signature_v4.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 
@@ -59,4 +66,112 @@ void validateSignedRequest(http.BaseRequest request) {
     request.headers[userAgentHeader],
     contains('aws-sigv4'),
   );
+}
+
+const testApiKeyConfig = AWSApiConfig(
+  endpointType: EndpointType.graphQL,
+  endpoint: 'https://abc123.appsync-api.us-east-1.amazonaws.com/graphql',
+  region: 'us-east-1',
+  authorizationType: APIAuthorizationType.apiKey,
+  apiKey: 'abc-123',
+);
+
+const expectedApiKeyWebSocketConnectionUrl =
+    'wss://abc123.appsync-realtime-api.us-east-1.amazonaws.com/graphql?header=eyJDb250ZW50LVR5cGUiOiJhcHBsaWNhdGlvbi9qc29uOyBjaGFyc2V0PVVURi04IiwiWC1BcGktS2V5IjoiYWJjLTEyMyIsIkFjY2VwdCI6ImFwcGxpY2F0aW9uL2pzb24sIHRleHQvamF2YXNjcmlwdCIsIkNvbnRlbnQtRW5jb2RpbmciOiJhbXotMS4wIiwiSG9zdCI6ImFiYzEyMy5hcHBzeW5jLWFwaS51cy1lYXN0LTEuYW1hem9uYXdzLmNvbSJ9&payload=e30%3D';
+
+AmplifyAuthProviderRepository getTestAuthProviderRepo() {
+  final testAuthProviderRepo = AmplifyAuthProviderRepository();
+  testAuthProviderRepo.registerAuthProvider(
+    APIAuthorizationType.apiKey.authProviderToken,
+    AppSyncApiKeyAuthProvider(),
+  );
+
+  return testAuthProviderRepo;
+}
+
+const mockSubscriptionData = {
+  'onCreatePost': {
+    'id': '49d54440-cb80-4f20-964b-91c142761e82',
+    'title':
+        'Integration Test post - subscription create aa779f0d-0c92-4677-af32-e43f71b3eb55',
+    'rating': 0,
+    'created': null,
+    'createdAt': '2022-08-15T18:22:15.410Z',
+    'updatedAt': '2022-08-15T18:22:15.410Z',
+    'blog': {
+      'id': '164bd1f1-544c-40cb-a656-a7563b046e71',
+      'name': 'Integration Test Blog with a post - create',
+      'createdAt': '2022-08-15T18:22:15.164Z',
+      'file': null,
+      'files': null,
+      'updatedAt': '2022-08-15T18:22:15.164Z'
+    }
+  }
+};
+
+/// Extension of [WebSocketConnection] that stores messages internally instead
+/// of sending them.
+class MockWebSocketConnection extends WebSocketConnection {
+  /// Instead of actually connecting, just set the URI here so it can be inspected
+  /// for testing.
+  Uri? connectedUri;
+
+  /// Instead of sending messages, they are pushed to end of list so they can be
+  /// inspected for testing.
+  final List<WebSocketMessage> sentMessages = [];
+
+  MockWebSocketConnection(
+      AWSApiConfig config, AmplifyAuthProviderRepository authProviderRepo)
+      : super(config, authProviderRepo, logger: AmplifyLogger());
+
+  WebSocketMessage? get lastSentMessage => sentMessages.lastOrNull;
+
+  final messageStream = StreamController<dynamic>();
+
+  @override
+  Future<void> connect(Uri connectionUri) async {
+    connectedUri = connectionUri;
+
+    // mock some message responses (acks and mock data) from server
+    final broadcast = messageStream.stream.asBroadcastStream();
+    broadcast.listen((event) {
+      final eventJson = json.decode(event as String);
+      final messageFromEvent = WebSocketMessage.fromJson(eventJson as Map);
+
+      // connection_init, respond with connection_ack
+      final mockResponseMessages = <WebSocketMessage>[];
+      if (messageFromEvent.messageType == MessageType.connectionInit) {
+        mockResponseMessages.add(WebSocketMessage(
+          messageType: MessageType.connectionAck,
+          payload: const ConnectionAckMessagePayload(10000),
+        ));
+        // start, respond with start_ack and mock data
+      } else if (messageFromEvent.messageType == MessageType.start) {
+        mockResponseMessages.add(WebSocketMessage(
+          messageType: MessageType.startAck,
+          id: messageFromEvent.id,
+        ));
+        mockResponseMessages.add(WebSocketMessage(
+          messageType: MessageType.data,
+          id: messageFromEvent.id,
+          payload: const SubscriptionDataPayload(mockSubscriptionData, null),
+        ));
+      }
+
+      for (var mockMessage in mockResponseMessages) {
+        messageStream.add(json.encode(mockMessage));
+      }
+    });
+
+    // ensures connected to _onDone events in parent class
+    getStreamSubscription(broadcast);
+  }
+
+  /// Pushes message in sentMessages and adds to stream (to support mocking result).
+  @override
+  void send(WebSocketMessage message) {
+    sentMessages.add(message);
+    final messageStr = json.encode(message.toJson());
+    messageStream.add(messageStr);
+  }
 }
