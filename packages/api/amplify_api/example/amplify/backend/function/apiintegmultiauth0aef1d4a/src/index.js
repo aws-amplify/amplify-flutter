4a03cad593126efd335be4e736bebdbcfc4b65f0
Travis Sheppard <tshepp@amazon.com>
2022-08-29 12:58:32 -0700

    add backend dir and modify provision script

---
+++ packages/api/amplify_api/example/amplify/backend/function/apiintegmultiauth0aef1d4a/src/index.js
@@ -0,0 +1,16 @@
+
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        }, 
+        body: 'Hello from Lambda!',
+    };
+};
