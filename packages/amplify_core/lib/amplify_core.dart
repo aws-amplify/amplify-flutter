7ebecfe42665cd355cda60df63de3bbea76356f3
Dillon Nys <nydillon@amazon.com>
2022-09-08 15:56:45 -0700

    test
    commit-id:355cfa26

--- packages/amplify_core/lib/amplify_core.dart
+++ packages/amplify_core/lib/amplify_core.dart
@@ -1,4 +1,3 @@
-//
 // Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License").
@@ -11,7 +10,6 @@
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
-//
 
 library amplify_core;
 
