982d62e5465ea2a0c7814206f5847c268f8a3686
Dillon Nys <nydillon@amazon.com>
2022-09-20 15:09:03 -0700

    chore(datastore): Add Dart package
    Adds a Dart package skeleton for DataStore
    
    commit-id:1ae708bc

---
+++ packages/datastore/amplify_datastore_dart/lib/amplify_datastore_dart.dart
@@ -0,0 +1,23 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// ## Amplify DataStore for Dart
+///
+/// Amplify DataStore provides a programming model for leveraging shared and
+/// distributed data without writing additional code for offline and online
+/// scenarios, which makes working with distributed, cross-user data just as
+/// simple as working with local-only data.
+///
+/// [Amplify Docs](https://docs.amplify.aws/lib/datastore/getting-started/q/platform/flutter/)
+library amplify_datastore_dart;
