1e97c1cc79a18f65736e4d5868beb38080b456d1
Elijah Quartey <Equartey@users.noreply.github.com>
2022-09-30 13:41:48 -0500

    feat(api): Subscription Reconnection (#2074)

--- packages/amplify_core/lib/src/types/api/api_types.dart
+++ packages/amplify_core/lib/src/types/api/api_types.dart
@@ -13,6 +13,9 @@
  * permissions and limitations under the License.
  */
 
+// Packages
+export 'package:retry/retry.dart' show RetryOptions;
+
 // API Authorization
 export 'auth/api_auth_provider.dart';
 export 'auth/api_authorization_type.dart';
@@ -26,6 +29,10 @@ export 'graphql/graphql_request_type.dart';
 export 'graphql/graphql_response.dart';
 export 'graphql/graphql_response_error.dart';
 export 'graphql/graphql_subscription_operation.dart';
+export 'graphql/graphql_subscription_options.dart';
+
+export 'hub/api_hub_event.dart';
+export 'hub/api_subscription_hub_event.dart';
 
 export 'rest/rest_exception.dart';
 export 'rest/rest_operation.dart';
