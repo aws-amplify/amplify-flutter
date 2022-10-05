d27f50bd5a7deea7a4cddb0bc588530e3ca77402
Travis Sheppard <tshepp@amazon.com>
2022-10-05 09:37:21 -0700

    fix(storage): include storage in ios/android plugins

--- packages/amplify/amplify_flutter/lib/src/hybrid_impl.dart
+++ packages/amplify/amplify_flutter/lib/src/hybrid_impl.dart
@@ -35,6 +35,7 @@ class AmplifyHybridImpl extends AmplifyClassImpl {
     await Future.wait(
       [
         ...Auth.plugins,
+        ...Storage.plugins,
       ].map((p) => p.configure(
           config: amplifyConfig, authProviderRepo: authProviderRepo)),
       eagerError: true,
