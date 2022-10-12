56573a9f68a9f5fbf9841b5f63f0ecbca009538d
Hui Zhao <zhohz@amazon.com>
2022-10-12 15:24:13 -0700

    chore(storage): add example app for amplify_storage_s3

---
+++ packages/storage/amplify_storage_s3/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import amplify_secure_storage
+import path_provider_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AmplifySecureStoragePlugin.register(with: registry.registrar(forPlugin: "AmplifySecureStoragePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
