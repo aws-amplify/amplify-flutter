3dc751b7681d0e284befb94c7bd6549a4de4ba3e
Hui Zhao <zhohz@amazon.com>
2022-10-13 10:22:38 -0700

    feat(aws-common): add dataUrl support to HttpPayload

--- packages/aws_common/test/http/http_payload_test.dart
+++ packages/aws_common/test/http/http_payload_test.dart
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:convert';
+
 import 'package:aws_common/aws_common.dart';
 import 'package:test/test.dart';
 
@@ -81,6 +83,68 @@ void main() {
           emitsDone,
         ]),
       );
+    });
+
+    group('dataUrl', () {
+      test('text without base64 encoding', () {
+        const testDataUrl =
+            'data:text/plain;charset=UTF-8;page=21,the%20data:1234,5678';
+        final payload = HttpPayload.dataUrl(testDataUrl);
+        expect(payload.contentType, 'text/plain;charset=UTF-8;page=21');
+        expect(
+          payload,
+          emitsInOrder([utf8.encode('the data:1234,5678'), emitsDone]),
+        );
+      });
+
+      test('text with base64 encoding', () {
+        const testDataUrl = 'data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==';
+        final payload = HttpPayload.dataUrl(testDataUrl);
+        expect(payload.contentType, 'text/plain');
+        expect(
+          payload,
+          emitsInOrder([utf8.encode('Hello, World!'), emitsDone]),
+        );
+      });
+
+      test('image with base64 encoding', () {
+        const testDataUrl =
+            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
+        final payload = HttpPayload.dataUrl(testDataUrl);
+        expect(payload.contentType, 'image/png');
+        expect(
+          payload,
+          emitsInOrder([
+            base64Decode(
+              'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
+            ),
+            emitsDone
+          ]),
+        );
+      });
+
+      test('js script', () {
+        const testDataUrl = 'data:text/html,<script>alert(\'hi\');</script>';
+        final payload = HttpPayload.dataUrl(testDataUrl);
+        expect(payload.contentType, 'text/html');
+        expect(
+          payload,
+          emitsInOrder(
+            [utf8.encode('<script>alert(\'hi\');</script>'), emitsDone],
+          ),
+        );
+      });
+
+      test('utf8 encoding', () {
+        const testDataUrl =
+            'data:text/plain;charset=utf-8,Hello%20Amplify%20%E2%9D%A4%EF%B8%8F%F0%9F%91%8B%21';
+        final payload = HttpPayload.dataUrl(testDataUrl);
+        expect(payload.contentType, 'text/plain;charset=utf-8');
+        expect(
+          payload,
+          emitsInOrder([utf8.encode('Hello Amplify ❤️👋!'), emitsDone]),
+        );
+      });
     });
   });
 }
