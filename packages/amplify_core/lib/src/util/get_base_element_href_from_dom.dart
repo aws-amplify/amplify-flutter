b6b74d3d86e344edea3075f2900f107e3eb9e9f8
Jordan Nelson <nejrd@amazon.com>
2022-10-14 15:36:05 -0400

    fix: add wrapper around stub and web impl

--- packages/amplify_core/lib/src/util/get_base_element_href_from_dom.dart
+++ packages/amplify_core/lib/src/util/get_base_element_href_from_dom.dart
@@ -12,17 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:html';
-
-import 'package:amplify_core/amplify_core.dart';
-
-/// Returns the href attribute of the element in the document for flutter apps,
-/// and null for non flutter apps.
-///
-/// Returns null if the element isn't found within a flutter app.
-///
-/// Based on [getBaseElementHrefFromDom](https://api.flutter.dev/flutter/flutter_web_plugins/getBaseElementHrefFromDom.html)
-String? getBaseElementHrefFromDom() {
-  if (!zIsFlutter) return null;
-  return document.querySelector('base')?.getAttribute('href');
-}
+export 'get_base_element_href_from_dom.dart'
+    if (dart.library.html) 'get_base_element_href_from_dom.web.dart';
