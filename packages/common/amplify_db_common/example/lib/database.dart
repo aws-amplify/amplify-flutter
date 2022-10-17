7bd034f77ad627810fceb12e58c42239a950ed05
Jordan Nelson <nejrd@amazon.com>
2022-10-17 11:11:50 -0400

    chore: update comments in vm, example

--- packages/common/amplify_db_common/example/lib/database.dart
+++ packages/common/amplify_db_common/example/lib/database.dart
@@ -17,12 +17,8 @@
 import 'package:amplify_db_common/amplify_db_common.dart';
 import 'package:drift/drift.dart';
 
-// assuming that your file is called filename.dart. This will give an error at
-// first, but it's needed for drift to know about the generated code
 part 'database.g.dart';
 
-// this will generate a table called "todos" for us. The rows of that table will
-// be represented by a class called "Todo".
 class Todos extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get title => text().withLength(min: 6, max: 32)();
@@ -30,24 +26,16 @@ class Todos extends Table {
   IntColumn get category => integer().nullable()();
 }
 
-// This will make drift generate a class called "Category" to represent a row in
-// this table. By default, "Categorie" would have been used because it only
-//strips away the trailing "s" in the table name.
 @DataClassName('Category')
 class Categories extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get description => text()();
 }
 
-// this annotation tells drift to prepare a database class that uses both of the
-// tables we just defined. We'll see how to use that database class in a moment.
 @DriftDatabase(tables: [Todos, Categories])
 class MyDatabase extends _$MyDatabase {
-  // we tell the database where to store the data with this constructor
   MyDatabase() : super(connect(name: 'MyDatabase', path: '/tmp'));
 
-  // you should bump this number whenever you change or add a table definition.
-  // Migrations are covered later in the documentation.
   @override
   int get schemaVersion => 1;
 }
