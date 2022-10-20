2bec0b888ce24a82550b884297adfb88545b758d
Travis Sheppard <tshepp@amazon.com>
2022-10-20 10:08:00 -0800

    chore: use AWS secrets manager for CI integ secrets (#2247)

--- packages/amplify_datastore/example/tool/pull_test_backend.sh
+++ packages/amplify_datastore/example/tool/pull_test_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-APP_ID=$DATASTORE_APP_ID ../../../build-support/pull_backend_by_app_id.sh 
+APP_ID=$AFS_DATASTORE_APP_ID ../../../build-support/pull_backend_by_app_id.sh 
--- packages/api/amplify_api/example/tool/pull_test_backend.sh
+++ packages/api/amplify_api/example/tool/pull_test_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-APP_ID=$API_APP_ID ../../../../build-support/pull_backend_by_app_id.sh 
+APP_ID=$AFS_API_APP_ID ../../../../build-support/pull_backend_by_app_id.sh 
