f660abb3639d03dcfc0562d20ab73b152de66754
Kyle <kylcheng@gmail.com>
2022-10-22 08:21:00 -0700

    chore(flutter): update flutter example app

--- packages/amplify/amplify_flutter/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/amplify/amplify_flutter/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  Sqlite3FlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
 }
--- packages/amplify/amplify_flutter/example/windows/flutter/generated_plugins.cmake
+++ packages/amplify/amplify_flutter/example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  sqlite3_flutter_libs
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
