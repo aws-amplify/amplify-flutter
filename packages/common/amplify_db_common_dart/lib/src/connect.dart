04517d09e94381374123519932cf05a54f3589b2
Hui Zhao <zhohz@amazon.com>
2022-10-28 10:19:52 -0700

    feat(db_common): make path param FutureOr

--- packages/common/amplify_db_common_dart/lib/src/connect.dart
+++ packages/common/amplify_db_common_dart/lib/src/connect.dart
@@ -12,6 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:async';
+
+import 'package:drift/backends.dart';
+
 export 'connect_stub.dart'
     if (dart.library.html) 'connect_html.dart'
     if (dart.library.io) 'connect_io.dart';
+
+/// Interface of the Drift DB connect function.
+typedef Connect = QueryExecutor Function({
+  required String name,
+  FutureOr<String>? path,
+});
