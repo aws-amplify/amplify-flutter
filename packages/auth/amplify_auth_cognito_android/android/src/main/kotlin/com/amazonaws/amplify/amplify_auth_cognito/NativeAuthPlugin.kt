85a38789cd3ecaa69136f3ba626d391adc35122e
Dillon Nys <nydillon@amazon.com>
2022-11-01 16:42:11 -0700

    Android GA updates

--- packages/auth/amplify_auth_cognito_android/android/src/main/kotlin/com/amazonaws/amplify/amplify_auth_cognito/NativeAuthPlugin.kt
+++ packages/auth/amplify_auth_cognito_android/android/src/main/kotlin/com/amazonaws/amplify/amplify_auth_cognito/NativeAuthPlugin.kt
@@ -18,8 +18,8 @@ package com.amazonaws.amplify.amplify_auth_cognito
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import aws.sdk.kotlin.runtime.auth.credentials.Credentials
 import aws.smithy.kotlin.runtime.time.Instant
+import com.amplifyframework.auth.AWSCredentials
 import com.amplifyframework.auth.AuthCodeDeliveryDetails
 import com.amplifyframework.auth.AuthDevice
 import com.amplifyframework.auth.AuthException
@@ -29,13 +29,14 @@ import com.amplifyframework.auth.AuthSession
 import com.amplifyframework.auth.AuthUser
 import com.amplifyframework.auth.AuthUserAttribute
 import com.amplifyframework.auth.AuthUserAttributeKey
-import com.amplifyframework.auth.cognito.AWSCognitoAuthServiceBehavior
-import com.amplifyframework.auth.cognito.AWSCognitoAuthSession
-import com.amplifyframework.auth.cognito.AWSCognitoUserPoolTokens
+import com.amplifyframework.auth.cognito.AWSCognitoAuthService
 import com.amplifyframework.auth.cognito.BuildConfig
+import com.amplifyframework.auth.cognito.helpers.FlutterFactory
+import com.amplifyframework.auth.exceptions.UnknownException
 import com.amplifyframework.auth.options.AuthConfirmResetPasswordOptions
 import com.amplifyframework.auth.options.AuthConfirmSignInOptions
 import com.amplifyframework.auth.options.AuthConfirmSignUpOptions
+import com.amplifyframework.auth.options.AuthFetchSessionOptions
 import com.amplifyframework.auth.options.AuthResendSignUpCodeOptions
 import com.amplifyframework.auth.options.AuthResendUserAttributeConfirmationCodeOptions
 import com.amplifyframework.auth.options.AuthResetPasswordOptions
@@ -48,6 +49,7 @@ import com.amplifyframework.auth.options.AuthWebUISignInOptions
 import com.amplifyframework.auth.result.AuthResetPasswordResult
 import com.amplifyframework.auth.result.AuthSessionResult
 import com.amplifyframework.auth.result.AuthSignInResult
+import com.amplifyframework.auth.result.AuthSignOutResult
 import com.amplifyframework.auth.result.AuthSignUpResult
 import com.amplifyframework.auth.result.AuthUpdateAttributeResult
 import com.amplifyframework.core.Action
@@ -63,11 +65,11 @@ import org.json.JSONObject
  */
 class NativeAuthPlugin(
     private val nativeAuthPlugin: () -> NativeAuthPluginBindingsPigeon.NativeAuthPlugin?
-): AuthPlugin<AWSCognitoAuthServiceBehavior>() {
+) : AuthPlugin<AWSCognitoAuthService>() {
 
     override fun getPluginKey(): String = "awsCognitoAuthPlugin"
 
-    override fun getEscapeHatch(): AWSCognitoAuthServiceBehavior? = null
+    override fun getEscapeHatch(): AWSCognitoAuthService? = null
 
     override fun getVersion(): String = BuildConfig.VERSION_NAME
 
@@ -82,19 +84,15 @@ class NativeAuthPlugin(
         val nativePlugin = nativeAuthPlugin()
         if (nativePlugin == null) {
             onError.accept(
-                AuthException.UnknownException(
-                    Exception("No native plugin registered")
-                )
+                UnknownException("No native plugin registered")
             )
             return
         }
         MainScope().launch {
             nativePlugin.fetchAuthSession(true) { session ->
-                val couldNotFetchException = AuthException.UnknownException(
-                    Exception("Could not fetch")
-                )
+                val couldNotFetchException = UnknownException("Could not fetch")
                 val userPoolTokens = if (session.userPoolTokens != null) {
-                    val tokens = AWSCognitoUserPoolTokens(
+                    val tokens = FlutterFactory.createAWSCognitoUserPoolTokens(
                         session.userPoolTokens!!.accessToken,
                         session.userPoolTokens!!.idToken,
                         session.userPoolTokens!!.refreshToken,
@@ -103,21 +101,22 @@ class NativeAuthPlugin(
                 } else {
                     AuthSessionResult.failure(couldNotFetchException)
                 }
-                val awsCredentials = if (session.awsCredentials != null) {
-                    val sessionCredentials = session.awsCredentials!!
-                    val credentials = Credentials(
-                        sessionCredentials.accessKeyId,
-                        sessionCredentials.secretAccessKey,
-                        sessionCredentials.sessionToken,
-                        if (sessionCredentials.expirationIso8601Utc != null) Instant.fromIso8601(
-                            sessionCredentials.expirationIso8601Utc!!
-                        ) else null,
-                    )
-                    AuthSessionResult.success(credentials)
-                } else {
-                    AuthSessionResult.failure(couldNotFetchException)
-                }
-                val authSession = AWSCognitoAuthSession(
+                val awsCredentials: AuthSessionResult<AWSCredentials> =
+                    if (session.awsCredentials != null) {
+                        val sessionCredentials = session.awsCredentials!!
+                        val credentials = AWSCredentials.createAWSCredentials(
+                            sessionCredentials.accessKeyId,
+                            sessionCredentials.secretAccessKey,
+                            sessionCredentials.sessionToken,
+                            if (sessionCredentials.expirationIso8601Utc != null) Instant.fromIso8601(
+                                sessionCredentials.expirationIso8601Utc!!
+                            ).epochSeconds else null,
+                        )
+                        AuthSessionResult.success(credentials)
+                    } else {
+                        AuthSessionResult.failure(couldNotFetchException)
+                    }
+                val authSession = FlutterFactory.createAWSCognitoAuthSession(
                     session.isSignedIn,
                     AuthSessionResult.success(session.identityId),
                     awsCredentials,
@@ -129,8 +128,12 @@ class NativeAuthPlugin(
         }
     }
 
-    override fun getCurrentUser(): AuthUser {
-        unsupported("getCurrentUser")
+    override fun fetchAuthSession(
+        options: AuthFetchSessionOptions,
+        onSuccess: Consumer<AuthSession>,
+        onError: Consumer<AuthException>
+    ) {
+        return fetchAuthSession(onSuccess, onError)
     }
 
     override fun signUp(
@@ -163,18 +166,18 @@ class NativeAuthPlugin(
     }
 
     override fun resendSignUpCode(
-        username: String,
-        options: AuthResendSignUpCodeOptions,
-        onSuccess: Consumer<AuthSignUpResult>,
-        onError: Consumer<AuthException>
+        p0: String,
+        p1: AuthResendSignUpCodeOptions,
+        p2: Consumer<AuthCodeDeliveryDetails>,
+        p3: Consumer<AuthException>
     ) {
         unsupported("resendSignUpCode")
     }
 
     override fun resendSignUpCode(
-        username: String,
-        onSuccess: Consumer<AuthSignUpResult>,
-        onError: Consumer<AuthException>
+        p0: String,
+        p1: Consumer<AuthCodeDeliveryDetails>,
+        p2: Consumer<AuthException>
     ) {
         unsupported("resendSignUpCode")
     }
@@ -296,20 +299,22 @@ class NativeAuthPlugin(
     }
 
     override fun confirmResetPassword(
-        newPassword: String,
-        confirmationCode: String,
-        options: AuthConfirmResetPasswordOptions,
-        onSuccess: Action,
-        onError: Consumer<AuthException>
+        p0: String,
+        p1: String,
+        p2: String,
+        p3: AuthConfirmResetPasswordOptions,
+        p4: Action,
+        p5: Consumer<AuthException>
     ) {
         unsupported("confirmResetPassword")
     }
 
     override fun confirmResetPassword(
-        newPassword: String,
-        confirmationCode: String,
-        onSuccess: Action,
-        onError: Consumer<AuthException>
+        p0: String,
+        p1: String,
+        p2: String,
+        p3: Action,
+        p4: Consumer<AuthException>
     ) {
         unsupported("confirmResetPassword")
     }
@@ -390,15 +395,15 @@ class NativeAuthPlugin(
         unsupported("confirmUserAttribute")
     }
 
-    override fun signOut(onSuccess: Action, onError: Consumer<AuthException>) {
+    override fun getCurrentUser(p0: Consumer<AuthUser>, p1: Consumer<AuthException>) {
+        unsupported("getCurrentUser")
+    }
+
+    override fun signOut(p0: Consumer<AuthSignOutResult>) {
         unsupported("signOut")
     }
 
-    override fun signOut(
-        options: AuthSignOutOptions,
-        onSuccess: Action,
-        onError: Consumer<AuthException>
-    ) {
+    override fun signOut(p0: AuthSignOutOptions, p1: Consumer<AuthSignOutResult>) {
         unsupported("signOut")
     }
 
