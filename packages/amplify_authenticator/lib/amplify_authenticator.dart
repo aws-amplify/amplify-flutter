d50a9c7f5f2b7334924bace9ee45eb9263a58185
Jordan Nelson <nejrd@amazon.com>
2022-11-10 16:26:37 -0500

    chore: remove navigator from tree for AuthenticatedState

--- packages/amplify_authenticator/lib/amplify_authenticator.dart
+++ packages/amplify_authenticator/lib/amplify_authenticator.dart
@@ -759,21 +759,20 @@ class _AuthenticatorBody extends StatelessWidget {
     return _AuthStateBuilder(
       child: child,
       builder: (state, child) {
+        if (state is AuthenticatedState) return child;
         return Navigator(
           onPopPage: (_, dynamic __) => true,
           pages: [
-            if (state is AuthenticatedState) MaterialPage<void>(child: child),
-            if (state is! AuthenticatedState)
-              MaterialPage<void>(
-                child: ScaffoldMessenger(
-                  key: _AuthenticatorState.scaffoldMessengerKey,
-                  child: Scaffold(
-                    body: SizedBox.expand(
-                      child: child,
-                    ),
+            MaterialPage<void>(
+              child: ScaffoldMessenger(
+                key: _AuthenticatorState.scaffoldMessengerKey,
+                child: Scaffold(
+                  body: SizedBox.expand(
+                    child: child,
                   ),
                 ),
               ),
+            ),
           ],
         );
       },
