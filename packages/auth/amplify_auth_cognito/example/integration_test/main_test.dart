6bd9642754491a09562ec462d9a7f433a91068d0
Dillon Nys <nydillon@amazon.com>
2022-11-17 08:53:40 -0700

    test(auth): Update tests for API category changes
    Consolidates integration tests now that they can be run on all platforms.

--- packages/auth/amplify_auth_cognito/example/integration_test/main_test.dart
+++ packages/auth/amplify_auth_cognito/example/integration_test/main_test.dart
@@ -16,13 +16,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'custom_authorizer_test.dart' as custom_authorizer_tests;
+import 'confirm_sign_up_test.dart' as confirm_sign_up_tests;
+import 'custom_auth_test.dart' as custom_auth_tests;
+// import 'custom_authorizer_test.dart' as custom_authorizer_tests;
 import 'delete_user_test.dart' as delete_user_tests;
+import 'device_tracking_test.dart' as device_tracking_tests;
 import 'federated_sign_in_test.dart' as federated_sign_in_tests;
 import 'fetch_auth_session_test.dart' as fetch_auth_session_tests;
 import 'force_refresh_test.dart' as force_refresh_tests;
 import 'get_current_user_test.dart' as get_current_user_tests;
 import 'hub_events_test.dart' as hub_events_tests;
+import 'mfa_sms_test.dart' as mfa_sms_tests;
 import 'sign_in_sign_out_test.dart' as sign_in_sign_out_tests;
 import 'sign_up_test.dart' as sign_up_tests;
 import 'update_password_test.dart' as update_password_tests;
@@ -32,14 +36,18 @@ void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('amplify_auth_cognito', () {
-    // TODO(dnys1): Add tests requiring API when dartified
-    custom_authorizer_tests.main();
+    confirm_sign_up_tests.main();
+    custom_auth_tests.main();
+    // TODO(dnys1): Re-enable when custom authorizer backends are deployed
+    // custom_authorizer_tests.main();
     delete_user_tests.main();
+    device_tracking_tests.main();
     federated_sign_in_tests.main();
     fetch_auth_session_tests.main();
     force_refresh_tests.main();
     get_current_user_tests.main();
     hub_events_tests.main();
+    mfa_sms_tests.main();
     sign_in_sign_out_tests.main();
     sign_up_tests.main();
     update_password_tests.main();
