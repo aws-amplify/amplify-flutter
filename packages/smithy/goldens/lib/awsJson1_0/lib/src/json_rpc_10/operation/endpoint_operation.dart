9cef07f5e324dfadc6db0929b068bc28a9cb1d74
Dillon Nys <nydillon@amazon.com>
2022-11-18 08:54:30 -0700

    chore(awsJson1_0_v1): Update goldens

--- packages/smithy/goldens/lib/awsJson1_0/lib/src/json_rpc_10/operation/endpoint_operation.dart
+++ packages/smithy/goldens/lib/awsJson1_0/lib/src/json_rpc_10/operation/endpoint_operation.dart
@@ -1,4 +1,4 @@
-// Generated with smithy-dart 0.1.1. DO NOT MODIFY.
+// Generated with smithy-dart 0.3.0. DO NOT MODIFY.
 
 library aws_json1_0_v1.json_rpc_10.operation.endpoint_operation; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
@@ -31,7 +31,7 @@ class EndpointOperation
           'X-Amz-Target',
           'JsonRpc10.EndpointOperation',
         ),
-        const _i1.WithUserAgent('aws-sdk-dart/0.1.1'),
+        const _i1.WithUserAgent('aws-sdk-dart/0.3.0'),
         const _i2.WithSdkInvocationId(),
         const _i2.WithSdkRequest(),
       ],
