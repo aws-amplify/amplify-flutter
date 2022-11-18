e6024f28f0626b33f124361a31448b7a6b53f1d0
Dillon Nys <nydillon@amazon.com>
2022-11-18 08:57:43 -0700

    chore(restXmlWithNamespace_v2): Update goldens

--- packages/smithy/goldens/lib2/restXmlWithNamespace/lib/src/rest_xml_protocol_namespace/operation/simple_scalar_properties_operation.dart
+++ packages/smithy/goldens/lib2/restXmlWithNamespace/lib/src/rest_xml_protocol_namespace/operation/simple_scalar_properties_operation.dart
@@ -1,4 +1,4 @@
-// Generated with smithy-dart 0.1.1. DO NOT MODIFY.
+// Generated with smithy-dart 0.3.0. DO NOT MODIFY.
 
 library rest_xml_with_namespace_v2.rest_xml_protocol_namespace.operation.simple_scalar_properties_operation; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
@@ -38,7 +38,7 @@ class SimpleScalarPropertiesOperation extends _i1.HttpOperation<
       requestInterceptors: [
         const _i1.WithHost(),
         const _i1.WithContentLength(),
-        const _i1.WithUserAgent('aws-sdk-dart/0.1.1'),
+        const _i1.WithUserAgent('aws-sdk-dart/0.3.0'),
         const _i3.WithSdkInvocationId(),
         const _i3.WithSdkRequest(),
       ],
