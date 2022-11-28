34e4ad1b82a23ce6a57384175585666dfc09ffa2
Dillon Nys <nydillon@amazon.com>
2022-11-28 16:59:21 -0700

    chore(storage): Regenerate SDK

--- packages/storage/amplify_storage_s3_dart/lib/src/sdk/src/s3/operation/delete_objects_operation.dart
+++ packages/storage/amplify_storage_s3_dart/lib/src/sdk/src/s3/operation/delete_objects_operation.dart
@@ -1,4 +1,4 @@
-// Generated with smithy-dart 0.1.1. DO NOT MODIFY.
+// Generated with smithy-dart 0.3.0. DO NOT MODIFY.
 
 library amplify_storage_s3_dart.s3.operation.delete_objects_operation; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
@@ -97,7 +97,7 @@ class DeleteObjectsOperation extends _i1.HttpOperation<
           serviceConfiguration: _s3ClientConfig.signerConfiguration ??
               _i6.S3ServiceConfiguration(),
         ),
-        const _i1.WithUserAgent('aws-sdk-dart/0.1.1'),
+        const _i1.WithUserAgent('aws-sdk-dart/0.3.0'),
         const _i5.WithSdkInvocationId(),
         const _i5.WithSdkRequest(),
       ],
