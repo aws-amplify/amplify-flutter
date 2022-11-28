aec509f2dbcc1211df4164ab1f6bd9d1e65bd1a3
Dillon Nys <nydillon@amazon.com>
2022-11-28 16:48:55 -0700

    chore(analytics): Regenerate SDK

--- packages/analytics/amplify_analytics_pinpoint_dart/lib/src/sdk/src/pinpoint/operation/update_endpoint_operation.dart
+++ packages/analytics/amplify_analytics_pinpoint_dart/lib/src/sdk/src/pinpoint/operation/update_endpoint_operation.dart
@@ -1,4 +1,4 @@
-// Generated with smithy-dart 0.1.1. DO NOT MODIFY.
+// Generated with smithy-dart 0.3.0. DO NOT MODIFY.
 
 library amplify_analytics_pinpoint_dart.pinpoint.operation.update_endpoint_operation; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
@@ -63,7 +63,7 @@ class UpdateEndpointOperation extends _i1.HttpOperation<_i2.EndpointRequest,
           service: _i9.AWSService.pinpoint,
           credentialsProvider: _credentialsProvider,
         ),
-        const _i1.WithUserAgent('aws-sdk-dart/0.1.1'),
+        const _i1.WithUserAgent('aws-sdk-dart/0.3.0'),
         const _i7.WithSdkInvocationId(),
         const _i7.WithSdkRequest(),
       ],
--- packages/analytics/amplify_analytics_pinpoint_dart/lib/src/sdk/src/pinpoint/operation/update_endpoints_batch_operation.dart
+++ packages/analytics/amplify_analytics_pinpoint_dart/lib/src/sdk/src/pinpoint/operation/update_endpoints_batch_operation.dart
@@ -1,4 +1,4 @@
-// Generated with smithy-dart 0.1.1. DO NOT MODIFY.
+// Generated with smithy-dart 0.3.0. DO NOT MODIFY.
 
 library amplify_analytics_pinpoint_dart.pinpoint.operation.update_endpoints_batch_operation; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
@@ -69,7 +69,7 @@ class UpdateEndpointsBatchOperation extends _i1.HttpOperation<
           service: _i9.AWSService.pinpoint,
           credentialsProvider: _credentialsProvider,
         ),
-        const _i1.WithUserAgent('aws-sdk-dart/0.1.1'),
+        const _i1.WithUserAgent('aws-sdk-dart/0.3.0'),
         const _i7.WithSdkInvocationId(),
         const _i7.WithSdkRequest(),
       ],
