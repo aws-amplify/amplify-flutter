22242500c0d7784b63024825196c33c067982b4c
Jordan Nelson <nejrd@amazon.com>
2022-12-02 16:54:51 -0500

    chore: create new package for test utils

---
+++ packages/authenticator/amplify_authenticator_test/lib/amplify_authenticator_test.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library amplify_authenticator_test;
+
+export 'src/amplify_authenticator_test_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
