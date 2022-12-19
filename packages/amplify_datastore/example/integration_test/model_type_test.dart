8347c3bfedfb73a8edcd5572335344cce76ca1fe
Hui Zhao <zhohz@amazon.com>
2022-12-19 12:01:16 -0800

    replace /**/ wrapped license headers (2)

--- packages/amplify_datastore/example/integration_test/model_type_test.dart
+++ packages/amplify_datastore/example/integration_test/model_type_test.dart
@@ -1,27 +1,14 @@
-/*
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
 import 'package:amplify_datastore/amplify_datastore.dart';
-
 import 'package:amplify_datastore_example/models/ModelProvider.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 import 'utils/constants.dart';
 import 'utils/model_test_operation_utils.dart';
--- packages/amplify_datastore/example/integration_test/query_test/query_predicate_test/query_predicate_test.dart
+++ packages/amplify_datastore/example/integration_test/query_test/query_predicate_test/query_predicate_test.dart
@@ -1,34 +1,22 @@
-/*
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
-import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
-import 'string_query_predicate_test.dart' as string_query_predicate_tests;
-import 'int_query_predicate_test.dart' as int_query_predicate_tests;
-import 'float_query_predicate_test.dart' as float_query_predicate_tests;
-import 'boolean_query_predicate_test.dart' as boolean_query_predicate_tests;
-import 'enum_query_predicate_test.dart' as enum_query_predicate_tests;
-import 'compound_query_predicate_test.dart' as compound_query_predicate_tests;
 import 'aws_date_query_predicate_test.dart' as aws_date_query_predicate_test;
 import 'aws_date_time_query_predicate_test.dart'
     as aws_date_time_query_predicate_test;
 import 'aws_time_query_predicate_test.dart' as aws_time_query_predicate_test;
 import 'aws_timestamp_query_predicate_test.dart'
     as aws_timestamp_query_predicate_tests;
+import 'boolean_query_predicate_test.dart' as boolean_query_predicate_tests;
+import 'compound_query_predicate_test.dart' as compound_query_predicate_tests;
+import 'enum_query_predicate_test.dart' as enum_query_predicate_tests;
+import 'float_query_predicate_test.dart' as float_query_predicate_tests;
+import 'int_query_predicate_test.dart' as int_query_predicate_tests;
 import 'list_query_predicate_test.dart' as list_query_predicate_tests;
+import 'string_query_predicate_test.dart' as string_query_predicate_tests;
 
 void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
