eff27dd8b54a3cfb635484838690019c0ea23fd2
Hui Zhao <zhohz@amazon.com>
2022-12-19 14:00:32 -0800

    add missing license headers

--- packages/aws_signature_v4/test/canonical_request_test.dart
+++ packages/aws_signature_v4/test/canonical_request_test.dart
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import 'package:aws_common/aws_common.dart';
 import 'package:aws_signature_v4/aws_signature_v4.dart';
 import 'package:test/test.dart';
