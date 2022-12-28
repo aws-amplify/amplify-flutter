2074a67e36f032beca9b849d5d37cec22d6d8cc0
Hui Zhao <10602282+HuiSF@users.noreply.github.com>
2022-12-28 11:48:01 -0800

    chore(repo): standardize license headers (#2501)
    * replace /**/ wrapped license header
    
    * replace /**/ wrapped license headers (2)
    
    * replace // wrapped license headers
    
    * add missing license headers

--- packages/amplify_datastore/test/observe_query_executor_test.dart
+++ packages/amplify_datastore/test/observe_query_executor_test.dart
@@ -1,24 +1,14 @@
-/*
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 import 'dart:async';
+
 import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:fake_async/fake_async.dart';
-import '../lib/types/observe_query_executor.dart';
 import 'package:amplify_test/test_models/ModelProvider.dart';
+import 'package:fake_async/fake_async.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/types/observe_query_executor.dart';
 
 var syncQueriesStartedEvent = DataStoreHubEvent(
   'syncQueriesStarted',
