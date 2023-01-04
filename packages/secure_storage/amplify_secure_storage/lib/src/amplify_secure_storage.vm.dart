eb2640a72f31988693acc529602fb3ceea4d632f
equartey <equartey19@gmail.com>
2023-01-04 13:31:33 -0600

    fix: added import ignores

--- packages/secure_storage/amplify_secure_storage/lib/src/amplify_secure_storage.vm.dart
+++ packages/secure_storage/amplify_secure_storage/lib/src/amplify_secure_storage.vm.dart
@@ -7,6 +7,7 @@ import 'dart:io';
 import 'package:amplify_secure_storage/src/amplify_secure_storage.android.dart';
 import 'package:amplify_secure_storage/src/messages.cupertino.g.dart';
 import 'package:amplify_secure_storage_dart/amplify_secure_storage_dart.dart';
+// ignore: implementation_imports
 import 'package:amplify_secure_storage_dart/src/utils/file_key_value_store.dart';
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
