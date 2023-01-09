a1f83bf3abbd9ede86926aa47d52220fb6e180c6
Kyle <kylcheng@gmail.com>
2023-01-09 12:03:36 -0800

    chore: bump amplify-android dep to 2.1.0
    - fixes customer issue: https://github.com/aws-amplify/amplify-flutter/issues/2193

--- packages/amplify_datastore/android/src/test/kotlin/com/amazonaws/amplify/amplify_datastore/AmplifyDataStoreHubTest.kt
+++ packages/amplify_datastore/android/src/test/kotlin/com/amazonaws/amplify/amplify_datastore/AmplifyDataStoreHubTest.kt
@@ -82,7 +82,7 @@ class AmplifyDataStoreHubTest {
         var metadataMap: HashMap<String, Any>
         val modelMap: HashMap<String, Any> = element["model"] as HashMap<String, Any>
         val serializedData: HashMap<String, Any> = modelMap["serializedData"] as HashMap<String, Any>
-        val modelMetadata = ModelMetadata(modelMap["id"] as String, null, null, null, "Post")
+        val modelMetadata = ModelMetadata(modelMap["id"] as String, null, null, null)
         val modelData = mapOf(
             "id" to serializedData["id"] as String,
             "title" to serializedData["title"] as String,
@@ -136,7 +136,6 @@ class AmplifyDataStoreHubTest {
             metadataMap["_deleted"] as Boolean,
             metadataMap["_version"] as Int,
             time,
-            "Post"
         )
         val modelData = mapOf(
             "id" to serializedData["id"] as String,
