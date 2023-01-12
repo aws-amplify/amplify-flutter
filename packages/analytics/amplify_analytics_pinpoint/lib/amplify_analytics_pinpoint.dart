c33008b62117f9e44dd8c485c65505a090248205
kc <kylcheng@gmail.com>
2023-01-11 21:11:35 -0800

    chore(analytics): support Pinpoint User Attributes (#2517)
    * chore(analytics): support Pinpoint User Attributes
    
    Co-authored-by: Jordan Nelson <nejrd@amazon.com>
    Co-authored-by: Dillon Nys <24740863+dnys1@users.noreply.github.com>

--- packages/analytics/amplify_analytics_pinpoint/lib/amplify_analytics_pinpoint.dart
+++ packages/analytics/amplify_analytics_pinpoint/lib/amplify_analytics_pinpoint.dart
@@ -6,7 +6,7 @@ library amplify_analytics_pinpoint;
 
 /// Overridable Flutter injected dependencies
 export 'package:amplify_analytics_pinpoint_dart/amplify_analytics_pinpoint_dart.dart'
-    show AppLifecycleProvider;
+    show AppLifecycleProvider, AWSPinpointUserProfile;
 
 /// Category Implementation
 export 'src/analytics_plugin_impl.dart';
