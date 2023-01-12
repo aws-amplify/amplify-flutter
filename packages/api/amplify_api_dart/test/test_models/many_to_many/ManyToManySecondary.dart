de968df599276132b153f17ec7700b28e23d7075
Travis Sheppard <tshepp@amazon.com>
2023-01-12 15:58:29 -0800

    fix imports in example codegen

--- packages/api/amplify_api_dart/test/test_models/many_to_many/ManyToManySecondary.dart
+++ packages/api/amplify_api_dart/test/test_models/many_to_many/ManyToManySecondary.dart
@@ -13,10 +13,6 @@ import 'package:meta/meta.dart';
 
 import 'MtmModelProvider.dart';
 
-// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
-
-import 'MtmModelProvider.dart';
-
 /** This is an auto generated class representing the ManyToManySecondary type in your schema. */
 @immutable
 class ManyToManySecondary extends Model {
