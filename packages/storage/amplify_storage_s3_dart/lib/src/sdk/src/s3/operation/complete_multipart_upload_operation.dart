8b635c441303f147b26d3fe5eb8725c979b3d242
Dillon Nys <nydillon@amazon.com>
2023-01-17 17:38:19 -0700

    chore(storage): Re-generate S3 SDK

--- packages/storage/amplify_storage_s3_dart/lib/src/sdk/src/s3/operation/complete_multipart_upload_operation.dart
+++ packages/storage/amplify_storage_s3_dart/lib/src/sdk/src/s3/operation/complete_multipart_upload_operation.dart
@@ -1,4 +1,4 @@
-// Generated with smithy-dart 0.3.0. DO NOT MODIFY.
+// Generated with smithy-dart 0.3.1. DO NOT MODIFY.
 
 library amplify_storage_s3_dart.s3.operation.complete_multipart_upload_operation; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
@@ -134,10 +134,14 @@ class CompleteMultipartUploadOperation extends _i1.HttpOperation<
     _i5.S3ClientConfig s3ClientConfig = const _i5.S3ClientConfig(),
     _i6.AWSCredentialsProvider credentialsProvider =
         const _i6.AWSCredentialsProvider.environment(),
+    List<_i1.HttpRequestInterceptor> requestInterceptors = const [],
+    List<_i1.HttpResponseInterceptor> responseInterceptors = const [],
   })  : _region = region,
         _baseUri = baseUri,
         _s3ClientConfig = s3ClientConfig,
-        _credentialsProvider = credentialsProvider;
+        _credentialsProvider = credentialsProvider,
+        _requestInterceptors = requestInterceptors,
+        _responseInterceptors = responseInterceptors;
 
   @override
   late final List<
@@ -149,21 +153,25 @@ class CompleteMultipartUploadOperation extends _i1.HttpOperation<
     _i5.RestXmlProtocol(
       serializers: _i7.serializers,
       builderFactories: _i7.builderFactories,
-      requestInterceptors: [
-        const _i1.WithHost(),
-        const _i1.WithContentLength(),
-        _i5.WithSigV4(
-          region: _region,
-          service: _i8.AWSService.s3,
-          credentialsProvider: _credentialsProvider,
-          serviceConfiguration: _s3ClientConfig.signerConfiguration ??
-              _i6.S3ServiceConfiguration(),
-        ),
-        const _i1.WithUserAgent('aws-sdk-dart/0.3.0'),
-        const _i5.WithSdkInvocationId(),
-        const _i5.WithSdkRequest(),
-      ],
-      responseInterceptors: [const _i5.CheckErrorOnSuccess()],
+      requestInterceptors: <_i1.HttpRequestInterceptor>[
+            const _i1.WithHost(),
+            const _i1.WithContentLength(),
+            _i5.WithSigV4(
+              region: _region,
+              service: _i8.AWSService.s3,
+              credentialsProvider: _credentialsProvider,
+              serviceConfiguration: _s3ClientConfig.signerConfiguration ??
+                  _i6.S3ServiceConfiguration(),
+            ),
+            const _i1.WithUserAgent('aws-sdk-dart/0.3.1'),
+            const _i5.WithSdkInvocationId(),
+            const _i5.WithSdkRequest(),
+          ] +
+          _requestInterceptors,
+      responseInterceptors: <_i1.HttpResponseInterceptor>[
+            const _i5.CheckErrorOnSuccess()
+          ] +
+          _responseInterceptors,
       noErrorWrapping: true,
     )
   ];
@@ -180,6 +188,10 @@ class CompleteMultipartUploadOperation extends _i1.HttpOperation<
   final _i5.S3ClientConfig _s3ClientConfig;
 
   final _i6.AWSCredentialsProvider _credentialsProvider;
+
+  final List<_i1.HttpRequestInterceptor> _requestInterceptors;
+
+  final List<_i1.HttpResponseInterceptor> _responseInterceptors;
 
   @override
   _i1.HttpRequest buildRequest(_i3.CompleteMultipartUploadRequest input) =>
