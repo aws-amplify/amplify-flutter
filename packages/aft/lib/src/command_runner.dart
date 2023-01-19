1d1817ce6529ac2ce448b33c81b5bbba6a1fb3db
Dillon Nys <nydillon@amazon.com>
2023-01-19 08:20:12 -0700

    fix(aft): Passthrough command flags
    When a passthrough command has flags/options, the parser will fail. A separate codepath is needed to handle this case.

--- packages/aft/lib/src/command_runner.dart
+++ packages/aft/lib/src/command_runner.dart
@@ -3,8 +3,17 @@
 
 import 'package:aft/aft.dart';
 import 'package:aft/src/commands/passthrough_command.dart';
+import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 
+/// Passes through the command specified by [args] to either `dart`/`flutter`
+/// depending on the package in the current working directory.
+Future<void> passthrough(List<String> args) async {
+  final passthroughCommand = PassthroughCommand(args);
+  return passthroughCommand.run();
+}
+
+/// Runs the `aft` command using the given [args].
 Future<void> run(List<String> args) async {
   final runner = CommandRunner<void>('aft', 'Amplify Flutter repo tools')
     ..argParser.addFlag(
@@ -27,14 +36,23 @@ Future<void> run(List<String> args) async {
     ..addCommand(BootstrapCommand())
     ..addCommand(VersionBumpCommand())
     ..addCommand(ExecCommand());
+
   try {
-    final argResults = runner.argParser.parse(args);
-    // If we cannot resolve a command, try a passthrough to `dart`/`flutter`.
-    if (argResults.command == null) {
-      final passthroughCommand = PassthroughCommand(argResults.arguments);
-      return await passthroughCommand.run();
+    try {
+      final argResults = runner.argParser.parse(args);
+      // If we cannot resolve a command, try a passthrough to `dart`/`flutter`.
+      if (argResults.command == null) {
+        return await passthrough(argResults.arguments);
+      }
+      await runner.runCommand(argResults);
+    } on ArgParserException {
+      // If we fail to parse the arguments, also passthrough the command since
+      // adding flags or options will trigger a parse failure.
+      if (args.isNotEmpty) {
+        return await passthrough(args);
+      }
+      rethrow;
     }
-    await runner.runCommand(argResults);
   } finally {
     // Free up resources before exiting..
     for (final command in runner.commands.values.whereType<AmplifyCommand>()) {
