7d1a08e275976f54c6d742074ed041427bb14e7f
Dillon Nys <nydillon@amazon.com>
2023-01-24 15:05:02 -0700

    chore(repo): Add some missing license headers

--- packages/analytics/amplify_analytics_pinpoint/example/android/app/src/main/kotlin/com/amazonaws/amplify/amplify_analytics_pinpoint_example/MainActivity.kt
+++ packages/analytics/amplify_analytics_pinpoint/example/android/app/src/main/kotlin/com/amazonaws/amplify/amplify_analytics_pinpoint_example/MainActivity.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.amazonaws.amplify.amplify_analytics_pinpoint_example
 
 import io.flutter.embedding.android.FlutterActivity
--- packages/secure_storage/amplify_secure_storage/example/android/app/src/main/kotlin/com/amazonaws/amplify/amplify_secure_storage_example/MainActivity.kt
+++ packages/secure_storage/amplify_secure_storage/example/android/app/src/main/kotlin/com/amazonaws/amplify/amplify_secure_storage_example/MainActivity.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.amazonaws.amplify.amplify_secure_storage_example
 
 import io.flutter.embedding.android.FlutterActivity
