165e56f7b9c65dfee91c93d1bb73556e415c0e46
Jordan Nelson <nejrd@amazon.com>
2023-01-24 10:55:33 -0500

    chore: add doc comment to AWSResultType

--- packages/aws_common/lib/src/result/aws_result_type.dart
+++ packages/aws_common/lib/src/result/aws_result_type.dart
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/// Indicates whether or not the result was successful.
 enum AWSResultType {
   /// A successful result.
   success,
