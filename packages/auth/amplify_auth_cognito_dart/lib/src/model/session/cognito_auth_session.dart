f2ee6f3d0e19afe5b1bcd67abbe07a4ce23b4432
Jordan Nelson <nejrd@amazon.com>
2023-01-24 10:46:08 -0500

    chore: update CognitoAuthSession.toJson

--- packages/auth/amplify_auth_cognito_dart/lib/src/model/session/cognito_auth_session.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/model/session/cognito_auth_session.dart
@@ -5,13 +5,10 @@ import 'package:amplify_auth_cognito_dart/amplify_auth_cognito_dart.dart';
 import 'package:amplify_auth_cognito_dart/src/model/auth_result.dart';
 import 'package:amplify_core/amplify_core.dart';
 
-part 'cognito_auth_session.g.dart';
-
 /// {@template amplify_auth_cognito.model.cognito_auth_session}
 /// The current Cognito auth session, with AWS credentials and User Pool tokens
 /// of the active user.
 /// {@endtemplate}
-@zAmplifySerializable
 class CognitoAuthSession extends AuthSession
     with
         AWSEquatable<CognitoAuthSession>,
@@ -46,5 +43,7 @@ class CognitoAuthSession extends AuthSession
       ];
 
   @override
-  Map<String, Object?> toJson() => _$CognitoAuthSessionToJson(this);
+  Map<String, Object?> toJson() => <String, dynamic>{
+        'isSignedIn': isSignedIn,
+      };
 }
