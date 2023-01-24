e776039cbcbdb8c2a93388d6e1c8141d94bb93a2
Dillon Nys <nydillon@amazon.com>
2023-01-24 13:42:56 -0700

    chore(analytics): Regenerate example app

--- packages/analytics/amplify_analytics_pinpoint/example/android/app/src/main/kotlin/com/amazonaws/amplify/amplify_analytics_pinpoint_example/MainActivity.kt
+++ packages/analytics/amplify_analytics_pinpoint/example/android/app/src/main/kotlin/com/amazonaws/amplify/amplify_analytics_pinpoint_example/MainActivity.kt
@@ -1,6 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 package com.amazonaws.amplify.amplify_analytics_pinpoint_example
 
 import io.flutter.embedding.android.FlutterActivity
--- packages/analytics/amplify_analytics_pinpoint/example/android/app/src/main/kotlin/com/amazonaws/amplify/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.amazonaws.amplify.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
