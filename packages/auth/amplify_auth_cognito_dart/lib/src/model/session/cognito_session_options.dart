84a3ee968bad24bb0f394cb88777bc94b71b0d6e
Jordan Nelson <nejrd@amazon.com>
2023-01-25 15:28:40 -0500

    chore: add getAWSCredentials back as deprecated

--- packages/auth/amplify_auth_cognito_dart/lib/src/model/session/cognito_session_options.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/model/session/cognito_session_options.dart
@@ -5,6 +5,10 @@ import 'package:amplify_core/amplify_core.dart';
 
 part 'cognito_session_options.g.dart';
 
+const _getAWSCredentialsDeprecation = '`getAWSCredentials` is ignored. AWS '
+    'Credentials will always be retrieved. `credentialsResult` will contain '
+    'the result of retrieving credentials, which may be an error';
+
 /// {@template amplify_auth_cognito.model.cognito_session_options}
 /// Cognito options for `Amplify.Auth.fetchAuthSession`.
 /// {@endtemplate}
@@ -13,6 +17,8 @@ class CognitoSessionOptions extends AuthSessionOptions
     with AWSEquatable<CognitoSessionOptions>, AWSDebuggable {
   /// {@macro amplify_auth_cognito.model.cognito_session_options}
   const CognitoSessionOptions({
+    // ignore: avoid_unused_constructor_parameters
+    @Deprecated(_getAWSCredentialsDeprecation) bool? getAWSCredentials = false,
     super.forceRefresh = false,
   });
 
