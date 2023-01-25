16524b8bf6dc288081280303615b1ffab0d5071d
Jordan Nelson <nejrd@amazon.com>
2023-01-25 14:59:53 -0500

    chore: add back members to CognitoAuthSession, update toJson

--- packages/auth/amplify_auth_cognito_dart/lib/src/model/session/cognito_auth_session.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/model/session/cognito_auth_session.dart
@@ -22,17 +22,33 @@ class CognitoAuthSession extends AuthSession
     required this.identityIdResult,
   });
 
-  /// The User Pool tokens.
+  /// The User Pool tokens Result.
   final AuthResult<CognitoUserPoolTokens> userPoolTokensResult;
 
-  /// The user ID (subject).
+  /// The user ID (subject) Result.
   final AuthResult<String> userSubResult;
 
-  /// The AWS credentials.
+  /// The AWS credentials Result.
   final AuthResult<AWSCredentials> credentialsResult;
 
-  /// The AWS identity ID.
+  /// The AWS identity ID Result.
   final AuthResult<String> identityIdResult;
+
+  /// The AWS credentials.
+  @Deprecated('Use `credentialsResult.value` instead')
+  AWSCredentials? get credentials => credentialsResult.valueOrNull;
+
+  /// The User Pool tokens.
+  @Deprecated('Use `userPoolTokensResult.value` instead')
+  CognitoUserPoolTokens? get userPoolTokens => userPoolTokensResult.valueOrNull;
+
+  /// The user ID (subject).
+  @Deprecated('Use `userSubResult.value` instead')
+  String? get userSub => userSubResult.valueOrNull;
+
+  /// The AWS identity ID.
+  @Deprecated('Use `identityIdResult.value` instead')
+  String? get identityId => identityIdResult.valueOrNull;
 
   @override
   List<Object?> get props => [
@@ -45,5 +61,9 @@ class CognitoAuthSession extends AuthSession
   @override
   Map<String, Object?> toJson() => <String, dynamic>{
         'isSignedIn': isSignedIn,
+        'userSub': userSubResult.valueOrNull,
+        'userPoolTokens': userPoolTokensResult.valueOrNull,
+        'credentials': credentialsResult.valueOrNull,
+        'identityId': identityIdResult.valueOrNull,
       };
 }
