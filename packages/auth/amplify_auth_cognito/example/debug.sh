b90cdfede4c28acabe199b7b9628a7248154236d
Dillon Nys <nydillon@amazon.com>
2023-01-25 14:02:44 -0700

    debug

--- packages/auth/amplify_auth_cognito/example/debug.sh
+++ packages/auth/amplify_auth_cognito/example/debug.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 pushd $@
-dart pub global activate fvm
 fvm use local
+dart pub global activate fvm
 fvm flutter pub get
 fvm flutter test -d macos integration_test/main_test.dart
 popd
