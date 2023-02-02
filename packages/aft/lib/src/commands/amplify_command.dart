8350a5c31ab4f0650c72b2dfe97ce9b24b0575bf
Dillon Nys <nydillon@amazon.com>
2023-02-02 08:49:43 -0700

    chore(aft): Refine `deps update`
    - Enforce that all packages must exactly meet the global constraint. It's too easy to run into conflicts otherwise.
    - Revise constraint updates based on new algorithm:
    
    - If the range specifies a lower bound only, e.g. `^1.0.0`, do nothing but warn if a new breaking change is available.
    - If the range specifies a sliding window for a single minor version, e.g. `>=1.1.0 <1.2.0`, and the latest version is greater than the upper bound, slide the window.
    - If the range specifies a window larger than a single minor version, keep the lower bound and move the upper bound unless it's a major version bump.

--- packages/aft/lib/src/commands/amplify_command.dart
+++ packages/aft/lib/src/commands/amplify_command.dart
@@ -142,14 +142,16 @@ abstract class AmplifyCommand extends Command<void>
   }();
 
   /// The global `aft` configuration for the repo.
-  late final AftConfig aftConfig = () {
-    final configFile = File(p.join(rootDir.path, 'aft.yaml'));
+  ///
+  /// Should not be `late final` to allow re-reading from disk, e.g. when
+  /// updated as part of a command.
+  AftConfig get aftConfig {
+    final configFile = File(aftConfigPath);
     assert(configFile.existsSync(), 'Could not find aft.yaml');
     final configYaml = configFile.readAsStringSync();
     final config = checkedYamlDecode(configYaml, AftConfig.fromJson);
-    logger.verbose('$config');
     return config;
-  }();
+  }
 
   /// The environment to inject into subcommands.
   late final Map<String, String> environment = {
