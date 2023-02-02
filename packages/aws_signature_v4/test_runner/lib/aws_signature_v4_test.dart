480c383386a24b1b7acc8ae48a41e88d7ae8012f
Dillon Nys <nydillon@amazon.com>
2023-02-02 14:24:07 -0700

    chore(sigv4): Regenerate workflows

---
+++ packages/aws_signature_v4/test_runner/lib/aws_signature_v4_test.dart
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Tests for the `aws_signature_v4` package.
+library aws_signature_v4_test;
