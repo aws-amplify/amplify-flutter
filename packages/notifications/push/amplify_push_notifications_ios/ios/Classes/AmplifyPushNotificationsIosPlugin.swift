ab30f5a3a3ece55d66ecad5629b41b9efb215ac6
Manoj NB <manojnb@amazon.com>
2023-02-15 22:41:52 -0800

    feat(push): configure works on iOS but unable to get the token

--- packages/notifications/push/amplify_push_notifications_ios/ios/Classes/AmplifyPushNotificationsIosPlugin.swift
+++ packages/notifications/push/amplify_push_notifications_ios/ios/Classes/AmplifyPushNotificationsIosPlugin.swift
@@ -1,19 +1,65 @@
 import Flutter
 import UIKit
+import Amplify
+import amplify_flutter_ios
 
 public class AmplifyPushNotificationsIosPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amplify_push_notifications_ios", binaryMessenger: registrar.messenger())
-    let instance = AmplifyPushNotificationsIosPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    
+    var _result:FlutterResult!
+    
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.amazonaws.amplify/push_notifications_plugin", binaryMessenger: registrar.messenger())
+        let instance = AmplifyPushNotificationsIosPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let atomicResult = AtomicResult(result, call.method)
+        innerHandle(method: call.method, callArgs: call.arguments as Any?, result: atomicResult)
+        
+    }
+    public func innerHandle(method: String, callArgs: Any?, result: @escaping FlutterResult) {
+        self._result = result
+        let this = AmplifyPushNotificationsIosPlugin.self
+        switch method {
+        case "registerForRemoteNotifications": do {
+            print("registerForRemoteNotifications")
+
+            UIApplication.shared.registerForRemoteNotifications()
+            UNUserNotificationCenter.current().delegate = self
+            
+        }
+        case "getToken": do {
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    // TODO: Debug why this is not being called
+    public func application(_ application: UIApplication,
+                            didRegisterForRemoteNotificationsWithDeviceToken
+                            deviceToken: Data) {
+        let deviceTokenString = deviceToken.hexString
+        print("deviceToken : \(deviceTokenString)")
+        
+        if(_result != nil ){
+            _result(deviceTokenString);
+        }
+    }
+    
+    public func application(_ application: UIApplication,
+                            didFailToRegisterForRemoteNotificationsWithError
+                            error: Error) {
+        print("error getting token : \(error)")
+    }
+    
+}
+extension Data {
+    var hexString: String {
+        let hexString = map { String(format: "%02.2hhx", $0) }.joined()
+        return hexString
+    }
 }
--- packages/notifications/push/amplify_push_notifications_ios/ios/amplify_push_notifications_ios.podspec
+++ packages/notifications/push/amplify_push_notifications_ios/ios/amplify_push_notifications_ios.podspec
@@ -15,6 +15,8 @@ A new Flutter plugin project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
+  s.dependency 'Amplify', '1.28.3'
+  s.dependency 'amplify_flutter_ios'
   s.platform = :ios, '9.0'
 
   # Flutter.framework does not contain a i386 slice.
