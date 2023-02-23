34cb4dc6cb3edfd0af550d25c655d5f1fa190e30
Dillon Nys <nydillon@amazon.com>
2023-02-23 09:29:36 -0700

    test(auth): Split out sign in/sign out tests
    Instead of having a single file for both suites, create separate files for each.

--- packages/auth/amplify_auth_cognito/example/integration_test/sign_out_test.dart
+++ packages/auth/amplify_auth_cognito/example/integration_test/sign_out_test.dart
@@ -16,135 +16,6 @@ import 'utils/test_utils.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('signIn (SRP)', () {
-    late String username;
-    late String password;
-
-    tearDownAll(Amplify.reset);
-
-    setUp(() async {
-      await configureAuth();
-
-      // create new user for each test
-      username = generateUsername();
-      password = generatePassword();
-
-      await adminCreateUser(
-        username,
-        password,
-        autoConfirm: true,
-        verifyAttributes: true,
-      );
-
-      await signOutUser();
-    });
-
-    asyncTest('should signIn a user', (_) async {
-      final res = await Amplify.Auth.signIn(
-        username: username,
-        password: password,
-      );
-      expect(res.isSignedIn, true);
-    });
-
-    asyncTest(
-      'should throw a NotAuthorizedException with an incorrect password',
-      (_) async {
-        final incorrectPassword = generatePassword();
-        await expectLater(
-          Amplify.Auth.signIn(
-            username: username,
-            password: incorrectPassword,
-          ),
-          throwsA(isA<AuthNotAuthorizedException>()),
-        );
-      },
-    );
-
-    asyncTest(
-      'should throw an InvalidParameterException if a password is not provided '
-      'and Custom Auth is not configured',
-      (_) async {
-        await expectLater(
-          Amplify.Auth.signIn(username: username),
-          throwsA(isA<InvalidParameterException>()),
-        );
-      },
-    );
-
-    asyncTest(
-      'should throw a UserNotFoundException with a non-existent user',
-      (_) async {
-        final incorrectUsername = generateUsername();
-        await expectLater(
-          Amplify.Auth.signIn(
-            username: incorrectUsername,
-            password: password,
-          ),
-          throwsA(isA<UserNotFoundException>()),
-        );
-      },
-    );
-
-    asyncTest('additionalInfo should be null for SRP sign-in', (_) async {
-      final result = await Amplify.Auth.signIn(
-        username: username,
-        password: password,
-      );
-      expect(result.nextStep.additionalInfo, isNull);
-    });
-
-    asyncTest('identity ID should be the same between sessions', (_) async {
-      // Get unauthenticated identity
-      final unauthSession =
-          await Amplify.Auth.fetchAuthSession() as CognitoAuthSession;
-
-      // Sign in
-      {
-        final signInRes = await Amplify.Auth.signIn(
-          username: username,
-          password: password,
-        );
-        expect(signInRes.nextStep.signInStep, AuthSignInStep.done);
-      }
-
-      // Get authenticated identity
-      final authSession =
-          await Amplify.Auth.fetchAuthSession() as CognitoAuthSession;
-      final authenticatedIdentity = authSession.identityIdResult;
-      expect(
-        authenticatedIdentity,
-        isNot(unauthSession.identityIdResult.value),
-        reason:
-            'Unauthenticated identities should be distinct from authenticated '
-            'identities, since unauthenticated identities are vended to all '
-            'new devices when guest access is enabled but should converge to '
-            'a singular authenticated identity across all devices',
-      );
-      expect(
-        authSession.credentialsResult.value,
-        isNot(unauthSession.credentialsResult.value),
-      );
-
-      await Amplify.Auth.signOut();
-      {
-        final signInRes = await Amplify.Auth.signIn(
-          username: username,
-          password: password,
-        );
-        expect(signInRes.nextStep.signInStep, AuthSignInStep.done);
-      }
-
-      final newSession =
-          await Amplify.Auth.fetchAuthSession() as CognitoAuthSession;
-      expect(
-        newSession.identityIdResult.value,
-        authenticatedIdentity.value,
-        reason: 'Authenticated identity should be the same between sessions',
-      );
-    });
-  });
-
   group('signOut', () {
     late final cognitoPlugin = Amplify.Auth.getPlugin(
       AmplifyAuthCognito.pluginKey,
@@ -228,8 +99,8 @@ void main() {
       final session2Tokens = session2.userPoolTokensResult.value;
       expect(session1Tokens.userId, session2Tokens.userId);
       expect(
-        session1Tokens.idToken.raw,
-        isNot(session2Tokens.idToken.raw),
+        session1Tokens.accessToken.raw,
+        isNot(session2Tokens.accessToken.raw),
       );
 
       await check(session1Tokens.accessToken.raw, isValid: true);
