cd46b64cbe52f3c46d86ae65b5365e96f7603b4d
Dillon Nys <nydillon@amazon.com>
2023-02-23 09:29:28 -0700

    test(auth): Add global sign out e2e test
    Tests that tokens are invalidated across devices when signing out with `globalSignOut`.

--- packages/auth/amplify_auth_cognito/example/integration_test/sign_in_sign_out_test.dart
+++ packages/auth/amplify_auth_cognito/example/integration_test/sign_in_sign_out_test.dart
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+// ignore: implementation_imports
+import 'package:amplify_auth_cognito_dart/src/sdk/cognito_identity_provider.dart'
+    as cognito_idp;
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_test/amplify_test.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -143,19 +146,42 @@ void main() {
   });
 
   group('signOut', () {
+    late final cognitoPlugin = Amplify.Auth.getPlugin(
+      AmplifyAuthCognito.pluginKey,
+    );
+    late String username;
+    late String password;
+    late final AWSHttpClient client;
+    late final cognito_idp.CognitoIdentityProviderClient cognitoClient;
+
+    Future<void> check(String accessToken, {required bool isValid}) async {
+      await expectLater(
+        cognitoClient
+            .getUser(cognito_idp.GetUserRequest(accessToken: accessToken))
+            .result,
+        isValid
+            ? completes
+            : throwsA(isA<cognito_idp.NotAuthorizedException>()),
+      );
+    }
+
     setUpAll(() async {
       await configureAuth();
+
+      final config = await Amplify.asyncConfig;
+      final authConfig = config.auth!.awsPlugin!.cognitoUserPool!.default$!;
+      client = AWSHttpClient()..supportedProtocols = SupportedProtocols.http1;
+      cognitoClient = cognito_idp.CognitoIdentityProviderClient(
+        region: authConfig.region,
+      );
+      addTearDown(client.close);
     });
 
     setUp(() async {
       await signOutUser();
-    });
 
-    tearDownAll(Amplify.reset);
-
-    asyncTest('should sign a user out', (_) async {
-      final username = generateUsername();
-      final password = generatePassword();
+      username = generateUsername();
+      password = generatePassword();
 
       await adminCreateUser(
         username,
@@ -163,12 +189,18 @@ void main() {
         autoConfirm: true,
         verifyAttributes: true,
       );
+      addTearDown(() => deleteUser(username));
+    });
 
+    tearDownAll(Amplify.reset);
+
+    asyncTest('should sign a user out', (_) async {
       await Amplify.Auth.signIn(username: username, password: password);
       final authSession = await Amplify.Auth.fetchAuthSession();
       expect(authSession.isSignedIn, isTrue);
 
-      await Amplify.Auth.signOut();
+      final signOutResult = await Amplify.Auth.signOut();
+      expect(signOutResult, isA<CognitoCompleteSignOut>());
       final finalAuthSession = await Amplify.Auth.fetchAuthSession();
       expect(finalAuthSession.isSignedIn, isFalse);
     });
@@ -178,8 +210,38 @@ void main() {
       (_) async {
         final authSession = await Amplify.Auth.fetchAuthSession();
         expect(authSession.isSignedIn, isFalse);
-        await expectLater(Amplify.Auth.signOut(), completes);
+        final signOutResult = await Amplify.Auth.signOut();
+        expect(signOutResult, isA<CognitoCompleteSignOut>());
       },
     );
+
+    asyncTest('global signout invalidates previous sessions', (_) async {
+      await cognitoPlugin.signIn(username: username, password: password);
+      final session1 = await cognitoPlugin.fetchAuthSession();
+      final session1Tokens = session1.userPoolTokensResult.value;
+      // Clear but do not sign out so that tokens are still valid.
+      // ignore: invalid_use_of_protected_member
+      await cognitoPlugin.stateMachine.clearCredentials();
+
+      await cognitoPlugin.signIn(username: username, password: password);
+      final session2 = await cognitoPlugin.fetchAuthSession();
+      final session2Tokens = session2.userPoolTokensResult.value;
+      expect(session1Tokens.userId, session2Tokens.userId);
+      expect(
+        session1Tokens.idToken.raw,
+        isNot(session2Tokens.idToken.raw),
+      );
+
+      await check(session1Tokens.accessToken.raw, isValid: true);
+      await check(session2Tokens.accessToken.raw, isValid: true);
+
+      final signOutResult = await cognitoPlugin.signOut(
+        options: const SignOutOptions(globalSignOut: true),
+      );
+      expect(signOutResult, isA<CognitoCompleteSignOut>());
+
+      await check(session1Tokens.accessToken.raw, isValid: false);
+      await check(session2Tokens.accessToken.raw, isValid: false);
+    });
   });
 }
