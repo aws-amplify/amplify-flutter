5744d7bee5d9a88b7905663c31fc9cbaa82d5e85
Kyle <kylcheng@gmail.com>
2023-03-06 13:21:52 -0800

    chore(analytics): export missing files

--- packages/analytics/amplify_analytics_pinpoint_dart/lib/amplify_analytics_pinpoint_dart.dart
+++ packages/analytics/amplify_analytics_pinpoint_dart/lib/amplify_analytics_pinpoint_dart.dart
@@ -1,13 +1,17 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-/// Amplify Analytics Pinpoint for Dart
+/// Amplify Analytics Pinpoint for Dart.
 library amplify_analytics_pinpoint_dart;
 
+/// For calls to SetChannelType().
+export 'package:amplify_analytics_pinpoint_dart/src/sdk/src/pinpoint/model/channel_type.dart';
 export 'src/analytics_plugin_impl.dart';
 export 'src/impl/analytics_client/analytics_client.dart';
 export 'src/impl/analytics_client/endpoint_client/aws_pinpoint_user_profile.dart';
 export 'src/impl/analytics_client/endpoint_client/endpoint_info_store_manager.dart';
+
+/// For Integration Test.
 export 'src/impl/analytics_client/endpoint_client/endpoint_store_keys.dart';
 export 'src/impl/flutter_provider_interfaces/app_lifecycle_provider.dart';
 export 'src/impl/flutter_provider_interfaces/cached_events_path_provider.dart';
