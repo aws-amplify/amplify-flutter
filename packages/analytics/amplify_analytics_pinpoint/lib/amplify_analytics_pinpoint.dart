126d792c6e21b22b0203a448e896d06a83dcde8b
kc <kylcheng@gmail.com>
2023-03-07 11:16:56 -0800

    chore(analytics): export missing files (#2728)

--- packages/analytics/amplify_analytics_pinpoint/lib/amplify_analytics_pinpoint.dart
+++ packages/analytics/amplify_analytics_pinpoint/lib/amplify_analytics_pinpoint.dart
@@ -1,18 +1,30 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-/// Amplify Analytics Pinpoint
+/// Amplify Analytics Pinpoint.
 library amplify_analytics_pinpoint;
 
 /// Overridable Flutter injected dependencies.
 export 'package:amplify_analytics_pinpoint_dart/amplify_analytics_pinpoint_dart.dart'
-    show AppLifecycleProvider, AWSPinpointUserProfile;
+    show
+        AppLifecycleProvider,
+        AWSPinpointUserProfile,
+
+        /// For Push Notifications.
+        // TODO(kylecheng): export via SecureStorage instead.
+        EndpointInfoStoreManager,
+
+        /// For calls to SetChannelType().
+        ChannelType,
+        ChannelTypeHelpers;
 
 /// Category Implementation.
 export 'src/analytics_plugin_impl.dart';
 
-/// Push Notifications
+/// Push Notifications.
+// TODO(kylecheng): check if still needed after secure storage factory change.
 export 'src/flutter_analytics_client.dart';
 
-/// Cognito Auth endpoint management
+/// Cognito Auth endpoint management.
+// TODO(kylecheng): change to secure storage factory instead
 export 'src/flutter_endpoint_info_store_manager.dart';
