3187bf187c8899e2b202d2ec7e68365082876b03
Manoj NB <manojnb@amazon.com>
2023-03-07 18:13:53 -0800

    fix: linter errors and update makfile

--- packages/notifications/push/amplify_push_notifications_pinpoint/lib/src/pinpoint_provider.dart
+++ packages/notifications/push/amplify_push_notifications_pinpoint/lib/src/pinpoint_provider.dart
@@ -2,9 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:amplify_core/amplify_core.dart';
-
 // TODO(Samaritan1011001): Complete the implementation using Analytics Client
+
+/// {@template amplify_push_notifications_pinpoint.pinpoint_provider}
+/// The AWS Pinpoint implementation of the Amplify Push Notification category.
+/// {@endtemplate}
 class PinpointProvider extends ServiceProviderClient {
+  /// {@macro amplify_push_notifications_pinpoint.pinpoint_provider}
   @override
   Future<void> init({
     AmplifyConfig? config,
