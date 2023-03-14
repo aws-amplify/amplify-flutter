5358b00fec48822bc07d9d0c5fbc9f538832940a
Manoj NB <manojnb@amazon.com>
2023-03-14 13:52:24 -0700

    fix: clean up example main.dart

--- packages/notifications/push/amplify_push_notifications_pinpoint/example/lib/main.dart
+++ packages/notifications/push/amplify_push_notifications_pinpoint/example/lib/main.dart
@@ -45,7 +45,6 @@ class _MyAppState extends State<MyApp> {
       final prefs = await SharedPreferences.getInstance();
       await prefs.reload();
       globalBgCallbackCount = prefs.getInt(globalBgCallbackKey) ?? 0;
-      // print('globalBgCallbackCount -> $globalBgCallbackCount');
       return globalBgCallbackCount;
     } on Exception catch (e) {
       print('Error when get call $e');
@@ -71,21 +70,21 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       backgroundMessage = pushNotificationMessage;
     });
-    // try {
-    //   WidgetsFlutterBinding.ensureInitialized();
-    //   final prefs = await SharedPreferences.getInstance();
-    //   await prefs.reload();
-    //   var globalBgCallbackCount = prefs.getInt(globalBgCallbackKey);
-    //   globalBgCallbackCount =
-    //       globalBgCallbackCount != null ? (globalBgCallbackCount + 1) : 1;
-    //   await prefs.setInt(
-    //     globalBgCallbackKey,
-    //     globalBgCallbackCount,
-    //   );
-    // } on Exception catch (e) {
-    //   print(' error in handler: $e');
-    // }
-    // return;
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.reload();
+      var globalBgCallbackCount = prefs.getInt(globalBgCallbackKey);
+      globalBgCallbackCount =
+          globalBgCallbackCount != null ? (globalBgCallbackCount + 1) : 1;
+      await prefs.setInt(
+        globalBgCallbackKey,
+        globalBgCallbackCount,
+      );
+    } on Exception catch (e) {
+      print(' error in handler: $e');
+    }
+    return;
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -102,10 +101,6 @@ class _MyAppState extends State<MyApp> {
         print('Configuring Amplify.configure');
         await Amplify.addPlugins([authPlugin, notificationsPlugin]);
         await Amplify.configure(amplifyconfig);
-
-        // Amplify.Notifications.Push.onNotificationReceivedInBackground(
-        //   bgHandler,
-        // );
         setState(() {
           isConfigured = true;
         });
