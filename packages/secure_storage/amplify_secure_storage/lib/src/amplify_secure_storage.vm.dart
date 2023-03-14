17ae50cad4923711f47eddbd9eec1a92cfdfae14
Jordan Nelson <nejrd@amazon.com>
2023-03-14 12:53:24 -0400

    feat(secure_storage)!: Use Local App Data Directory on Windows (#2744)
    feat(secure_storage)!: Use Local App Dir on Windows

--- packages/secure_storage/amplify_secure_storage/lib/src/amplify_secure_storage.vm.dart
+++ packages/secure_storage/amplify_secure_storage/lib/src/amplify_secure_storage.vm.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 
 import 'package:amplify_secure_storage/src/amplify_secure_storage.android.dart';
 import 'package:amplify_secure_storage/src/messages.cupertino.g.dart';
+import 'package:amplify_secure_storage/src/path_provider_local.dart';
 import 'package:amplify_secure_storage_dart/amplify_secure_storage_dart.dart';
 // ignore: implementation_imports
 import 'package:amplify_secure_storage_dart/src/utils/file_key_value_store.dart';
@@ -43,7 +44,7 @@ class AmplifySecureStorage extends AmplifySecureStorageInterface {
             config: config.copyWith(
               windowsOptions: config.windowsOptions.copyWith(
                 storagePath: config.windowsOptions.storagePath ??
-                    (await getApplicationSupportDirectory()).path,
+                    (await getApplicationLocalSupportDirectory()).path,
               ),
             ),
           );
