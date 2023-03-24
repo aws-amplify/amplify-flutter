2168452420dd9d79b1d8c2ebc8779aaae63d5247
Dillon Nys <nydillon@amazon.com>
2023-03-24 14:13:17 -0700

    chore(auth): Add empty plugin options
    Adds Cognito plugin options for all APIs, even if there are no plugin options. This reassures the developer they are not missing anything and adds a placeholder for future plugin options.

---
+++ packages/auth/amplify_auth_cognito_dart/lib/src/model/attribute/cognito_confirm_user_attribute_plugin_options.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cognito_confirm_user_attribute_plugin_options.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CognitoConfirmUserAttributePluginOptions
+    _$CognitoConfirmUserAttributePluginOptionsFromJson(
+            Map<String, dynamic> json) =>
+        CognitoConfirmUserAttributePluginOptions();
+
+Map<String, dynamic> _$CognitoConfirmUserAttributePluginOptionsToJson(
+        CognitoConfirmUserAttributePluginOptions instance) =>
+    <String, dynamic>{};
