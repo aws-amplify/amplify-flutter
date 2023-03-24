906dc6c97526f9e5513017a8ecc8d504ecbf8b68
Dillon Nys <nydillon@amazon.com>
2023-03-24 12:54:40 -0700

    chore(authenticator): Add user agent tracking
    Injects an Authenticator user agent header in all requests made from the Authenticator service.

---
+++ packages/authenticator/amplify_authenticator/example/integration_test/http_test.dart
@@ -0,0 +1,47 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:amplify_authenticator/src/services/amplify_auth_service.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_test/amplify_test.dart';
+import 'package:aws_common/testing.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'config.dart';
+
+void main() {
+  AWSLogger().logLevel = LogLevel.verbose;
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('HTTP', () {
+    testWidgets('includes user agent', (_) async {
+      final client = MockAWSHttpClient(
+        expectAsync2((request, isCancelled) {
+          expect(
+            request.headers[AmplifyUserAgent.headerKey],
+            contains(AmplifyAuthService.userAgent),
+          );
+          return AWSHttpResponse(statusCode: 500);
+        }),
+      );
+
+      await loadConfiguration(environmentName: 'main');
+
+      // ignore: invalid_use_of_protected_member
+      final dependencies = Amplify.Auth.defaultPlugin.dependencies;
+      final amplifyClient = AmplifyHttpClient(dependencies, baseClient: client);
+      dependencies.addInstance(amplifyClient);
+
+      try {
+        await AmplifyAuthService().signUp(
+          generateUsername(),
+          generatePassword(),
+          const {},
+        );
+      } on Object {
+        // OK
+      }
+    });
+  });
+}
