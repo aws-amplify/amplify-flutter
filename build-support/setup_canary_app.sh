d79d8fb05fca247c993a8d6e5e2e9399012a4203
Travis Sheppard <tshepp@amazon.com>
2023-03-24 14:20:25 -0700

    add action config

--- build-support/setup_canary_app.sh
+++ build-support/setup_canary_app.sh
@@ -5,10 +5,9 @@ brew install gnu-sed
 # clean
 rm -rf amplified_todo
 
-# generate 
+# generate
 flutter create amplified_todo
 cd amplified_todo
-sed -i -e "s/minSdkVersion .*/minSdkVersion 21/" ./android/app/build.gradle && cat ./android/app/build.gradle
 flutter pub add amplify_flutter && flutter pub add amplify_datastore && flutter pub add amplify_storage_s3 && flutter pub add amplify_analytics_pinpoint && flutter pub add amplify_auth_cognito && flutter pub add amplify_api
 # add integration test package
 sed -i -e "s/dev_dependencies:/dev_dependencies:\n  integration_test:\n    sdk: flutter/" ./pubspec.yaml && cat ./pubspec.yaml
@@ -19,5 +18,7 @@ cp -r ../template/integration_test .
 cp -r ../template/lib .
 cp lib/dummy_amplifyconfiguration.dart lib/amplifyconfiguration.dart
 
-# run test
-flutter test --no-pub integration_test/main_test.dart -d sdk
+# Android
+sed -i -e "s/minSdkVersion .*/minSdkVersion 21/" ./android/app/build.gradle && cat ./android/app/build.gradle
+# iOS
+sed -i -e "s/# platform .*/platform :ios, '13.0'/" ./ios/Podfile && cat ./ios/Podfile
