6d4a4bf80640d537530de510e40c5dadb6399761
Jordan Nelson <nejrd@amazon.com>
2023-03-27 14:39:46 -0400

    chore: add plugin to wrap amplify native

---
+++ packages/amplify_native_legacy_wrapper/ios/Classes/AmplifyNativeLegacyWrapperPlugin.swift
@@ -0,0 +1,59 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import Flutter
+import AmplifyPlugins
+import AWSPluginsCore
+import Amplify
+import UIKit
+
+public class AmplifyNativeLegacyWrapperPlugin: NSObject, FlutterPlugin, LegacyNativePlugin {
+    
+    private let authPlugin: AWSCognitoAuthPlugin = AWSCognitoAuthPlugin()
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let api = AmplifyNativeLegacyWrapperPlugin()
+        let binaryMessenger = registrar.messenger()
+        LegacyNativePluginSetup.setUp(binaryMessenger: binaryMessenger, api: api)
+    }
+    
+    func configure(config: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let decoder = JSONDecoder()
+        do {
+            let sourceData = config.data(using: .utf8)
+            let decodedObject = try decoder.decode(JSONValue.self, from: sourceData!)
+            let authConfig = decodedObject.value(at: "auth.plugins.awsCognitoAuthPlugin")
+            try authPlugin.configure(using: authConfig)
+            return completion(.success(()))
+        } catch let error {
+            return completion(.failure("Failed to configure Amplify: \(error)"))
+        }
+    }
+    
+    func signIn(username: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        _ = authPlugin.signIn(username: username, password: password, options: nil) { response in
+            switch response {
+            case .success(_):
+                return completion(.success(()))
+            case .failure(let signInError):
+                return completion(.failure("Failed to sign the user in: \(signInError)"))
+            }
+        }
+    }
+    
+    func signOut(completion: @escaping (Result<Void, Error>) -> Void) {
+        _ = authPlugin.signOut(options: nil) { response in
+            switch response {
+            case .success(_):
+                return completion(.success(()))
+            case .failure(let signOutError):
+                return completion(.failure("Failed to sign the user in: \(signOutError)"))
+            }
+        }
+    }
+    
+}
+
+extension String: Error {}
+
+
---
+++ packages/amplify_native_legacy_wrapper/ios/amplify_native_legacy_wrapper.podspec
@@ -0,0 +1,26 @@
+#
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
+# Run `pod lib lint amplify_native_legacy_wrapper.podspec` to validate before publishing.
+#
+Pod::Spec.new do |s|
+  s.name             = 'amplify_native_legacy_wrapper'
+  s.version          = '0.0.1'
+  s.summary          = 'A new Flutter plugin project.'
+  s.description      = <<-DESC
+A new Flutter plugin project.
+                       DESC
+  s.homepage         = 'http://example.com'
+  s.license          = { :file => '../LICENSE' }
+  s.author           = { 'Your Company' => 'email@example.com' }
+  s.source           = { :path => '.' }
+  s.source_files = 'Classes/**/*'
+  s.dependency 'Flutter'
+  s.dependency 'Amplify', '1.29.1'
+  s.dependency 'AWSPluginsCore', '1.29.1'
+  s.dependency 'AmplifyPlugins/AWSCognitoAuthPlugin', '1.29.1'
+  s.platform = :ios, '13.0'
+
+  # Flutter.framework does not contain a i386 slice.
+  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
+  s.swift_version = '5.0'
+end
