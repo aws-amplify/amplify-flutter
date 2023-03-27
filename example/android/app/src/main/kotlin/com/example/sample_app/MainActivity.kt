1ebff56e153fd49e23076778e8d0091fa96c7554
Dillon Nys <nydillon@amazon.com>
2023-03-27 11:41:33 -0700

    chore(repo): Remove example app
    Removes the example app from the root of the repo.

--- example/android/app/src/main/kotlin/com/example/sample_app/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package com.example.sample_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
