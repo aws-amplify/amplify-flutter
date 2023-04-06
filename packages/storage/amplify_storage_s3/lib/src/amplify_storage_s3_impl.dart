819643ba072f06cc9136721c9fc7be95549f416c
Dillon Nys <nydillon@amazon.com>
2023-04-06 10:57:58 -0700

    refactor(storage): Remove generic types
    Removes generic types from public APIs and removes the category wrapper interface for `getPlugin`.

--- packages/storage/amplify_storage_s3/lib/src/amplify_storage_s3_impl.dart
+++ packages/storage/amplify_storage_s3/lib/src/amplify_storage_s3_impl.dart
@@ -19,21 +19,8 @@ class AmplifyStorageS3 extends AmplifyStorageS3Dart {
 
   /// A plugin key which can be used with `Amplify.Storage.getPlugin` to retrieve
   /// a S3-specific Storage category interface.
-  static const StoragePluginKey<
-      S3ListOperation,
-      S3GetPropertiesOperation,
-      S3GetUrlOperation,
-      S3UploadDataOperation,
-      S3UploadFileOperation,
-      S3DownloadDataOperation,
-      S3DownloadFileOperation,
-      S3CopyOperation,
-      S3MoveOperation,
-      S3RemoveOperation,
-      S3RemoveManyOperation,
-      S3Item,
-      S3TransferProgress,
-      AmplifyStorageS3> pluginKey = _AmplifyStorageS3PluginKey();
+  static const StoragePluginKey<AmplifyStorageS3> pluginKey =
+      _AmplifyStorageS3PluginKey();
 
   @override
   Future<void> configure({
@@ -49,21 +36,7 @@ class AmplifyStorageS3 extends AmplifyStorageS3Dart {
   }
 }
 
-class _AmplifyStorageS3PluginKey extends StoragePluginKey<
-    S3ListOperation,
-    S3GetPropertiesOperation,
-    S3GetUrlOperation,
-    S3UploadDataOperation,
-    S3UploadFileOperation,
-    S3DownloadDataOperation,
-    S3DownloadFileOperation,
-    S3CopyOperation,
-    S3MoveOperation,
-    S3RemoveOperation,
-    S3RemoveManyOperation,
-    S3Item,
-    S3TransferProgress,
-    AmplifyStorageS3> {
+class _AmplifyStorageS3PluginKey extends StoragePluginKey<AmplifyStorageS3> {
   const _AmplifyStorageS3PluginKey();
 
   @override
