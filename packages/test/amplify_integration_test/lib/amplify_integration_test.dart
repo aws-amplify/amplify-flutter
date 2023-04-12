3ae9dffababd3e4a64112a30f57ef2e1f0cf99a6
Dillon Nys <nydillon@amazon.com>
2023-04-11 18:21:12 -0700

    test: Clean up

--- packages/test/amplify_integration_test/lib/amplify_integration_test.dart
+++ packages/test/amplify_integration_test/lib/amplify_integration_test.dart
@@ -1,4 +1,18 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 /// Utilities for running Amplify integration tests.
+///
+/// In the Amplify Flutter repo, most package follow the same pattern of having
+/// unit tests in the `<package>/test` directory. Integration and E2E tests are run
+/// via the package's `example/` application. For Flutter apps, these are driven
+/// by `package:integration_test` whereas Dart packages employ various mechanisms.
+///
+/// This package hosts common code used by integration/e2e tests. Since the example
+/// packages are leaves of the repo's dependency graph, it is safe for them to import
+/// any published package without causing circular dependency errors. Likewise, this
+/// package is free to import any published package from the repo without causing
+/// errors during publishing.
 library amplify_integration_test;
 
 export 'package:amplify_test/amplify_test.dart';
