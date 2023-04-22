2452b075f68f990aa63a36d307a7afc823792b6c
Dillon Nys <dillon.andre.nys@gmail.com>
2023-04-22 13:29:07 -0700

    ci: Add `pub_server` workflow

---
+++ packages/test/pub_server/test/pub_server_test.dart
@@ -0,0 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:test/test.dart';
+
+void main() {
+  test('PubServer', () {});
+}
