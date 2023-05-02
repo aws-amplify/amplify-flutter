e9f10a84d95e2abe75ac46ea2f613c5519e97cc9
Dillon Nys <nydillon@amazon.com>
2023-05-02 15:31:21 -0700

    chore(aft): Remove passthrough
    Buggy and doesn't add much value

--- packages/aft/lib/src/command_runner.dart
+++ packages/aft/lib/src/command_runner.dart
@@ -2,18 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:aft/aft.dart';
-import 'package:aft/src/commands/passthrough_command.dart';
 import 'package:aft/src/commands/save_repo_state_command.dart';
-import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
-import 'package:collection/collection.dart';
-
-/// Passes through the command specified by [args] to either `dart`/`flutter`
-/// depending on the package in the current working directory.
-Future<void> passthrough(List<String> args) async {
-  final passthroughCommand = PassthroughCommand(args);
-  return passthroughCommand.run();
-}
 
 /// Runs the `aft` command using the given [args].
 Future<void> run(List<String> args) async {
@@ -49,25 +39,8 @@ Future<void> run(List<String> args) async {
     ..addCommand(RunCommand());
 
   try {
-    try {
-      final argResults = runner.argParser.parse(args);
-      // If we cannot resolve a command, try a passthrough to `dart`/`flutter`.
-      if (argResults.command == null) {
-        if (argResults.arguments.firstOrNull?.startsWith('--') ?? false) {
-          runner.printUsage();
-          return;
-        }
-        return await passthrough(argResults.arguments);
-      }
-      await runner.runCommand(argResults);
-    } on ArgParserException {
-      // If we fail to parse the arguments, also passthrough the command since
-      // adding flags or options will trigger a parse failure.
-      if (args.isNotEmpty) {
-        return await passthrough(args);
-      }
-      rethrow;
-    }
+    final argResults = runner.argParser.parse(args);
+    await runner.runCommand(argResults);
   } finally {
     // Free up resources before exiting..
     for (final command in runner.commands.values.whereType<AmplifyCommand>()) {
