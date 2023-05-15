4ce92552119eb9e93ac5966ac6e3538b0752308d
Jordan Nelson <Jordanryannelson@gmail.com>
2023-05-15 16:00:00 -0400

    chore: update example apps to use material 3

--- packages/auth/amplify_auth_cognito/example/lib/main.dart
+++ packages/auth/amplify_auth_cognito/example/lib/main.dart
@@ -129,9 +129,8 @@ class _MyAppState extends State<MyApp> {
       child: MaterialApp.router(
         title: 'Flutter Demo',
         builder: Authenticator.builder(),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        theme: ThemeData.light(useMaterial3: true),
+        darkTheme: ThemeData.dark(useMaterial3: true),
         routeInformationParser: _router.routeInformationParser,
         routerDelegate: _router.routerDelegate,
         debugShowCheckedModeBanner: false,
