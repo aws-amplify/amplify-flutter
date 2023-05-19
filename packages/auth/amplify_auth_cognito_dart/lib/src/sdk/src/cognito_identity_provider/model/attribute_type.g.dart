6b819d9b58f3fef1ce04d090d4f73c6cced31a15
Dillon Nys <nydillon@amazon.com>
2023-05-19 09:14:25 -0700

    chore(auth): Run `build_runner`

--- packages/auth/amplify_auth_cognito_dart/lib/src/sdk/src/cognito_identity_provider/model/attribute_type.g.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/sdk/src/cognito_identity_provider/model/attribute_type.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of amplify_auth_cognito_dart.cognito_identity_provider.model.attribute_type;
+part of 'attribute_type.dart';
 
 // **************************************************************************
 // BuiltValueGenerator
--- packages/auth/amplify_auth_cognito_dart/lib/src/sdk/src/cognito_identity_provider/model/forbidden_exception.g.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/sdk/src/cognito_identity_provider/model/forbidden_exception.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of amplify_auth_cognito_dart.cognito_identity_provider.model.forbidden_exception;
+part of 'forbidden_exception.dart';
 
 // **************************************************************************
 // BuiltValueGenerator
