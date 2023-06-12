f09052ea991f5e380953b68ce9b44755c9bac6a1
Travis Sheppard <tshepp@amazon.com>
2023-06-12 13:53:44 -0700

    check again

--- packages/analytics/amplify_analytics_pinpoint_dart/test/queued_item_store_test.dart
+++ packages/analytics/amplify_analytics_pinpoint_dart/test/queued_item_store_test.dart
@@ -1,27 +1,39 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
 import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/event_client/queued_item_store/dart_queued_item_store.dart';
 import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/event_client/queued_item_store/queued_item_store.dart';
-import 'package:amplify_core/amplify_core.dart';
 import 'package:test/test.dart';
 
 void main() {
-  late DartQueuedItemStore db;
-
-  final id = uuid();
+  final db = DartQueuedItemStore('/tmp');
 
   group('DartQueuedItemStore ', () {
-    setUpAll(() {
-      db = DartQueuedItemStore('/tmp');
-    });
-
     tearDownAll(() async {
       await db.clear();
     });
 
     setUp(() async {
-      await db.clear();
+      print('start clearing');
+      const attempts = [0, 1, 2];
+      final items = await db.getCount(100);
+      if (items.isEmpty) {
+        print('nothing in db, stop now');
+        return;
+      }
+      for (final attempt in attempts) {
+        try {
+          await db.clear();
+          break;
+        } on TimeoutException {
+          if (attempt == 2) rethrow;
+        }
+      }
+      print('done clearing');
     });
 
     Future<Iterable<QueuedItem>> getAll() async {
@@ -30,24 +42,17 @@ void main() {
 
     test('writes values to storage', () async {
       const values = ['0', '1', '2', '3', '4', '5'];
-      final testValues = values.map((e) => '$id$e');
-      for (final value in testValues) {
+      for (final value in values) {
         await db.addItem(value);
       }
+
       final readItems = await db.getCount(values.length);
       final readValues = readItems.map((e) => e.value);
-      for (final read in readValues) {
-        if (!read.contains(id)) {
-          fail('VALUE DOES NOT CONTAIN EXPECTED TEST ID $id. Actual $read');
-        }
-      }
-      expect(readValues, equals(testValues));
+      expect(readValues, equals(values));
     });
 
     test('returns first n items in storage', () async {
-      const values0 = ['0', '1', '2', '3', '4', '5'];
-      final values = values0.map((e) => '$id$e').toList();
-      // const values = ['0', '1', '2', '3', '4', '5'];
+      const values = ['a', 'b', 'c', 'd', 'e', 'f'];
       for (final value in values) {
         await db.addItem(value);
       }
@@ -60,9 +65,7 @@ void main() {
     test(
         'returns all stored items when get request size exceeds stored item count',
         () async {
-      const values0 = ['0', '1', '2', '3', '4', '5'];
-      final values = values0.map((e) => '$id$e').toList();
-      // const values = ['0', '1', '2', '3', '4', '5'];
+      const values = ['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta'];
       for (final value in values) {
         await db.addItem(value);
       }
@@ -73,9 +76,7 @@ void main() {
     });
 
     test('deletes all items in storage', () async {
-      const values0 = ['0', '1', '2', '3', '4', '5'];
-      final values = values0.map((e) => '$id$e').toList();
-      // const values = ['0', '1', '2', '3', '4', '5'];
+      const values = ['0a', '1b', '2c', '3d', '4e', '5f'];
       for (final value in values) {
         await db.addItem(value);
       }
@@ -86,133 +87,134 @@ void main() {
       expect(readItems, isEmpty);
     });
 
-    // test('deletes first subset of stored items', () async {
-    //   const _values = ['0', '1', '2', '3', '4', '5'];
-    //   final values = _values.map((e) => '$id$e').toList();
-    //   // const values = ['0', '1', '2', '3', '4', '5'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes first subset of stored items', () async {
+      const values = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine'
+      ];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   await db.deleteItems(await db.getCount(3));
+      await db.deleteItems(await db.getCount(3));
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, values.sublist(3));
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, values.sublist(3));
+    });
 
-    // test('deletes middle subset of stored items', () async {
-    //   const _values = ['0', '1', '2', '3', '4', '5'];
-    //   final values = _values.map((e) => '$id$e').toList();
-    //   // const values = ['0', '1', '2', '3', '4', '5'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes middle subset of stored items', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   final itemsToDelete = (await getAll()).toList().sublist(3, 7);
-    //   await db.deleteItems(itemsToDelete);
+      final itemsToDelete = (await getAll()).toList().sublist(3, 7);
+      await db.deleteItems(itemsToDelete);
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, const ['0', '1', '2', '7', '8', '9']);
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, const ['0', '1', '2', '7', '8', '9']);
+    });
 
-    // test('deletes last subset of stored items', () async {
-    //   const _values = ['0', '1', '2', '3', '4', '5'];
-    //   final values = _values.map((e) => '$id$e').toList();
-    //   // const values = ['0', '1', '2', '3', '4', '5'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes last subset of stored items', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   final itemsToDelete = (await getAll()).toList().sublist(7);
-    //   await db.deleteItems(itemsToDelete);
+      final itemsToDelete = (await getAll()).toList().sublist(7);
+      await db.deleteItems(itemsToDelete);
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, values.sublist(0, 7));
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, values.sublist(0, 7));
+    });
 
-    // test('deletes first, middle, and last subsets of stored items', () async {
-    //   const _values = ['0', '1', '2', '3', '4', '5'];
-    //   final values = _values.map((e) => '$id$e').toList();
-    //   // const values = ['0', '1', '2', '3', '4', '5'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes first, middle, and last subsets of stored items', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   final valuesToDelete = ['0', '1', '4', '5', '8', '9'].map((e) => '$id$e').toList();
-    //   final itemsToDelete = (await getAll()).where((item) {
-    //     return valuesToDelete.contains(item.value);
-    //   });
-    //   await db.deleteItems(itemsToDelete);
+      const valuesToDelete = ['0', '1', '4', '5', '8', '9'];
+      final itemsToDelete = (await getAll()).where((item) {
+        return valuesToDelete.contains(item.value);
+      });
+      await db.deleteItems(itemsToDelete);
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, const ['2', '3', '6', '7']);
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, const ['2', '3', '6', '7']);
+    });
 
-    // test('deletes inner left and right subsets of stored items', () async {
-    //   const _values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    //   final values = _values.map((e) => '$id$e').toList();
-    //   // const values = ['0', '1', '2', '3', '4', '5'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes inner left and right subsets of stored items', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   const valuesToDelete = ['1', '2', '4', '5', '7', '8'];
-    //   final itemsToDelete = (await getAll()).where((item) {
-    //     return valuesToDelete.contains(item.value);
-    //   });
-    //   await db.deleteItems(itemsToDelete);
+      const valuesToDelete = ['1', '2', '4', '5', '7', '8'];
+      final itemsToDelete = (await getAll()).where((item) {
+        return valuesToDelete.contains(item.value);
+      });
+      await db.deleteItems(itemsToDelete);
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, const ['0', '3', '6', '9']);
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, const ['0', '3', '6', '9']);
+    });
 
-    // test('deletes the first stored item', () async {
-    //   const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes the first stored item', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   final itemsToDelete = (await getAll()).toList().sublist(0, 1);
-    //   await db.deleteItems(itemsToDelete);
+      final itemsToDelete = (await getAll()).toList().sublist(0, 1);
+      await db.deleteItems(itemsToDelete);
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, values.sublist(1));
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, values.sublist(1));
+    });
 
-    // test('deletes the last stored item', () async {
-    //   const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('deletes the last stored item', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   final itemsToDelete = (await getAll()).toList().sublist(9);
-    //   await db.deleteItems(itemsToDelete);
+      final itemsToDelete = (await getAll()).toList().sublist(9);
+      await db.deleteItems(itemsToDelete);
 
-    //   final readItems = await getAll();
-    //   final readValues = readItems.map((e) => e.value);
-    //   expect(readValues, values.sublist(0, 9));
-    // });
+      final readItems = await getAll();
+      final readValues = readItems.map((e) => e.value);
+      expect(readValues, values.sublist(0, 9));
+    });
 
-    // test('throws no error when deleting all items twice', () async {
-    //   const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    //   for (final value in values) {
-    //     await db.addItem(value);
-    //   }
+    test('throws no error when deleting all items twice', () async {
+      const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (final value in values) {
+        await db.addItem(value);
+      }
 
-    //   var readValues = await getAll();
+      var readValues = await getAll();
 
-    //   await db.deleteItems(readValues);
-    //   await db.deleteItems(readValues);
+      await db.deleteItems(readValues);
+      await db.deleteItems(readValues);
 
-    //   readValues = await getAll();
+      readValues = await getAll();
 
-    //   expect(readValues, isEmpty);
-    // });
+      expect(readValues, isEmpty);
+    });
   });
 }
