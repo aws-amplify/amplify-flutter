4f93f13bf2f013ab6f9a618c6f52c23e96d28aac
Dillon Nys <nydillon@amazon.com>
2023-06-14 13:35:20 -0700

    chore(analytics): Fix tests

--- packages/analytics/amplify_analytics_pinpoint_dart/test/analytics_plugin_config_test.dart
+++ packages/analytics/amplify_analytics_pinpoint_dart/test/analytics_plugin_config_test.dart
@@ -1,11 +1,61 @@
 import 'package:amplify_analytics_pinpoint_dart/src/analytics_plugin_impl.dart';
+import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/analytics_client.dart';
+import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/endpoint_client/endpoint_client.dart';
+import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/event_client/event_client.dart';
+import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/event_client/queued_item_store/queued_item_store.dart';
 import 'package:amplify_analytics_pinpoint_dart/src/impl/flutter_provider_interfaces/cached_events_path_provider.dart';
+import 'package:amplify_analytics_pinpoint_dart/src/sdk/src/pinpoint/model/session.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'common/mock_secure_storage.dart';
 import 'common/mocktail_mocks.dart';
+
+class MockAnalyticsClient implements AnalyticsClient {
+  @override
+  EndpointClient endpointClient = MockEndpointClient();
+
+  @override
+  EventClient eventClient = MockEventClient();
+
+  @override
+  Future<void> init({
+    required String pinpointAppId,
+    required String region,
+    required AWSCredentialsProvider authProvider,
+    QueuedItemStore? eventStore,
+  }) async {}
+}
+
+class MockEndpointClient extends Mock implements EndpointClient {
+  @override
+  String get fixedEndpointId => uuid();
+
+  @override
+  Future<void> updateEndpoint() async {}
+}
+
+class MockEventClient implements EventClient {
+  @override
+  Future<void> close() async {}
+
+  @override
+  Future<void> flushEvents() async {}
+
+  @override
+  Future<void> recordEvent({
+    required String eventType,
+    Session? session,
+    CustomProperties? properties,
+  }) async {}
+
+  @override
+  void registerGlobalProperties(CustomProperties globalProperties) {}
+
+  @override
+  void unregisterGlobalProperties(List<String> propertyNames) {}
+}
 
 void main() {
   group('Analytics Config eventFlushInterval ', () {
@@ -25,6 +75,8 @@ void main() {
       when(mockAuthProvider.retrieve).thenAnswer(
         (_) async => const AWSCredentials('accessKeyId', 'secretAccessKey'),
       );
+
+      Amplify.dependencies.addInstance<AnalyticsClient>(MockAnalyticsClient());
     });
 
     test('throws ConfigurationError when negative', () async {
