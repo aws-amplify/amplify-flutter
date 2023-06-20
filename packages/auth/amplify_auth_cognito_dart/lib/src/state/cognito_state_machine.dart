80007ff2956e3040ce5080a07ae88d70a115aab2
Dillon Nys <nydillon@amazon.com>
2023-06-20 16:21:37 -0700

    chore(auth): Disable ASF

--- packages/auth/amplify_auth_cognito_dart/lib/src/state/cognito_state_machine.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/state/cognito_state_machine.dart
@@ -35,8 +35,6 @@ final defaultDependencies = <Token, DependencyBuilder>{
       AuthPluginCredentialsProviderImpl.new,
   const Token<DeviceMetadataRepository>():
       DeviceMetadataRepository.fromDependencies,
-  const Token<ASFDeviceInfoCollector>(): (_) => ASFDeviceInfoCollector(),
-  const Token<ASFContextDataProvider>(): ASFContextDataProvider.new,
 };
 
 /// {@template amplify_auth_cognito.cognito_auth_state_machine}
