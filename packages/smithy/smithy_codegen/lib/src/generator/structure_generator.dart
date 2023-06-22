662c413bfbae777ae7a0bf4095960cfc4e47bbeb
Dillon Nys <nydillon@amazon.com>
2023-06-22 11:51:06 -0700

    chore(smithy): Various cleanup
    Various cleanups which improves generated code to conform to more lints.

--- packages/smithy/smithy_codegen/lib/src/generator/structure_generator.dart
+++ packages/smithy/smithy_codegen/lib/src/generator/structure_generator.dart
@@ -584,7 +584,9 @@ class StructureGenerator extends LibraryGenerator<StructureShape>
         (f) => f
           ..static = true
           ..modifier = FieldModifier.constant
-          ..type = DartTypes.core.list(DartTypes.smithy.smithySerializer())
+          ..type = DartTypes.core.list(
+            DartTypes.smithy.smithySerializer(payloadSymbol),
+          )
           ..name = 'serializers'
           ..assignment = literalList(
             serializerClasses.map((name) => refer(name).newInstance([])),
@@ -920,12 +922,8 @@ class StructureGenerator extends LibraryGenerator<StructureShape>
   Method _toString({required bool isPayload}) {
     final builder = BlockBuilder();
     final helper = refer('helper');
-    builder.addExpression(
-      declareFinal('helper').assign(
-        DartTypes.builtValue.newBuiltValueToStringHelper
-            .call([literalString(isPayload ? payloadClassName! : className)]),
-      ),
-    );
+    var helperExpression = DartTypes.builtValue.newBuiltValueToStringHelper
+        .call([literalString(isPayload ? payloadClassName! : className)]);
     final members = isPayload ? payloadMembers : this.members;
     for (final member in members) {
       final dartName = member.dartName(ShapeType.structure);
@@ -934,14 +932,16 @@ class StructureGenerator extends LibraryGenerator<StructureShape>
           context.shapeFor(member.target).hasTrait<SensitiveTrait>();
       final stringValue =
           isSensitive ? literalString('***SENSITIVE***') : refer(dartName);
-      builder.addExpression(
-        helper.property('add').call([
-          literalString(dartName),
-          stringValue,
-        ]),
-      );
+      helperExpression = helperExpression.cascade('add').call([
+        literalString(dartName),
+        stringValue,
+      ]);
     }
-    builder.addExpression(helper.property('toString').call([]).returned);
+    builder
+      ..addExpression(
+        declareFinal('helper').assign(helperExpression),
+      )
+      ..addExpression(helper.property('toString').call([]).returned);
     return Method(
       (m) => m
         ..annotations.add(DartTypes.core.override)
