b0e8c043ff0b040b3f4f7b6c7d1ae4f2805ab457
Nika Hassani <nikaws@amazon.com>
2023-07-19 16:20:25 -0700

    update sdk.yaml file to remove unused apis

--- packages/aws_logging_cloudwatch/lib/src/sdk/src/cloud_watch_logs/cloud_watch_logs_client.dart
+++ packages/aws_logging_cloudwatch/lib/src/sdk/src/cloud_watch_logs/cloud_watch_logs_client.dart
@@ -6,28 +6,15 @@ library aws_logging_cloudwatch.cloud_watch_logs.cloud_watch_logs_client; // igno
 import 'package:aws_common/aws_common.dart' as _i1;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/create_log_stream_request.dart'
     as _i4;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/describe_log_groups_request.dart'
-    as _i8;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/describe_log_streams_request.dart'
-    as _i11;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/log_group.dart'
-    as _i7;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/log_stream.dart'
-    as _i10;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/put_log_events_request.dart'
-    as _i14;
+    as _i7;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/put_log_events_response.dart'
-    as _i13;
+    as _i6;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/operation/create_log_stream_operation.dart'
     as _i5;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/operation/describe_log_groups_operation.dart'
-    as _i9;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/operation/describe_log_streams_operation.dart'
-    as _i12;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/operation/put_log_events_operation.dart'
-    as _i15;
+    as _i8;
 import 'package:aws_signature_v4/aws_signature_v4.dart' as _i2;
-import 'package:built_collection/built_collection.dart' as _i6;
 import 'package:smithy/smithy.dart' as _i3;
 
 /// You can use Amazon CloudWatch Logs to monitor, store, and access your log files from EC2 instances, CloudTrail, and other sources. You can then retrieve the associated log data from CloudWatch Logs using the CloudWatch console. Alternatively, you can use CloudWatch Logs commands in the Amazon Web Services CLI, CloudWatch Logs API, or CloudWatch Logs SDK.
@@ -104,56 +91,6 @@ class CloudWatchLogsClient {
     );
   }
 
-  /// Lists the specified log groups. You can list all your log groups or filter the results by prefix. The results are ASCII-sorted by log group name.
-  ///
-  /// CloudWatch Logs doesn’t support IAM policies that control access to the `DescribeLogGroups` action by using the `aws:ResourceTag/_key-name_` condition key. Other CloudWatch Logs actions do support the use of the `aws:ResourceTag/_key-name_` condition key to control access. For more information about using tags to control access, see [Controlling access to Amazon Web Services resources using tags](https://docs.aws.amazon.com/IAM/latest/UserGuide/access_tags.html).
-  ///
-  /// If you are using CloudWatch cross-account observability, you can use this operation in a monitoring account and view data from the linked source accounts. For more information, see [CloudWatch cross-account observability](https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Unified-Cross-Account.html).
-  _i3.SmithyOperation<
-          _i3.PaginatedResult<_i6.BuiltList<_i7.LogGroup>, int, String>>
-      describeLogGroups(
-    _i8.DescribeLogGroupsRequest input, {
-    _i1.AWSHttpClient? client,
-    _i2.AWSCredentialsProvider? credentialsProvider,
-  }) {
-    return _i9.DescribeLogGroupsOperation(
-      region: _region,
-      baseUri: _baseUri,
-      credentialsProvider: credentialsProvider ?? _credentialsProvider,
-      requestInterceptors: _requestInterceptors,
-      responseInterceptors: _responseInterceptors,
-    ).runPaginated(
-      input,
-      client: client ?? _client,
-    );
-  }
-
-  /// Lists the log streams for the specified log group. You can list all the log streams or filter the results by prefix. You can also control how the results are ordered.
-  ///
-  /// You can specify the log group to search by using either `logGroupIdentifier` or `logGroupName`. You must include one of these two parameters, but you can't include both.
-  ///
-  /// This operation has a limit of five transactions per second, after which transactions are throttled.
-  ///
-  /// If you are using CloudWatch cross-account observability, you can use this operation in a monitoring account and view data from the linked source accounts. For more information, see [CloudWatch cross-account observability](https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Unified-Cross-Account.html).
-  _i3.SmithyOperation<
-          _i3.PaginatedResult<_i6.BuiltList<_i10.LogStream>, int, String>>
-      describeLogStreams(
-    _i11.DescribeLogStreamsRequest input, {
-    _i1.AWSHttpClient? client,
-    _i2.AWSCredentialsProvider? credentialsProvider,
-  }) {
-    return _i12.DescribeLogStreamsOperation(
-      region: _region,
-      baseUri: _baseUri,
-      credentialsProvider: credentialsProvider ?? _credentialsProvider,
-      requestInterceptors: _requestInterceptors,
-      responseInterceptors: _responseInterceptors,
-    ).runPaginated(
-      input,
-      client: client ?? _client,
-    );
-  }
-
   /// Uploads a batch of log events to the specified log stream.
   ///
   /// The sequence token is now ignored in `PutLogEvents` actions. `PutLogEvents` actions are always accepted and never return `InvalidSequenceTokenException` or `DataAlreadyAcceptedException` even if the sequence token is not valid. You can use parallel `PutLogEvents` actions on the same log stream.
@@ -176,12 +113,12 @@ class CloudWatchLogsClient {
   ///
   ///
   /// If a call to `PutLogEvents` returns "UnrecognizedClientException" the most likely cause is a non-valid Amazon Web Services access key ID or secret key.
-  _i3.SmithyOperation<_i13.PutLogEventsResponse> putLogEvents(
-    _i14.PutLogEventsRequest input, {
+  _i3.SmithyOperation<_i6.PutLogEventsResponse> putLogEvents(
+    _i7.PutLogEventsRequest input, {
     _i1.AWSHttpClient? client,
     _i2.AWSCredentialsProvider? credentialsProvider,
   }) {
-    return _i15.PutLogEventsOperation(
+    return _i8.PutLogEventsOperation(
       region: _region,
       baseUri: _baseUri,
       credentialsProvider: credentialsProvider ?? _credentialsProvider,
--- packages/aws_logging_cloudwatch/lib/src/sdk/src/cloud_watch_logs/common/serializers.dart
+++ packages/aws_logging_cloudwatch/lib/src/sdk/src/cloud_watch_logs/common/serializers.dart
@@ -6,35 +6,19 @@ library aws_logging_cloudwatch.cloud_watch_logs.common.serializers; // ignore_fo
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/create_log_stream_request.dart'
     as _i2;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/data_already_accepted_exception.dart'
-    as _i19;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/data_protection_status.dart'
-    as _i8;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/describe_log_groups_request.dart'
-    as _i7;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/describe_log_groups_response.dart'
-    as _i10;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/describe_log_streams_request.dart'
-    as _i12;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/describe_log_streams_response.dart'
-    as _i14;
+    as _i11;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/input_log_event.dart'
-    as _i15;
+    as _i7;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/invalid_parameter_exception.dart'
     as _i3;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/invalid_sequence_token_exception.dart'
-    as _i20;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/log_group.dart'
-    as _i9;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/log_stream.dart'
-    as _i13;
-import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/order_by.dart'
-    as _i11;
+    as _i12;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/put_log_events_request.dart'
-    as _i16;
+    as _i8;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/put_log_events_response.dart'
-    as _i18;
+    as _i10;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/rejected_log_events_info.dart'
-    as _i17;
+    as _i9;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/resource_already_exists_exception.dart'
     as _i4;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/resource_not_found_exception.dart'
@@ -42,8 +26,8 @@ import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/resour
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/service_unavailable_exception.dart'
     as _i6;
 import 'package:aws_logging_cloudwatch/src/sdk/src/cloud_watch_logs/model/unrecognized_client_exception.dart'
-    as _i21;
-import 'package:built_collection/built_collection.dart' as _i22;
+    as _i13;
+import 'package:built_collection/built_collection.dart' as _i14;
 import 'package:built_value/serializer.dart';
 import 'package:smithy/smithy.dart' as _i1;
 
@@ -53,37 +37,17 @@ const List<_i1.SmithySerializer> serializers = [
   ..._i4.ResourceAlreadyExistsException.serializers,
   ..._i5.ResourceNotFoundException.serializers,
   ..._i6.ServiceUnavailableException.serializers,
-  ..._i7.DescribeLogGroupsRequest.serializers,
-  ..._i8.DataProtectionStatus.serializers,
-  ..._i9.LogGroup.serializers,
-  ..._i10.DescribeLogGroupsResponse.serializers,
-  ..._i11.OrderBy.serializers,
-  ..._i12.DescribeLogStreamsRequest.serializers,
-  ..._i13.LogStream.serializers,
-  ..._i14.DescribeLogStreamsResponse.serializers,
-  ..._i15.InputLogEvent.serializers,
-  ..._i16.PutLogEventsRequest.serializers,
-  ..._i17.RejectedLogEventsInfo.serializers,
-  ..._i18.PutLogEventsResponse.serializers,
-  ..._i19.DataAlreadyAcceptedException.serializers,
-  ..._i20.InvalidSequenceTokenException.serializers,
-  ..._i21.UnrecognizedClientException.serializers,
+  ..._i7.InputLogEvent.serializers,
+  ..._i8.PutLogEventsRequest.serializers,
+  ..._i9.RejectedLogEventsInfo.serializers,
+  ..._i10.PutLogEventsResponse.serializers,
+  ..._i11.DataAlreadyAcceptedException.serializers,
+  ..._i12.InvalidSequenceTokenException.serializers,
+  ..._i13.UnrecognizedClientException.serializers,
 ];
 final Map<FullType, Function> builderFactories = {
   const FullType(
-    _i22.BuiltList,
-    [FullType(_i9.LogGroup)],
-  ): _i22.ListBuilder<_i9.LogGroup>.new,
-  const FullType(
-    _i22.BuiltList,
-    [FullType(String)],
-  ): _i22.ListBuilder<String>.new,
-  const FullType(
-    _i22.BuiltList,
-    [FullType(_i13.LogStream)],
-  ): _i22.ListBuilder<_i13.LogStream>.new,
-  const FullType(
-    _i22.BuiltList,
-    [FullType(_i15.InputLogEvent)],
-  ): _i22.ListBuilder<_i15.InputLogEvent>.new,
+    _i14.BuiltList,
+    [FullType(_i7.InputLogEvent)],
+  ): _i14.ListBuilder<_i7.InputLogEvent>.new
 };
