2e9420c2aa5e18c72baa02e77e86b0bacadc568a
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-08-07 15:01:26 -0700

    chore: changed remoteconfig to typescript but still need types

---
+++ infra/lib/logging/resources/lambda/remoteconfig.ts
@@ -0,0 +1,52 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3"
+
+const s3 = new S3Client({})
+const bucketName = process.env.BUCKET
+const key = process.env.KEY
+let cachedConfig = {
+    expiresOn: 0, 
+    ETag: '', 
+    config: ''
+}
+
+export async function main (event: any, context: any): Promise<any> {
+  try {
+    if (event.httpMethod === "GET") {
+      if (!cachedConfig.config || Date.now() > cachedConfig.expiresOn) {
+        // refresh cache if cache is invalid
+        const command = new GetObjectCommand({ Bucket: bucketName, Key: key })
+        const s3Resp = await s3.send(command)
+        await setCachedConfig(s3Resp)
+      }
+
+      if (event.headers['If-None-Match'] === cachedConfig.ETag) {
+        // return 304 not modified if config has not changed
+        return {
+          statusCode: 304,
+        }
+      } else {
+        // return updated/modified config with latest ETag
+        return {
+          statusCode: 200,
+          headers: {"'If-None-Match'": cachedConfig.ETag },
+          body: cachedConfig.config
+        }
+      }
+    }
+  } catch(error: any) {
+    const resp = error.stack || JSON.stringify(error, null, 2)
+    return {
+      statusCode: 400,
+        headers: {},
+        body: JSON.stringify(resp)
+    }
+  }
+}
+
+const setCachedConfig = async (s3Resp: any) => {
+  cachedConfig = {
+    expiresOn: Date.now() + 600 * 1000, //10 minutes
+    ETag: s3Resp.ETag.replace(/\"/gi, ''), //remove \" from string
+    config: await s3Resp.Body.transformToString()
+  }
+}
