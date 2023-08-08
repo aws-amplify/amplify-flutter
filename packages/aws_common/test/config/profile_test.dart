bd5e0fb85ecd9da2e4bc500356f1cbaaa871a6b5
Dillon Nys <nydillon@amazon.com>
2023-08-08 11:24:26 -0700

    chore(aws_common): Add missing license headers

--- packages/aws_common/test/config/profile_test.dart
+++ packages/aws_common/test/config/profile_test.dart
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // Generated with tool/generate_tests.dart. Do not modify by hand.
 
 import 'dart:convert';
