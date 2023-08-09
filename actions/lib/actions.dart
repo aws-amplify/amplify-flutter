23b7495653e8c93e5eca526dcc955259338cd791
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-08-08 17:02:46 -0700

    ci: Introduce Dart-based GH Action package (#3475)

---
+++ actions/lib/actions.dart
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Dart wrapper for the GitHub Actions Node.js modules.
+library;
+
+export 'src/node/actions/core.dart';
+export 'src/node/actions/exec.dart';
+export 'src/node/fs.dart';
