b2b9c99ea20e8bd12f20da6764545066ccfd2cd9
Travis Sheppard <tshepp@amazon.com>
2023-08-10 15:28:13 -0700

    chore: social buttons example

--- packages/authenticator/amplify_authenticator/example/lib/main.dart
+++ packages/authenticator/amplify_authenticator/example/lib/main.dart
@@ -3,6 +3,7 @@
 
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_authenticator/src/widgets/social/social_button.dart';
 import 'package:amplify_authenticator_example/resolvers/localized_button_resolver.dart';
 import 'package:amplify_authenticator_example/resolvers/localized_dial_code_resolver.dart';
 import 'package:amplify_authenticator_example/resolvers/localized_input_resolver.dart';
@@ -93,37 +94,71 @@ class _MyAppState extends State<MyApp> {
     // the user is signed in, the Authenticator will use your MaterialApp's
     // navigator to show the correct screen.
     return Authenticator(
-      stringResolver: stringResolver,
-      onException: (exception) {
-        print('[ERROR]: $exception');
+      authenticatorBuilder: (BuildContext context, AuthenticatorState state) {
+        switch (state.currentStep) {
+          // case AuthenticatorStep.signIn:
+          //   return CustomScaffold(
+          //     state: state,
+          //     // A prebuilt Sign In form from amplify_authenticator
+          //     body: SignInForm(),
+          //     // A custom footer with a button to take the user to sign up
+          //     footer: Row(
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: [
+          //         const Text('Don\'t have an account?'),
+          //         TextButton(
+          //           onPressed: () => state.changeStep(
+          //             AuthenticatorStep.signUp,
+          //           ),
+          //           child: const Text('Sign Up'),
+          //         ),
+          //       ],
+          //     ),
+          //   );
+          case AuthenticatorStep.signUp:
+            return CustomScaffold(
+              state: state,
+              body: Column(
+                children: [
+                  SignUpForm(),
+                  const SizedBox(height: 24),
+                  const Divider(
+                    height: 1,
+                  ),
+                  const SizedBox(height: 24),
+                  const SocialSignInButtons(
+                    providers: [
+                      AuthProvider.apple,
+                      AuthProvider.google,
+                      AuthProvider.facebook,
+                    ],
+                  ),
+                ],
+              ),
+            );
+          // case AuthenticatorStep.confirmSignUp:
+          //   return CustomScaffold(
+          //     state: state,
+          //     // A prebuilt Confirm Sign Up form from amplify_authenticator
+          //     body: ConfirmSignUpForm(),
+          //   );
+          // case AuthenticatorStep.resetPassword:
+          //   return CustomScaffold(
+          //     state: state,
+          //     // A prebuilt Reset Password form from amplify_authenticator
+          //     body: ResetPasswordForm(),
+          //   );
+          // case AuthenticatorStep.confirmResetPassword:
+          //   return CustomScaffold(
+          //     state: state,
+          //     // A prebuilt Confirm Reset Password form from amplify_authenticator
+          //     body: const ConfirmResetPasswordForm(),
+          //   );
+          default:
+            // Returning null defaults to the prebuilt authenticator for all other steps
+            return null;
+        }
       },
-
-      // Next, we create a custom Sign Up form which uses our custom username
-      // validator.
-      //
-      // Providing a custom SignUpForm allows for simple customizations such as
-      // adding a sign up attribute or adding a custom validator. More complex
-      // customizations can be achieved by providing a custom builder method to
-      // Authenticator.builder()
-      signUpForm: SignUpForm.custom(
-        fields: [
-          SignUpFormField.username(
-            validator: _validateUsername,
-          ),
-          SignUpFormField.email(required: true),
-          SignUpFormField.password(),
-          SignUpFormField.passwordConfirmation(),
-          SignUpFormField.address(),
-          SignUpFormField.custom(
-            title: 'Bio',
-            attributeKey: const CognitoUserAttributeKey.custom('bio'),
-          ),
-          SignUpFormField.custom(
-            title: 'Age',
-            attributeKey: const CognitoUserAttributeKey.custom('age'),
-          ),
-        ],
-      ),
 
       // Your MaterialApp should be the child of the Authenticator.
       child: MaterialApp(
@@ -268,6 +303,45 @@ class RouteB extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+/// A widget that displays a logo, a body, and an optional footer.
+class CustomScaffold extends StatelessWidget {
+  const CustomScaffold({
+    super.key,
+    required this.state,
+    required this.body,
+    this.footer,
+  });
+
+  final AuthenticatorState state;
+  final Widget body;
+  final Widget? footer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // App logo
+              const Padding(
+                padding: EdgeInsets.only(top: 32),
+                child: Center(child: FlutterLogo(size: 100)),
+              ),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: body,
+              ),
+            ],
+          ),
+        ),
+      ),
+      persistentFooterButtons: footer != null ? [footer!] : null,
     );
   }
 }
