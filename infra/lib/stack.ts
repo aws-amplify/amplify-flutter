060eb06478f35fc0490cdea1ada9ea3ed037f01f
Dillon Nys <nydillon@amazon.com>
2023-08-11 15:59:07 -0700

    test(auth): Custom auth device SRP
    Adds an e2e test for using device SRP as part of a custom auth flow.

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -232,6 +232,13 @@ export class AmplifyFlutterIntegStack extends cdk.Stack {
       {
         associateWithWaf,
         type: "FULL",
+        environmentName: "custom-auth-device-srp",
+        customAuth: "WITH_SRP",
+        deviceTracking: deviceTrackingAlways,
+      },
+      {
+        associateWithWaf,
+        type: "FULL",
         environmentName: "with-client-secret",
         withClientSecret: true,
         deviceTracking: deviceTrackingOptIn,
