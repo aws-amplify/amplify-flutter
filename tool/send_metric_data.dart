38fdb15afdb6a34c33d40944c47bd1e8fbf82fc3
kyle <kylechen@amazon.com>
2023-08-11 11:59:10 -0700

    chore: update metric reporting canaries

--- tool/send_metric_data.dart
+++ tool/send_metric_data.dart
@@ -3,34 +3,91 @@
 
 import 'dart:io';
 
-/// Parse and send metric data using AWS CLI
+final testTypes = [
+  'canary',
+  'integration',
+  'unit test',
+];
+
+final frameworkTypes = [
+  'flutter',
+  'dart',
+];
+
+final platformTypes = ['web', 'android', 'ios', 'linux', 'windows'];
+
 void main(List<String> args) {
-  final metricName = args[0];
-  final value = args[1];
-  final dimensions = args.length > 2 ? args[2] : '';
+  final metricName = args[0].trim();
+  final isFailed = args[1] == 'failed';
+  final testType = args[2].trim();
+  var category = args[3].trim();
+  final workflowName = args[4].trim();
+  final framework = args[5].trim();
+  final flutterDartChannel = args[6].trim();
+  final dartVersion = args[7].trim();
+  final flutterVersion = args[8].trim();
+  final dartCompiler = args[9].trim();
+  final platform = args[10].trim();
+  final platformVersion = args[11].trim();
+  final failingStep = args[12].trim();
 
-  final metricNameTrimmed = metricName.trim();
-  final valueTrimmed = value.trim();
-  final dimensionsTrimmed = dimensions.trim();
-
-  final metricNameRegex = RegExp(r'^[a-zA-Z0-9\ \_\-]+$');
-  final valueRegex = RegExp(r'^[-+]?[0-9]+\.?[0-9]*$');
-  final dimensionsRegex = RegExp(r'^([^=,]+=[^=,]+(?:,[^=,]+=[^=,]+)*)?$');
-
-  if (!metricNameRegex.hasMatch(metricNameTrimmed)) {
-    print(
-        'Metric name can only contain alphanumeric characters, space character, -, and _.');
+  if (metricName.isEmpty) {
+    print('Must provide metricName');
     exit(1);
   }
-  if (!valueRegex.hasMatch(valueTrimmed)) {
-    print('Metric value must be a valid number');
+
+  final value = isFailed ? '1' : '0';
+
+  if (testType.isEmpty) {
+    print('Must provide testType dimension');
+    exit(1);
+  } else if (!testTypes.contains(testType)) {
+    print('TestType is not valid: ${testType}');
     exit(1);
   }
-  if (!dimensionsRegex.hasMatch(dimensionsTrimmed)) {
-    print(
-        'Dimensions must be empty or be in format string=string,string=string,...');
+
+  if (category.isEmpty) {
+    print('Must provide category dimension');
+    exit(1);
+  } else if (category.contains('/')) {
+    // For working directory "packages/analytics/amplify_analytics_pinpoint"
+    category = category.split('/')[1];
+  } else if (category.contains('_')) {
+    // For integration test scope "amplify_analytics_pinpoint_example"
+    category = category.split('_')[1];
+  }
+
+  if (workflowName.isEmpty) {
+    print('Must provide workflowName dimension');
     exit(1);
   }
+
+  if (framework.isNotEmpty && !frameworkTypes.contains(framework)) {
+    print('Framework is not valid: ${framework}');
+    exit(1);
+  }
+
+  if (platform.isNotEmpty && !platformTypes.contains(platform)) {
+    print('Platform is not valid: ${platform}');
+    exit(1);
+  }
+
+  final dimensions = {
+    'testType': testType,
+    'category': category,
+    'workflowName': workflowName,
+    if (framework.isNotEmpty) 'framework': framework,
+    if (flutterDartChannel.isNotEmpty) 'flutterDartChannel': flutterDartChannel,
+    if (dartVersion.isNotEmpty) 'dartVersion': dartVersion,
+    if (flutterVersion.isNotEmpty) 'flutterVersion': flutterVersion,
+    if (dartCompiler.isNotEmpty) 'dartCompiler': dartCompiler,
+    if (platform.isNotEmpty) 'platform': platform,
+    if (platformVersion.isNotEmpty) 'platformVersion': platformVersion,
+    if (failingStep.isNotEmpty) 'failingStep': failingStep,
+  };
+
+  final dimensionString =
+      dimensions.entries.map((e) => '${e.key}=${e.value}').join(',');
 
   final cloudArgs = [
     'cloudwatch',
@@ -41,12 +98,9 @@ void main(List<String> args) {
     'GithubCanaryApps',
     '--value',
     value,
+    '--dimension',
+    dimensionString,
   ];
-
-  if (!dimensionsTrimmed.isEmpty) {
-    cloudArgs.add('--dimensions');
-    cloudArgs.add(dimensionsTrimmed);
-  }
 
   Process.runSync('aws', cloudArgs);
 }
