e3a127078bbf7edb9d3816dd416ea60de5cd0d69
Elijah Quartey <quaelija@amazon.com>
2023-08-11 16:10:32 -0700

    fix: missing ignore

--- packages/authenticator/amplify_authenticator/lib/src/mixins/totp_copy_key_field.dart
+++ packages/authenticator/amplify_authenticator/lib/src/mixins/totp_copy_key_field.dart
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_authenticator/src/widgets/form_field.dart';
 import 'package:flutter/material.dart';
