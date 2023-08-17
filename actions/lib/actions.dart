b9a6c57e2473e95230d7d7184bf5671b85ec767c
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-08-16 17:06:09 -0700

    ci: Add setup-chromedriver action (#3511)
    The action we've been using is very flaky and asks customers to use workarounds constantly.

--- actions/lib/actions.dart
+++ actions/lib/actions.dart
@@ -4,6 +4,11 @@
 /// Dart wrapper for the GitHub Actions Node.js modules.
 library;
 
+export 'src/chromedriver/downloads.dart';
 export 'src/node/actions/core.dart';
 export 'src/node/actions/exec.dart';
+export 'src/node/actions/http_request.dart';
+export 'src/node/actions/tool_cache.dart';
 export 'src/node/fs.dart';
+export 'src/node/process.dart';
+export 'src/os.dart';
