c494e50833dbbcf14f614112ee7f3489ad1841df
kyle <kylechen@amazon.com>
2023-08-23 10:51:01 -0700

    update github token access

--- tool/send_metric_data.dart
+++ tool/send_metric_data.dart
@@ -2,37 +2,113 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'dart:io';
+import 'package:args/args.dart';
 
-/// Parse and send metric data using AWS CLI
+
+final testTypes = [
+  'canary',
+  'integration',
+  'unit test',
+];
+
+final frameworkTypes = [
+  'flutter',
+  'dart',
+];
+
+final platformTypes = ['web', 'android', 'ios', 'linux', 'windows'];
+
 void main(List<String> args) {
-  final metricName = args[0];
-  final value = args[1];
-  final dimensions = args.length > 2 ? args[2] : '';
+  final parser = ArgParser()
+    ..addOption('metric-name')
+    ..addOption('is-failed')
+    ..addOption('testType')
+    ..addOption('category')
+    ..addOption('workflowName')
+    ..addOption('framework')
+    ..addOption('flutterDartChannel')
+    ..addOption('dartVersion')
+    ..addOption('flutterVersion')
+    ..addOption('dartCompiler')
+    ..addOption('platform')
+    ..addOption('platformVersion')
+    ..addOption('failingStep');
 
-  final metricNameTrimmed = metricName.trim();
-  final valueTrimmed = value.trim();
-  final dimensionsTrimmed = dimensions.trim();
+  final results = parser.parse(args);
 
-  final metricNameRegex = RegExp(r'^[a-zA-Z0-9\ \_\-]+$');
-  final valueRegex = RegExp(r'^[-+]?[0-9]+\.?[0-9]*$');
-  final dimensionsRegex = RegExp(r'^([^=,]+=[^=,]+(?:,[^=,]+=[^=,]+)*)?$');
+  final metricName = results['metric-name']?.trim();
+  final isFailed = results['is-failed'] == 'true';
+  final testType = results['testType']?.trim();
+  var category = results['category']?.trim();
+  final workflowName = results['workflowName']?.trim();
+  final framework = results['framework']?.trim();
+  final flutterDartChannel = results['flutterDartChannel']?.trim();
+  final dartVersion = results['dartVersion']?.trim();
+  final flutterVersion = results['flutterVersion']?.trim();
+  final dartCompiler = results['dartCompiler']?.trim();
+  final platform = results['platform']?.trim();
+  final platformVersion = results['platformVersion']?.trim();
+  final failingStep = results['failingStep']?.trim();
 
-  if (!metricNameRegex.hasMatch(metricNameTrimmed)) {
-    print(
-        'Metric name can only contain alphanumeric characters, space character, -, and _.');
+  if (metricName.isEmpty) {
+    print('Must provide metricName');
     exit(1);
   }
-  if (!valueRegex.hasMatch(valueTrimmed)) {
-    print('Metric value must be a valid number');
+
+  final value = isFailed ? '1' : '0';
+
+  if (testType.isEmpty) {
+    print('Must provide testType dimension');
     exit(1);
-  }
-  if (!dimensionsRegex.hasMatch(dimensionsTrimmed)) {
-    print(
-        'Dimensions must be empty or be in format string=string,string=string,...');
+  } else if (!testTypes.contains(testType)) {
+    print('TestType is not valid: ${testType}');
     exit(1);
   }
 
-  final cloudArgs = [
+  if (category.isEmpty) {
+    print('Must provide category dimension');
+    exit(1);
+  } else if (category.contains('/')) {
+    // For working directory "packages/analytics/amplify_analytics_pinpoint"
+    category = category.split('/')[1];
+  } else if (category.contains('_')) {
+    // For integration test scope "amplify_analytics_pinpoint_example"
+    category = category.split('_')[1];
+  }
+
+  if (workflowName.isEmpty) {
+    print('Must provide workflowName dimension');
+    exit(1);
+  }
+
+  if (framework.isNotEmpty && !frameworkTypes.contains(framework)) {
+    print('Framework is not valid: ${framework}');
+    exit(1);
+  }
+
+  if (platform.isNotEmpty && !platformTypes.contains(platform)) {
+    print('Platform is not valid: ${platform}');
+    exit(1);
+  }
+
+  final dimensions = {
+    'testType': testType,
+    'category': category,
+    'workflowName': workflowName,
+    if (framework.isNotEmpty) 'framework': framework,
+    if (flutterDartChannel.isNotEmpty) 'flutterDartChannel': flutterDartChannel,
+    if (dartVersion.isNotEmpty) 'dartVersion': dartVersion,
+    if (flutterVersion.isNotEmpty) 'flutterVersion': flutterVersion,
+    if (dartCompiler.isNotEmpty) 'dartCompiler': dartCompiler,
+    if (platform.isNotEmpty) 'platform': platform,
+    if (platformVersion.isNotEmpty) 'platformVersion': platformVersion,
+    if (failingStep.isNotEmpty) 'failingStep': failingStep,
+  };
+
+  final dimensionString =
+      dimensions.entries.map((e) => '${e.key}=${e.value}').join(',');
+
+  final List<String> cloudArgs = [
     'cloudwatch',
     'put-metric-data',
     '--metric-name',
@@ -41,12 +117,9 @@ void main(List<String> args) {
     'GithubCanaryApps',
     '--value',
     value,
+    '--dimension',
+    dimensionString,
   ];
-
-  if (!dimensionsTrimmed.isEmpty) {
-    cloudArgs.add('--dimensions');
-    cloudArgs.add(dimensionsTrimmed);
-  }
 
   Process.runSync('aws', cloudArgs);
 }
