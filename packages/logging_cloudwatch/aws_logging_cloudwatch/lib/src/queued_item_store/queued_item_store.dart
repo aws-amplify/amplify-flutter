17cdaeea900a31d308603e247a82fdff373168c8
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-08-25 14:48:05 -0700

    chore: fixed a comment

--- packages/logging_cloudwatch/aws_logging_cloudwatch/lib/src/queued_item_store/queued_item_store.dart
+++ packages/logging_cloudwatch/aws_logging_cloudwatch/lib/src/queued_item_store/queued_item_store.dart
@@ -44,12 +44,10 @@ class QueuedItem {
   /// The size of the item, in bytes, when stored in the database.
   int get byteSize => _byteSize;
 
-  /// Gets the size of the item, in bytes, without creating the item.
+  /// Returns the size of an item, in bytes.
   static int getByteSize(String value, String timestamp) {
     return value.length + timestamp.length + 8;
   }
-
-  //TEST COMMENT TO TRIGGER WORKFLOWS
 
   /// The ID in the item.
   final int id;
