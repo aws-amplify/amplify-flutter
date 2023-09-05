802c7c8893019c55ca79069ec6f254328d4cf5ea
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-09-05 08:33:06 -0700

    fix(auth): Uncaught Hosted UI cancellation (#3686)
    * chore(auth): Use `EventCompleter.ignore` instead of `unawaited`
    
    `unawaited` will still raise an error if there is one. By using `EventCompleter.ignore` we drop all completions, success or error.
    
    * fix(auth): Uncaught Hosted UI cancellation
    
    The HostedUIPlatform had a bug due to the fact that it both called `dispatcher.dispatchAndComplete` (which throws on failure states) and throwing a `UserCancelledException` which triggers a failure state. This lead to the error being thrown from `signInWithWebUI` and an uncaught exception being raised at the `dispatchAndComplete` call.
    
    This resolves the issue by only throwing from the HostedUiPlatform and letting the state machine resolving the cancellation.
    
    This also fixes an issue where `cancelSignIn` was never being called because the state machine was in a `signingIn` state at the time the cancellation was dispatched.

--- packages/auth/amplify_auth_cognito/example/integration_test/hosted_ui_webview_test.dart
+++ packages/auth/amplify_auth_cognito/example/integration_test/hosted_ui_webview_test.dart
@@ -13,7 +13,6 @@ import 'package:amplify_auth_cognito_dart/src/flows/hosted_ui/hosted_ui_platform
 import 'package:amplify_auth_cognito_dart/src/model/hosted_ui/oauth_parameters.dart';
 import 'package:amplify_auth_cognito_dart/src/state/cognito_state_machine.dart';
 import 'package:amplify_auth_cognito_dart/src/state/state.dart';
-import 'package:amplify_auth_cognito_test/amplify_auth_cognito_test.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_integration_test/amplify_integration_test.dart';
 import 'package:flutter/material.dart';
@@ -56,21 +55,52 @@ void main() {
         );
       });
 
-      Future<void> signIn(WidgetTester tester) async {
+      Future<void> signIn(
+        WidgetTester tester, {
+        bool cancel = false,
+      }) async {
         stateMachine.addInstance<HostedUiPlatform>(
           HostedUiTestPlatform(
             tester,
             stateMachine,
             username: username,
             password: password,
+            cancel: cancel,
           ),
         );
         _logger.debug('Signing in with Web UI');
-        final result = await plugin.signInWithWebUI(
-          provider: AuthProvider.cognito,
-        );
-        _logger.debug('Signed in with Web UI');
-        expect(result.isSignedIn, isTrue);
+        if (cancel) {
+          final expectation = expectLater(
+            plugin.signInWithWebUI(
+              provider: AuthProvider.cognito,
+            ),
+            throwsA(isA<UserCancelledException>()),
+          );
+          final hostedUiMachine =
+              stateMachine.expect(HostedUiStateMachine.type);
+          expect(
+            hostedUiMachine.stream,
+            emitsInOrder([
+              isA<HostedUiSigningIn>(),
+              isA<HostedUiFailure>().having(
+                (s) => s.exception,
+                'exception',
+                isA<UserCancelledException>(),
+              ),
+              emitsDone,
+            ]),
+          );
+          await expectation;
+          // Ensure queue is flushed and done event is emitted after
+          // signInWithWebUI completes.
+          await hostedUiMachine.close();
+        } else {
+          final result = await plugin.signInWithWebUI(
+            provider: AuthProvider.cognito,
+          );
+          _logger.debug('Signed in with Web UI');
+          expect(result.isSignedIn, isTrue);
+        }
       }
 
       Future<void> signOut({required bool globalSignOut}) async {
@@ -112,14 +142,17 @@ void main() {
         await signOut(globalSignOut: false);
       });
 
+      testWidgets('cancel sign-in', (tester) async {
+        await signIn(tester, cancel: true);
+      });
+
       testWidgets('global sign out', (tester) async {
         await signIn(tester);
         await signOut(globalSignOut: true);
       });
     },
     // Add remaining platforms as `webview_flutter` adds support.
-    // TODO(dnys1): Investigate Android failures in CI on Android 33+
-    skip: zIsWeb || !((Platform.isAndroid && !isCI) || Platform.isIOS),
+    skip: zIsWeb || !(Platform.isAndroid || Platform.isIOS),
   );
 }
 
@@ -129,10 +162,14 @@ class HostedUiTestPlatform extends HostedUiPlatformImpl {
     DependencyManager manager, {
     required this.username,
     required this.password,
+    required this.cancel,
   }) : super(manager);
 
   final String username;
   final String password;
+
+  /// Whether to cancel the sign-in flow once initiated.
+  final bool cancel;
 
   final WidgetTester tester;
   final Completer<WebViewController> _controller = Completer();
@@ -143,6 +180,9 @@ class HostedUiTestPlatform extends HostedUiPlatformImpl {
     required CognitoSignInWithWebUIPluginOptions options,
     AuthProvider? provider,
   }) async {
+    if (cancel) {
+      throw const UserCancelledException('Cancelled');
+    }
     final signInUri = await getSignInUri(provider: provider);
     await tester.pumpWidget(
       HostedUiApp(
--- packages/auth/amplify_auth_cognito_test/test/flows/hostedui/hosted_ui_platform_test.dart
+++ packages/auth/amplify_auth_cognito_test/test/flows/hostedui/hosted_ui_platform_test.dart
@@ -1,9 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import 'dart:async';
+
+import 'package:amplify_auth_cognito_dart/amplify_auth_cognito_dart.dart';
 import 'package:amplify_auth_cognito_dart/src/credentials/cognito_keys.dart';
 import 'package:amplify_auth_cognito_dart/src/flows/hosted_ui/hosted_ui_platform.dart';
 import 'package:amplify_auth_cognito_dart/src/model/hosted_ui/oauth_parameters.dart';
+import 'package:amplify_auth_cognito_dart/src/state/cognito_state_machine.dart';
 import 'package:amplify_auth_cognito_dart/src/state/state.dart';
 import 'package:amplify_auth_cognito_test/common/mock_config.dart';
 import 'package:amplify_auth_cognito_test/common/mock_dispatcher.dart';
@@ -12,6 +16,7 @@ import 'package:amplify_auth_cognito_test/common/mock_secure_storage.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:amplify_secure_storage_dart/amplify_secure_storage_dart.dart';
 import 'package:http/http.dart' as http;
+import 'package:test/fake.dart';
 import 'package:test/test.dart';
 
 final throwsInvalidStateException = throwsA(isA<InvalidStateException>());
@@ -22,6 +27,8 @@ void main() {
   late HostedUiPlatform platform;
   late DependencyManager dependencyManager;
   const keys = HostedUiKeys(hostedUiConfig);
+
+  AWSLogger().logLevel = LogLevel.verbose;
 
   group('HostedUiPlatform', () {
     setUp(() {
@@ -108,5 +115,75 @@ void main() {
         );
       });
     });
+
+    group('signIn', () {
+      late AmplifyAuthCognitoDart plugin;
+
+      setUp(() async {
+        dependencyManager.addInstance<HostedUiPlatform>(
+          CancelingHostedUiPlatform(
+            cancelSignIn: expectAsync0(() async {}),
+          ),
+        );
+        plugin = AmplifyAuthCognitoDart()
+          ..stateMachine = CognitoAuthStateMachine(
+            dependencyManager: dependencyManager,
+          );
+        await plugin.stateMachine.acceptAndComplete(
+          ConfigurationEvent.configure(mockConfig),
+        );
+      });
+
+      tearDown(() => plugin.close());
+
+      test('can cancel flow', () async {
+        final expectation = expectLater(
+          plugin.signInWithWebUI(
+            provider: AuthProvider.cognito,
+          ),
+          throwsA(isA<UserCancelledException>()),
+        );
+        final hostedUiMachine =
+            plugin.stateMachine.expect(HostedUiStateMachine.type);
+        expect(
+          hostedUiMachine.stream,
+          emitsInOrder([
+            isA<HostedUiSigningIn>(),
+            isA<HostedUiFailure>().having(
+              (s) => s.exception,
+              'exception',
+              isA<UserCancelledException>(),
+            ),
+            emitsDone,
+          ]),
+        );
+        await expectation;
+        // Ensure queue is flushed and done event is emitted after
+        // signInWithWebUI completes.
+        await hostedUiMachine.close();
+      });
+    });
   });
+}
+
+final class CancelingHostedUiPlatform extends Fake implements HostedUiPlatform {
+  CancelingHostedUiPlatform({
+    required Future<void> Function() cancelSignIn,
+  }) : _cancelSignIn = cancelSignIn;
+
+  final Future<void> Function() _cancelSignIn;
+
+  @override
+  Future<void> signIn({
+    required CognitoSignInWithWebUIPluginOptions options,
+    AuthProvider? provider,
+  }) async {
+    throw const UserCancelledException('Cancelled');
+  }
+
+  @override
+  Future<void> cancelSignIn() => _cancelSignIn();
+
+  @override
+  void close() {}
 }
