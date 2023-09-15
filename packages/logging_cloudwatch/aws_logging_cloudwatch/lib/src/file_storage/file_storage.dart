9ecffbf8e002355d0df319524a4dbf27aa2f689f
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-09-15 15:54:47 -0700

    Update packages/logging_cloudwatch/aws_logging_cloudwatch/lib/src/file_storage/file_storage.dart
    Co-authored-by: Dillon Nys <24740863+dnys1@users.noreply.github.com>

--- packages/logging_cloudwatch/aws_logging_cloudwatch/lib/src/file_storage/file_storage.dart
+++ packages/logging_cloudwatch/aws_logging_cloudwatch/lib/src/file_storage/file_storage.dart
@@ -1,12 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export 'file_storage_stub.dart'
+import 'file_storage_stub.dart'
     if (dart.library.io) 'file_storage_vm.dart'
     if (dart.library.html) 'file_storage_web.dart';
 
 /// File storage interface for saving and loading constraint locally
 abstract interface class FileStorage {
+  factory FileStorage(AppPathProvider pathProvider) = FileStorageImpl;
+
   /// Save constraint locally to file
   Future<void> save(String fileName, String data);
 
