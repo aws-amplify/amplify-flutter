768933f1d28aae2dce8106939e41286bdeddf02e
Jordan Nelson <Jordanryannelson@gmail.com>
2023-09-21 16:25:53 -0400

    chore: remove unused imports

--- packages/auth/amplify_auth_cognito/example/integration_test/version_upgrade_test.dart
+++ packages/auth/amplify_auth_cognito/example/integration_test/version_upgrade_test.dart
@@ -1,11 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'dart:io';
-
 import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:amplify_auth_cognito_example/amplifyconfiguration.dart';
 import 'package:amplify_auth_integration_test/amplify_auth_integration_test.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_integration_test/amplify_integration_test.dart';
