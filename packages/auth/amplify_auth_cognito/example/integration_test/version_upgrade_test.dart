0a64bbaa184347740c34343270ff35d8ff7d0541
Jordan Nelson <Jordanryannelson@gmail.com>
2023-09-22 11:28:52 -0400

    chore: trigger tests

--- packages/auth/amplify_auth_cognito/example/integration_test/version_upgrade_test.dart
+++ packages/auth/amplify_auth_cognito/example/integration_test/version_upgrade_test.dart
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// comment to trigger tests
+
 import 'dart:io';
 
 import 'package:amplify_api/amplify_api.dart';
