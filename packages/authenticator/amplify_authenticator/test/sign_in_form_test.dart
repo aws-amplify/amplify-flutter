6112492d868f56aaa992cd8205f300e7def6f19a
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-10-03 13:05:08 -0700

    chore: saving tests to clean up local repo

--- packages/authenticator/amplify_authenticator/test/sign_in_form_test.dart
+++ packages/authenticator/amplify_authenticator/test/sign_in_form_test.dart
@@ -2,11 +2,49 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_authenticator/src/services/amplify_auth_service.dart';
 import 'package:amplify_authenticator_test/amplify_authenticator_test.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthService extends Mock implements AmplifyAuthService {}
+
+class MockSignInResult extends Mock implements SignInResult {}
+
+class CustomMockAuthService extends AmplifyAuthService {
+  String? capturedUsername;
+  String? capturedPassword;
+
+  @override
+  Future<SignInResult> signIn(String username, String password) {
+    capturedUsername = username;
+    capturedPassword = password;
+    return Future.value(MockSignInResult());
+  }
+}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  final mockAuthService = CustomMockAuthService();
+
+  final signInArguments = <dynamic>[];
+
+  setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    registerFallbackValue(MockSignInResult());
+
+    // when(() => mockAuthService.signIn(any(), any())).thenAnswer((_) {
+    //   // capturedUsername = username;
+    //   // capturedPassword = password;
+    //   print(_);
+    //   return Future.value(
+    //     MockSignInResult(),
+    //   );
+    // });
+  });
 
   group('Sign In View', () {
     group('navigation', () {
@@ -15,7 +53,6 @@ void main() {
         await tester.pumpAndSettle();
 
         final signInPage = SignInPage(tester: tester);
-        // ignore: cascade_invocations
         signInPage.expectStep(AuthenticatorStep.signIn);
 
         // Go to Sign Up
@@ -39,7 +76,6 @@ void main() {
           await tester.pumpAndSettle();
 
           final signInPage = SignInPage(tester: tester);
-
           await signInPage.submitSignIn();
 
           final usernameFieldError = find.descendant(
@@ -99,6 +135,39 @@ void main() {
           );
 
           expect(usernameFieldError, findsNothing);
+        },
+      );
+
+      testWidgets(
+        'ensures email passed to the API is trimmed',
+        (tester) async {
+          await tester.pumpWidget(
+            MockAuthenticatorApp(
+              mockAuthService: CustomMockAuthService(),
+            ),
+          );
+          await tester.pumpAndSettle();
+
+          final signInPage = SignInPage(tester: tester);
+
+          // Enter email with trailing space and a valid password
+          await signInPage.enterUsername('user@example.com ');
+          await signInPage.enterPassword('Password123');
+
+          var capturedUsername;
+
+          // when(() => mockAuthService.signIn(any(), any())).thenAnswer(
+          //   (_) async {
+          //     capturedUsername = _.positionalArguments[0];
+          //     return MockSignInResult();
+          //   },
+          // );
+
+          await signInPage.submitSignIn();
+          await tester.pumpAndSettle();
+
+          // Check if captured username is trimmed
+          // expect(mockAuthService.capturedUsername, 'user@example.com');
         },
       );
     });
