1f8a6a8e3dab12831fd477ca25396de1e6f69af6
Nika Hassani <nikaws@amazon.com>
2023-10-20 10:02:46 -0700

    chore(logging): example app to not use data protection on macOS

--- packages/logging_cloudwatch/amplify_logging_cloudwatch/example/lib/main.dart
+++ packages/logging_cloudwatch/amplify_logging_cloudwatch/example/lib/main.dart
@@ -28,8 +28,16 @@ class _MyAppState extends State<MyApp> {
   }
 
   static Future<void> _configureAmplify() async {
-    final authPlugin = AmplifyAuthCognito();
-
+    final authPlugin = AmplifyAuthCognito(
+      // FIXME: In your app, make sure to remove this line and set up
+      /// Keychain Sharing in Xcode as described in the docs:
+      /// https://docs.amplify.aws/lib/project-setup/platform-setup/q/platform/flutter/#enable-keychain
+      secureStorageFactory: AmplifySecureStorage.factoryFrom(
+        macOSOptions:
+            // ignore: invalid_use_of_visible_for_testing_member
+            MacOSSecureStorageOptions(useDataProtection: false),
+      ),
+    );
     final amplifyConfigWithLogging = AmplifyConfig.fromJson(
       jsonDecode(amplifyconfig) as Map<String, dynamic>,
     ).copyWith(
