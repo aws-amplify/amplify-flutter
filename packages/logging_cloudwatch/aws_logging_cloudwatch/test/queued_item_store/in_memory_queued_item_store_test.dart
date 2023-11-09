59e1ff7462dddbe734310015cb997fdd37a229e9
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2023-11-09 09:17:04 -0800

    chore: removed await in in_memory tests

--- packages/logging_cloudwatch/aws_logging_cloudwatch/test/queued_item_store/in_memory_queued_item_store_test.dart
+++ packages/logging_cloudwatch/aws_logging_cloudwatch/test/queued_item_store/in_memory_queued_item_store_test.dart
@@ -236,14 +236,14 @@ void main() {
           await db.addItem(largeItem, DateTime.now().toIso8601String());
         }
 
-        var result = await db.isFull(capacityLimit);
+        var result = db.isFull(capacityLimit);
         expect(result, isFalse);
 
         for (var i = 0; i < 100; i++) {
           await db.addItem(largeItem, DateTime.now().toIso8601String());
         }
 
-        result = await db.isFull(capacityLimit);
+        result = db.isFull(capacityLimit);
         expect(result, isTrue);
       },
     );
