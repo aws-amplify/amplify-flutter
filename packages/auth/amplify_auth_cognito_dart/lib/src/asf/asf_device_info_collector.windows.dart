ba3407fe708766a33b59811dfb7da1e2f8765430
Elijah Quartey <quaelija@amazon.com>
2024-01-17 12:20:43 -0600

    fix comment

--- packages/auth/amplify_auth_cognito_dart/lib/src/asf/asf_device_info_collector.windows.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/asf/asf_device_info_collector.windows.dart
@@ -102,7 +102,7 @@ final class ASFDeviceInfoWindows extends ASFDeviceInfoPlatform {
       final langCodepageArr = lpTranslate.value;
       final n = lenTranslate.value / sizeOf<_LANGANDCODEPAGE>();
       final langCodepages = [
-        // TODO(equartey): Use `(langCodepageArr + 1).ref` when Dart 3.30 is released.
+        // TODO(equartey): Use `(langCodepageArr + i).ref` when Dart 3.30 is released.
         // ignore: deprecated_member_use
         for (var i = 0; i < n; i++) langCodepageArr.elementAt(i).ref,
       ];
