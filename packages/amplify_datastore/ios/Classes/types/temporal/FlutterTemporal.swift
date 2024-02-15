633296f4ee86fdd1c4a402e5de4cd6614e06b265
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-02-15 11:18:33 -0800

    chore: add timezone to the FlutterTemporal type

--- packages/amplify_datastore/ios/Classes/types/temporal/FlutterTemporal.swift
+++ packages/amplify_datastore/ios/Classes/types/temporal/FlutterTemporal.swift
@@ -8,9 +8,11 @@ import Foundation
 // Stores an ISO 8601 String to be saved to Appsync
 struct FlutterTemporal: TemporalSpec {
     let iso8601: String
+    var timeZone: TimeZone?
     init(iso8601String: String) {
         self.iso8601 = iso8601String
         self.foundationDate = Date()
+        self.timeZone = timeZone
     }
 
     var iso8601String: String {
@@ -24,6 +26,7 @@ struct FlutterTemporal: TemporalSpec {
     init(_ date: Date) {
         self.iso8601 = ""
         self.foundationDate = date
+        self.timeZone = timeZone
     }
 
     static func now() -> FlutterTemporal {
