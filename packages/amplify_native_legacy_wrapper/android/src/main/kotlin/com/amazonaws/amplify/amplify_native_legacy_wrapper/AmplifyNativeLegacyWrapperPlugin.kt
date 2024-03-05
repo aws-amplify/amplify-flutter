976613a1598ca430911c7400b1ff96fa3deebe20
Jordan Nelson <Jordanryannelson@gmail.com>
2024-03-04 16:28:41 -0800

    chore: use coroutines, add plugin

--- packages/amplify_native_legacy_wrapper/android/src/main/kotlin/com/amazonaws/amplify/amplify_native_legacy_wrapper/AmplifyNativeLegacyWrapperPlugin.kt
+++ packages/amplify_native_legacy_wrapper/android/src/main/kotlin/com/amazonaws/amplify/amplify_native_legacy_wrapper/AmplifyNativeLegacyWrapperPlugin.kt
@@ -6,9 +6,12 @@ package com.amazonaws.amplify.amplify_native_legacy_wrapper
 import android.content.Context
 import com.amazonaws.amplify.amplify_native_legacy_wrapper.pigeons.LegacyNativePlugin
 import com.amplifyframework.AmplifyException
-import com.amplifyframework.core.Amplify
+import com.amplifyframework.auth.AuthException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.kotlin.core.Amplify
 import com.amplifyframework.core.AmplifyConfiguration
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import kotlinx.coroutines.runBlocking
 import org.json.JSONObject
 
 
@@ -31,6 +34,7 @@ class AmplifyNativeLegacyWrapperPlugin: FlutterPlugin, LegacyNativePlugin {
       val configuration = AmplifyConfiguration.builder(JSONObject(config))
         .devMenuEnabled(false)
         .build()
+      Amplify.addPlugin(AWSCognitoAuthPlugin())
       Amplify.configure(configuration, context)
       callback(Result.success(Unit))
     } catch (e: AmplifyException) {
@@ -39,26 +43,36 @@ class AmplifyNativeLegacyWrapperPlugin: FlutterPlugin, LegacyNativePlugin {
   }
 
   override fun signIn(username: String, password: String, callback: (Result<Unit>) -> Unit) {
-    Amplify.Auth.signIn(
-      username,
-      password,
-      { callback(Result.success(Unit)) },
-      { e -> callback(Result.failure(e)) }
-    )
+    runBlocking {
+      try {
+        Amplify.Auth.signIn(username, password)
+        callback(Result.success(Unit))
+      } catch (error: AuthException) {
+        callback(Result.failure(error))
+      }
+    }
   }
 
   override fun signOut(callback: (Result<Unit>) -> Unit) {
-    Amplify.Auth.signOut(
-      { callback(Result.success(Unit)) },
-      { e -> callback(Result.failure(e)) }
-    )
+    runBlocking {
+      try {
+        Amplify.Auth.signOut()
+        callback(Result.success(Unit))
+      } catch (error: AuthException) {
+        callback(Result.failure(error))
+      }
+    }
   }
 
   override fun rememberDevice(callback: (Result<Unit>) -> Unit) {
-    Amplify.Auth.rememberDevice(
-      { callback(Result.success(Unit)) },
-      { e -> callback(Result.failure(e)) }
-    )
+    runBlocking {
+      try {
+        Amplify.Auth.rememberDevice()
+        callback(Result.success(Unit))
+      } catch (error: AuthException) {
+        callback(Result.failure(error))
+      }
+    }
   }
 
 }
