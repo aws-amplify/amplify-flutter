84efbf2878c67e6ed160bcb5d1b9ea56ff4ef35d
Jordan Nelson <Jordanryannelson@gmail.com>
2024-03-28 11:27:26 -0400

    chore: remove `StorageOperationOptions`

--- packages/amplify_core/lib/src/types/storage/download_data_options.dart
+++ packages/amplify_core/lib/src/types/storage/download_data_options.dart
@@ -1,13 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'package:amplify_core/src/types/storage/base/storage_operation_options.dart';
 import 'package:aws_common/aws_common.dart';
 
 /// {@template amplify_core.storage.download_data_options}
 /// Configurable options for `Amplify.Storage.downloadData`.
 /// {@endtemplate}
-class StorageDownloadDataOptions extends StorageOperationOptions
+class StorageDownloadDataOptions
     with
         AWSEquatable<StorageDownloadDataOptions>,
         AWSSerializable<Map<String, Object?>>,
