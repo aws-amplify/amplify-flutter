220eb325dc6dfd46e1b39429713e8caf28108d86
Jordan Nelson <Jordanryannelson@gmail.com>
2024-04-01 12:57:31 -0400

    chore: format TODO

--- packages/auth/amplify_auth_cognito_dart/lib/src/asf/asf_device_info_collector.windows.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/asf/asf_device_info_collector.windows.dart
@@ -214,14 +214,14 @@ final class ASFDeviceInfoWindows extends ASFDeviceInfoPlatform {
   });
 
   @override
-  // TODO[Jordan-Nelson]: Use SYSTEM_METRICS_INDEX enum when min win32 version is
-  // v5.4.0 or higher
+  // TODO(Jordan-Nelson): Use SYSTEM_METRICS_INDEX enum when min win32 version
+  // is v5.4.0 or higher
   // ignore: deprecated_member_use
   Future<int?> get screenHeightPixels async => GetSystemMetrics(SM_CYSCREEN);
 
   @override
-  // TODO[Jordan-Nelson]: Use SYSTEM_METRICS_INDEX enum when min win32 version is
-  // v5.4.0 or higher
+  // TODO(Jordan-Nelson): Use SYSTEM_METRICS_INDEX enum when min win32 version
+  // is v5.4.0 or higher
   // ignore: deprecated_member_use
   Future<int?> get screenWidthPixels async => GetSystemMetrics(SM_CXSCREEN);
 
