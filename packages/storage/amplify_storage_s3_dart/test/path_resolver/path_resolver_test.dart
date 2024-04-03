4b516c1e54d43b741c8cb3014e92919da777769c
Jordan Nelson <Jordanryannelson@gmail.com>
2024-04-03 12:44:07 -0400

    chore: add tests for path resolver

--- packages/storage/amplify_storage_s3_dart/test/path_resolver/path_resolver_test.dart
+++ packages/storage/amplify_storage_s3_dart/test/path_resolver/path_resolver_test.dart
@@ -7,6 +7,8 @@ import 'package:test/test.dart';
 
 class MockTokenIdentityAmplifyAuthProvider
     implements TokenIdentityAmplifyAuthProvider {
+  int getIdCount = 0;
+
   @override
   Future<AWSBaseHttpRequest> authorizeRequest(
     AWSBaseHttpRequest request, {
@@ -17,6 +19,7 @@ class MockTokenIdentityAmplifyAuthProvider
 
   @override
   Future<String> getIdentityId() async {
+    getIdCount++;
     return 'mock-id';
   }
 
@@ -38,6 +41,15 @@ void main() {
       );
     });
 
+    test('should resolve multiple static strings', () async {
+      const path1 = StoragePath.fromString('foo/bar/picture1.png');
+      const path2 = StoragePath.fromString('foo/bar/picture2.png');
+      expect(
+        await pathResolver.resolvePaths(paths: [path1, path2]),
+        ['foo/bar/picture1.png', 'foo/bar/picture2.png'],
+      );
+    });
+
     test('should resolve path with identity Id', () async {
       final path = StoragePath.fromIdentityId((id) => 'foo/$id/picture.png');
       expect(
@@ -46,8 +58,30 @@ void main() {
       );
     });
 
+    test('should resolve multiple paths with identity Id', () async {
+      final tokenAmplifyAuthProvider = MockTokenIdentityAmplifyAuthProvider();
+      final pathResolver = S3PathResolver(
+        identityProvider: tokenAmplifyAuthProvider,
+      );
+      final path1 = StoragePath.fromIdentityId((id) => 'foo/$id/picture1.png');
+      final path2 = StoragePath.fromIdentityId((id) => 'foo/$id/picture2.png');
+      expect(
+        await pathResolver.resolvePaths(paths: [path1, path2]),
+        ['foo/mock-id/picture1.png', 'foo/mock-id/picture2.png'],
+      );
+      expect(tokenAmplifyAuthProvider.getIdCount, 1);
+    });
+
     test('should throw if the path starts with a leading "/"', () async {
       const path = StoragePath.fromString('/foo/bar/picture.png');
+      expect(
+        () => pathResolver.resolvePath(path: path),
+        throwsA(isA<StoragePathValidationException>()),
+      );
+    });
+
+    test('should throw if the path is empty', () async {
+      const path = StoragePath.fromString('');
       expect(
         () => pathResolver.resolvePath(path: path),
         throwsA(isA<StoragePathValidationException>()),
