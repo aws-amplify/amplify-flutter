107ec2592dc839ba74ab09d75d481d11c7a4c01f
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-04-10 14:16:59 -0700

    chore: remove test comment used to trigger tests

--- packages/amplify_datastore/lib/amplify_datastore.dart
+++ packages/amplify_datastore/lib/amplify_datastore.dart
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// COMMENT TO REMOVE: TRIGGER TESTS
-
 import 'dart:async';
 import 'dart:convert';
 
