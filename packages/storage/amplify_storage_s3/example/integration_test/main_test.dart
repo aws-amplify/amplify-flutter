e252c946686bb09917c5f7b3f35bae6c7aa3769a
Jordan Nelson <Jordanryannelson@gmail.com>
2024-04-19 15:24:45 -0400

    chore update main_test file

--- packages/storage/amplify_storage_s3/example/integration_test/main_test.dart
+++ packages/storage/amplify_storage_s3/example/integration_test/main_test.dart
@@ -4,10 +4,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'copy_test.dart' as copy_test;
 import 'get_properties_test.dart' as get_properties_test;
 import 'get_url_test.dart' as get_url_test;
 import 'issues/get_url_special_characters_test.dart'
     as get_url_special_characters_tests;
+import 'remove_many_test.dart' as remove_many_test;
+import 'remove_test.dart' as remove_test;
 import 'upload_data_test.dart' as upload_data_test;
 import 'upload_file_test.dart' as upload_file_test;
 import 'use_case_test.dart' as use_case_tests;
@@ -18,9 +21,12 @@ void main() {
   group('amplify_storage_s3', () {
     get_url_special_characters_tests.main();
     use_case_tests.main();
+    copy_test.main();
     get_url_test.main();
     get_properties_test.main();
     upload_file_test.main();
     upload_data_test.main();
+    remove_many_test.main();
+    remove_test.main();
   });
 }
