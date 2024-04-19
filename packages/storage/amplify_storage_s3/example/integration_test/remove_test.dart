6f89a65c647731cfe972c37f8e21819fee67e435
Jordan Nelson <Jordanryannelson@gmail.com>
2024-04-19 12:27:36 -0400

    chore: add e2e tests for `remove`, `removeMany`

---
+++ packages/storage/amplify_storage_s3/example/integration_test/remove_test.dart
@@ -0,0 +1,93 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:amplify_core/amplify_core.dart';
+import 'package:amplify_storage_s3_example/amplifyconfiguration.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'utils/configure.dart';
+import 'utils/sign_in_new_user.dart';
+
+/// Returns true if an object exists at the given [path].
+Future<bool> objectExists(StoragePath path) async {
+  try {
+    await Amplify.Storage.getProperties(path: path).result;
+    return true;
+  } on StorageKeyNotFoundException {
+    return false;
+  }
+}
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('remove()', () {
+    setUpAll(() async {
+      await configure(amplifyEnvironments['main']!);
+    });
+
+    group('StoragePath from string', () {
+      final path = 'public/remove-${uuid()}';
+      final storagePath = StoragePath.fromString(path);
+      setUp(() async {
+        await Amplify.Storage.uploadData(
+          data: HttpPayload.bytes('data'.codeUnits),
+          path: storagePath,
+        ).result;
+      });
+
+      testWidgets('removes object', (_) async {
+        expect(await objectExists(storagePath), true);
+        final result = await Amplify.Storage.remove(
+          path: storagePath,
+        ).result;
+        expect(await objectExists(storagePath), false);
+        expect(result.removedItem.path, path);
+      });
+    });
+
+    group('StoragePath from identity Id', () {
+      final fileName = 'remove-${uuid()}';
+      final storagePath = StoragePath.fromIdentityId(
+        ((identityId) => 'private/$identityId/$fileName'),
+      );
+      late String expectedResolvedPath;
+      setUp(() async {
+        final identityId = await signInNewUser();
+        expectedResolvedPath = 'private/$identityId/$fileName';
+        await Amplify.Storage.uploadData(
+          data: HttpPayload.bytes('data'.codeUnits),
+          path: StoragePath.fromString(expectedResolvedPath),
+        ).result;
+      });
+
+      testWidgets('removes object', (_) async {
+        expect(await objectExists(storagePath), true);
+        final result = await Amplify.Storage.remove(
+          path: storagePath,
+        ).result;
+        expect(await objectExists(storagePath), false);
+        expect(result.removedItem.path, expectedResolvedPath);
+      });
+    });
+
+    testWidgets('unauthorized path', (_) async {
+      await expectLater(
+        () => Amplify.Storage.remove(
+          path: const StoragePath.fromString('unauthorized/path'),
+        ).result,
+        throwsA(isA<StorageAccessDeniedException>()),
+      );
+    });
+
+    testWidgets('non existent path', (_) async {
+      await expectLater(
+        Amplify.Storage.remove(
+          path: const StoragePath.fromString('public/not-existent-path'),
+        ).result,
+        completes,
+      );
+    });
+  });
+}
