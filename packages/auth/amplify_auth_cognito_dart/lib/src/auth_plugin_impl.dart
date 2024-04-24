c99eb943b3431971684e321f414519bf85449963
Jordan Nelson <Jordanryannelson@gmail.com>
2024-04-24 14:03:18 -0400

    chore: rename `EnableSoftwareTokenMfaException` to `CodeMismatchException`

--- packages/auth/amplify_auth_cognito_dart/lib/src/auth_plugin_impl.dart
+++ packages/auth/amplify_auth_cognito_dart/lib/src/auth_plugin_impl.dart
@@ -929,8 +929,7 @@ class AmplifyAuthCognitoDart extends AuthPluginInterface
 
     switch (state) {
       case TotpSetupRequiresVerification _:
-        // TODO(equartey): Change to `CodeMismatchException` in next major version as breaking change
-        throw const EnableSoftwareTokenMfaException(
+        throw const CodeMismatchException(
           'The code provided was incorrect, try again',
         );
       case TotpSetupFailure(:final exception, :final stackTrace):
