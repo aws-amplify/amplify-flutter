7c5f48e111ed12331255b57fe643a8c1a3e26f8e
Elijah Quartey <Equartey@users.noreply.github.com>
2024-04-26 17:54:13 -0500

    chore(amplify_test): regenerated test models (#4778)

--- packages/test/amplify_test/lib/test_models/Post.dart
+++ packages/test/amplify_test/lib/test_models/Post.dart
@@ -17,7 +17,7 @@
 // Generated files can be excluded from analysis in analysis_options.yaml
 // For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
 
-// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
+// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, override_on_non_overriding_member, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
 
 import 'ModelProvider.dart';
 import 'package:amplify_core/amplify_core.dart' as amplify_core;
@@ -136,27 +136,6 @@ class Post extends amplify_core.Model {
             comments != null ? List<Comment>.unmodifiable(comments) : comments);
   }
 
-  // TODO(Jordan-Nelson): Remove at next major version bump.
-  // This was added manually so that Post can be extended in tests.
-  const Post.internal(
-      {required this.id,
-      required title,
-      required rating,
-      created,
-      likeCount,
-      blog,
-      comments,
-      createdAt,
-      updatedAt})
-      : _title = title,
-        _rating = rating,
-        _created = created,
-        _likeCount = likeCount,
-        _blog = blog,
-        _comments = comments,
-        _createdAt = createdAt,
-        _updatedAt = updatedAt;
-
   bool equals(Object other) {
     return this == other;
   }
@@ -245,14 +224,26 @@ class Post extends amplify_core.Model {
             : null,
         _likeCount = (json['likeCount'] as num?)?.toInt(),
         _blog = json['blog'] != null
-            ? Blog.fromJson(new Map<String, dynamic>.from(json['blog']))
+            ? json['blog']['serializedData'] != null
+                ? Blog.fromJson(new Map<String, dynamic>.from(
+                    json['blog']['serializedData']))
+                : Blog.fromJson(new Map<String, dynamic>.from(json['blog']))
             : null,
-        _comments = json['comments'] != null
-            ? (json['comments']['items'] as List)
-                .where((e) => e != null)
-                .map((e) => Comment.fromJson(new Map<String, dynamic>.from(e)))
-                .toList()
-            : null,
+        _comments = json['comments'] is Map
+            ? (json['comments']['items'] is List
+                ? (json['comments']['items'] as List)
+                    .where((e) => e != null)
+                    .map((e) =>
+                        Comment.fromJson(new Map<String, dynamic>.from(e)))
+                    .toList()
+                : null)
+            : (json['comments'] is List
+                ? (json['comments'] as List)
+                    .where((e) => e?['serializedData'] != null)
+                    .map((e) => Comment.fromJson(
+                        new Map<String, dynamic>.from(e?['serializedData'])))
+                    .toList()
+                : null),
         _createdAt = json['createdAt'] != null
             ? amplify_core.TemporalDateTime.fromString(json['createdAt'])
             : null,
