1d78bc854a66e218734e268ae33814d478038755
Di Wu <diwud@amazon.com>
2024-05-03 12:24:21 -0700

    some code cleanup

---
+++ packages/amplify_datastore/ios/Classes/api/GraphQLRequest+Extension.swift
@@ -0,0 +1,26 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+
+import Foundation
+import Amplify
+
+extension GraphQLRequest {
+    func toNativeGraphQLRequest() -> NativeGraphQLRequest {
+        let variablesJson = self.variables
+            .flatMap { try? JSONSerialization.data(withJSONObject: $0, options: []) }
+            .flatMap { String(data: $0, encoding: .utf8) }
+
+        return NativeGraphQLRequest(
+            apiName: self.apiName,
+            document: self.document,
+            variablesJson: variablesJson ?? "{}",
+            responseType: String(describing: self.responseType),
+            decodePath: self.decodePath
+        )
+    }
+}
