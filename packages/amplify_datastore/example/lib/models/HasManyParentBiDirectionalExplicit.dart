d28e30d496b42ef2076e7e488041c09a9ba4f01e
Elijah Quartey <quaelija@amazon.com>
2024-05-14 17:56:06 -0500

    chore: regenerated models

--- packages/amplify_datastore/example/lib/models/HasManyParentBiDirectionalExplicit.dart
+++ packages/amplify_datastore/example/lib/models/HasManyParentBiDirectionalExplicit.dart
@@ -17,7 +17,7 @@
 // Generated files can be excluded from analysis in analysis_options.yaml
 // For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
 
-// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
+// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, override_on_non_overriding_member, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
 
 import 'ModelProvider.dart';
 import 'package:amplify_core/amplify_core.dart' as amplify_core;
@@ -143,14 +143,22 @@ class HasManyParentBiDirectionalExplicit extends amplify_core.Model {
   HasManyParentBiDirectionalExplicit.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         _name = json['name'],
-        _biDirectionalExplicitChildren =
-            json['biDirectionalExplicitChildren'] is List
+        _biDirectionalExplicitChildren = json['biDirectionalExplicitChildren']
+                is Map
+            ? (json['biDirectionalExplicitChildren']['items'] is List
+                ? (json['biDirectionalExplicitChildren']['items'] as List)
+                    .where((e) => e != null)
+                    .map((e) => HasManyChildBiDirectionalExplicit.fromJson(
+                        new Map<String, dynamic>.from(e)))
+                    .toList()
+                : null)
+            : (json['biDirectionalExplicitChildren'] is List
                 ? (json['biDirectionalExplicitChildren'] as List)
                     .where((e) => e?['serializedData'] != null)
                     .map((e) => HasManyChildBiDirectionalExplicit.fromJson(
-                        new Map<String, dynamic>.from(e['serializedData'])))
+                        new Map<String, dynamic>.from(e?['serializedData'])))
                     .toList()
-                : null,
+                : null),
         _createdAt = json['createdAt'] != null
             ? amplify_core.TemporalDateTime.fromString(json['createdAt'])
             : null,
--- packages/amplify_datastore/example/lib/models/PostTags.dart
+++ packages/amplify_datastore/example/lib/models/PostTags.dart
@@ -17,7 +17,7 @@
 // Generated files can be excluded from analysis in analysis_options.yaml
 // For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
 
-// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
+// ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, override_on_non_overriding_member, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
 
 import 'ModelProvider.dart';
 import 'package:amplify_core/amplify_core.dart' as amplify_core;
@@ -140,13 +140,17 @@ class PostTags extends amplify_core.Model {
 
   PostTags.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        _post = json['post']?['serializedData'] != null
-            ? Post.fromJson(
-                new Map<String, dynamic>.from(json['post']['serializedData']))
+        _post = json['post'] != null
+            ? json['post']['serializedData'] != null
+                ? Post.fromJson(new Map<String, dynamic>.from(
+                    json['post']['serializedData']))
+                : Post.fromJson(new Map<String, dynamic>.from(json['post']))
             : null,
-        _tag = json['tag']?['serializedData'] != null
-            ? Tag.fromJson(
-                new Map<String, dynamic>.from(json['tag']['serializedData']))
+        _tag = json['tag'] != null
+            ? json['tag']['serializedData'] != null
+                ? Tag.fromJson(new Map<String, dynamic>.from(
+                    json['tag']['serializedData']))
+                : Tag.fromJson(new Map<String, dynamic>.from(json['tag']))
             : null,
         _createdAt = json['createdAt'] != null
             ? amplify_core.TemporalDateTime.fromString(json['createdAt'])
