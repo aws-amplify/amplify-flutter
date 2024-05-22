a8682729745f42c952a9ccbf207baf342fbf5863
Elijah Quartey <quaelija@amazon.com>
2024-05-22 14:00:40 -0500

    fix: added trim

--- actions/bin/launch_ios_simulator.dart
+++ actions/bin/launch_ios_simulator.dart
@@ -107,7 +107,7 @@ Future<String> getLatest() async {
   if (version.exitCode != 0) {
     throw Exception('Could not get latest version');
   }
-  return version.stdout;
+  return version.stdout.trim();
 }
 
 /// Installs the iOS runtime for the given [iosVersion].
