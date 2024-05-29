71c5f8f5a2b6e7944e36e279aa2df9dec857fbdc
Elijah Quartey <Equartey@users.noreply.github.com>
2024-05-29 09:05:12 -0500

    Updated headers
    Co-authored-by: Jordan Nelson <Jordanryannelson@gmail.com>

--- packages/amplify_datastore/example/ios/unit_tests/GraphQLRequestExtensionTests.swift
+++ packages/amplify_datastore/example/ios/unit_tests/GraphQLRequestExtensionTests.swift
@@ -1,9 +1,5 @@
-//
-// Copyright Amazon.com Inc. or its affiliates.
-// All Rights Reserved.
-//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-//
 
 
 import XCTest
