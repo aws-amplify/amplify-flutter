aa7140a142c2b004132b11f93915923f12cd7e6b
Tyler-Larkin <tyllark@amazon.com>
2024-06-12 10:56:10 -0700

    chore(core): updated exports to expose MFA/Password Policies/Identity Providers

--- packages/amplify_core/lib/src/config/amplify_outputs/auth/auth_outputs.dart
+++ packages/amplify_core/lib/src/config/amplify_outputs/auth/auth_outputs.dart
@@ -2,9 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:amplify_core/amplify_core.dart';
-import 'package:amplify_core/src/config/amplify_outputs/auth/mfa.dart';
 import 'package:amplify_core/src/config/amplify_outputs/auth/oauth_outputs.dart';
-import 'package:amplify_core/src/config/amplify_outputs/auth/password_policy.dart';
 
 part 'auth_outputs.g.dart';
 
--- packages/amplify_core/lib/src/config/amplify_outputs/auth/oauth_outputs.dart
+++ packages/amplify_core/lib/src/config/amplify_outputs/auth/oauth_outputs.dart
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:amplify_core/amplify_core.dart';
-import 'package:amplify_core/src/config/amplify_outputs/auth/identity_provider.dart';
 import 'package:amplify_core/src/config/amplify_outputs/auth/oauth_response_type.dart';
 
 part 'oauth_outputs.g.dart';
