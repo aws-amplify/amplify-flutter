e941d5b14f86907acc7cb5ecda3aabc64ffcb826
Elijah Quartey <quaelija@amazon.com>
2024-06-13 15:04:39 -0500

    chore: remove base script

--- packages/api/amplify_api/example/tool/pull_test_gen2_backend.sh
+++ packages/api/amplify_api/example/tool/pull_test_gen2_backend.sh
@@ -5,3 +5,5 @@
 set -e
 
 STACK_ID=$AFS_GEN2_STACK_ID ../../../../build-support/pull_gen2_backend_by_stack_id.sh 
+
+npx ampx generate outputs --stack $AFS_GEN2_STACK_ID --profile ${AWS_PROFILE:-default} --format dart \--out-dir lib
