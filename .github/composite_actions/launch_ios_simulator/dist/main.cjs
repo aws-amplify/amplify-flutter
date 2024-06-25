ceeddbeae637df206f4c2eda6c6322b69328a852
Andrew Hahn <58017052+hahnandrew@users.noreply.github.com>
2024-06-24 18:03:44 -0700

    fix (TEMP): added phantomato/patched/xcodes xcodes branch for ci fix

--- .github/composite_actions/launch_ios_simulator/dist/main.cjs
+++ .github/composite_actions/launch_ios_simulator/dist/main.cjs
@@ -15042,7 +15042,7 @@
             case 0:
               // Function start
               $async$goto = 2;
-              return A._asyncAwait(A.Exec_exec(type$.JSObject._as(self.exec), "brew", A._setArrayType(["install", "xcodesorg/made/xcodes", "aria2"], type$.JSArray_String), true), $async$call$0);
+              return A._asyncAwait(A.Exec_exec(type$.JSObject._as(self.exec), "brew", A._setArrayType(["install", "phantomato/patched/xcodes", "aria2"], type$.JSArray_String), true), $async$call$0);
             case 2:
               // returning from await.
               if ($async$result.exitCode !== 0)
@@ -15068,7 +15068,7 @@
             case 0:
               // Function start
               $async$goto = 2;
-              return A._asyncAwait(A.Exec_exec(type$.JSObject._as(self.exec), "sudo", A._setArrayType(["xcodes", "runtimes", "install", $async$self.iosVersion, "--no-color", "&&", "xcrun", "simctl", "list", "runtimes"], type$.JSArray_String), true), $async$call$0);
+              return A._asyncAwait(A.Exec_exec(type$.JSObject._as(self.exec), "sudo", A._setArrayType(["xcodes", "runtimes", "install",  $async$self.iosVersion,  "--no-color"], type$.JSArray_String), true), $async$call$0);
             case 2:
               // returning from await.
               if ($async$result.exitCode !== 0)
