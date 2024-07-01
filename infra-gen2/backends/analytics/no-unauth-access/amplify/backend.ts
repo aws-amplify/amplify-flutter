85f4b4dff612c1230727dffc13771d1332011c9c
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-07-01 16:00:41 -0700

    chore: add analytics to deploy script and configure

--- infra-gen2/backends/analytics/no-unauth-access/amplify/backend.ts
+++ infra-gen2/backends/analytics/no-unauth-access/amplify/backend.ts
@@ -1,11 +1,45 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { CfnApp } from "aws-cdk-lib/aws-pinpoint";
+import { Stack } from "aws-cdk-lib/core";
+import { auth } from "./auth/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
-  data,
+});
+
+const analyticsStack = backend.createStack("analytics-main-stack");
+
+// create a Pinpoint app
+const pinpoint = new CfnApp(analyticsStack, "Pinpoint", {
+  name: "analytics-main",
+});
+
+// create an IAM policy to allow interacting with Pinpoint
+const pinpointPolicy = new Policy(analyticsStack, "PinpointPolicy", {
+  policyName: "PinpointPolicy",
+  statements: [
+    new PolicyStatement({
+      actions: ["mobiletargeting:UpdateEndpoint", "mobiletargeting:PutEvents"],
+      resources: [pinpoint.attrArn + "/*"],
+    }),
+  ],
+});
+
+// apply the policy to the authenticated and unauthenticated roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
+  pinpointPolicy
+);
+
+// patch the custom Pinpoint resource to the expected output configuration
+backend.addOutput({
+  analytics: {
+    amazon_pinpoint: {
+      app_id: pinpoint.ref,
+      aws_region: Stack.of(pinpoint).region,
+    },
+  },
 });
