482847cc0b6f71de5534e85191d21d7262f21349
Andrew Hahn <dev.andrewhahn@gmail.com>
2024-07-05 15:20:09 -0700

    chore(storage): add todo comment about macros

--- packages/storage/amplify_storage_s3_dart/lib/src/model/s3_subpath_strategy.dart
+++ packages/storage/amplify_storage_s3_dart/lib/src/model/s3_subpath_strategy.dart
@@ -5,18 +5,42 @@ import 'package:amplify_core/amplify_core.dart';
 
 part 's3_subpath_strategy.g.dart';
 
+// TODO(hahnand): change json serializable to dart macros when it hits stable & min version includes feature - https://dart.dev/language/macros
+
 /// {@template storage.amplify_storage_s3.subpath_strategy}
 /// The configurable parameters for the Storage S3 plugin `list` API.
 /// {@endtemplate}
 @zAmplifySerializable
 class SubpathStrategy extends StorageListPluginOptions {
   /// {@macro amplify_core.storage.subpath_strategy}
+  const SubpathStrategy({
+    bool excludeSubPaths = false,
+    String delimiter = '/',
+  }) : this._(
+          excludeSubPaths: excludeSubPaths,
+          delimiter: delimiter,
+        );
 
+  /// {@macro storage.amplify_storage_s3.subpath_strategy}
+  /// Constructor for listing all objects without pagination.
+  const SubpathStrategy.listAll({
+    bool excludeSubPaths = false,
+  }) : this._(
+          excludeSubPaths: excludeSubPaths,
+          listAll: true,
+        );
+
+  /// {@macro storage.amplify_storage_s3.subpath_strategy}
+  factory SubpathStrategy.fromJson(Map<String, Object?> json) =>
+      _$SubpathStrategyFromJson(json);
+
+  /// Default constructor syntactic sugar for DX parity
   const SubpathStrategy.include() : this._();
 
   const SubpathStrategy._({
     this.excludeSubPaths = false,
     this.delimiter = '/',
+    this.listAll = false,
   });
 
   /// Constructor for excludes with optional delimiter arg
@@ -33,14 +57,23 @@ class SubpathStrategy extends StorageListPluginOptions {
   /// false, this value has no impact on behavior.
   final String? delimiter;
 
-  @override
-  List<Object?> get props => throw UnimplementedError();
+  /// Whether to list all objects under a given path without pagination. The
+  /// default value is `false`.
+  ///
+  /// This can be set by using [SubpathStrategy.listAll].
+  ///
+  /// Use with caution if numerous objects are under the given path.
+  final bool listAll;
 
   @override
-  String get runtimeTypeName => throw UnimplementedError();
+  List<Object?> get props => [
+        excludeSubPaths,
+        listAll,
+      ];
 
   @override
-  Map<String, String> toJson() {
-    throw UnimplementedError();
-  }
+  String get runtimeTypeName => 'SubpathStrategy';
+
+  @override
+  Map<String, Object?> toJson() => _$SubpathStrategyToJson(this);
 }
