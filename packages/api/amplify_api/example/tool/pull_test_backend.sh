9cd866215422e7938e2ea14a24f071cede59646f
Elijah Quartey <quaelija@amazon.com>
2024-07-12 14:02:14 -0600

    fix: config typo

--- packages/api/amplify_api/example/tool/pull_test_backend.sh
+++ packages/api/amplify_api/example/tool/pull_test_backend.sh
@@ -5,5 +5,4 @@
 set -e
 
 aws s3 cp s3://$AFS_API_GEN2_BUCKET_NAME/amplify_outputs.dart lib/amplify_outputs.dart 
-aws s3 cp s3://$AFS_API_GEN2_BUCKET_NAME/amplifyconfiuration.dart lib/amplifyconfiuration.dart 
-
+aws s3 cp s3://$AFS_API_GEN2_BUCKET_NAME/amplifyconfiguration.dart lib/amplifyconfiguration.dart 
