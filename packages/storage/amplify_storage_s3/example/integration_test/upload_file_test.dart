edc4ac07d4e20b300860a1d60bd03c516bd58f42
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-07-25 13:46:42 -0700

    chore(infra): Storage migrate to Gen 2 E2E
    chore(infra): Storage migrate to Gen 2 E2E

--- packages/storage/amplify_storage_s3/example/integration_test/upload_file_test.dart
+++ packages/storage/amplify_storage_s3/example/integration_test/upload_file_test.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 
 import 'package:amplify_core/amplify_core.dart';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
-import 'package:amplify_storage_s3_example/amplifyconfiguration.dart';
+import 'package:amplify_storage_s3_example/amplify_outputs.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
