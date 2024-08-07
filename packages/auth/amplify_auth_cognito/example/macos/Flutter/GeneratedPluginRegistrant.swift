9ac450a86e96fc69d98c2100fcf4df65f9ff858f
Andrew Hahn <dev.andrewhahn@gmail.com>
2024-08-07 16:42:09 -0700

    temp: generate goldens png

--- packages/auth/amplify_auth_cognito/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/auth/amplify_auth_cognito/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import amplify_auth_cognito
-import amplify_secure_storage
 import connectivity_plus
 import device_info_plus
 import package_info_plus
@@ -15,7 +14,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AmplifyAuthCognitoPlugin.register(with: registry.registrar(forPlugin: "AmplifyAuthCognitoPlugin"))
-  AmplifySecureStoragePlugin.register(with: registry.registrar(forPlugin: "AmplifySecureStoragePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
