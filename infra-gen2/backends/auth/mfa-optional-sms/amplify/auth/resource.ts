325cacc19aab889421a9cbb72e3fed83c74be45e
Jordan Nelson <Jordanryannelson@gmail.com>
2024-08-07 16:16:08 -0400

    chore: add license headers

--- infra-gen2/backends/auth/mfa-optional-sms/amplify/auth/resource.ts
+++ infra-gen2/backends/auth/mfa-optional-sms/amplify/auth/resource.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
