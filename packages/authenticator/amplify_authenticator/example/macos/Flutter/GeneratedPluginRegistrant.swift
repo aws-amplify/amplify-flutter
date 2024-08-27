c96d7f9bdb20025e060ac6bf41f99d662cb3b0c4
Jordan Nelson <Jordanryannelson@gmail.com>
2024-08-27 15:25:32 -0400

    chore: update plugin registrant for example apps that depend on secure storage (#5379)

--- packages/authenticator/amplify_authenticator/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/authenticator/amplify_authenticator/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import amplify_auth_cognito
+import amplify_secure_storage
 import connectivity_plus
 import device_info_plus
 import package_info_plus
@@ -14,6 +15,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AmplifyAuthCognitoPlugin.register(with: registry.registrar(forPlugin: "AmplifyAuthCognitoPlugin"))
+  AmplifySecureStoragePlugin.register(with: registry.registrar(forPlugin: "AmplifySecureStoragePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
