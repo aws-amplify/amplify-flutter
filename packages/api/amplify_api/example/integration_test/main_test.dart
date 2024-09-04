e17dc3574b0e2f6e06d88db487593772e6896329
Elijah Quartey <Equartey@users.noreply.github.com>
2024-09-04 13:59:27 -0500

    chore(datastore): Add multi auth integration tests (#5204)

--- packages/api/amplify_api/example/integration_test/main_test.dart
+++ packages/api/amplify_api/example/integration_test/main_test.dart
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_integration_test/amplify_integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -13,55 +14,64 @@ import 'util.dart';
 
 void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  TestUser? testUser = TestUser();
 
   group('amplify_api gen 1 config', () {
     setUpAll(() async {
       await configureAmplify(useGen1: true);
-      await signUpTestUser();
-      await signInTestUser();
+      await signUpTestUser(testUser);
+      await signInTestUser(testUser);
     });
 
     tearDownAll(() async {
-      await deleteTestUser();
       await Amplify.reset();
     });
 
     graph_api_key_test.main(
       useExistingTestUser: true,
       useGen1: true,
+      testUser: testUser,
     );
     graph_iam_test.main(
       useExistingTestUser: true,
       useGen1: true,
+      testUser: testUser,
     );
     graph_user_pools_test.main(
       useExistingTestUser: true,
       useGen1: true,
+      testUser: testUser,
     );
   });
 
   group('amplify_api gen 2 config', () {
     setUpAll(() async {
       await configureAmplify();
-      await signUpTestUser();
-      await signInTestUser();
+      testUser ??= await signUpTestUser(testUser);
+      await signInTestUser(testUser);
     });
 
     tearDownAll(() async {
-      await deleteTestUser();
+      await deleteTestUser(testUser);
       await Amplify.reset();
     });
 
     graph_api_key_test.main(
       useExistingTestUser: true,
+      testUser: testUser,
     );
     graph_iam_test.main(
       useExistingTestUser: true,
+      testUser: testUser,
     );
     graph_user_pools_test.main(
       useExistingTestUser: true,
+      testUser: testUser,
     );
 
-    rest_test.main(useExistingTestUser: true);
+    rest_test.main(
+      useExistingTestUser: true,
+      testUser: testUser,
+    );
   });
 }
