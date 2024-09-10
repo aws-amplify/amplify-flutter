aad4eebecd24d656b52bfcbdff77d9bc4a15d3af
Jordan Nelson <Jordanryannelson@gmail.com>
2024-09-10 11:16:57 -0400

    chore: only include first change log entry

--- packages/aft/tool/repo_snapshot.sh
+++ packages/aft/tool/repo_snapshot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This script copies the pubspec.yaml and changelog.md files from /packages to /test/snapshot.
-# Run this script as needed when updating tests.
+# This script copies the pubspec.yaml files and the content of *.md files up to the second H2 header from /packages to /test/snapshot.
 
 ROOT_DIR="../.."
 PACKAGES_DIR="$ROOT_DIR/packages"
@@ -10,7 +9,25 @@ DEST_DIR="$SNAPOSHOT_DIR/packages"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
-# Find pubspec.yaml and changelog.md files while skipping directories containing "example" or "test"
+
+# Function to process markdown files
+process_markdown() {
+    local input_file="$1"
+    local output_file="$2"
+    awk '
+    BEGIN {count = 0; print_line = 1}
+    /^## / {count++}
+    {
+        if (count < 2 && print_line) {
+            print $0 > "'"$output_file"'"
+        }
+    }
+    count == 2 {print_line = 0}
+    ' "$input_file"
+}
+
+
+# Find pubspec.yaml and *.md files while skipping directories containing "example" or "test"
 find "$PACKAGES_DIR" \( -name "pubspec.yaml" -o -name "CHANGELOG.md" \) | while read -r file; do
     # Get the directory path of the file
     DIR_PATH=$(dirname "$file")
@@ -22,8 +39,14 @@ find "$PACKAGES_DIR" \( -name "pubspec.yaml" -o -name "CHANGELOG.md" \) | while 
     RELATIVE_PATH="${file#$PACKAGES_DIR/}"
     # Create the directory in the destination if it doesn't exist
     mkdir -p "$DEST_DIR/$(dirname "$RELATIVE_PATH")"
-    # Copy the file to the destination
-    cp "$file" "$DEST_DIR/$RELATIVE_PATH"
+    
+    # Process the file based on its extension
+    if [[ "$file" == *.md ]]; then
+        process_markdown "$file" "$DEST_DIR/$RELATIVE_PATH"
+    else
+        # Copy non-markdown files as is
+        cp "$file" "$DEST_DIR/$RELATIVE_PATH"
+    fi
 done
 
 cp "$ROOT_DIR/pubspec.yaml" "$SNAPOSHOT_DIR/pubspec.yaml"
