0d88aa2d9d682d4a360fc9a7083a2723c9b4c88d
NikaHsn <nika.hasani@gmail.com>
2024-09-16 13:45:37 -0700

    chore(auth): legacy credential provider to use AuthOutputs instead of AmplifyConfig types (#5303)

--- packages/auth/amplify_auth_cognito/lib/src/credentials/legacy_credential_provider_android.dart
+++ packages/auth/amplify_auth_cognito/lib/src/credentials/legacy_credential_provider_android.dart
@@ -10,9 +10,7 @@ import 'package:amplify_auth_cognito/src/native_auth_plugin.g.dart'
 import 'package:amplify_auth_cognito_dart/src/credentials/legacy_credential_provider.dart';
 import 'package:amplify_auth_cognito_dart/src/state/cognito_state_machine.dart';
 import 'package:amplify_auth_cognito_dart/src/state/state.dart';
-import 'package:amplify_core/src/config/auth/cognito/credentials_provider.dart';
-import 'package:amplify_core/src/config/auth/cognito/oauth.dart';
-import 'package:amplify_core/src/config/auth/cognito/user_pool.dart';
+import 'package:amplify_core/src/config/amplify_outputs/auth/auth_outputs.dart';
 
 /// {@template amplify_auth_cognito.legacy_android_credential_provider}
 /// The implementation of [LegacyCredentialProvider] for migrating
@@ -25,54 +23,51 @@ class LegacyCredentialProviderAndroid implements LegacyCredentialProvider {
   final CognitoAuthStateMachine _stateMachine;
 
   @override
-  Future<CredentialStoreData?> fetchLegacyCredentials({
-    CognitoUserPoolConfig? userPoolConfig,
-    CognitoIdentityCredentialsProvider? identityPoolConfig,
-    CognitoOAuthConfig? hostedUiConfig,
-  }) async {
+  Future<CredentialStoreData?> fetchLegacyCredentials(
+    AuthOutputs authOutputs,
+  ) async {
     final bridge = _stateMachine.expect<auth_cognito.NativeAuthBridge>();
     final legacyCredentials = await bridge.getLegacyCredentials(
-      identityPoolConfig?.poolId,
-      userPoolConfig?.appClientId,
+      authOutputs.identityPoolId,
+      authOutputs.userPoolClientId,
     );
     return legacyCredentials.toCredentialStoreData();
   }
 
   @override
-  Future<void> deleteLegacyCredentials({
-    CognitoUserPoolConfig? userPoolConfig,
-    CognitoIdentityCredentialsProvider? identityPoolConfig,
-    CognitoOAuthConfig? hostedUiConfig,
-  }) {
+  Future<void> deleteLegacyCredentials(
+    AuthOutputs authOutputs,
+  ) {
     final bridge = _stateMachine.expect<auth_cognito.NativeAuthBridge>();
     return bridge.clearLegacyCredentials();
   }
 
   @override
-  Future<LegacyDeviceDetails?> fetchLegacyDeviceSecrets({
-    required String username,
-    CognitoUserPoolConfig? userPoolConfig,
-  }) async {
-    if (userPoolConfig == null) return null;
+  Future<LegacyDeviceDetails?> fetchLegacyDeviceSecrets(
+    String username,
+    AuthOutputs authOutputs,
+  ) async {
+    final userPoolId = authOutputs.userPoolId;
+    if (userPoolId == null) return null;
     final bridge = _stateMachine.expect<auth_cognito.NativeAuthBridge>();
     final device = await bridge.fetchLegacyDeviceSecrets(
       username,
-      userPoolConfig.poolId,
+      userPoolId,
     );
     return device?.toLegacyDeviceDetails();
   }
 
   @override
-  Future<void> deleteLegacyDeviceSecrets({
-    required String username,
-    CognitoUserPoolConfig? userPoolConfig,
-  }) async {
-    if (userPoolConfig != null) {
-      final bridge = _stateMachine.expect<auth_cognito.NativeAuthBridge>();
-      return bridge.deleteLegacyDeviceSecrets(
-        username,
-        userPoolConfig.poolId,
-      );
-    }
+  Future<void> deleteLegacyDeviceSecrets(
+    String username,
+    AuthOutputs authOutputs,
+  ) async {
+    final userPoolId = authOutputs.userPoolId;
+    if (userPoolId == null) return;
+    final bridge = _stateMachine.expect<auth_cognito.NativeAuthBridge>();
+    return bridge.deleteLegacyDeviceSecrets(
+      username,
+      userPoolId,
+    );
   }
 }
