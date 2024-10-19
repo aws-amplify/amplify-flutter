769bada8b45d8cdca3a28d46a39d7886c1ff841b
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-10-19 12:13:01 -0700

    chore: add authenticator test for sign in with email mfa

--- packages/test/amplify_auth_integration_test/lib/amplify_auth_integration_test.dart
+++ packages/test/amplify_auth_integration_test/lib/amplify_auth_integration_test.dart
@@ -6,6 +6,7 @@
 library amplify_auth_integration_test;
 
 export 'src/async_test.dart';
+export 'src/email_utils.dart';
 export 'src/environments.dart';
 export 'src/test_auth_plugin.dart';
 export 'src/test_runner.dart';
