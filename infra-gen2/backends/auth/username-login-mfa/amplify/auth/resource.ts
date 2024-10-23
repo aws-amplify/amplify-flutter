ddb8ab7464b34bdc7237d77ccdfa624d2dd351b0
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-10-23 14:12:56 -0700

    chore: add tests to main_test file and make some backend changes

--- infra-gen2/backends/auth/username-login-mfa/amplify/auth/resource.ts
+++ infra-gen2/backends/auth/username-login-mfa/amplify/auth/resource.ts
@@ -4,7 +4,7 @@
 import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
-  name: "mfa-required-email",
+  name: "mfa-username-login",
   loginWith: {
     phone: true,
   },
