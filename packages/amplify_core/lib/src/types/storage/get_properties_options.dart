81a4c7a13f7931440e8272e21c2ed737846fe579
Ekjot <43255916+ekjotmultani@users.noreply.github.com>
2024-10-24 09:07:01 -0700

    feat(storage): multi bucket get properties api (#5577)

--- packages/amplify_core/lib/src/types/storage/get_properties_options.dart
+++ packages/amplify_core/lib/src/types/storage/get_properties_options.dart
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'package:aws_common/aws_common.dart';
+import 'package:amplify_core/amplify_core.dart';
 
 /// {@template amplify_core.storage.get_properties_options}
 /// Configurable options for `Amplify.Storage.getProperties`.
@@ -14,13 +14,17 @@ class StorageGetPropertiesOptions
   /// {@macro amplify_core.storage.get_properties_options}
   const StorageGetPropertiesOptions({
     this.pluginOptions,
+    this.bucket,
   });
 
   /// {@macro amplify_core.storage.download_get_properties_plugin_options}
   final StorageGetPropertiesPluginOptions? pluginOptions;
 
+  /// Optionally specify which bucket to retrieve
+  final StorageBucket? bucket;
+
   @override
-  List<Object?> get props => [pluginOptions];
+  List<Object?> get props => [pluginOptions, bucket];
 
   @override
   String get runtimeTypeName => 'StorageGetPropertiesOptions';
@@ -28,6 +32,7 @@ class StorageGetPropertiesOptions
   @override
   Map<String, Object?> toJson() => {
         'pluginOptions': pluginOptions?.toJson(),
+        'bucket': bucket,
       };
 }
 
