8472d501ebf3045e9f620e3cceeec0ce539cb024
Tyler-Larkin <tyllark@amazon.com>
2025-02-17 11:43:17 -0800

    chore(): Updated settings.gradle and build.gradle's to have the extension .kts

--- build-support/build_canary.sh
+++ build-support/build_canary.sh
@@ -24,21 +24,21 @@ cp -r $ROOT_DIR/canaries/lib .
 cp $ROOT_DIR/build-support/dummy_amplifyconfiguration.dart lib/amplifyconfiguration.dart
 
 # Android
-sed -i '' -e "s/id \"com.android.application\" .*/id \"com.android.application\" version \"8.1.0\" apply false/" ./android/settings.gradle
-sed -i '' -e "s/id \"org.jetbrains.kotlin.android\" .*/id \"org.jetbrains.kotlin.android\" version \"1.9.10\" apply false/" ./android/settings.gradle
-cat ./android/settings.gradle
+sed -i '' -e "s/id \"com.android.application\" .*/id \"com.android.application\" version \"8.1.0\" apply false/" ./android/settings.gradle.kts
+sed -i '' -e "s/id \"org.jetbrains.kotlin.android\" .*/id \"org.jetbrains.kotlin.android\" version \"1.9.10\" apply false/" ./android/settings.gradle.kts
+cat ./android/settings.gradle.kts
 
 # TODO(khatruong2009): remove this line after the next stable release (3.22.0 or 4.0)
-sed -i '' -e "s/minSdkVersion .*/minSdkVersion 24/" ./android/app/build.gradle
-sed -i '' -e "s/minSdk .*/minSdk 24/" ./android/app/build.gradle
+sed -i '' -e "s/minSdkVersion .*/minSdkVersion 24/" ./android/app/build.gradle.kts
+sed -i '' -e "s/minSdk .*/minSdk 24/" ./android/app/build.gradle.kts
 sed -i '' -e '/id "kotlin-android"/a\
-id '\''kotlin-parcelize'\''' ./android/app/build.gradle
-sed -i '' -e "s/compileSdk .*/compileSdk = 35/" ./android/app/build.gradle
-sed -i '' -e "s/sourceCompatibility .*/sourceCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle
-sed -i '' -e "s/targetCompatibility .*/targetCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle
+id '\''kotlin-parcelize'\''' ./android/app/build.gradle.kts
+sed -i '' -e "s/compileSdk .*/compileSdk = 35/" ./android/app/build.gradle.kts
+sed -i '' -e "s/sourceCompatibility .*/sourceCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle.kts
+sed -i '' -e "s/targetCompatibility .*/targetCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle.kts
 # TODO(equartey): remove this line after the next stable release (3.22.0 or 4.0)
-sed -i '' -e '/kotlinOptions {/,/}/ s/jvmTarget = .*/jvmTarget = '\''17'\''/' ./android/app/build.gradle
-cat ./android/app/build.gradle
+sed -i '' -e '/kotlinOptions {/,/}/ s/jvmTarget = .*/jvmTarget = '\''17'\''/' ./android/app/build.gradle.kts
+cat ./android/app/build.gradle.kts
 
 sed -i '' -e "s#distributionUrl=.*#distributionUrl=https\\://services.gradle.org/distributions/gradle-8.1-all.zip#" ./android/gradle/wrapper/gradle-wrapper.properties
 cat ./android/gradle/wrapper/gradle-wrapper.properties
