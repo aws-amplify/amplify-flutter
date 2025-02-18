14c74c32779638bc43d22de3e24936f8801a2904
Tyler-Larkin <tyllark@amazon.com>
2025-02-18 13:01:29 -0800

    chore(): Cleaned up Android configuration files and corrected version numbers

--- build-support/build_canary.sh
+++ build-support/build_canary.sh
@@ -25,8 +25,8 @@ cp $ROOT_DIR/build-support/dummy_amplifyconfiguration.dart lib/amplifyconfigurat
 
 # Android
 # TODO(tyllark): replace 'gradle*'' with 'gradle.kts' Flutter version is updated to 3.29.0 or higher
-sed -i '' -e "s/id \"com.android.application\" .*/id \"com.android.application\" version \"8.9.0\" apply false/" ./android/settings.gradle* #<3.29.0 only
-sed -i '' -e "s/id(\"com.android.application\") .*/id (\"com.android.application\") version \"8.9.0\" apply false/" ./android/settings.gradle*
+sed -i '' -e "s/id \"com.android.application\" .*/id \"com.android.application\" version \"8.1.0\" apply false/" ./android/settings.gradle* #<3.29.0 only
+sed -i '' -e "s/id(\"com.android.application\") .*/id(\"com.android.application\") version \"8.1.0\" apply false/" ./android/settings.gradle*
 
 sed -i '' -e "s/id \"org.jetbrains.kotlin.android\" .*/id \"org.jetbrains.kotlin.android\" version \"1.9.10\" apply false/" ./android/settings.gradle* #<3.29.0 only
 sed -i '' -e "s/id(\"org.jetbrains.kotlin.android\") .*/id(\"org.jetbrains.kotlin.android\") version \"1.9.10\" apply false/" ./android/settings.gradle*
@@ -43,10 +43,8 @@ sed -i '' -e "s/compileSdk = .*/compileSdk = 35/" ./android/app/build.gradle*
 
 sed -i '' -e "s/sourceCompatibility = .*/sourceCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle*
 
-sed -i '' -e "s/targetCompatibility = .*/targetCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle*
-
 # TODO(equartey): remove this line after the next stable release (3.22.0 or 4.0)
-sed -i '' -e '/kotlinOptions {/,/}/ s/jvmTarget = .*/jvmTarget = JavaVersion.VERSION_17.toString()/' ./android/app/build.gradle*
+sed -i '' -e "s/targetCompatibility = .*/targetCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle*
 
 sed -i '' -e "s/compileOptions {.*/compileOptions {\n\t\tisCoreLibraryDesugaringEnabled = true/" ./android/app/build.gradle*
 sed -i '' -e "s/flutter {.*/dependencies {\n\tcoreLibraryDesugaring(\"com.android.tools:desugar_jdk_libs:2.0.3\")\n}\n\nflutter {/" ./android/app/build.gradle*
