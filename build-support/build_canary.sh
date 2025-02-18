dcbec776c1202ceece000435783715d0453f45f3
Tyler-Larkin <tyllark@amazon.com>
2025-02-17 16:06:19 -0800

    chore(): Updated canaries to support both gradle and gradle.kts extensions

--- build-support/build_canary.sh
+++ build-support/build_canary.sh
@@ -24,24 +24,34 @@ cp -r $ROOT_DIR/canaries/lib .
 cp $ROOT_DIR/build-support/dummy_amplifyconfiguration.dart lib/amplifyconfiguration.dart
 
 # Android
-sed -i '' -e "s/id \"com.android.application\" .*/id \"com.android.application\" version \"8.9.0\" apply false/" ./android/settings.gradle.kts
-sed -i '' -e "s/id \"org.jetbrains.kotlin.android\" .*/id \"org.jetbrains.kotlin.android\" version \"1.9.10\" apply false/" ./android/settings.gradle.kts
-cat ./android/settings.gradle.kts
+# TODO(tyllark): replace 'gradle*'' with 'gradle.kts' Flutter version is updated to 3.29.0 or higher
+sed -i '' -e "s/id \"com.android.application\" .*/id \"com.android.application\" version \"8.9.0\" apply false/" ./android/settings.gradle* #<3.29.0 only
+sed -i '' -e "s/id(\"com.android.application\") .*/id (\"com.android.application\") version \"8.9.0\" apply false/" ./android/settings.gradle*
+
+sed -i '' -e "s/id \"org.jetbrains.kotlin.android\" .*/id \"org.jetbrains.kotlin.android\" version \"1.9.10\" apply false/" ./android/settings.gradle* #<3.29.0 only
+sed -i '' -e "s/id(\"org.jetbrains.kotlin.android\") .*/id(\"org.jetbrains.kotlin.android\") version \"1.9.10\" apply false/" ./android/settings.gradle*
+cat ./android/settings.gradle*
 
 # TODO(khatruong2009): remove this line after the next stable release (3.22.0 or 4.0)
-sed -i '' -e "s/minSdkVersion = .*/minSdkVersion = 24/" ./android/app/build.gradle.kts
-sed -i '' -e "s/minSdk = .*/minSdk = 24/" ./android/app/build.gradle.kts
-sed -i '' -e '/id "kotlin-android"/a\
-id '\''kotlin-parcelize'\''' ./android/app/build.gradle.kts
-sed -i '' -e "s/compileSdk = .*/compileSdk = 35/" ./android/app/build.gradle.kts
-sed -i '' -e "s/sourceCompatibility = .*/sourceCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle.kts
-sed -i '' -e "s/targetCompatibility = .*/targetCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle.kts
-# TODO(equartey): remove this line after the next stable release (3.22.0 or 4.0)
-sed -i '' -e '/kotlinOptions {/,/}/ s/jvmTarget = .*/jvmTarget = JavaVersion.VERSION_17.toString()/' ./android/app/build.gradle.kts
+sed -i '' -e "s/minSdkVersion = .*/minSdkVersion = 24/" ./android/app/build.gradle*
+sed -i '' -e "s/minSdk = .*/minSdk = 24/" ./android/app/build.gradle*
 
-sed -i '' -e "s/compileOptions {.*/compileOptions {\n\t\tisCoreLibraryDesugaringEnabled = true/" ./android/app/build.gradle.kts
-sed -i '' -e "s/flutter {.*/dependencies {\n\tcoreLibraryDesugaring(\"com.android.tools:desugar_jdk_libs:2.0.3\")\n}\n\nflutter {/" ./android/app/build.gradle.kts
-cat ./android/app/build.gradle.kts
+sed -i '' -e '/id "kotlin-android"/a\
+
+id '\''kotlin-parcelize'\''' ./android/app/build.gradle*
+
+sed -i '' -e "s/compileSdk = .*/compileSdk = 35/" ./android/app/build.gradle*
+
+sed -i '' -e "s/sourceCompatibility = .*/sourceCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle*
+
+sed -i '' -e "s/targetCompatibility = .*/targetCompatibility = JavaVersion.VERSION_17/" ./android/app/build.gradle*
+
+# TODO(equartey): remove this line after the next stable release (3.22.0 or 4.0)
+sed -i '' -e '/kotlinOptions {/,/}/ s/jvmTarget = .*/jvmTarget = JavaVersion.VERSION_17.toString()/' ./android/app/build.gradle*
+
+sed -i '' -e "s/compileOptions {.*/compileOptions {\n\t\tisCoreLibraryDesugaringEnabled = true/" ./android/app/build.gradle*
+sed -i '' -e "s/flutter {.*/dependencies {\n\tcoreLibraryDesugaring(\"com.android.tools:desugar_jdk_libs:2.0.3\")\n}\n\nflutter {/" ./android/app/build.gradle*
+cat ./android/app/build.gradle*
 
 sed -i '' -e "s#distributionUrl=.*#distributionUrl=https\\://services.gradle.org/distributions/gradle-8.9-all.zip#" ./android/gradle/wrapper/gradle-wrapper.properties
 cat ./android/gradle/wrapper/gradle-wrapper.properties
