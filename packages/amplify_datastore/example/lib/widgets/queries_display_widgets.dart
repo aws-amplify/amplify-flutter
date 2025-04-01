e160a82f8de5a1c897467e10c17aeb6d321406cb
Tyler-Larkin <tyllark@amazon.com>
2025-04-01 16:09:40 -0700

    chore(datastore): fixed part_of_unnamed_library error

--- packages/amplify_datastore/example/lib/widgets/queries_display_widgets.dart
+++ packages/amplify_datastore/example/lib/widgets/queries_display_widgets.dart
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-part of sample_app;
+part of '../../main.dart';
 
 Widget displayQueryButtons(
   bool isAmplifyConfigured,
