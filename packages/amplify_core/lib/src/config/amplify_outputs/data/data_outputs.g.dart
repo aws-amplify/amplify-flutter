a3b793afa2f5677836bf52d3acf4467397f387b5
Tyler-Larkin <tyllark@amazon.com>
2025-04-14 14:35:16 -0700

    chore(): regenerated json_serializable files

--- packages/amplify_core/lib/src/config/amplify_outputs/data/data_outputs.g.dart
+++ packages/amplify_core/lib/src/config/amplify_outputs/data/data_outputs.g.dart
@@ -9,56 +9,43 @@ part of 'data_outputs.dart';
 // **************************************************************************
 
 DataOutputs _$DataOutputsFromJson(Map<String, dynamic> json) => $checkedCreate(
-  'DataOutputs',
-  json,
-  ($checkedConvert) {
-    final val = DataOutputs(
-      awsRegion: $checkedConvert('aws_region', (v) => v as String),
-      url: $checkedConvert('url', (v) => v as String),
-      apiKey: $checkedConvert('api_key', (v) => v as String?),
-      defaultAuthorizationType: $checkedConvert(
-        'default_authorization_type',
-        (v) => $enumDecode(_$APIAuthorizationTypeEnumMap, v),
-      ),
-      authorizationTypes: $checkedConvert(
-        'authorization_types',
-        (v) =>
-            (v as List<dynamic>)
-                .map((e) => $enumDecode(_$APIAuthorizationTypeEnumMap, e))
-                .toList(),
-      ),
+      'DataOutputs',
+      json,
+      ($checkedConvert) {
+        final val = DataOutputs(
+          awsRegion: $checkedConvert('aws_region', (v) => v as String),
+          url: $checkedConvert('url', (v) => v as String),
+          apiKey: $checkedConvert('api_key', (v) => v as String?),
+          defaultAuthorizationType: $checkedConvert(
+              'default_authorization_type',
+              (v) => $enumDecode(_$APIAuthorizationTypeEnumMap, v)),
+          authorizationTypes: $checkedConvert(
+              'authorization_types',
+              (v) => (v as List<dynamic>)
+                  .map((e) => $enumDecode(_$APIAuthorizationTypeEnumMap, e))
+                  .toList()),
+        );
+        return val;
+      },
+      fieldKeyMap: const {
+        'awsRegion': 'aws_region',
+        'apiKey': 'api_key',
+        'defaultAuthorizationType': 'default_authorization_type',
+        'authorizationTypes': 'authorization_types'
+      },
     );
-    return val;
-  },
-  fieldKeyMap: const {
-    'awsRegion': 'aws_region',
-    'apiKey': 'api_key',
-    'defaultAuthorizationType': 'default_authorization_type',
-    'authorizationTypes': 'authorization_types',
-  },
-);
 
-Map<String, dynamic> _$DataOutputsToJson(DataOutputs instance) {
-  final val = <String, dynamic>{
-    'aws_region': instance.awsRegion,
-    'url': instance.url,
-  };
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('api_key', instance.apiKey);
-  val['default_authorization_type'] =
-      _$APIAuthorizationTypeEnumMap[instance.defaultAuthorizationType]!;
-  val['authorization_types'] =
-      instance.authorizationTypes
+Map<String, dynamic> _$DataOutputsToJson(DataOutputs instance) =>
+    <String, dynamic>{
+      'aws_region': instance.awsRegion,
+      'url': instance.url,
+      if (instance.apiKey case final value?) 'api_key': value,
+      'default_authorization_type':
+          _$APIAuthorizationTypeEnumMap[instance.defaultAuthorizationType]!,
+      'authorization_types': instance.authorizationTypes
           .map((e) => _$APIAuthorizationTypeEnumMap[e]!)
-          .toList();
-  return val;
-}
+          .toList(),
+    };
 
 const _$APIAuthorizationTypeEnumMap = {
   APIAuthorizationType.none: 'NONE',
