36eaa2a3eb066da7f3f69a70fa563130fa24f31e
Tyler-Larkin <tyllark@amazon.com>
2025-05-13 16:18:20 -0700

    chore(): removed unused functions.

--- packages/aws_common/lib/src/js/fetch.dart
+++ packages/aws_common/lib/src/js/fetch.dart
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'dart:js_interop';
-
 import 'package:aws_common/src/js/common.dart';
 import 'package:web/web.dart';
 
@@ -19,14 +17,4 @@ enum RequestRedirectValues with JSEnum {
 
   /// Caller intends to process the response in another context.
   manual,
-}
-
-@JS('fetch')
-external JSPromise<Response> _fetch(String url, [RequestInit? init]);
-
-/// The global fetch() method starts the process of fetching a resource from
-/// the network, returning a promise which is fulfilled once the response is
-/// available.
-Future<Response> fetch(String url, [RequestInit? init]) {
-  return _fetch(url, init).toDart;
 }
