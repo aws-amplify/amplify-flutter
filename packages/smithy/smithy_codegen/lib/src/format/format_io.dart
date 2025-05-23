c9cf6143adafefc73d08e875af8de5209e59fd39
Tyler-Larkin <tyllark@amazon.com>
2025-05-23 15:46:53 -0700

    chore: pinned Workerbee Builder Dart format version to 3.7.0 to match our package Dart SDK version

--- packages/smithy/smithy_codegen/lib/src/format/format_io.dart
+++ packages/smithy/smithy_codegen/lib/src/format/format_io.dart
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:dart_style/dart_style.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 /// Formats Dart source code.
 ///
 /// This is a no-op on Web since `dart_style` (`analyzer`) is not supported.
 String format(String source) {
   final formatter = DartFormatter(
-    languageVersion: DartFormatter.latestLanguageVersion,
+    languageVersion: Version(3, 7, 0),
   );
   return formatter.format(source);
 }
