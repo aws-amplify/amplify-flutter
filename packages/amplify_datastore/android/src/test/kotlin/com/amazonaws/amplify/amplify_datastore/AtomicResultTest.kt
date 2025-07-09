c4f2ab80158ce2d157650eceb094faa38a4e7c6a
Tyler-Larkin <tyllark@amazon.com>
2025-07-09 11:35:21 -0700

    Chore(): dependabot fixes (#6187)

--- packages/amplify_datastore/android/src/test/kotlin/com/amazonaws/amplify/amplify_datastore/AtomicResultTest.kt
+++ packages/amplify_datastore/android/src/test/kotlin/com/amazonaws/amplify/amplify_datastore/AtomicResultTest.kt
@@ -1,13 +1,16 @@
 package com.amazonaws.amplify.amplify_datastore
 
 import com.amazonaws.amplify.amplify_datastore.util.AtomicResult
+import io.flutter.Log
 import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.any
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
@@ -23,31 +26,46 @@ class AtomicResultTest {
     private lateinit var mockResult: MethodChannel.Result
 
     @Test
-    fun successIsForwarded(): Unit = coroutinesTestRule.testDispatcher.runBlockingTest {
+    fun successIsForwarded(): Unit = runTest(coroutinesTestRule.testDispatcher) {
         val atomicResult = AtomicResult(mockResult, "successIsForwarded")
         atomicResult.success(null)
+
+        advanceUntilIdle()
+        
         verify(mockResult).success(null)
     }
 
     @Test
-    fun errorIsForwarded(): Unit = coroutinesTestRule.testDispatcher.runBlockingTest {
+    fun errorIsForwarded(): Unit = runTest(coroutinesTestRule.testDispatcher) {
         val atomicResult = AtomicResult(mockResult, "errorIsForwarded")
         atomicResult.error("", null, null)
+
+        advanceUntilIdle()
+
         verify(mockResult).error("", null, null)
     }
 
     @Test
-    fun notImplementedIsForwarded(): Unit = coroutinesTestRule.testDispatcher.runBlockingTest {
+    fun notImplementedIsForwarded(): Unit = runTest(coroutinesTestRule.testDispatcher) {
         val atomicResult = AtomicResult(mockResult, "notImplementedIsForwarded")
         atomicResult.notImplemented()
+
+        advanceUntilIdle()
+
         verify(mockResult).notImplemented()
     }
 
     @Test
-    fun multipleSynchronousRepliesAreNotSent(): Unit = coroutinesTestRule.testDispatcher.runBlockingTest {
+    fun multipleSynchronousRepliesAreNotSent(): Unit = runTest(coroutinesTestRule.testDispatcher) {
+        val mockedLog = Mockito.mockStatic(Log::class.java)
+        
         val atomicResult = AtomicResult(mockResult, "multipleSynchronousRepliesAreNotSent")
         atomicResult.success(null)
         atomicResult.success(null)
-        verify(mockResult, times(1)).success(any())
+        
+        advanceUntilIdle()
+        
+        verify(mockResult, times(1)).success(any())       
+        mockedLog.close()
     }
 }
