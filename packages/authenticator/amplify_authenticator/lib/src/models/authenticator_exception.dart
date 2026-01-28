a89cdfac1bc6aee67a9eda59010aa68eb63972d6
Jan Vincent Hoffbauer <jvhoff@amazon.de>
2026-01-28 17:50:50 +0100

    Fix analyzer

--- packages/authenticator/amplify_authenticator/lib/src/models/authenticator_exception.dart
+++ packages/authenticator/amplify_authenticator/lib/src/models/authenticator_exception.dart
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'package:amplify_auth_cognito_dart/src/sdk/sdk_exception.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/material.dart';
