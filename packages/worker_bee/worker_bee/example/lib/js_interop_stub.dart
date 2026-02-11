610753c5869c7bb0ac4abe76b1a00201d75c6868
Jonas Greifenhain <jonasgre@amazon.de>
2026-02-11 16:30:01 +0100

    Add worker_bee example

---
+++ packages/worker_bee/worker_bee/example/lib/js_interop_stub.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+/// No-op JS interop setup for non-web platforms.
+void setupJsInterop(GlobalKey key) {
+  // JS interop is only available on web platforms.
+}
