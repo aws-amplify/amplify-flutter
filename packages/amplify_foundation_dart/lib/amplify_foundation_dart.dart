33426a0218ba5bb99280ad1ff67e6b1f1e9048d4
Ekjot <43255916+ekjotmultani@users.noreply.github.com>
2026-02-23 22:50:00 -0800

    feat: add amplify_foundation_dart package with core types for V3
    Adds the foundational package with minimal constructs needed for
    the Kinesis client libraries:
    - AmplifyException base class
    - AWSCredentialsProvider<T> and AWSCredentials sealed hierarchy
    - Logger interface with AmplifyLogger implementation
    - LogLevel enum
    - Result<T, E> sealed type

---
+++ packages/amplify_foundation_dart/lib/amplify_foundation_dart.dart
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Common types and utilities used across AWS and Amplify packages.
+library;
+
+// Amplify Exceptions
+export 'package:amplify_foundation_dart/src/amplify_exception/amplify_exception.dart';
+// AWS Credendials
+export 'package:amplify_foundation_dart/src/credentials/aws_credendials_provider.dart';
+export 'package:amplify_foundation_dart/src/credentials/aws_credentials.dart';
+// Logger
+export 'package:amplify_foundation_dart/src/logger/amplify_log.dart';
+export 'package:amplify_foundation_dart/src/logger/log_level.dart';
+export 'package:amplify_foundation_dart/src/logger/logger.dart';
+export 'package:amplify_foundation_dart/src/logger/logger_provider.dart';
+// Results
+export 'package:amplify_foundation_dart/src/result/result.dart';
+export 'package:amplify_foundation_dart/src/result/result_failure_exception.dart';
+// Utils
+export 'package:amplify_foundation_dart/src/util/globals.dart';
+export 'package:amplify_foundation_dart/src/util/print.dart';
