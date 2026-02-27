22b110d304581711e728682b5d0af2c4d797225a
Jonas Greifenhain <jonasgre@amazon.de>
2026-02-27 18:35:26 +0100

    Satisfy the linter

--- packages/amplify_foundation_dart_bridge/test/v2_credentials_provider_bridge_test.dart
+++ packages/amplify_foundation_dart_bridge/test/v2_credentials_provider_bridge_test.dart
@@ -1,8 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import 'package:amplify_foundation_dart/amplify_foundation_dart.dart'
-    as v3;
+import 'package:amplify_foundation_dart/amplify_foundation_dart.dart' as v3;
 import 'package:amplify_foundation_dart_bridge/amplify_foundation_dart_bridge.dart';
 import 'package:aws_common/aws_common.dart' as v2;
 import 'package:test/test.dart';
@@ -10,9 +9,9 @@ import 'package:test/test.dart';
 void main() {
   group('V2CredentialsProviderBridge', () {
     test('bridges static credentials', () async {
-      final v2Creds = v2.AWSCredentials('accessKey', 'secretKey');
-      final v2Provider = v2.AWSCredentialsProvider(v2Creds);
-      final bridge = V2CredentialsProviderBridge(v2Provider);
+      const v2Creds = v2.AWSCredentials('accessKey', 'secretKey');
+      const v2Provider = v2.AWSCredentialsProvider(v2Creds);
+      const bridge = V2CredentialsProviderBridge(v2Provider);
 
       final v3Creds = await bridge.resolve();
 
@@ -43,13 +42,13 @@ void main() {
     });
 
     test('defaults expiration to 1 hour when not provided', () async {
-      final v2Creds = v2.AWSCredentials(
+      const v2Creds = v2.AWSCredentials(
         'accessKey',
         'secretKey',
         'sessionToken',
       );
-      final v2Provider = v2.AWSCredentialsProvider(v2Creds);
-      final bridge = V2CredentialsProviderBridge(v2Provider);
+      const v2Provider = v2.AWSCredentialsProvider(v2Creds);
+      const bridge = V2CredentialsProviderBridge(v2Provider);
 
       final before = DateTime.now().add(const Duration(minutes: 59));
       final v3Creds = await bridge.resolve() as v3.TemporaryCredentials;
