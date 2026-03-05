cbc16316cc1d6a3606a677d8569e5ad711adbcc6
Ekjot <43255916+ekjotmultani@users.noreply.github.com>
2026-03-05 08:41:23 -0800

    feat(foundation): Add foundation (#6687)
    Co-authored-by: Jonas Greifenhain <jonasgre@amazon.de>

---
+++ packages/amplify_foundation/amplify_foundation_dart_bridge/lib/amplify_foundation_dart_bridge.dart
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/// Bridges `aws_common` credential types to `amplify_foundation_dart` types.
+library;
+
+export 'package:amplify_foundation_dart_bridge/src/aws_common_credentials_provider_bridge.dart';
