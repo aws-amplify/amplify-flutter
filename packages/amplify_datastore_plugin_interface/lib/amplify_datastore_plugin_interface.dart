d7cc5e61d72fe810d1522adc13f314854d4d0897
Jonas Greifenhain <jonasgre@amazon.de>
2026-03-11 01:27:35 +0100

    chore(datastore): Add documentation (#6755)

--- packages/amplify_datastore_plugin_interface/lib/amplify_datastore_plugin_interface.dart
+++ packages/amplify_datastore_plugin_interface/lib/amplify_datastore_plugin_interface.dart
@@ -1,6 +1,17 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/// Platform interface for the Amplify Flutter DataStore plugin.
+///
+/// This package exposes the core DataStore types used by the
+/// [amplify_datastore](https://pub.dev/packages/amplify_datastore) plugin,
+/// including model definitions, temporal types, sync expressions,
+/// subscription events, and conflict-handling primitives.
+///
+/// **This is an internal interface package.** Application code should depend
+/// on `amplify_datastore` instead.
+library amplify_datastore_plugin_interface;
+
 export 'package:amplify_core/src/types/datastore/datastore_types.dart'
     hide DateTimeParse;
 export 'package:amplify_core/src/util/uuid.dart';
