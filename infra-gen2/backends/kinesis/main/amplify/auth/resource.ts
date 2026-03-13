14ee87799fc53f0e5a85648776e190809f3b7b38
Jonas Greifenhain <jonasgre@amazon.de>
2026-03-13 15:40:44 +0100

    Add pre sign up handler

--- infra-gen2/backends/kinesis/main/amplify/auth/resource.ts
+++ infra-gen2/backends/kinesis/main/amplify/auth/resource.ts
@@ -1,7 +1,15 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, defineFunction } from "@aws-amplify/backend";
+
+export const preSignUp = defineFunction({
+  name: "pre-sign-up",
+  entry: "./pre-sign-up-handler.ts",
+});
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
+  },
+  triggers: {
+    preSignUp,
   },
 });
