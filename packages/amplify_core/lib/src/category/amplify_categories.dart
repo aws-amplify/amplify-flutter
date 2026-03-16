773dffadc45fb6e6ad603e3a415f484b78de8bde
Ekjot <43255916+ekjotmultani@users.noreply.github.com>
2026-03-16 11:33:53 -0700

    refactor: remove Category.kinesis enum, use string categories in deploy script
    Decouple deploy_gen2.dart from the amplify_core Category enum by
    changing AmplifyBackendGroup.category to a plain String. This allows
    removing Category.kinesis from the enum and its dead switch case in
    AmplifyClassImpl, since the kinesis client is standalone and does not
    participate in the Amplify plugin registration system.
    
    The --category CLI flag now derives allowed values from infraConfig
    instead of Category.values.

--- packages/amplify_core/lib/src/category/amplify_categories.dart
+++ packages/amplify_core/lib/src/category/amplify_categories.dart
@@ -50,10 +50,7 @@ enum Category {
   storage,
 
   /// Push Notifications
-  pushNotifications,
-
-  /// Kinesis streaming (Data Streams + Firehose)
-  kinesis;
+  pushNotifications;
 
   String get name => switch (this) {
     Category.analytics => 'Analytics',
@@ -63,7 +60,6 @@ enum Category {
     Category.hub => 'Hub',
     Category.storage => 'Storage',
     Category.pushNotifications => 'PushNotifications',
-    Category.kinesis => 'Kinesis',
   };
 }
 
