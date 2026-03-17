eb1a6694d11af9395e18d5000d82ed3ceb6aeee8
Jonas Greifenhain <jonasgre@amazon.de>
2026-03-17 15:02:42 +0100

    chore(aft): Extend tests

--- packages/aft/test/config/package_selector_test.dart
+++ packages/aft/test/config/package_selector_test.dart
@@ -44,12 +44,14 @@ void main() {
             'packages/common/amplify_db_common/example',
             'packages/common/amplify_db_common_dart/example',
             'packages/example_common/example',
+            'packages/kinesis/amplify_kinesis/example',
             'packages/notifications/push/amplify_push_notifications/example',
             'packages/notifications/push/amplify_push_notifications_pinpoint/example',
             'packages/secure_storage/amplify_secure_storage/example',
             'packages/secure_storage/amplify_secure_storage_dart/example',
             'packages/storage/amplify_storage_s3/example',
             'packages/storage/amplify_storage_s3_dart/example',
+            'packages/worker_bee/worker_bee/example',
           ]),
         );
       });
@@ -136,6 +138,8 @@ void main() {
           'packages/notifications/push/amplify_push_notifications_pinpoint/example',
           'packages/secure_storage/amplify_secure_storage/example',
           'packages/storage/amplify_storage_s3/example',
+          'packages/kinesis/amplify_kinesis/example',
+          'packages/worker_bee/worker_bee/example',
         ]),
       );
     });
