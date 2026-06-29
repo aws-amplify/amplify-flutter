ee28a71cb6e5e257322b8b037d231da7b311dc4b
Jonas Greifenhain <51089187+cadivus@users.noreply.github.com>
2026-06-29 23:47:37 +0200

    test: Anchor OS-only `@TestOn` selectors on vm to exclude browser runs under Dart 3.12 (#7067)

--- packages/auth/amplify_auth_cognito_test/test/flows/hostedui/hosted_ui_platform_io_test.dart
+++ packages/auth/amplify_auth_cognito_test/test/flows/hostedui/hosted_ui_platform_io_test.dart
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-@TestOn('windows || mac-os || linux')
+@TestOn('vm && (windows || mac-os || linux)')
 library;
 
 import 'dart:async';
--- packages/secure_storage/amplify_secure_storage_test/test/linux_test.dart
+++ packages/secure_storage/amplify_secure_storage_test/test/linux_test.dart
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-@TestOn('linux')
+@TestOn('vm && linux')
 library;
 
 import 'package:amplify_secure_storage_dart/amplify_secure_storage_dart.dart';
