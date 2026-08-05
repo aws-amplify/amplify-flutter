e6087b89cfaa68248f813968886bd4de714a1261
Varshitha Pamisetty <varshpam@amazon.com>
2026-08-05 15:18:07 +0200

    chore(api): apply SPDX header and dart format to test fixture

--- packages/api/amplify_api_dart/test/test_models/CpkTemporalPrimaryKey.dart
+++ packages/api/amplify_api_dart/test/test_models/CpkTemporalPrimaryKey.dart
@@ -1,21 +1,5 @@
-/*
-* Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License").
-* You may not use this file except in compliance with the License.
-* A copy of the License is located at
-*
-*  http://aws.amazon.com/apache2.0
-*
-* or in the "license" file accompanying this file. This file is distributed
-* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-* express or implied. See the License for the specific language governing
-* permissions and limitations under the License.
-*/
-
-// NOTE: This file is generated and may not follow lint rules defined in your app
-// Generated files can be excluded from analysis in analysis_options.yaml
-// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 // ignore_for_file: public_member_api_docs, annotate_overrides, dead_code, dead_codepublic_member_api_docs, depend_on_referenced_packages, file_names, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, non_constant_identifier_names, null_check_on_nullable_type_parameter, override_on_non_overriding_member, prefer_adjacent_string_concatenation, prefer_const_constructors, prefer_if_null_operators, prefer_interpolation_to_compose_strings, slash_for_doc_comments, sort_child_properties_last, unnecessary_const, unnecessary_constructor_name, unnecessary_late, unnecessary_new, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations, unnecessary_string_interpolations, use_build_context_synchronously
 
@@ -102,11 +86,9 @@ class CpkTemporalPrimaryKey extends amplify_core.Model {
     }
   }
 
-  const CpkTemporalPrimaryKey._internal({
-    required hwid,
-    required sessionStart,
-  }) : _hwid = hwid,
-       _sessionStart = sessionStart;
+  const CpkTemporalPrimaryKey._internal({required hwid, required sessionStart})
+    : _hwid = hwid,
+      _sessionStart = sessionStart;
 
   factory CpkTemporalPrimaryKey({
     required String hwid,
@@ -184,7 +166,9 @@ class CpkTemporalPrimaryKey extends amplify_core.Model {
   MODEL_IDENTIFIER =
       amplify_core.QueryModelIdentifier<CpkTemporalPrimaryKeyModelIdentifier>();
   static final HWID = amplify_core.QueryField(fieldName: "hwid");
-  static final SESSIONSTART = amplify_core.QueryField(fieldName: "sessionStart");
+  static final SESSIONSTART = amplify_core.QueryField(
+    fieldName: "sessionStart",
+  );
   static var schema = amplify_core.Model.defineSchema(
     define: (amplify_core.ModelSchemaDefinition modelSchemaDefinition) {
       modelSchemaDefinition.name = "CpkTemporalPrimaryKey";
@@ -254,10 +238,8 @@ class CpkTemporalPrimaryKeyModelIdentifier
   });
 
   @override
-  Map<String, dynamic> serializeAsMap() => (<String, dynamic>{
-    'hwid': hwid,
-    'sessionStart': sessionStart,
-  });
+  Map<String, dynamic> serializeAsMap() =>
+      (<String, dynamic>{'hwid': hwid, 'sessionStart': sessionStart});
 
   @override
   List<Map<String, dynamic>> serializeAsList() => serializeAsMap().entries
