877bd59cd6a42a5bdfea1c07ac5b556d2b9c13c1
Jonas Greifenhain <51089187+cadivus@users.noreply.github.com>
2026-08-18 14:44:02 +0200

    chore: Regenerate files for Flutter 3.47 (#7285)

--- packages/kinesis/amplify_firehose/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/kinesis/amplify_firehose/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import amplify_auth_cognito
 import amplify_secure_storage
 import device_info_plus
 import package_info_plus
+import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AmplifySecureStoragePlugin.register(with: registry.registrar(forPlugin: "AmplifySecureStoragePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
