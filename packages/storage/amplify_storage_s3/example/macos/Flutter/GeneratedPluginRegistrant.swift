c1422885ffb755c461a805d3ab2f0ce06ddeb2ed
Jonas Greifenhain <jonasgre@amazon.de>
2026-09-17 15:31:57 +0200

    chore: Regenerate plugin registrants

--- packages/storage/amplify_storage_s3/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/storage/amplify_storage_s3/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import amplify_secure_storage
 import device_info_plus
 import file_selector_macos
 import package_info_plus
-import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -19,6 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
